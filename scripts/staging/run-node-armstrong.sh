794c16ef64ffe4b39692beb3dbd5a62d73f1c01a
tgmichel <telmo@purestake.com>
2020-05-08 12:13:45 +0200

    remove scripts directory

--- scripts/staging/run-node-armstrong.sh
+++
@@ -1,8 +0,0 @@
-../../node/target/release/moonbeam \
-  --base-path ./tmp/Moonbeam-Armstrong-01 \
-  --chain ./rawspec.json \
-  --port 30333 \
-  --ws-port 9944 \
-  --rpc-port 9933 \
-  --validator \
-  --name Moonbeam-Armstrong-01
