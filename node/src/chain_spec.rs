0dbd0d70f796f4b09daa20324b338500236ebda0
tgmichel <telmo@purestake.com>
2020-05-15 09:52:47 +0200

    local testnet alice and bob

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -99,7 +99,8 @@ pub fn local_testnet_config() -> ChainSpec {
 		|| {
 			testnet_genesis(
 				vec![
-					authority_keys_from_seed("Alice")
+					authority_keys_from_seed("Alice"),
+					authority_keys_from_seed("Bob")
 				],
 				get_account_id_from_seed::<sr25519::Public>("Alice"),
 				vec![
