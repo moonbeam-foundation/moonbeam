a93b391e0245a6449f078c1f9639ac9b90339caf
dtyoo <derek@purestake.com>
2020-06-30 14:59:44 -0400

    removed erc20 functions from Migrations.sol returning it to its default state.  truffle migrate was failing with the customizations to this file.  we shouldn't need them with the erc20 example in the separate MyToken.sol file.

--- tools/truffle/contracts/Migrations.sol
+++ tools/truffle/contracts/Migrations.sol
@@ -1,26 +1,19 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity >=0.4.21 <0.7.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
-contract Migrations is ERC20 {
+contract Migrations {
   address public owner;
   uint public last_completed_migration;
-  
-  constructor() ERC20() public {
+
+  constructor() public {
     owner = msg.sender;
-    _mint(msg.sender,  2110000);
   }
 
-  function setCompleted(uint completed) public restricted {
-    last_completed_migration = completed;
-  }
   modifier restricted() {
     if (msg.sender == owner) _;
   }
 
-  function upgrade(address new_address) public restricted {
-    Migrations upgraded = Migrations(new_address);
-    upgraded.setCompleted(last_completed_migration);
+  function setCompleted(uint completed) public restricted {
+    last_completed_migration = completed;
   }
 }
