34546df460c045653e2f9e589fe7ab16f52183fc
Crystalin <alan.sapede@gmail.com>
2020-06-30 13:14:39 -0400

    Adds nonce provider to truffle

--- tools/truffle/private-provider.js
+++ tools/truffle/private-provider.js
@@ -2,7 +2,6 @@ const ProviderEngine = require("web3-provider-engine");
 const WalletSubprovider = require('web3-provider-engine/subproviders/wallet');
 const RpcSubprovider = require('web3-provider-engine/subproviders/rpc');
 const EthereumjsWallet = require('ethereumjs-wallet');
-const NonceSubprovider = require('web3-provider-engine/subproviders/nonce-tracker');
 
 
 
@@ -23,6 +22,32 @@ ChainIdSubProvider.prototype.handleRequest = function(payload, next, end) {
 }
 
 
+function NonceSubProvider() {
+}
+
+NonceSubProvider.prototype.setEngine = function(engine) {
+  const self = this
+  if (self.engine) return
+  self.engine = engine
+}
+NonceSubProvider.prototype.handleRequest = function(payload, next, end) {
+  if (payload.method == "eth_sendTransaction") {
+    this.engine.sendAsync({
+      jsonrpc:"2.0",
+      id: Math.ceil(Math.random() * 4415011859092441),
+      method: "eth_getTransactionCount",
+      params: [payload.params[0].from, "latest"]
+    }, (err, result) => {
+      const nonce = typeof result.result == "string" ?
+        result.result == "0x" ? 0 : parseInt(result.result.substring(2), 16) : 0
+      payload.params[0].nonce = nonce;
+      next();
+    })
+  } else {
+    next()
+  }
+}
+
 function PrivateKeyProvider(privateKey, providerUrl, chainId) {
   if (!privateKey) {
     throw new Error(`Private Key missing, non-empty string expected, got "${privateKey}"`);
@@ -38,11 +63,12 @@ function PrivateKeyProvider(privateKey, providerUrl, chainId) {
   this.engine = new ProviderEngine({useSkipCache: false});
   
   this.engine.addProvider(new ChainIdSubProvider(chainId));
+  this.engine.addProvider(new NonceSubProvider());
   this.engine.addProvider(new WalletSubprovider(this.wallet, {}));
-  this.engine.addProvider(new NonceSubprovider());
   this.engine.addProvider(new RpcSubprovider({ rpcUrl: providerUrl }));
 
   this.engine.start();
+  
 }
 
 
