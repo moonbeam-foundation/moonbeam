e97c54cdf52ac111cd3bd26da840abe95d4b0926
albertov19 <64150856+albertov19@users.noreply.github.com>
2020-07-20 17:39:10 +0200

    Fixed Truffle Deploy Initial Supply

--- tools/truffle/migrations/2_deploy_contracts.js
+++ tools/truffle/migrations/2_deploy_contracts.js
@@ -2,6 +2,6 @@ var MyToken = artifacts.require("MyToken");
 
 module.exports = function (deployer) {
   // deployment steps
-  deployer.deploy(MyToken, 8000000, { gas: 4294967295 });
+  deployer.deploy(MyToken, "8000000000000000000000000", { gas: 4294967295 });
 };
 
