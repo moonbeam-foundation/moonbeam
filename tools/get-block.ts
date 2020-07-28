18934e43a2a294803247d1007509b239ad7f3788
crystalin <alan.sapede@gmail.com>
2020-07-28 11:22:49 -0400

    Fixes tool script

--- tools/get-block.ts
+++ tools/get-block.ts
@@ -9,13 +9,12 @@ const displayBlock = async (blockNumber) => {
 	const block = await web3.eth.getBlock(blockNumber);
 	console.log(`${blockNumber.toString().padStart(5, " ")}: ${block.hash} (${diffTime} s)`);
 	block.transactions.forEach((t: any, index) => {
-		console.log(`     [${index}] ${t.hash} (input: ${t.input.length} bytes)`);
+		console.log(`     [${index}] ${t.hash} (input: ${t.input && t.input.length} bytes)`);
 		console.log(`     [${index}] from ${t.from} (nonce: ${t.nonce}) to ${t.to}`);
 		console.log(`     [${index}] value: ${t.value} gas: ${t.gas} gasPrice: ${t.gasPrice}`);
 		if (t.creates) {
 			console.log(`     [${index}] creates: ${t.creates}`);
 		}
-		
 	});
 };
 
