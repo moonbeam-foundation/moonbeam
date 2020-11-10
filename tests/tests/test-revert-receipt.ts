a2cc65d3cc839cb89b77b97964949ee2fa6eb494
Alan Sapede <alan@scg-moonbuild-1.dev.purestake.tech>
2020-11-10 17:29:47 +0000

    fixes test setup after merge

--- tests/tests/test-revert-receipt.ts
+++ tests/tests/test-revert-receipt.ts
@@ -1,8 +1,8 @@
 import { expect } from "chai";
 
-import { createAndFinalizeBlock, customRequest, describeWithFrontier } from "./util";
+import { createAndFinalizeBlock, customRequest, describeWithMoonbeam } from "./util";
 
-describeWithFrontier("Frontier RPC (Constructor Revert)", `simple-specs.json`, (context) => {
+describeWithMoonbeam("Frontier RPC (Constructor Revert)", `simple-specs.json`, (context) => {
   const GENESIS_ACCOUNT = "0x6be02d1d3665660d22ff9624b7be0551ee1ac91b";
   const GENESIS_ACCOUNT_PRIVATE_KEY =
     "0x99B3C12287537E38C90A9219D4CB074A89A16E9CDB20BF85728EBD97C343E342";
