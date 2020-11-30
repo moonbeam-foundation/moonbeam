affe4f28eb0a7c09967d725d40abf227ad0b40a6
Joshy Orndorff <admin@joshyorndorff.com>
2020-11-30 12:32:18 -0500

    remove commented code

--- node/parachain/src/command.rs
+++ node/parachain/src/command.rs
@@ -126,35 +126,6 @@ impl SubstrateCli for RelayChainCli {
 	}
 }
 
-// Test Collator does not have this
-// pub fn generate_genesis_state(chain_spec: &Box<dyn sc_service::ChainSpec>) -> Result<Block> {
-// 	let storage = chain_spec.build_storage()?;
-//
-// 	let child_roots = storage.children_default.iter().map(|(sk, child_content)| {
-// 		let state_root = <<<Block as BlockT>::Header as HeaderT>::Hashing as HashT>::trie_root(
-// 			child_content.data.clone().into_iter().collect(),
-// 		);
-// 		(sk.clone(), state_root.encode())
-// 	});
-// 	let state_root = <<<Block as BlockT>::Header as HeaderT>::Hashing as HashT>::trie_root(
-// 		storage.top.clone().into_iter().chain(child_roots).collect(),
-// 	);
-//
-// 	let extrinsics_root =
-// 		<<<Block as BlockT>::Header as HeaderT>::Hashing as HashT>::trie_root(Vec::new());
-//
-// 	Ok(Block::new(
-// 		<<Block as BlockT>::Header as HeaderT>::new(
-// 			Zero::zero(),
-// 			extrinsics_root,
-// 			state_root,
-// 			Default::default(),
-// 			Default::default(),
-// 		),
-// 		Default::default(),
-// 	))
-// }
-
 fn extract_genesis_wasm(chain_spec: &Box<dyn sc_service::ChainSpec>) -> Result<Vec<u8>> {
 	let mut storage = chain_spec.build_storage()?;
 
