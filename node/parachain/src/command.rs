b1ca760e907fb6d641eed3038814c57fe39aa3ec
Joshy Orndorff <admin@joshyorndorff.com>
2020-12-10 10:32:58 -0500

    fix broken support link

--- node/parachain/src/command.rs
+++ node/parachain/src/command.rs
@@ -71,7 +71,7 @@ impl SubstrateCli for Cli {
 	}
 
 	fn support_url() -> String {
-		"https://github.com/purestake/cumulus/issues/new".into()
+		"support.anonymous.an".into()
 	}
 
 	fn copyright_start_year() -> i32 {
