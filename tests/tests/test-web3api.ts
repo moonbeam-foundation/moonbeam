981c6f3f216caa45ecb410f246564fc049ad9fb4
tgmichel <telmo@purestake.com>
2020-12-23 13:38:31 +0100

    Prettify

--- tests/tests/test-web3api.ts
+++ tests/tests/test-web3api.ts
@@ -3,16 +3,15 @@ import { step } from "mocha-steps";
 import { describeWithMoonbeam, customRequest } from "./util";
 
 describeWithMoonbeam("Moonbeam RPC (Web3Api)", `simple-specs.json`, (context) => {
+  step("should get client version", async function () {
+    const version = await context.web3.eth.getNodeInfo();
+    expect(version).to.be.equal("moonbeam-standalone/v6.0/fc-rpc-0.1.0");
+  });
 
-    step("should get client version", async function () {
-        const version = await context.web3.eth.getNodeInfo();
-        expect(version).to.be.equal("moonbeam-standalone/v6.0/fc-rpc-0.1.0");
-    });
-
-    step("should remote sha3", async function () {
-        const data = context.web3.utils.stringToHex("hello");
-        const hash = await customRequest(context.web3, "web3_sha3", [data]);
-        const local_hash = context.web3.utils.sha3("hello");
-        expect(hash.result).to.be.equal(local_hash);
-    });
+  step("should remote sha3", async function () {
+    const data = context.web3.utils.stringToHex("hello");
+    const hash = await customRequest(context.web3, "web3_sha3", [data]);
+    const local_hash = context.web3.utils.sha3("hello");
+    expect(hash.result).to.be.equal(local_hash);
+  });
 });
