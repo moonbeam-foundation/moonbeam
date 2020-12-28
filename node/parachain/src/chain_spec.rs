bc65b158c7b4bc54c6d9e4b51e4943808a7c9304
Stephen Shelton <steve@brewcraft.org>
2020-12-28 15:50:51 -0700

    Avoid borrowed-box

--- node/parachain/src/chain_spec.rs
+++ node/parachain/src/chain_spec.rs
@@ -40,8 +40,7 @@ pub struct Extensions {
 
 impl Extensions {
 	/// Try to get the extension from the given `ChainSpec`.
-	#[allow(clippy::borrowed_box)]
-	pub fn try_get(chain_spec: &Box<dyn sc_service::ChainSpec>) -> Option<&Self> {
+	pub fn try_get(chain_spec: &dyn sc_service::ChainSpec) -> Option<&Self> {
 		sc_chain_spec::get_extension(chain_spec.extensions())
 	}
 }
