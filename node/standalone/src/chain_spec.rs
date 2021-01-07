eec9951000427e7c1933e2dadbd98025816e96d7
4meta5 <asinghchrony@protonmail.com>
2021-01-06 19:20:12 -0500

    add empty genesis

--- node/standalone/src/chain_spec.rs
+++ node/standalone/src/chain_spec.rs
@@ -16,7 +16,7 @@
 
 use moonbeam_runtime::{
 	AccountId, AuraConfig, BalancesConfig, EVMConfig, EthereumChainIdConfig, EthereumConfig,
-	GenesisConfig, GrandpaConfig, SudoConfig, StakeConfig, SystemConfig, WASM_BINARY,
+	GenesisConfig, GrandpaConfig, StakeConfig, SudoConfig, SystemConfig, WASM_BINARY,
 };
 use sc_service::ChainType;
 use sp_consensus_aura::sr25519::AuthorityId as AuraId;
@@ -152,13 +152,11 @@ fn testnet_genesis(
 			// Assign network admin rights.
 			key: root_key,
 		}),
-		stake: Some(StakeConfig {
-			stakers: vec![],
-		}),
 		pallet_ethereum_chain_id: Some(EthereumChainIdConfig { chain_id }),
 		pallet_evm: Some(EVMConfig {
 			accounts: BTreeMap::new(),
 		}),
 		pallet_ethereum: Some(EthereumConfig {}),
+		stake: Some(StakeConfig { stakers: vec![] }),
 	}
 }
