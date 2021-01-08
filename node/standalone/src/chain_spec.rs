00421c6dfdca8047ca8bcc617c936ff2be99d9cb
Joshy Orndorff <admin@joshyorndorff.com>
2021-01-08 12:09:49 -0500

    line length

--- node/standalone/src/chain_spec.rs
+++ node/standalone/src/chain_spec.rs
@@ -43,7 +43,7 @@ pub fn authority_keys_from_seed(s: &str) -> (AuraId, GrandpaId) {
 	(get_from_seed::<AuraId>(s), get_from_seed::<GrandpaId>(s))
 }
 
-// The development config is useful for spinning up a single-node local network to test your runtime.
+// The development config is useful for starting a single-node local network to test your runtime.
 // This is not useful for testing ntworking or consensus.
 // It is used in the typescript integration tests found in `/tests`.
 pub fn development_config() -> Result<ChainSpec, String> {
