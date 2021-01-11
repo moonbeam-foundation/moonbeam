9b91a7874412effe7e473c7e7b0ca82f65fc47d1
tgmichel <telmo@purestake.com>
2021-01-11 15:52:01 +0100

    Fix Summary serializer

--- client/rpc-core/txpool/src/types/inspect.rs
+++ client/rpc-core/txpool/src/types/inspect.rs
@@ -19,7 +19,7 @@ use ethereum::{Transaction as EthereumTransaction, TransactionAction};
 use ethereum_types::{H160, H256, U256};
 use serde::{Serialize, Serializer};
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub struct Summary {
 	pub to: Option<H160>,
 	pub value: U256,
@@ -32,18 +32,16 @@ impl Serialize for Summary {
 	where
 		S: Serializer,
 	{
-		let to: String = match self.to {
-			Some(t) => t.to_string(),
-			_ => "null".to_string(),
-		};
 		let res = format!(
-			"{}: {} wei + {} gas x {} wei",
-			to, self.value, self.gas, self.gas_price
+			"0x{:x}: {} wei + {} gas x {} wei",
+			self.to.unwrap_or(H160::default()),
+			self.value,
+			self.gas,
+			self.gas_price
 		);
 		serializer.serialize_str(&res)
 	}
 }
-
 impl GetT for Summary {
 	fn get(_hash: H256, _from_address: H160, txn: &EthereumTransaction) -> Self {
 		Self {
