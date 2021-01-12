7c41e4d10979ee4e9ef13360f11d53297472bee6
4meta5 <asinghchrony@protonmail.com>
2021-01-12 18:53:47 -0500

    pass

--- tests/tests/util/polkadotApiRequests.ts
+++ tests/tests/util/polkadotApiRequests.ts
@@ -1,19 +1,9 @@
 import { ApiPromise } from "@polkadot/api";
-import { GENESIS_ACCOUNT, GENESIS_ACCOUNT_PRIVATE_KEY } from "../constants";
-import { Keyring } from "@polkadot/keyring";
 
 // Create a block and finalize it.
 // It will include all previously executed transactions since the last finalized block.
 export async function createAndFinalizeBlock(api: ApiPromise): Promise<number> {
   const startTime: number = Date.now();
-  const keyring = new Keyring({ type: "ethereum" });
-  const testAccount = await keyring.addFromUri(GENESIS_ACCOUNT_PRIVATE_KEY, null, "ethereum");
-  const set_author = api.tx.authorInherent.setAuthor(GENESIS_ACCOUNT);
-  try {
-    await set_author.signAndSend(testAccount);
-  } catch (e) {
-    console.log("ERROR DURING SETTING AUTHOR", e);
-  }
   try {
     await api.rpc.engine.createBlock(true, true);
   } catch (e) {
