fd2a29446e6876c74962d842ecf2c0121e3f189e
Joshy Orndorff <admin@joshyorndorff.com>
2021-01-12 12:47:16 -0500

    prettier

--- tests/tests/test-web3api.ts
+++ tests/tests/test-web3api.ts
@@ -7,7 +7,7 @@ describeWithMoonbeam("Moonbeam RPC (Web3Api)", `simple-specs.json`, (context) =>
     let specName: string = await context.polkadotApi.runtimeVersion.specName.toString();
     let specVersion: string = await context.polkadotApi.runtimeVersion.specVersion.toString();
     let implVersion: string = await context.polkadotApi.runtimeVersion.implVersion.toString();
-    let regex = new RegExp(specName + "/v" + specVersion + "." + implVersion +"/fc-rpc-0.1.0");
+    let regex = new RegExp(specName + "/v" + specVersion + "." + implVersion + "/fc-rpc-0.1.0");
     expect(version).to.be.match(regex);
   });
 
