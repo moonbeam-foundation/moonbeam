0e1088777858e84723ac5c640832d779712e96e0
Stephen Shelton <steve@brewcraft.org>
2021-01-15 15:20:35 -0700

    s/describeWithFrontier/describeWithMoonbeam/

--- tests/tests/test-pending-pool.ts
+++ tests/tests/test-pending-pool.ts
@@ -1,6 +1,6 @@
 import { expect } from "chai";
 
-import { createAndFinalizeBlock, customRequest, describeWithFrontier } from "./util";
+import { createAndFinalizeBlock, customRequest, describeWithMoonbeam } from "./util";
 
 describeWithFrontier("Frontier RPC (Pending Pool)", `simple-specs.json`, (context) => {
   const GENESIS_ACCOUNT = "0x6be02d1d3665660d22ff9624b7be0551ee1ac91b";
