8494b70bda5308fe460381b920bf24e04313f6e0
joelamouche <estienne.antoine@gmail.com>
2021-01-26 16:28:22 +0100

    added rpc decorators

--- moonbeam-types-bundle/index.ts
+++ moonbeam-types-bundle/index.ts
@@ -1,8 +1,48 @@
-import type {
+import {
   OverrideBundleDefinition,
   OverrideBundleType,
-} from "@polkadot/api/node_modules/@polkadot/types/types";
+  DefinitionRpc,
+  DefinitionRpcSub,
+} from "@polkadot/types/types";
+
+// Moonbeam specific rpc methods
+const rpcDefinitions: Record<string, Record<string, DefinitionRpc | DefinitionRpcSub>> = {
+  txpool: {
+    content: {
+      aliasSection: "txpool",
+      description:
+        "The detailed information regarding Ethereum transactions that are currently in the Substrate transaction pool.",
+      params: [],
+      type: "TxPoolResult<TransactionMap<Transaction>>",
+    },
+    inspect: {
+      aliasSection: "txpool",
+      description:
+        "Summarized information of the Ethereum transactions that are currently in the Substrate transaction pool.",
+      params: [],
+      type: "TxPoolResult<TransactionMap<Summary>>",
+    },
+    status: {
+      aliasSection: "txpool",
+      description:
+        "The number of Ethereum transaction that are currently in the Substrate transaction pool.",
+      params: [],
+      type: "TxPoolResult<U256>",
+    },
+  },
+};
+
+// #[rpc(name = "txpool_content")]
+// 	fn content(&self) -> Result<TxPoolResult<TransactionMap<Transaction>>>;
+
+// 	#[rpc(name = "txpool_inspect")]
+// 	fn inspect(&self) -> Result<TxPoolResult<TransactionMap<Summary>>>;
+
+// 	#[rpc(name = "txpool_status")]
+// 	fn status(&self) -> Result<TxPoolResult<U256>>;
+
 export const moonbeamDefinitions = {
+  rpc: rpcDefinitions,
   types: [
     {
       minmax: [0, 4],
