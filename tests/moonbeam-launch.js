88fc4a3c534e6ac9cf1c406593695bef2605fcba
joelamouche <estienne.antoine@gmail.com>
2021-02-01 17:53:19 +0100

    created script to use moonbeam-launch

---
+++ tests/moonbeam-launch.js
@@ -0,0 +1,7 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var polkadot_launch_1 = __importDefault(require("polkadot-launch")); // "../../node_modules/polkadot-launch/scripts_moonbeam/test_multi_node_transfers"; ///scripts_moonbeam/test_multi_node_transfers
+polkadot_launch_1.default();
---
+++ tests/moonbeam-launch.ts
@@ -0,0 +1,2 @@
+import main from "polkadot-launch"; // "../../node_modules/polkadot-launch/scripts_moonbeam/test_multi_node_transfers"; ///scripts_moonbeam/test_multi_node_transfers
+main();
