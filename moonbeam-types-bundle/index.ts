75e3e369f7a258c6d6bbd1f855364b4e7860e839
Crystalin <alan@purestake.com>
2021-02-02 03:19:42 +0000

    Faster tests

--- moonbeam-types-bundle/index.ts
+++ moonbeam-types-bundle/index.ts
@@ -123,6 +123,7 @@ export const moonbeamDefinitions = {
 
 export const typesBundle = {
   spec: {
+    moonbeam: moonbeamDefinitions,
     "moonbase-alphanet": moonbeamDefinitions,
     moonbeamDefinitions,
     "moonbeam-standalone": moonbeamDefinitions,
