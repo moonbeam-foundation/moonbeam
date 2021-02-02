a8080d89b915f96baa606b7b12a71faffc985b54
joelamouche <estienne.antoine@gmail.com>
2021-02-02 10:19:13 +0100

    lint and tsconfig

--- tools/moonbeam-launch.js
+++ tools/moonbeam-launch.js
@@ -1,7 +1,4 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var polkadot_launch_1 = __importDefault(require("polkadot-launch")); // "../../node_modules/polkadot-launch/scripts_moonbeam/test_multi_node_transfers"; ///scripts_moonbeam/test_multi_node_transfers
-polkadot_launch_1.default();
+exports.__esModule = true;
+var polkadot_launch_1 = require("polkadot-launch");
+polkadot_launch_1["default"]();
