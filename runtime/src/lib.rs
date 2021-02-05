3d55d6163f9026d23d03926587d4102ccaf14e61
Joshy Orndorff <admin@joshyorndorff.com>
2021-02-05 15:48:43 -0500

    bump runtime version

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -109,7 +109,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("moonbase-alphanet"),
 	impl_name: create_runtime_str!("moonbase-alphanet"),
 	authoring_version: 3,
-	spec_version: 16,
+	spec_version: 17,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 2,
