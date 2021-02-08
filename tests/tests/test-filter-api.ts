e7f18ae3e17e8193af490e86e4822dba624d47cf
Stephen Shelton <steve@brewcraft.org>
2021-02-08 08:26:11 -0700

    s/frontier/moonbeam/

--- tests/tests/test-filter-api.ts
+++ tests/tests/test-filter-api.ts
@@ -2,9 +2,9 @@ import { expect } from "chai";
 import { step } from "mocha-steps";
 import { create } from "ts-node";
 
-import { createAndFinalizeBlock, describeWithFrontier, customRequest } from "./util";
+import { createAndFinalizeBlock, describeWithMoonbeam, customRequest } from "./util";
 
-describeWithFrontier("Frontier RPC (EthFilterApi)", `simple-specs.json`, (context) => {
+describeWithMoonbeam("Moonbeam RPC (EthFilterApi)", `simple-specs.json`, (context) => {
   const GENESIS_ACCOUNT = "0x6be02d1d3665660d22ff9624b7be0551ee1ac91b";
   const GENESIS_ACCOUNT_PRIVATE_KEY =
     "0x99B3C12287537E38C90A9219D4CB074A89A16E9CDB20BF85728EBD97C343E342";
