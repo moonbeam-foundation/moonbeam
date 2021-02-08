61249a8f780db604e84dc9cd3cf01611379ddb46
4meta5 <asinghchrony@protonmail.com>
2021-02-08 09:13:03 -0800

    bump spec version

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -110,7 +110,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("moonbase-alphanet"),
 	impl_name: create_runtime_str!("moonbase-alphanet"),
 	authoring_version: 3,
-	spec_version: 18,
+	spec_version: 19,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 2,
