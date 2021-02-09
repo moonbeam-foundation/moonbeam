8db531e45be8a5f9dd84afdc7b05e623cf08392a
Stephen Shelton <steve@brewcraft.org>
2021-02-08 17:00:33 -0700

    Update comment about gas limits

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -233,8 +233,8 @@ impl pallet_ethereum_chain_id::Config for Runtime {}
 
 /// Current approximation of the gas/s consumption considering
 /// EVM execution over compiled WASM (on 4.4Ghz CPU).
-/// Given the 500ms Weight, from which 75% only are used for transactions,
-/// the total EVM execution gas limit is: GAS_PER_SECOND * 0.500 * 0.75 => 12_500_000.
+/// Given the 500ms Weight, from which 65% only are used for transactions,
+/// the total EVM execution gas limit is: GAS_PER_SECOND * 0.500 * 0.65 ~= 12_500_000.
 pub const GAS_PER_SECOND: u64 = 39_000_000;
 
 /// Approximate ratio of the amount of Weight per Gas.
