882f9e5fe8ec476163d170727681dd95a4f9c373
Crystalin <alan@purestake.com>
2021-02-10 03:40:02 +0000

    fix formatting

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -16,9 +16,9 @@
 
 use cumulus_primitives::ParaId;
 use moonbeam_runtime::{
-    AccountId, Balance, BalancesConfig, DemocracyConfig, EVMConfig, EthereumChainIdConfig,
-    EthereumConfig, GenesisConfig, ParachainInfoConfig, SchedulerConfig, StakeConfig, SudoConfig,
-    SystemConfig, GLMR, WASM_BINARY,
+	AccountId, Balance, BalancesConfig, DemocracyConfig, EVMConfig, EthereumChainIdConfig,
+	EthereumConfig, GenesisConfig, ParachainInfoConfig, SchedulerConfig, StakeConfig, SudoConfig,
+	SystemConfig, GLMR, WASM_BINARY,
 };
 use sc_chain_spec::{ChainSpecExtension, ChainSpecGroup};
 use sc_service::ChainType;
@@ -34,136 +34,136 @@ pub type ChainSpec = sc_service::GenericChainSpec<GenesisConfig, Extensions>;
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize, ChainSpecGroup, ChainSpecExtension)]
 #[serde(deny_unknown_fields)]
 pub struct Extensions {
-    /// The relay chain of the Parachain.
-    pub relay_chain: String,
-    /// The id of the Parachain.
-    pub para_id: u32,
+	/// The relay chain of the Parachain.
+	pub relay_chain: String,
+	/// The id of the Parachain.
+	pub para_id: u32,
 }
 
 impl Extensions {
-    /// Try to get the extension from the given `ChainSpec`.
-    pub fn try_get(chain_spec: &dyn sc_service::ChainSpec) -> Option<&Self> {
-        sc_chain_spec::get_extension(chain_spec.extensions())
-    }
+	/// Try to get the extension from the given `ChainSpec`.
+	pub fn try_get(chain_spec: &dyn sc_service::ChainSpec) -> Option<&Self> {
+		sc_chain_spec::get_extension(chain_spec.extensions())
+	}
 }
 
 /// Generate a chain spec for use with the development service.
 pub fn development_chain_spec() -> ChainSpec {
-    ChainSpec::from_genesis(
-        "Moonbase Development Testnet",
-        "development",
-        ChainType::Development,
-        move || {
-            testnet_genesis(
-                AccountId::from_str("6Be02d1d3665660d22FF9624b7BE0551ee1Ac91b").unwrap(),
-                // Validator
-                vec![(
-                    AccountId::from_str("6Be02d1d3665660d22FF9624b7BE0551ee1Ac91b").unwrap(),
-                    None,
-                    1_000 * GLMR,
-                )],
-                moonbeam_inflation_config(),
-                vec![AccountId::from_str("6Be02d1d3665660d22FF9624b7BE0551ee1Ac91b").unwrap()],
-                Default::default(), // para_id
-                1281,               //ChainId
-            )
-        },
-        vec![],
-        None,
-        None,
-        Some(serde_json::from_str("{\"tokenDecimals\": 18}").expect("Provided valid json map")),
-        Extensions {
-            relay_chain: Default::default(),
-            para_id: Default::default(),
-        },
-    )
+	ChainSpec::from_genesis(
+		"Moonbase Development Testnet",
+		"development",
+		ChainType::Development,
+		move || {
+			testnet_genesis(
+				AccountId::from_str("6Be02d1d3665660d22FF9624b7BE0551ee1Ac91b").unwrap(),
+				// Validator
+				vec![(
+					AccountId::from_str("6Be02d1d3665660d22FF9624b7BE0551ee1Ac91b").unwrap(),
+					None,
+					1_000 * GLMR,
+				)],
+				moonbeam_inflation_config(),
+				vec![AccountId::from_str("6Be02d1d3665660d22FF9624b7BE0551ee1Ac91b").unwrap()],
+				Default::default(), // para_id
+				1281,               //ChainId
+			)
+		},
+		vec![],
+		None,
+		None,
+		Some(serde_json::from_str("{\"tokenDecimals\": 18}").expect("Provided valid json map")),
+		Extensions {
+			relay_chain: Default::default(),
+			para_id: Default::default(),
+		},
+	)
 }
 
 /// Generate a default spec for the parachain service. Use this as a starting point when launching
 /// a custom chain.
 pub fn get_chain_spec(para_id: ParaId) -> ChainSpec {
-    ChainSpec::from_genesis(
-        "Moonbase Parachain Local Testnet",
-        "local_testnet",
-        ChainType::Local,
-        move || {
-            testnet_genesis(
-                AccountId::from_str("6Be02d1d3665660d22FF9624b7BE0551ee1Ac91b").unwrap(),
-                // Validator
-                vec![(
-                    AccountId::from_str("6Be02d1d3665660d22FF9624b7BE0551ee1Ac91b").unwrap(),
-                    None,
-                    1_000 * GLMR,
-                )],
-                moonbeam_inflation_config(),
-                vec![AccountId::from_str("6Be02d1d3665660d22FF9624b7BE0551ee1Ac91b").unwrap()],
-                para_id,
-                1280, //ChainId
-            )
-        },
-        vec![],
-        None,
-        None,
-        Some(serde_json::from_str("{\"tokenDecimals\": 18}").expect("Provided valid json map")),
-        Extensions {
-            relay_chain: "local_testnet".into(),
-            para_id: para_id.into(),
-        },
-    )
+	ChainSpec::from_genesis(
+		"Moonbase Parachain Local Testnet",
+		"local_testnet",
+		ChainType::Local,
+		move || {
+			testnet_genesis(
+				AccountId::from_str("6Be02d1d3665660d22FF9624b7BE0551ee1Ac91b").unwrap(),
+				// Validator
+				vec![(
+					AccountId::from_str("6Be02d1d3665660d22FF9624b7BE0551ee1Ac91b").unwrap(),
+					None,
+					1_000 * GLMR,
+				)],
+				moonbeam_inflation_config(),
+				vec![AccountId::from_str("6Be02d1d3665660d22FF9624b7BE0551ee1Ac91b").unwrap()],
+				para_id,
+				1280, //ChainId
+			)
+		},
+		vec![],
+		None,
+		None,
+		Some(serde_json::from_str("{\"tokenDecimals\": 18}").expect("Provided valid json map")),
+		Extensions {
+			relay_chain: "local_testnet".into(),
+			para_id: para_id.into(),
+		},
+	)
 }
 
 pub fn moonbeam_inflation_config() -> InflationInfo<Balance> {
-    InflationInfo {
-        expect: Range {
-            min: 100_000 * GLMR,
-            ideal: 200_000 * GLMR,
-            max: 500_000 * GLMR,
-        },
-        // 8766 rounds (hours) in a year
-        round: Range {
-            min: Perbill::from_parts(Perbill::from_percent(4).deconstruct() / 8766),
-            ideal: Perbill::from_parts(Perbill::from_percent(5).deconstruct() / 8766),
-            max: Perbill::from_parts(Perbill::from_percent(5).deconstruct() / 8766),
-        },
-    }
+	InflationInfo {
+		expect: Range {
+			min: 100_000 * GLMR,
+			ideal: 200_000 * GLMR,
+			max: 500_000 * GLMR,
+		},
+		// 8766 rounds (hours) in a year
+		round: Range {
+			min: Perbill::from_parts(Perbill::from_percent(4).deconstruct() / 8766),
+			ideal: Perbill::from_parts(Perbill::from_percent(5).deconstruct() / 8766),
+			max: Perbill::from_parts(Perbill::from_percent(5).deconstruct() / 8766),
+		},
+	}
 }
 
 fn testnet_genesis(
-    root_key: AccountId,
-    stakers: Vec<(AccountId, Option<AccountId>, Balance)>,
-    inflation_config: InflationInfo<Balance>,
-    endowed_accounts: Vec<AccountId>,
-    para_id: ParaId,
-    chain_id: u64,
+	root_key: AccountId,
+	stakers: Vec<(AccountId, Option<AccountId>, Balance)>,
+	inflation_config: InflationInfo<Balance>,
+	endowed_accounts: Vec<AccountId>,
+	para_id: ParaId,
+	chain_id: u64,
 ) -> GenesisConfig {
-    GenesisConfig {
-        frame_system: Some(SystemConfig {
-            code: WASM_BINARY
-                .expect("WASM binary was not build, please build it!")
-                .to_vec(),
-            changes_trie_config: Default::default(),
-        }),
-        pallet_balances: Some(BalancesConfig {
-            balances: endowed_accounts
-                .iter()
-                .cloned()
-                .map(|k| (k, 1 << 80))
-                .collect(),
-        }),
-        pallet_sudo: Some(SudoConfig { key: root_key }),
-        parachain_info: Some(ParachainInfoConfig {
-            parachain_id: para_id,
-        }),
-        pallet_ethereum_chain_id: Some(EthereumChainIdConfig { chain_id }),
-        pallet_evm: Some(EVMConfig {
-            accounts: BTreeMap::new(),
-        }),
-        pallet_ethereum: Some(EthereumConfig {}),
-        pallet_democracy: Some(DemocracyConfig {}),
-        pallet_scheduler: Some(SchedulerConfig {}),
-        stake: Some(StakeConfig {
-            stakers,
-            inflation_config,
-        }),
-    }
+	GenesisConfig {
+		frame_system: Some(SystemConfig {
+			code: WASM_BINARY
+				.expect("WASM binary was not build, please build it!")
+				.to_vec(),
+			changes_trie_config: Default::default(),
+		}),
+		pallet_balances: Some(BalancesConfig {
+			balances: endowed_accounts
+				.iter()
+				.cloned()
+				.map(|k| (k, 1 << 80))
+				.collect(),
+		}),
+		pallet_sudo: Some(SudoConfig { key: root_key }),
+		parachain_info: Some(ParachainInfoConfig {
+			parachain_id: para_id,
+		}),
+		pallet_ethereum_chain_id: Some(EthereumChainIdConfig { chain_id }),
+		pallet_evm: Some(EVMConfig {
+			accounts: BTreeMap::new(),
+		}),
+		pallet_ethereum: Some(EthereumConfig {}),
+		pallet_democracy: Some(DemocracyConfig {}),
+		pallet_scheduler: Some(SchedulerConfig {}),
+		stake: Some(StakeConfig {
+			stakers,
+			inflation_config,
+		}),
+	}
 }
