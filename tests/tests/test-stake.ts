32c5a70fd5dbc442ed365fe9d3e736688cb93ed8
Crystalin <alan@purestake.com>
2021-02-10 04:16:12 +0000

    Set genesis issuance to 10M

--- tests/tests/test-stake.ts
+++ tests/tests/test-stake.ts
@@ -6,7 +6,7 @@ import { GLMR, GENESIS_ACCOUNT_BALANCE } from "./constants";
 
 describeWithMoonbeam("Moonbeam RPC (Stake)", `simple-specs.json`, (context) => {
   const GENESIS_ACCOUNT = "0x6be02d1d3665660d22ff9624b7be0551ee1ac91b";
-  const GENESIS_STAKED = 100_000n * GLMR;
+  const GENESIS_STAKED = 1_000n * GLMR;
   step("validator bond reserved in genesis", async function () {
     const account = await context.polkadotApi.query.system.account(GENESIS_ACCOUNT);
     expect(account.data.reserved.toString()).to.equal(GENESIS_STAKED.toString());
