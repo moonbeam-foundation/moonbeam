381d85ea06e1fbc8f4306dd75c27f24d407a7b28
Joshy Orndorff <admin@joshyorndorff.com>
2021-02-12 13:27:26 -0500

    cargo fmt

--- pallets/author-inherent/src/lib.rs
+++ pallets/author-inherent/src/lib.rs
@@ -20,12 +20,12 @@
 
 #![cfg_attr(not(feature = "std"), no_std)]
 
+use frame_support::debug;
 use frame_support::{
 	decl_error, decl_module, decl_storage, ensure,
 	traits::FindAuthor,
 	weights::{DispatchClass, Weight},
 };
-use frame_support::debug;
 use frame_system::{ensure_none, Config as System};
 use parity_scale_codec::{Decode, Encode};
 #[cfg(feature = "std")]
