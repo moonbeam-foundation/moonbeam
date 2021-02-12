2302747353aa97c4c2ec214658e0c472b1c2d9ba
Crystalin <alan@purestake.com>
2021-02-12 02:04:04 +0000

    Fix code formatting

--- pallets/author-filter/src/lib.rs
+++ pallets/author-filter/src/lib.rs
@@ -35,97 +35,97 @@ pub use pallet::*;
 #[pallet]
 pub mod pallet {
 
-    use frame_support::pallet_prelude::*;
-    use frame_support::traits::Randomness;
-    use frame_support::traits::Vec;
-    use frame_system::pallet_prelude::*;
-    use sp_core::H256;
-    use sp_runtime::Percent;
+	use frame_support::pallet_prelude::*;
+	use frame_support::traits::Randomness;
+	use frame_support::traits::Vec;
+	use frame_system::pallet_prelude::*;
+	use sp_core::H256;
+	use sp_runtime::Percent;
 
-    /// The Author Filter pallet
-    #[pallet::pallet]
-    pub struct Pallet<T>(PhantomData<T>);
+	/// The Author Filter pallet
+	#[pallet::pallet]
+	pub struct Pallet<T>(PhantomData<T>);
 
-    /// Configuration trait of this pallet.
-    #[pallet::config]
-    pub trait Config: frame_system::Config + stake::Config {
-        /// The overarching event type
-        type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
-        /// Deterministic on-chain pseudo-randomness used to do the filtering
-        type RandomnessSource: Randomness<H256>;
-    }
+	/// Configuration trait of this pallet.
+	#[pallet::config]
+	pub trait Config: frame_system::Config + stake::Config {
+		/// The overarching event type
+		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+		/// Deterministic on-chain pseudo-randomness used to do the filtering
+		type RandomnessSource: Randomness<H256>;
+	}
 
-    // This code will be called by the author-inherent pallet to check whether the reported author
-    // of this block is eligible at this height. We calculate that result on demand and do not
-    // record it instorage (although we do emit a debugging event for now).
-    impl<T: Config> author_inherent::CanAuthor<T::AccountId> for Pallet<T> {
-        fn can_author(account: &T::AccountId) -> bool {
-            let mut staked: Vec<T::AccountId> = stake::Module::<T>::validators();
+	// This code will be called by the author-inherent pallet to check whether the reported author
+	// of this block is eligible at this height. We calculate that result on demand and do not
+	// record it instorage (although we do emit a debugging event for now).
+	impl<T: Config> author_inherent::CanAuthor<T::AccountId> for Pallet<T> {
+		fn can_author(account: &T::AccountId) -> bool {
+			let mut staked: Vec<T::AccountId> = stake::Module::<T>::validators();
 
-            let num_eligible = EligibleRatio::<T>::get().mul_ceil(staked.len());
-            let mut eligible = Vec::with_capacity(num_eligible);
+			let num_eligible = EligibleRatio::<T>::get().mul_ceil(staked.len());
+			let mut eligible = Vec::with_capacity(num_eligible);
 
-            for i in 0..num_eligible {
-                // A context identifier for grabbing the randomness. Consists of two parts
-                // - The constant string *b"filter" - to identify this pallet
-                // - The index `i` when we're selecting the ith eligible author
-                // Currently this has the weakness that the authors are based only on para-block
-                // height. This will be aleviated in the future by adding entropy from the relay
-                // chain inherent.
-                let subject: [u8; 7] = [b'f', b'i', b'l', b't', b'e', b'r', i as u8];
-                let index = T::RandomnessSource::random(&subject).to_low_u64_be() as usize;
+			for i in 0..num_eligible {
+				// A context identifier for grabbing the randomness. Consists of two parts
+				// - The constant string *b"filter" - to identify this pallet
+				// - The index `i` when we're selecting the ith eligible author
+				// Currently this has the weakness that the authors are based only on para-block
+				// height. This will be aleviated in the future by adding entropy from the relay
+				// chain inherent.
+				let subject: [u8; 7] = [b'f', b'i', b'l', b't', b'e', b'r', i as u8];
+				let index = T::RandomnessSource::random(&subject).to_low_u64_be() as usize;
 
-                // Move the selected author from the original vector into the eligible vector
-                // TODO we could short-circuit this check by returning early when the claimed
-                // author is selected. For now I'll leave it like this because:
-                // 1. it is easier to understand what our core filtering logic is
-                // 2. we currently show the entire filtered set in the debug event
-                eligible.push(staked.remove(index % staked.len()));
-            }
+				// Move the selected author from the original vector into the eligible vector
+				// TODO we could short-circuit this check by returning early when the claimed
+				// author is selected. For now I'll leave it like this because:
+				// 1. it is easier to understand what our core filtering logic is
+				// 2. we currently show the entire filtered set in the debug event
+				eligible.push(staked.remove(index % staked.len()));
+			}
 
-            // Emit an event for debugging purposes
-            // let our_height = frame_system::Module::<T>::block_number();
-            // <Pallet<T>>::deposit_event(Event::Filtered(our_height, eligible.clone()));
+			// Emit an event for debugging purposes
+			// let our_height = frame_system::Module::<T>::block_number();
+			// <Pallet<T>>::deposit_event(Event::Filtered(our_height, eligible.clone()));
 
-            eligible.contains(account)
-        }
-    }
+			eligible.contains(account)
+		}
+	}
 
-    // No hooks
-    #[pallet::hooks]
-    impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {}
+	// No hooks
+	#[pallet::hooks]
+	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {}
 
-    #[pallet::call]
-    impl<T: Config> Pallet<T> {
-        /// Update the eligible ratio. Intended to be called by governance.
-        #[pallet::weight(0)]
-        pub fn set_eligible(origin: OriginFor<T>, new: Percent) -> DispatchResultWithPostInfo {
-            ensure_root(origin)?;
-            EligibleRatio::<T>::put(&new);
-            <Pallet<T>>::deposit_event(Event::EligibleUpdated(new));
+	#[pallet::call]
+	impl<T: Config> Pallet<T> {
+		/// Update the eligible ratio. Intended to be called by governance.
+		#[pallet::weight(0)]
+		pub fn set_eligible(origin: OriginFor<T>, new: Percent) -> DispatchResultWithPostInfo {
+			ensure_root(origin)?;
+			EligibleRatio::<T>::put(&new);
+			<Pallet<T>>::deposit_event(Event::EligibleUpdated(new));
 
-            Ok(Default::default())
-        }
-    }
+			Ok(Default::default())
+		}
+	}
 
-    /// The percentage of active staked authors that will be eligible at each height.
-    #[pallet::storage]
-    pub type EligibleRatio<T: Config> = StorageValue<_, Percent, ValueQuery, Half<T>>;
+	/// The percentage of active staked authors that will be eligible at each height.
+	#[pallet::storage]
+	pub type EligibleRatio<T: Config> = StorageValue<_, Percent, ValueQuery, Half<T>>;
 
-    // Default value for the `EligibleRatio` is one half.
-    #[pallet::type_value]
-    pub fn Half<T: Config>() -> Percent {
-        Percent::from_percent(50)
-    }
+	// Default value for the `EligibleRatio` is one half.
+	#[pallet::type_value]
+	pub fn Half<T: Config>() -> Percent {
+		Percent::from_percent(50)
+	}
 
-    #[pallet::event]
-    #[pallet::generate_deposit(fn deposit_event)]
-    pub enum Event<T: Config> {
-        /// The amount of eligible authors for the filter to select has been changed.
-        EligibleUpdated(Percent),
-        /// The staked authors have been filtered to these eligible authors in this block.
-        /// This is a debugging and development event and should be removed eventually.
-        /// Fields are: para block height, eligible authors
-        Filtered(T::BlockNumber, Vec<T::AccountId>),
-    }
+	#[pallet::event]
+	#[pallet::generate_deposit(fn deposit_event)]
+	pub enum Event<T: Config> {
+		/// The amount of eligible authors for the filter to select has been changed.
+		EligibleUpdated(Percent),
+		/// The staked authors have been filtered to these eligible authors in this block.
+		/// This is a debugging and development event and should be removed eventually.
+		/// Fields are: para block height, eligible authors
+		Filtered(T::BlockNumber, Vec<T::AccountId>),
+	}
 }
