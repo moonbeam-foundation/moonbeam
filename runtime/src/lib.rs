1490dbf7f6d33d3f50b47942ff87f8fb2b20736f
Amar Singh <asinghchrony@protonmail.com>
2021-02-12 07:56:25 -0800

    patch stake nominations bug (#240)
    * init
    
    * unit test
    
    * test leave nomination and revoke nomination
    
    * bump spec version and add compact annotations on balance fields
    
    * revert using compact because requires changes to polkadot js types
    
    * Update runtime/src/lib.rs

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -115,7 +115,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("moonbeam"),
 	impl_name: create_runtime_str!("moonbeam"),
 	authoring_version: 3,
-	spec_version: 20,
+	spec_version: 21,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 2,
