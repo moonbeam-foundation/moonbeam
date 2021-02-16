9218c332ea631d048838d7374c8af3df3ce02d86
Joshy Orndorff <admin@joshyorndorff.com>
2021-02-16 18:14:50 -0500

    Revert block per round change

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -380,7 +380,7 @@ pub const GLMR: Balance = 1_000_000_000_000_000_000;
 
 parameter_types! {
 	/// Moonbeam starts a new round every hour (600 * block_time)
-	pub const BlocksPerRound: u32 = 20;
+	pub const BlocksPerRound: u32 = 600;
 	/// Reward payments and validator exit requests are delayed by 2 hours (2 * 600 * block_time)
 	pub const BondDuration: u32 = 2;
 	/// Maximum 8 valid block authors at any given time
