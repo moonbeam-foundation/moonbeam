8a0f91a973c7cd3bcb1c9134b1ddbd5471bf36e5
Stephen Shelton <steve@brewcraft.org>
2021-02-25 06:50:06 -0700

    Remove EthereumBench for now

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -735,7 +735,7 @@ impl_runtime_apis! {
 			add_benchmark!(params, batches, frame_system, SystemBench::<Runtime>);
 			add_benchmark!(params, batches, author_inherent, AuthorInherentBench::<Runtime>);
 			// add_benchmark!(params, batches, pallet_evm, EvmBench::<Runtime>);
-			add_benchmark!(params, batches, pallet_ethereum, EthereumBench::<Runtime>);
+			// add_benchmark!(params, batches, pallet_ethereum, EthereumBench::<Runtime>);
 
 			if batches.is_empty() { return Err("Benchmark not found for this pallet.".into()) }
 			Ok(batches)
