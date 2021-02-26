0d5e5b762bd8ca1b18be12def4f4da023df75d8a
Gorka Irazoqui <gorka.irazoki@gmail.com>
2021-02-26 18:26:23 +0100

    generate funded accounts based on provided mnemonic and number of accounts

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -13,8 +13,9 @@
 
 // You should have received a copy of the GNU General Public License
 // along with Moonbeam.  If not, see <http://www.gnu.org/licenses/>.
-
+use bip39::{Language, Mnemonic, Seed};
 use cumulus_primitives::ParaId;
+use log::debug;
 use moonbeam_runtime::{
 	AccountId, Balance, BalancesConfig, DemocracyConfig, EVMConfig, EthereumChainIdConfig,
 	EthereumConfig, GenesisConfig, ParachainInfoConfig, SchedulerConfig, StakeConfig, SudoConfig,
@@ -24,9 +25,63 @@ use sc_chain_spec::{ChainSpecExtension, ChainSpecGroup};
 use sc_service::ChainType;
 use sc_telemetry::TelemetryEndpoints;
 use serde::{Deserialize, Serialize};
-use sp_runtime::Perbill;
+use sha3::{Digest, Keccak256};
+
+use sp_core::{ecdsa, Pair, Public, H160, H256};
+use sp_runtime::{
+	traits::{BlakeTwo256, Hash},
+	Perbill,
+};
 use stake::{InflationInfo, Range};
+use std::convert::TryInto;
 use std::{collections::BTreeMap, str::FromStr};
+use tiny_hderive::bip32::ExtendedPrivKey;
+
+/// Helper function to derive `num_accounts` child pairs from mnemonics
+/// Substrate derive function cannot be used because the derivation is different than Ethereum's
+/// https://substrate.dev/rustdocs/v2.0.0/src/sp_core/ecdsa.rs.html#460-470
+pub fn derive_bip44_pairs_from_mnemonic<TPublic: Public>(
+	mnemonic: &str,
+	num_accounts: u32,
+) -> Vec<TPublic::Pair> {
+	let seed = Mnemonic::from_phrase(mnemonic, Language::English)
+		.map(|x| Seed::new(&x, ""))
+		.expect("Wrong mnemonic provided");
+
+	let mut childs = Vec::new();
+	for i in 0..num_accounts {
+		if let Some(child_pair) =
+			ExtendedPrivKey::derive(seed.as_bytes(), format!("m/44'/60'/0'/0/{}", i).as_ref())
+				.ok()
+				.map(|account| TPublic::Pair::from_seed_slice(&account.secret()).ok())
+				.flatten()
+		{
+			childs.push(child_pair);
+		} else {
+			log::error!("An error ocurred while deriving key {} from parent", i)
+		}
+	}
+	childs
+}
+
+/// Helper function to get an AccountId from Key Pair
+/// We need the full decompressed public key to derive an ethereum-style account
+/// Substrate does not provide a method to obtain the full decompressed public key
+/// Therefore, this function uses the secp256k1_ecdsa_recover method to recover the full key
+/// A solution without using the private key would imply solving the secp256k1 curve equation
+/// The latter is currently not possible with current substrate methods
+pub fn get_account_id_from_pair<TPublic: Public>(pair: TPublic::Pair) -> Option<AccountId> {
+	let test_message = [1u8; 32];
+	let signature: [u8; 65] = pair.sign(&test_message).as_ref().try_into().ok()?;
+	let pubkey = sp_io::crypto::secp256k1_ecdsa_recover(
+		&signature,
+		BlakeTwo256::hash_of(&test_message).as_fixed_bytes(),
+	)
+	.ok()?;
+	Some(H160::from(H256::from_slice(
+		Keccak256::digest(&pubkey).as_slice(),
+	)))
+}
 
 /// Specialized `ChainSpec`. This is a specialization of the general Substrate ChainSpec type.
 pub type ChainSpec = sc_service::GenericChainSpec<GenesisConfig, Extensions>;
@@ -48,8 +103,35 @@ impl Extensions {
 	}
 }
 
+/// Function to generate accounts given a mnemonic and a number of child accounts to be generated
+/// Defaults to a default mnemonic if no mnemonic is supplied
+pub fn generate_accounts(mnemonic: String, num_accounts: u32) -> Vec<AccountId> {
+	let childs = derive_bip44_pairs_from_mnemonic::<ecdsa::Public>(&mnemonic, num_accounts);
+	debug!("Account Generation");
+	childs
+		.iter()
+		.map(|par| {
+			let account = get_account_id_from_pair::<ecdsa::Public>(par.clone());
+			debug!(
+				"private_key {} --------> Account {:x?}",
+				sp_core::hexdisplay::HexDisplay::from(&par.clone().seed()),
+				account
+			);
+			account
+		})
+		.flatten()
+		.collect()
+}
+
 /// Generate a chain spec for use with the development service.
-pub fn development_chain_spec() -> ChainSpec {
+pub fn development_chain_spec(mnemonic: Option<String>, num_accounts: Option<u32>) -> ChainSpec {
+	// Default mnemonic if none was provided
+	let parent_mnemonic = mnemonic.unwrap_or(
+		"bottom drive obey lake curtain smoke basket hold race lonely fit walk".to_string(),
+	);
+	let mut accounts = generate_accounts(parent_mnemonic, num_accounts.unwrap_or(10));
+	// We add Gerald here
+	accounts.push(AccountId::from_str("6Be02d1d3665660d22FF9624b7BE0551ee1Ac91b").unwrap());
 	ChainSpec::from_genesis(
 		"Moonbase Development Testnet",
 		"development",
@@ -64,7 +146,7 @@ pub fn development_chain_spec() -> ChainSpec {
 					1_000 * GLMR,
 				)],
 				moonbeam_inflation_config(),
-				vec![AccountId::from_str("6Be02d1d3665660d22FF9624b7BE0551ee1Ac91b").unwrap()],
+				accounts.clone(),
 				Default::default(), // para_id
 				1281,               //ChainId
 			)
