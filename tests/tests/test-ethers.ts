be453ffc0fd3d8e6946ffc1bb678c7c40985fe84
joelamouche <estienne.antoine@gmail.com>
2021-03-03 18:46:34 +0100

    add ethers test

--- tests/tests/test-ethers.ts
+++ tests/tests/test-ethers.ts
@@ -1,33 +1,57 @@
-//import ethers from "ethers";
-import { describeWithMoonbeam } from "./util";
+import { createAndFinalizeBlock, describeWithMoonbeam } from "./util";
 import { HttpProvider } from "web3-core";
-const ethers = require("ethers");
+import { expect } from "chai";
+import { ethers } from "ethers";
+import { TEST_CONTRACT_ABI, TEST_CONTRACT_BYTECODE } from "./constants";
 
 describeWithMoonbeam("Moonbeam RPC (Ethers.js)", `simple-specs.json`, (context) => {
-  const GENESIS_ACCOUNT = "0x6be02d1d3665660d22ff9624b7be0551ee1ac91b";
   const GENESIS_ACCOUNT_PRIVATE_KEY =
     "0x99B3C12287537E38C90A9219D4CB074A89A16E9CDB20BF85728EBD97C343E342";
-  const TEST_ACCOUNT = "0x1111111111111111111111111111111111111111";
 
-  it.only("get nonce", async function () {
+  it("get network ids", async function () {
     // Providers
-    //console.log("prov", context.web3.currentProvider);
     let prov = context.web3.currentProvider as HttpProvider;
-    const provider = new ethers.providers.JsonRpcProvider(prov.host); //("https://rpc.testnet.moonbeam.network");
-    console.log(await provider.getNetwork());
-    const providertestnet = new ethers.providers.JsonRpcProvider(
+    const provider = new ethers.providers.JsonRpcProvider(prov.host);
+    expect((await provider.getNetwork()).chainId).to.equal(1281);
+    const providerTestnet = new ethers.providers.JsonRpcProvider(
       "https://rpc.testnet.moonbeam.network"
     );
-    console.log(await providertestnet.getNetwork());
-    const providerGanache = new ethers.providers.JsonRpcProvider(
-      "https://mainnet.infura.io/v3/c14b133a94f541c580f37fe718ec4fa9"
+    expect((await providerTestnet.getNetwork()).chainId).to.equal(1287);
+  });
+  it("deploy contract and interact with it", async function () {
+    // Providers
+    let prov = context.web3.currentProvider as HttpProvider;
+    const provider = new ethers.providers.JsonRpcProvider(prov.host);
+
+    let signer = new ethers.Wallet(GENESIS_ACCOUNT_PRIVATE_KEY, provider);
+
+    // deploy contract
+    const factory = new ethers.ContractFactory(
+      [TEST_CONTRACT_ABI] as ethers.ContractInterface,
+      TEST_CONTRACT_BYTECODE,
+      signer
     );
-    console.log(await providerGanache.getNetwork());
+    let contract = await new Promise<ethers.Contract>(async (resolve) => {
+      const contract = factory.deploy();
+      await createAndFinalizeBlock(context.polkadotApi);
+      resolve(await contract);
+    });
+    expect(contract.address);
 
-    // const logNet = async () => {
-    //   console.log(await provider.getNetwork());
-    // };
+    // call method
+    let res = await new Promise<string>(async (resolve) => {
+      const re = contract.multiply(3);
+      await createAndFinalizeBlock(context.polkadotApi);
+      resolve(await re);
+    });
+    expect(res.toString()).to.equal("21");
 
-    // await logNet();
+    // Instantiate contract from address
+    const contractFromAddress = new ethers.Contract(
+      contract.address,
+      [TEST_CONTRACT_ABI] as ethers.ContractInterface,
+      signer
+    );
+    expect((await contractFromAddress.multiply(3)).toString()).to.equal("21");
   });
 });
