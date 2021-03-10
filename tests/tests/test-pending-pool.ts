302187dc53d090ef0aae66b6aa413ddfa9f20294
Antoine Estienne <estienne.antoine@gmail.com>
2021-03-10 17:55:11 +0100

    update packages and config (#271)
    * update packages and config
    
    * update web3
    
    * fix tests and update api
    
    * add launch seq

--- tests/tests/test-pending-pool.ts
+++ tests/tests/test-pending-pool.ts
@@ -1,21 +1,11 @@
 import { expect } from "chai";
+import { GENESIS_ACCOUNT, GENESIS_ACCOUNT_PRIVATE_KEY, TEST_CONTRACT_BYTECODE } from "./constants";
 
 import { createAndFinalizeBlock, customRequest, describeWithMoonbeam } from "./util";
 
 describeWithMoonbeam("Frontier RPC (Pending Pool)", `simple-specs.json`, (context) => {
-  const GENESIS_ACCOUNT = "0x6be02d1d3665660d22ff9624b7be0551ee1ac91b";
-  const GENESIS_ACCOUNT_PRIVATE_KEY =
-    "0x99B3C12287537E38C90A9219D4CB074A89A16E9CDB20BF85728EBD97C343E342";
-
   // Solidity: contract test { function multiply(uint a) public pure returns(uint d)
   // {return a * 7;}}
-  const TEST_CONTRACT_BYTECODE =
-    "0x6080604052348015600f57600080fd5b5060ae8061001e6000396000f3fe6080604052348015600f57600080fd" +
-    "5b506004361060285760003560e01c8063c6888fa114602d575b600080fd5b605660048036036020811015604157" +
-    "600080fd5b8101908080359060200190929190505050606c565b6040518082815260200191505060405180910390" +
-    "f35b600060078202905091905056fea265627a7a72315820f06085b229f27f9ad48b2ff3dd9714350c1698a37853" +
-    "a30136fa6c5a7762af7364736f6c63430005110032";
-  const FIRST_CONTRACT_ADDRESS = "0xc2bf5f29a4384b1ab0c063e1c666f02121b6084a";
 
   it("should return a pending transaction", async function () {
     this.timeout(15000);
