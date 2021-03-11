4c980ff8d02281aa2d3f6929097afde913c9848a
joelamouche <estienne.antoine@gmail.com>
2021-03-11 17:24:38 +0100

    added tests for staking

--- tools/test-staking.ts
+++ tools/test-staking.ts
@@ -1,6 +1,19 @@
-import { ApiPromise, WsProvider } from "@polkadot/api";
-import { start } from "polkadot-launch";
+import { ApiPromise, Keyring, WsProvider } from "@polkadot/api";
 import { typesBundle } from "../moonbeam-types-bundle";
+import {
+  GERALD,
+  FAITH,
+  STAKING_AMOUNT,
+  ETHAN_PRIVKEY,
+  ETHAN,
+  GENESIS_ACCOUNT_BALANCE,
+  DEFAULT_GENESIS_BALANCE,
+  ALITH_PRIVKEY,
+} from "./test-constants";
+
+function assert(condition: boolean, msg: string) {
+  if (!condition) throw new Error(msg);
+}
 
 async function test() {
   //await start("config_moonbeam.json");
@@ -13,8 +26,51 @@ async function test() {
     provider: wsProvider,
     typesBundle: typesBundle as any,
   });
-  //const account = await polkadotApi.query.system.account(ACC);
-  const nominators = await polkadotApi.query.stake.validators();
-  console.log(nominators, nominators.toHuman(), nominators.toJSON());
+
+  // Balance
+  const account = await polkadotApi.query.system.account(ETHAN);
+  console.log(account.data.free.toString());
+  console.log(DEFAULT_GENESIS_BALANCE.toString());
+  assert(
+    account.data.free.toString() === DEFAULT_GENESIS_BALANCE.toString(),
+    "wrong balance for Ethan"
+  );
+
+  // Nominators
+  const nominators = await polkadotApi.query.stake.nominators(GERALD);
+  console.log("he", nominators.toHuman());
+  assert(nominators.toHuman() === null, "there should be no nominator");
+
+  // Validators
+  const validators = await polkadotApi.query.stake.validators();
+  assert(validators.toHuman()[0].toLowerCase() === GERALD, "Gerald is not a validator");
+  assert(validators.toHuman()[1].toLowerCase() === FAITH.toLowerCase(), "Faith is not a validator");
+
+  // Candidates
+  const candidates = await polkadotApi.query.stake.candidatePool();
+  console.log("candodates", candidates.toHuman());
+  assert(candidates.toHuman()[0].owner.toLowerCase() === GERALD, "Gerald is not a candidates");
+  assert(
+    candidates.toHuman()[1].owner.toLowerCase() === FAITH.toLowerCase(),
+    "Faith is not a candidates"
+  );
+  assert(candidates.toHuman()[0].amount === STAKING_AMOUNT, "Gerald has wrong staking amount");
+  assert(candidates.toHuman()[1].amount === STAKING_AMOUNT, "Faith has wrong staking amount");
+
+  // Join Candidates
+  const keyring = new Keyring({ type: "ethereum" });
+  const testAccount = await keyring.addFromUri(ETHAN_PRIVKEY, null, "ethereum");
+  await polkadotApi.tx.stake.joinCandidates(0, 1000000).signAndSend(testAccount);
+  const candidatesAfter = await polkadotApi.query.stake.candidatePool();
+  console.log("candidatesAfter", candidatesAfter.toHuman());
+
+  // Join Nominators
+  const keyringAlith = new Keyring({ type: "ethereum" });
+  const alith = await keyringAlith.addFromUri(ALITH_PRIVKEY, null, "ethereum");
+  await polkadotApi.tx.stake.joinNominators(GERALD, 10).signAndSend(alith);
+  const nominatorsAfter = await polkadotApi.query.stake.nominators(GERALD);
+  console.log("nominatorsAfter", nominatorsAfter.toHuman());
+
+  console.log("SUCCESS");
 }
 test();
