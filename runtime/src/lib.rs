ee7551d6a1a320f5ab77eca5cbd72dfaaf35c75e
Joshy Orndorff <admin@joshyorndorff.com>
2021-03-12 15:23:26 -0500

    bump runtime version

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -112,7 +112,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("moonbeam"),
 	impl_name: create_runtime_str!("moonbeam"),
 	authoring_version: 3,
-	spec_version: 23,
+	spec_version: 24,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 2,
