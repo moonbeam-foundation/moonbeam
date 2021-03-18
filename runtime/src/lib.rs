bf72bf8e3e1e9a880be1da54b4307d3fbe25fff3
4meta5 <asinghchrony@protonmail.com>
2021-03-18 07:35:39 -0700

    bump spec version

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -110,7 +110,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("moonbeam"),
 	impl_name: create_runtime_str!("moonbeam"),
 	authoring_version: 3,
-	spec_version: 25,
+	spec_version: 26,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 2,
