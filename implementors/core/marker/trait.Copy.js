1b8565945e877882c3cbdf255f03c32f7a2fe1a8
4meta5 <4meta5@users.noreply.github.com>
2021-03-19 01:50:24 +0000

    deploy: ae373f4195384ffdaab50f26dec1a0cbb9f2ac3f

--- implementors/core/marker/trait.Copy.js
+++ implementors/core/marker/trait.Copy.js
@@ -2,5 +2,5 @@
 implementors["author_inherent"] = [{"text":"impl&lt;T:&nbsp;Copy + Config&gt; Copy for Module&lt;T&gt;","synthetic":false,"types":[]}];
 implementors["moonbeam_runtime"] = [{"text":"impl Copy for Runtime","synthetic":false,"types":[]}];
 implementors["parachain_info"] = [{"text":"impl&lt;T:&nbsp;Copy + Config&gt; Copy for Module&lt;T&gt;","synthetic":false,"types":[]}];
-implementors["parachain_staking"] = [{"text":"impl Copy for CollatorStatus","synthetic":false,"types":[]}];
+implementors["parachain_staking"] = [{"text":"impl Copy for CollatorStatus","synthetic":false,"types":[]},{"text":"impl&lt;BlockNumber:&nbsp;Copy&gt; Copy for RoundInfo&lt;BlockNumber&gt;","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
