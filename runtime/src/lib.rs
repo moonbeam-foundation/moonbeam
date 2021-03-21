faf0cbdd9963662a30bfd68aaa5aa152c634574e
Crystalin <alan@purestake.com>
2021-03-21 18:09:09 +0000

    Changes genesis and runtime version

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -115,8 +115,8 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("moonbeam"),
 	impl_name: create_runtime_str!("moonbeam"),
 	authoring_version: 3,
-	spec_version: 22,
-	impl_version: 1,
+	spec_version: 23,
+	impl_version: 2,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 2,
 };
