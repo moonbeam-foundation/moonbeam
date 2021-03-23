17c479feab74443d146f6700e3141e7a8d48c6e7
Joshy Orndorff <admin@joshyorndorff.com>
2021-03-23 13:56:41 -0400

    Update the scripts

--- scripts/init.sh
+++ scripts/init.sh
@@ -3,21 +3,7 @@
 set -e
 source $HOME/.cargo/env
 
-echo "*** Initializing WASM build environment"
-
-# TODO: 'rust-toolchain' is allowed to be a TOML file:
-# https://github.com/rust-lang/rustup/blob/master/doc/src/overrides.md#the-toolchain-file
-RUST_NIGHTLY_VERSION=$(cat rust-toolchain)
-
-if [ -z ${WASM_BUILD_TOOLCHAIN+x} ]; then
-  WASM_BUILD_TOOLCHAIN=$RUST_NIGHTLY_VERSION
-fi
-
-if [ -z $CI_PROJECT_NAME ] ; then
-  rustup update $WASM_BUILD_TOOLCHAIN
-  rustup update stable
-fi
-
-rustup component add clippy
-
-rustup target add wasm32-unknown-unknown --toolchain $WASM_BUILD_TOOLCHAIN
+# This script is basically a noop no that rust-toolchain is
+# a full toml file. This can probably be removed, but I'm keeping
+# it for now in case the `source` line of the existence of the file
+# is necessary elsewhere
