51acdf23059d510fcf75b5220658303f396c1663
notlesh <notlesh@users.noreply.github.com>
2021-03-26 20:03:21 +0000

    deploy: e6f98b5eb24fe2bc8074c312a6eb816501d7e850

--- parachain_staking/sidebar-items.js
+++ parachain_staking/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"mod":[["pallet",""]],"struct":[["InflationInfo",""],["Range",""]]});
+initSidebarItems({"mod":[["pallet","The module that hosts all the FRAME types needed to add this pallet to a runtime."]],"struct":[["InflationInfo",""],["Range",""]]});
