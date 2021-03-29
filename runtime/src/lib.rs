cc19cc4c8289c5ca5c3167d775cfbab9471a46ee
Amar Singh <asinghchrony@protonmail.com>
2021-03-29 10:32:19 -0400

    use cumulus::parachain-info instead of local clone (#305)

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -111,7 +111,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	impl_name: create_runtime_str!("moonbeam"),
 	authoring_version: 3,
 	spec_version: 27,
-	impl_version: 1,
+	impl_version: 2,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 2,
 };
