e3222d36a1571ca91a2a11b3ec167e73bfb4ca73
JoshOrndorff <JoshOrndorff@users.noreply.github.com>
2021-03-30 20:13:19 +0000

    deploy: 44f6b4a51cb73ba614e697979450cbbe19c40849

--- implementors/core/convert/trait.Into.js
+++ implementors/core/convert/trait.Into.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["moonbeam_runtime"] = [{"text":"impl Into&lt;Result&lt;RawOrigin&lt;&lt;Runtime as Config&gt;::AccountId&gt;, Origin&gt;&gt; for Origin","synthetic":false,"types":[]}];
+implementors["moonbeam_runtime"] = [{"text":"impl Into&lt;Result&lt;RawOrigin&lt;&lt;Runtime as Config&gt;::AccountId&gt;, Origin&gt;&gt; for Origin","synthetic":false,"types":[]},{"text":"impl Into&lt;Result&lt;RawOrigin&lt;&lt;Runtime as Config&gt;::AccountId, Instance1&gt;, Origin&gt;&gt; for Origin","synthetic":false,"types":[]},{"text":"impl Into&lt;Result&lt;RawOrigin&lt;&lt;Runtime as Config&gt;::AccountId, Instance2&gt;, Origin&gt;&gt; for Origin","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ implementors/pallet_collective/trait.Config.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["moonbeam_runtime"] = [{"text":"impl Config&lt;Instance1&gt; for Runtime","synthetic":false,"types":[]},{"text":"impl Config&lt;Instance2&gt; for Runtime","synthetic":false,"types":[]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
