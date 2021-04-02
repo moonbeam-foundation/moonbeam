e6ecd18c0b63c51952eb5ecb4d1a354090ebe818
nanocryk <6422796+nanocryk@users.noreply.github.com>
2021-04-02 08:46:21 +0000

    bump spec version

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -113,7 +113,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("moonbeam"),
 	impl_name: create_runtime_str!("moonbeam"),
 	authoring_version: 3,
-	spec_version: 30,
+	spec_version: 31,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 2,
