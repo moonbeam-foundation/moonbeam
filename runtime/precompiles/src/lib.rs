1dfd4fc84ee3c215aa649f703b9b7c8fc1d460b3
nanocryk <6422796+nanocryk@users.noreply.github.com>
2021-04-02 08:43:31 +0000

    Replace tuple by custom impl of PrecompileSet

--- runtime/precompiles/src/lib.rs
+++ runtime/precompiles/src/lib.rs
@@ -16,10 +16,16 @@
 
 #![cfg_attr(not(feature = "std"), no_std)]
 
+use evm::{ExitSucceed, ExitError, Context};
+use pallet_evm::{Precompile, PrecompileSet, Config};
 use pallet_evm_precompile_bn128::{Bn128Add, Bn128Mul, Bn128Pairing};
 use pallet_evm_precompile_dispatch::Dispatch;
 use pallet_evm_precompile_modexp::Modexp;
 use pallet_evm_precompile_simple::{ECRecover, Identity, Ripemd160, Sha256};
+use sp_core::H160;
+use frame_support::{dispatch::{Dispatchable, GetDispatchInfo, PostDispatchInfo}, weights::{Pays, DispatchClass}};
+use codec::Decode;
+use sp_std::{vec::Vec, marker::PhantomData};
 
 /// The PrecompileSet installed in the Moonbeam runtime.
 /// We include the nine Istanbul precompiles
@@ -28,14 +34,38 @@ use pallet_evm_precompile_simple::{ECRecover, Identity, Ripemd160, Sha256};
 ///
 /// TODO I had trouble getting the BN precompiles to compile.
 /// Also, Why are the BN precompiles in geth called bn256*, but in Frontier they are called Bn128*
-pub type MoonbeamPrecompiles<Runtime> = (
-	ECRecover,
-	Sha256,
-	Ripemd160,
-	Identity,
-	Modexp,
-	Bn128Add,
-	Bn128Mul,
-	Bn128Pairing,
-	Dispatch<Runtime>,
-);
+#[derive(Debug, Clone, Copy)]
+pub struct MoonbeamPrecompiles<R> (PhantomData<R>);
+
+impl<R> PrecompileSet for MoonbeamPrecompiles<R>
+where
+	R: Config,	
+	R::Call: Dispatchable<PostInfo = PostDispatchInfo> + GetDispatchInfo + Decode,
+    <R::Call as Dispatchable>::Origin: From<Option<R::AccountId>>{
+	fn execute(
+        address: H160, 
+        input: &[u8], 
+        target_gas: Option<u64>, 
+        context: &Context
+    ) -> Option<Result<(ExitSucceed, Vec<u8>, u64), ExitError>> {
+		match address {
+			// Ethereum precompiles :
+			a if a == hash(1) => Some(ECRecover::execute(input, target_gas, context)),
+			a if a == hash(2) => Some(Sha256::execute(input, target_gas, context)),
+			a if a == hash(3) => Some(Ripemd160::execute(input, target_gas, context)),
+			a if a == hash(4) => Some(Identity::execute(input, target_gas, context)),
+			a if a == hash(5) => Some(Modexp::execute(input, target_gas, context)),
+			a if a == hash(6) => Some(Bn128Add::execute(input, target_gas, context)),
+			a if a == hash(7) => Some(Bn128Mul::execute(input, target_gas, context)),
+			a if a == hash(8) => Some(Bn128Pairing::execute(input, target_gas, context)),
+			// Moonbeam precompiles :
+			a if a == hash(255) => Some(Dispatch::<R>::execute(input, target_gas, context)),
+			_ => None,
+		}
+	}
+}
+
+
+fn hash(a: u64) -> H160 {
+	H160::from_low_u64_be(a)
+}
