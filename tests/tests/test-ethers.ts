2961d51cf98b57e2a100bd4845d75d9225c95120
Antoine Estienne <estienne.antoine@gmail.com>
2021-04-07 11:42:31 +0200

    update @polkadot deps (#299)
    * update deps
    
    * update type package
    
    * fix ethers, packages and warnings

--- tests/tests/test-ethers.ts
+++ tests/tests/test-ethers.ts
@@ -2,7 +2,7 @@ import { createAndFinalizeBlock, describeWithMoonbeam } from "./util";
 import { HttpProvider } from "web3-core";
 import { expect } from "chai";
 import { ethers } from "ethers";
-import { TEST_CONTRACT_ABI, TEST_CONTRACT_BYTECODE } from "./constants";
+import { TEST_CONTRACT_ABI_ETHERS, TEST_CONTRACT_BYTECODE } from "./constants";
 
 describeWithMoonbeam("Moonbeam RPC (Ethers.js)", `simple-specs.json`, (context) => {
   const GENESIS_ACCOUNT_PRIVATE_KEY =
@@ -27,7 +27,7 @@ describeWithMoonbeam("Moonbeam RPC (Ethers.js)", `simple-specs.json`, (context) 
 
     // deploy contract
     const factory = new ethers.ContractFactory(
-      [TEST_CONTRACT_ABI] as ethers.ContractInterface,
+      [TEST_CONTRACT_ABI_ETHERS] as ethers.ContractInterface,
       TEST_CONTRACT_BYTECODE,
       signer
     );
@@ -49,7 +49,7 @@ describeWithMoonbeam("Moonbeam RPC (Ethers.js)", `simple-specs.json`, (context) 
     // Instantiate contract from address
     const contractFromAddress = new ethers.Contract(
       contract.address,
-      [TEST_CONTRACT_ABI] as ethers.ContractInterface,
+      [TEST_CONTRACT_ABI_ETHERS] as ethers.ContractInterface,
       signer
     );
     expect((await contractFromAddress.multiply(3)).toString()).to.equal("21");
