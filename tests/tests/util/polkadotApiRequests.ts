67c971b06c48caac7a5a0b0e9830080bc626f476
Gorka Irazoqui <gorka.irazoki@gmail.com>
2021-04-10 10:36:53 +0200

    Allow for non-finalization and ParentHash specification in createAndFinalizeBlock

--- tests/tests/util/polkadotApiRequests.ts
+++ tests/tests/util/polkadotApiRequests.ts
@@ -1,11 +1,20 @@
 import { ApiPromise } from "@polkadot/api";
+import { BlockHash } from "@polkadot/types/interfaces/chain";
 
 // Create a block and finalize it.
 // It will include all previously executed transactions since the last finalized block.
-export async function createAndFinalizeBlock(api: ApiPromise): Promise<number> {
+export async function createAndFinalizeBlock(
+  api: ApiPromise,
+  parentHash?: BlockHash,
+  finalize: boolean = true
+): Promise<number> {
   const startTime: number = Date.now();
   try {
-    await api.rpc.engine.createBlock(true, true);
+    if (parentHash == undefined) {
+      await api.rpc.engine.createBlock(true, finalize);
+    } else {
+      await api.rpc.engine.createBlock(true, finalize, parentHash);
+    }
   } catch (e) {
     console.log("ERROR DURING BLOCK FINALIZATION", e);
   }
