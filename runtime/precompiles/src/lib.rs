ad596859712c5631e2e21f1f107a489c82e6e722
Joshy Orndorff <admin@joshyorndorff.com>
2021-04-15 19:40:13 +0000

    Sketched idea, mostly right (I think), doesn't compile because of .dispatch not found

--- runtime/precompiles/src/lib.rs
+++ runtime/precompiles/src/lib.rs
@@ -16,18 +16,28 @@
 
 #![cfg_attr(not(feature = "std"), no_std)]
 
+use std::u64;
+
 use codec::Decode;
 use evm::{Context, ExitError, ExitSucceed};
 use frame_support::dispatch::{Dispatchable, GetDispatchInfo, PostDispatchInfo};
+use frame_support::traits::Currency;
+use num::BigUint;
+use pallet_evm::AddressMapping;
+use pallet_evm::GasWeightMapping;
 use pallet_evm::{Config, Precompile, PrecompileSet};
 use pallet_evm_precompile_bn128::{Bn128Add, Bn128Mul, Bn128Pairing};
 use pallet_evm_precompile_dispatch::Dispatch;
 use pallet_evm_precompile_modexp::Modexp;
 use pallet_evm_precompile_simple::{ECRecover, Identity, Ripemd160, Sha256};
-use sp_core::H160;
-use sp_std::{marker::PhantomData, vec::Vec};
-use sp_io::offchain;
 use sp_core::offchain::Duration;
+use sp_core::H160;
+use sp_core::U256;
+use sp_io::offchain;
+use sp_std::convert::TryFrom;
+use sp_std::convert::TryInto;
+use sp_std::{marker::PhantomData, vec::Vec};
+use std::fmt::Debug;
 
 /// A precompile intended to burn gas and/or time without actually doing any work.
 /// Meant for testing.
@@ -49,7 +59,8 @@ impl Precompile for Sacrifice {
 		// input should be exactly 16 bytes (two 8-byte unsigned ints in big endian)
 		if input.len() != INPUT_SIZE_BYTES {
 			return Err(ExitError::Other(
-				"input length for Sacrifice must be exactly 16 bytes".into()));
+				"input length for Sacrifice must be exactly 16 bytes".into(),
+			));
 		}
 
 		// create 8-byte buffers and populate them from calldata...
@@ -69,7 +80,7 @@ impl Precompile for Sacrifice {
 				return Err(ExitError::OutOfGas);
 			}
 		}
-		
+
 		// TODO: impose gas-per-second constraint?
 
 		if msec_cost > 0 {
@@ -79,7 +90,114 @@ impl Precompile for Sacrifice {
 			offchain::sleep_until(deadline);
 		}
 
+		// TODO Should this actually be stopped?
+		// https://ethervm.io/#F3
+		// REvisit: does solidity void contract issue stop or return
 		Ok((ExitSucceed::Returned, [0u8; 0].to_vec(), gas_cost))
+	}
+}
+
+/// A precompile to wrap the functionality from parachain_staking::nominate.
+///
+/// EXAMPLE USECASE:
+/// A simple example usecase is a contract that allows donors to donate, and stakes all the funds
+/// toward one fixed address chosen by the deployer.
+/// Such a contract could be deployed by a collator candidate, and the deploy address distributed to
+/// supporters who want to donate toward a perpetual nomination fund.
+pub struct NominateWrapper<Runtime> {
+	_phantom_data: PhantomData<(Runtime)>,
+}
+
+// type BalanceOf<Runtime> =
+// 	<<Runtime as parachain_staking::Config>::Currency as Currency<Runtime::AccountId>>::Balance;
+
+impl<Runtime> Precompile for NominateWrapper<Runtime>
+where
+	Runtime: parachain_staking::Config + pallet_evm::Config,
+	Runtime::AccountId: From<H160>,
+	<<Runtime as parachain_staking::Config>::Currency as Currency<Runtime::AccountId>>::Balance:
+		TryFrom<U256> + Debug,
+	<Runtime as frame_system::Config>::Call:
+		Dispatchable<PostInfo = PostDispatchInfo> + GetDispatchInfo,
+	<<Runtime as frame_system::Config>::Call as Dispatchable>::Origin:
+		From<Option<Runtime::AccountId>>,
+{
+	fn execute(
+		input: &[u8], //Reminder this is big-endian
+		target_gas: Option<u64>,
+		context: &Context,
+	) -> Result<(ExitSucceed, Vec<u8>, u64), ExitError> {
+		// Basic sanity checking for length
+		// https://solidity-by-example.org/primitives/
+
+		// Params are:
+		// Collator target (size 20 bytes, address)
+		// Nomination Amoutn: (size 32 bytes, U256 (for now))
+		const COLLATOR_SIZE_BYTES: usize = 20;
+		const AMOUNT_SIZE_BYTES: usize = 32;
+		if input.len() != COLLATOR_SIZE_BYTES + AMOUNT_SIZE_BYTES {
+			return Err(ExitError::Other(
+				"input length for Sacrifice must be exactly 16 bytes".into(),
+			));
+		}
+
+		// Parse input into Rust types
+		let mut collator_buf: [u8; COLLATOR_SIZE_BYTES] = [0; COLLATOR_SIZE_BYTES];
+		let mut amount_buf: [u8; AMOUNT_SIZE_BYTES] = [0; AMOUNT_SIZE_BYTES];
+
+		// TODO is this copy necessary? Probably.
+		collator_buf.copy_from_slice(&input[0..COLLATOR_SIZE_BYTES]);
+		amount_buf
+			.copy_from_slice(&input[COLLATOR_SIZE_BYTES..COLLATOR_SIZE_BYTES + AMOUNT_SIZE_BYTES]);
+
+		// Convert to right data types
+		let collator = H160::from_slice(&collator_buf);
+
+		// TODO make the currency type generic
+		let amount: <<Runtime as parachain_staking::Config>::Currency as Currency<
+			Runtime::AccountId,
+		>>::Balance = match sp_core::U256::from_big_endian(&amount_buf).try_into() {
+			Ok(x) => x,
+			_ => 0.into(),
+		};
+		// let temp = BigUint::from_bytes_be(&amount_buf);
+		// let amount: <<Runtime as parachain_staking::Config>::Currency as Currency<
+		// 	<Runtime as frame_system::pallet::Config>::AccountId,
+		// >>::Balance = temp.
+
+		println!("Collator account is {:?}", collator);
+		// println!("Amount is {:?}", amount);
+
+		// Construct a call
+		let call = <Runtime as frame_system::Config>::Call::ParachainStaking(parachain_staking::Call::<Runtime>::nominate(
+			collator.into(),
+			amount,
+		));
+
+		let info = call.get_dispatch_info();
+
+		// Make sure enough gas
+		if let Some(gas_limit) = target_gas {
+			let valid_weight = info.weight <= Runtime::GasWeightMapping::gas_to_weight(gas_limit);
+			if !valid_weight {
+				return Err(ExitError::OutOfGas);
+			}
+		}
+
+		// Dispatch that call
+		let origin = Runtime::AddressMapping::into_account_id(context.caller);
+
+		match call.dispatch(Some(origin).into()) {
+			Ok(post_info) => {
+				let gas_used = Runtime::GasWeightMapping::weight_to_gas(
+					post_info.actual_weight.unwrap_or(info.weight),
+				);
+				Ok((ExitSucceed::Stopped, Default::default(), gas_used))
+			}
+			Err(_) => Err(ExitError::Other(
+				"Parachain staking nomination failed".into(),
+			)),
+		}
 	}
 }
 
@@ -157,14 +275,18 @@ mod tests {
 		let input: [u8; 15] = [0; 15];
 		assert_eq!(
 			Sacrifice::execute(&input, Some(cost), &context),
-			Err(ExitError::Other("input length for Sacrifice must be exactly 16 bytes".into())),
+			Err(ExitError::Other(
+				"input length for Sacrifice must be exactly 16 bytes".into()
+			)),
 		);
 
 		// should fail with input of 17 byte length
 		let input: [u8; 17] = [0; 17];
 		assert_eq!(
 			Sacrifice::execute(&input, Some(cost), &context),
-			Err(ExitError::Other("input length for Sacrifice must be exactly 16 bytes".into())),
+			Err(ExitError::Other(
+				"input length for Sacrifice must be exactly 16 bytes".into()
+			)),
 		);
 
 		Ok(())
