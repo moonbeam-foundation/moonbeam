b65f6d5dd27e20e15e02844051b310abce9be324
Gorka Irazoqui <gorka.irazoki@gmail.com>
2021-04-15 18:53:13 +0200

    Use a different account for test-revert-receipt deployment contract to avoid non-determinism of the nonce

--- tests/tests/test-revert-receipt.ts
+++ tests/tests/test-revert-receipt.ts
@@ -1,12 +1,15 @@
 import { expect } from "chai";
 
 import { createAndFinalizeBlock, customRequest, describeWithMoonbeam } from "./util";
+import { GENESIS_ACCOUNT, GENESIS_ACCOUNT_PRIVATE_KEY, TEST_ACCOUNT } from "./constants";
+import { Keyring } from "@polkadot/keyring";
 
 describeWithMoonbeam("Frontier RPC (Constructor Revert)", `simple-specs.json`, (context) => {
   const GENESIS_ACCOUNT = "0x6be02d1d3665660d22ff9624b7be0551ee1ac91b";
   const GENESIS_ACCOUNT_PRIVATE_KEY =
     "0x99B3C12287537E38C90A9219D4CB074A89A16E9CDB20BF85728EBD97C343E342";
-
+  const TEST_ACCOUNT_PRIVATE_KEY =
+    "0x1111111111111111111111111111111111111111111111111111111111111111";
   // ```
   // pragma solidity >=0.4.22 <0.7.0;
   //
@@ -25,15 +28,24 @@ describeWithMoonbeam("Frontier RPC (Constructor Revert)", `simple-specs.json`, (
     "80fdfea2646970667358221220c70bc8b03cdfdf57b5f6c4131b836f9c2c4df01b8202f530555333f2a00e4b8364" +
     "736f6c63430006060033";
 
-  let expectedContractAddress;
-  beforeEach(async function () {
-    let txCount = await context.web3.eth.getTransactionCount(GENESIS_ACCOUNT);
-    if (txCount == 0) {
-      expectedContractAddress = "0xC2Bf5F29a4384b1aB0C063e1c666f02121B6084a";
-    } else {
-      expectedContractAddress = "0x5c4242beB94dE30b922f57241f1D02f36e906915";
-    }
+  before(async function () {
+    console.log(await context.web3.eth.getBalance("f24FF3a9CF04c71Dbc94D0b566f7A27B94566cac"));
+    // We send some money to make TEST_ACCOUNT so that it can also deploy a contract at the nonce is deterministic
+    const testAccount1 = context.web3.eth.accounts.privateKeyToAccount(TEST_ACCOUNT_PRIVATE_KEY);
+    const tx = await context.web3.eth.accounts.signTransaction(
+      {
+        from: GENESIS_ACCOUNT,
+        to: testAccount1.address,
+        value: "0x200000",
+        gasPrice: "0x01",
+        gas: "0x100000",
+      },
+      GENESIS_ACCOUNT_PRIVATE_KEY
+    );
+    await customRequest(context.web3, "eth_sendRawTransaction", [tx.rawTransaction]);
+    await createAndFinalizeBlock(context.polkadotApi);
   });
+
   it("should provide a tx receipt after successful deployment", async function () {
     this.timeout(15000);
 
@@ -65,7 +77,7 @@ describeWithMoonbeam("Frontier RPC (Constructor Revert)", `simple-specs.json`, (
     const receipt = await context.web3.eth.getTransactionReceipt(goodTxHash);
     expect(receipt).to.include({
       blockNumber: currentHeight,
-      contractAddress: expectedContractAddress,
+      contractAddress: "0x5c4242beB94dE30b922f57241f1D02f36e906915",
       cumulativeGasUsed: 67231,
       from: "0x6be02d1d3665660d22ff9624b7be0551ee1ac91b",
       gasUsed: 67231,
@@ -87,7 +99,7 @@ describeWithMoonbeam("Frontier RPC (Constructor Revert)", `simple-specs.json`, (
         gasPrice: "0x01",
         gas: "0x100000",
       },
-      GENESIS_ACCOUNT_PRIVATE_KEY
+      TEST_ACCOUNT_PRIVATE_KEY
     );
 
     const failTxHash = context.web3.utils.keccak256(tx.rawTransaction);
@@ -106,9 +118,9 @@ describeWithMoonbeam("Frontier RPC (Constructor Revert)", `simple-specs.json`, (
     const receipt = await context.web3.eth.getTransactionReceipt(failTxHash);
     expect(receipt).to.include({
       blockNumber: current_height,
-      contractAddress: expectedContractAddress,
+      contractAddress: "0xAE519FC2Ba8e6fFE6473195c092bF1BAe986ff90",
       cumulativeGasUsed: 54600,
-      from: "0x6be02d1d3665660d22ff9624b7be0551ee1ac91b",
+      from: "0x19e7e376e7c213b7e7e7e46cc70a5dd086daff2a",
       gasUsed: 54600,
       to: null,
       transactionHash: failTxHash,
