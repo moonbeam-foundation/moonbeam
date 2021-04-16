f7affa2860c895523f0bd6342be15de961cd583a
joelamouche <estienne.antoine@gmail.com>
2021-04-16 13:01:19 +0000

    use sudo

--- tests/tests/test-polkadot-js-contract.ts
+++ tests/tests/test-polkadot-js-contract.ts
@@ -10,7 +10,7 @@ import {
   TEST_CONTRACT_BYTECODE_INCR,
 } from "./constants";
 
-describeWithMoonbeam("Moonbeam RPC (Contract Methods)", `simple-specs.json`, (context) => {
+describeWithMoonbeam("Moonbeam RPC (Direct EVM Call)", `simple-specs.json`, (context) => {
   before("create the contract", async function () {
     this.timeout(15000);
     const tx = await context.web3.eth.accounts.signTransaction(
@@ -50,6 +50,7 @@ describeWithMoonbeam("Moonbeam RPC (Contract Methods)", `simple-specs.json`, (co
   });
 
   it.only("should return contract method result", async function () {
+    this.timeout(20000);
     const keyring = new Keyring({ type: "ethereum" });
     const testAccount = await keyring.addFromUri(GENESIS_ACCOUNT_PRIVATE_KEY, null, "ethereum");
     const contract = new context.web3.eth.Contract(TEST_CONTRACT_INCR_ABI, FIRST_CONTRACT_ADDRESS, {
@@ -57,27 +58,57 @@ describeWithMoonbeam("Moonbeam RPC (Contract Methods)", `simple-specs.json`, (co
       gasPrice: "0x01",
     });
     let methodCallBytes: string = contract.methods.incr().encodeABI();
-
+    console.log("methodCallBytes", methodCallBytes);
     let nonce = await context.web3.eth.getTransactionCount(GENESIS_ACCOUNT, "latest");
     console.log("nonce", nonce);
-    let hash = await context.polkadotApi.tx.evm
-      .call(
-        GENESIS_ACCOUNT,
-        FIRST_CONTRACT_ADDRESS,
-        methodCallBytes,
-        "0x00",
-        "0x100000",
-        "0x01",
-        nonce
+
+    // let hash = await context.polkadotApi.tx.evm
+    //   .call(
+    //     GENESIS_ACCOUNT,
+    //     FIRST_CONTRACT_ADDRESS,
+    //     methodCallBytes,
+    //     "0x00",
+    //     "0x100000",
+    //     "0x01",
+    //     nonce
+    //   )
+    //   .signAndSend(testAccount);
+    //await new Promise<void>(async (res) => {
+    let unsub = await context.polkadotApi.tx.sudo
+      .sudo(
+        context.polkadotApi.tx.evm.call(
+          GENESIS_ACCOUNT,
+          FIRST_CONTRACT_ADDRESS,
+          methodCallBytes,
+          "0x00",
+          "0x100000",
+          "0x01",
+          nonce
+        )
       )
-      .signAndSend(testAccount);
-    console.log("tx call hash", hash);
+      .signAndSend(testAccount, { nonce: nonce, era: 0 }, (result) => {
+        console.log(`Current registration status is ${result.status}`);
+        if (result.status.isInBlock) {
+          console.log(`Transaction included at blockHash ${result.status.asInBlock}`);
+          unsub();
+          //res();
+        } else if (result.status.isFinalized) {
+          console.log(`Transaction finalized at blockHash ${result.status.asFinalized}`);
+          unsub();
+          // res();
+        }
+      });
+    //});
+
+    //console.log("tx call hash", hash);
     await createAndFinalizeBlock(context.polkadotApi);
+    await new Promise((res) => setTimeout(res, 10000));
     let res = await context.polkadotApi.query.evm.accountStorages(
       FIRST_CONTRACT_ADDRESS,
       "0x0000000000000000000000000000000000000000000000000000000000000000"
     );
     console.log("res", res, Number(res));
+    console.log("res from web3", await contract.methods.count().call());
     expect(Number(res)).to.eq(1);
     // console.log("hash", hash.toHex());
     // const latestBlock = await context.web3.eth.getBlock("latest");
