ae1cc27bc2624e93958091e45bdf550e4cba4124
joelamouche <estienne.antoine@gmail.com>
2021-04-16 13:49:45 +0000

    optimize fixture

--- tests/fixtures/contract-fixtures.ts
+++ tests/fixtures/contract-fixtures.ts
@@ -4,9 +4,6 @@ import { getCompiled } from "../tests/util/contracts";
 exports.mochaGlobalSetup = async function () {
   // First compile all contracts
   console.log("Making sure all contracts are compiled...");
-  let keys = Object.keys(contractSources);
-  for (let i = 0; i < keys.length; i++) {
-    await getCompiled(keys[i]);
-  }
+  await Promise.all(Object.keys(contractSources).map(getCompiled));
   console.log("done");
 };
