e34f7c8b25fd1d29f49f9ab98be40fb6a8ca111d
Stephen Shelton <steve@brewcraft.org>
2021-04-21 14:54:54 -0600

    Pass runtime-benchmarks use flag on to parachain-staking

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -1069,7 +1069,7 @@ impl_runtime_apis! {
 			let mut batches = Vec::<BenchmarkBatch>::new();
 			let params = (&config, &whitelist);
 
-			add_benchmark!(params, batches, parachain_staking, ParachainStakingBench);
+			add_benchmark!(params, batches, parachain_staking, ParachainStakingBench::<Runtime>);
 			add_benchmark!(params, batches, frame_system, SystemBench::<Runtime>);
 
 			if batches.is_empty() { return Err("Benchmark not found for this pallet.".into()) }
