a1549cd4086bbb1865d55079d9ce14ff26319208
Stephen Shelton <steve@brewcraft.org>
2021-05-03 14:37:26 -0600

    Move benchmarking outside tools/

--- scripts/run_benchmarks.sh
+++ scripts/run_benchmarks.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# This script is an example for running Moonbeam's benchmarks.
+# It requires Moonbeam to be compiled with --features=runtime-benchmarks
+
 export WASMTIME_BACKTRACE_DETAILS=1
 
 ./../../target/release/moonbeam benchmark \
