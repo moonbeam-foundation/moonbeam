75c8a04d113c150ea7dc9559aa077e6beeb8ab3a
4meta5 <4meta5@users.noreply.github.com>
2021-05-04 00:20:36 +0000

    deploy: f77274583f0fa3a64e7247af7ad421833c82e425

---
+++ implementors/parachain_staking/weights/trait.WeightInfo.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["parachain_staking"] = [];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- source-files.js
+++ source-files.js
@@ -14,6 +14,6 @@ sourcesIndex["moonbeam_rpc_txpool"] = {"name":"","files":["lib.rs"]};
 sourcesIndex["moonbeam_runtime"] = {"name":"","files":["lib.rs"]};
 sourcesIndex["pallet_author_filter"] = {"name":"","files":["lib.rs"]};
 sourcesIndex["pallet_ethereum_chain_id"] = {"name":"","files":["lib.rs"]};
-sourcesIndex["parachain_staking"] = {"name":"","files":["inflation.rs","lib.rs","set.rs"]};
+sourcesIndex["parachain_staking"] = {"name":"","files":["inflation.rs","lib.rs","set.rs","weights.rs"]};
 sourcesIndex["precompiles"] = {"name":"","files":["lib.rs","staking.rs"]};
 createSourceSidebar();
