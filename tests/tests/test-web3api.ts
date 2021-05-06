68dfae25ed737aeff19cea69a19c46f5879a5243
Stephen Shelton <steve@brewcraft.org>
2021-05-06 07:20:09 -0600

    Reflect updated fc-rpc version in test

--- tests/tests/test-web3api.ts
+++ tests/tests/test-web3api.ts
@@ -8,7 +8,7 @@ describeDevMoonbeam("Web3Api Information", (context) => {
     let specName: string = await context.polkadotApi.runtimeVersion.specName.toString();
     let specVersion: string = await context.polkadotApi.runtimeVersion.specVersion.toString();
     let implVersion: string = await context.polkadotApi.runtimeVersion.implVersion.toString();
-    let regex = new RegExp(specName + "/v" + specVersion + "." + implVersion + "/fc-rpc-0.1.0");
+    let regex = new RegExp(specName + "/v" + specVersion + "." + implVersion + "/fc-rpc-2.0.0");
     expect(version).to.be.match(regex);
   });
 
