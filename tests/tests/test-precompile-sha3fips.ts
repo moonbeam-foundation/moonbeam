b2d92ac2a7701c6516d1cd1b76547fb6a1de863e
Alan Sapede <alan.sapede@gmail.com>
2021-05-14 14:23:09 -0400

    Sha3 precompile is moved to 1024 (#411)
    * Sha3 precompile is moved to 1024
    
    * Updates sha3fips test

--- tests/tests/test-precompile-sha3fips.ts
+++ tests/tests/test-precompile-sha3fips.ts
@@ -12,7 +12,7 @@ describeDevMoonbeam("Precompiles - sha3fips", (context) => {
         value: "0x0",
         gas: "0x10000",
         gasPrice: "0x01",
-        to: "0x0000000000000000000000000000000000000401",
+        to: "0x0000000000000000000000000000000000000400",
         data:
           "0x0448250ebe88d77e0a12bcf530fe6a2cf1ac176945638d309b840d631940c93b78c2bd6d16f227a8877e" +
           "3f1604cd75b9c5a8ab0cac95174a8a0a0f8ea9e4c10bca",
