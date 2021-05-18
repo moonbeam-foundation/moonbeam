1eb74efd471bcc22795adc659dd609fe6af69320
Antoine Estienne <estienne.antoine@gmail.com>
2021-05-18 18:53:44 +0100

    add new network names to type bundle

--- moonbeam-types-bundle/index.ts
+++ moonbeam-types-bundle/index.ts
@@ -352,5 +352,8 @@ export const typesBundle = {
     moonbeamDefinitions,
     "moonbeam-standalone": moonbeamDefinitions,
     "node-moonbeam": moonbeamDefinitions,
+    moonbase: moonbeamDefinitions,
+    moonriver: moonbeamDefinitions,
+    moonshadow: moonbeamDefinitions,
   },
 } as OverrideBundleType;
