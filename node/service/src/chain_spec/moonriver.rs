04144e8f08bdc8637d3b790f8e6be02f2dcc0387
tgmichel <telmo@purestake.com>
2021-05-18 10:14:37 +0200

    labels

--- node/service/src/chain_spec/moonriver.rs
+++ node/service/src/chain_spec/moonriver.rs
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Moonbeam.  If not, see <http://www.gnu.org/licenses/>.
 
-//! Moonbeam Chain Specifications and utilities for building them.
+//! Moonriver Chain Specifications and utilities for building them.
 //!
 //! Learn more about Substrate chain specifications at
 //! https://substrate.dev/docs/en/knowledgebase/integrate/chain-spec
