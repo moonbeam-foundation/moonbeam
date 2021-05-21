b13a56d022e5d5aae7b502e5dceeb2215f7595c6
4meta5 <4meta5@users.noreply.github.com>
2021-05-21 00:34:31 +0000

    deploy: 3573aaa90c4020dd3dd0dd96bca8b6f623ee8ed4

---
+++ implementors/frame_support/traits/filter/trait.Filter.js
@@ -0,0 +1,5 @@
+(function() {var implementors = {};
+implementors["moonbeam_runtime"] = [{"text":"impl Filter&lt;Call&gt; for BaseFilter","synthetic":false,"types":[]}];
+implementors["moonriver_runtime"] = [{"text":"impl Filter&lt;Call&gt; for BaseFilter","synthetic":false,"types":[]}];
+implementors["moonshadow_runtime"] = [{"text":"impl Filter&lt;Call&gt; for BaseFilter","synthetic":false,"types":[]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
