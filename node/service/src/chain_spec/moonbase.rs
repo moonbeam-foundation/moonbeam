0864ac659033e749e4187a064d63ce38351db5c0
Joshy Orndorff <admin@joshyorndorff.com>
2021-05-21 20:29:57 +0000

    Percent in chain_spec

--- node/service/src/chain_spec/moonbase.rs
+++ node/service/src/chain_spec/moonbase.rs
@@ -217,7 +217,9 @@ pub fn testnet_genesis(
 			phantom: Default::default(),
 			members: vec![], // TODO : Set members
 		},
-		pallet_author_slot_filter: AuthorFilterConfig { eligible_ratio: 50 },
+		pallet_author_slot_filter: AuthorFilterConfig {
+			eligible_ratio: sp_runtime::Percent::from_percent(50),
+		},
 		pallet_author_mapping: AuthorMappingConfig {
 			// Pretty hacky. We just set the first staker to use alice's session keys.
 			// Maybe this is the moment we should finally make the `--alice` flags make sense.
