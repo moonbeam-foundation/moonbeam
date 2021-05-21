2496e0580ef938c2def5a2b7dd0d5c77d08d2de1
4meta5 <4meta5@users.noreply.github.com>
2021-05-21 22:54:08 +0000

    deploy: c69ff10049e706a61dfdd84512539a158a200a70

---
+++ implementors/pallet_treasury/trait.Config.js
@@ -0,0 +1,6 @@
+(function() {var implementors = {};
+implementors["moonbase_runtime"] = [{"text":"impl Config&lt;Instance1&gt; for Runtime","synthetic":false,"types":[]},{"text":"impl Config&lt;Instance2&gt; for Runtime","synthetic":false,"types":[]}];
+implementors["moonbeam_runtime"] = [{"text":"impl Config&lt;Instance1&gt; for Runtime","synthetic":false,"types":[]},{"text":"impl Config&lt;Instance2&gt; for Runtime","synthetic":false,"types":[]}];
+implementors["moonriver_runtime"] = [{"text":"impl Config&lt;Instance1&gt; for Runtime","synthetic":false,"types":[]},{"text":"impl Config&lt;Instance2&gt; for Runtime","synthetic":false,"types":[]}];
+implementors["moonshadow_runtime"] = [{"text":"impl Config&lt;Instance1&gt; for Runtime","synthetic":false,"types":[]},{"text":"impl Config&lt;Instance2&gt; for Runtime","synthetic":false,"types":[]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
