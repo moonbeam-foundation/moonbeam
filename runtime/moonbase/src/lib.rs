7320e10fe30dc83be168278640a6ab6153f1e4c2
4meta5 <asinghchrony@protonmail.com>
2021-05-21 15:41:00 -0400

    fix by benchmarks by commenting out crowdloan benchmarks

--- runtime/moonbase/src/lib.rs
+++ runtime/moonbase/src/lib.rs
@@ -1089,16 +1089,16 @@ impl_runtime_apis! {
 			impl frame_system_benchmarking::Config for Runtime {}
 
 			use parachain_staking::Pallet as ParachainStakingBench;
-			use pallet_crowdloan_rewards::Pallet as PalletCrowdloanRewardsBench;
+			//use pallet_crowdloan_rewards::Pallet as PalletCrowdloanRewardsBench;
 			let whitelist: Vec<TrackedStorageKey> = vec![];
 
 			let mut batches = Vec::<BenchmarkBatch>::new();
 			let params = (&config, &whitelist);
 
 			add_benchmark!(params, batches, parachain_staking, ParachainStakingBench::<Runtime>);
-			add_benchmark!(
-				params, batches, pallet_crowdloan_rewards, PalletCrowdloanRewardsBench::<Runtime>
-			);
+			// add_benchmark!(
+			// 	params, batches, pallet_crowdloan_rewards, PalletCrowdloanRewardsBench::<Runtime>
+			// );
 			add_benchmark!(params, batches, frame_system, SystemBench::<Runtime>);
 
 			if batches.is_empty() { return Err("Benchmark not found for this pallet.".into()) }
