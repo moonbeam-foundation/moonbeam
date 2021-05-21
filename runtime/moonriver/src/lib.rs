8a8bed5f61cce7946319fd72448e42c6107ce750
nanocryk <6422796+nanocryk@users.noreply.github.com>
2021-05-21 12:30:54 +0000

    bump spec versions

--- runtime/moonriver/src/lib.rs
+++ runtime/moonriver/src/lib.rs
@@ -112,7 +112,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("moonriver"),
 	impl_name: create_runtime_str!("moonriver"),
 	authoring_version: 3,
-	spec_version: 37,
+	spec_version: 38,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 2,
