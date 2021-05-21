abd2e39be4f646b6026a6da10c15dbe5d448d6b9
Crystalin <alan@purestake.com>
2021-05-21 03:26:43 +0000

    Update integration test gas price

--- runtime/moonbeam/tests/integration_test.rs
+++ runtime/moonbeam/tests/integration_test.rs
@@ -474,13 +474,13 @@ fn initialize_crowdloan_addresses_with_batch_and_pay() {
 				CrowdloanRewards::accounts_payable(&AccountId::from(CHARLIE))
 					.unwrap()
 					.claimed_reward,
-				300002777777777777777777
+				300002976190476190476190
 			);
 			assert_eq!(
 				CrowdloanRewards::accounts_payable(&AccountId::from(DAVE))
 					.unwrap()
 					.claimed_reward,
-				300002777777777777777777
+				300002976190476190476190
 			);
 
 			assert_noop!(
