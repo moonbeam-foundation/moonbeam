81d917100b617f2795856cb3d1d87a9c2ef4e8a4
Joshy Orndorff <admin@joshyorndorff.com>
2021-05-25 17:51:32 +0000

    spec versions

--- runtime/moonbase/src/lib.rs
+++ runtime/moonbase/src/lib.rs
@@ -115,7 +115,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("moonbase"),
 	impl_name: create_runtime_str!("moonbase"),
 	authoring_version: 3,
-	spec_version: 39,
+	spec_version: 40,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 2,
