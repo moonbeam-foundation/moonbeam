b82c7e70e3cdfc3fd49046f202885faba16f2c9b
Antoine Estienne <antoine@purestake.com>
2021-05-26 12:06:16 +0000

    updated genesis author ids

--- tests/tests/test-author-mapping.ts
+++ tests/tests/test-author-mapping.ts
@@ -8,8 +8,11 @@ import {
 } from "../util/constants";
 import { describeDevMoonbeam } from "../util/setup-dev-tests";
 
-const testAuthorId = "0x0af46a0ee747ab44191404543214f634ab09b237aa14d355fb0f5f92c3cd414f";
-const testAuthorId2 = "0x8c8618d1daab13e60fe2af2b17dc1a223326b8dc425dcca24c1d27661889e140";
+// const testAuthorId = "0x0af46a0ee747ab44191404543214f634ab09b237aa14d355fb0f5f92c3cd414f";
+// const testAuthorId2 = "0x8c8618d1daab13e60fe2af2b17dc1a223326b8dc425dcca24c1d27661889e140";
+
+const testAuthorId = "5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY";
+const testAuthorId2 = "5FHneW46xGXgs5mUiveU4sbTyGBzmstUspZC92UhjJM694ty";
 
 describeDevMoonbeam("Author Mapping - Genesis", (context) => {
   it("should match collator reserved bond reserved", async function () {
