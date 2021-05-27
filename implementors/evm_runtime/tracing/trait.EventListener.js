dc0551af66711c8c11f7e5df05443ed55e81728b
crystalin <crystalin@users.noreply.github.com>
2021-05-27 15:36:45 +0000

    deploy: 2b693dcb86181a86c1ebc41281e0e38db57d1df2

---
+++ implementors/evm_runtime/tracing/trait.EventListener.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["moonbeam_evm_tracer"] = [{"text":"impl EventListener for CallListTracer","synthetic":false,"types":[]},{"text":"impl EventListener for RawTracer","synthetic":false,"types":[]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- implementors/moonbeam_extensions_evm/executor/wrapper/trait.HandlerT.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["moonbeam_extensions_evm"] = [{"text":"impl&lt;'config, S:&nbsp;StackStateT&lt;'config&gt;&gt; Handler for TraceExecutorWrapper&lt;'config, S&gt;","synthetic":false,"types":[]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
