26c771a262ff6d71c265379521e0cc9437dab391
Joshy Orndorff <admin@joshyorndorff.com>
2021-05-27 18:29:50 +0000

    bump runtime versions

--- runtime/moonriver/src/lib.rs
+++ runtime/moonriver/src/lib.rs
@@ -114,7 +114,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("moonriver"),
 	impl_name: create_runtime_str!("moonriver"),
 	authoring_version: 3,
-	spec_version: 42,
+	spec_version: 43,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 2,
