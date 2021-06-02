922ca8cd48975138ac6779b5bcf6172489f4b229
crystalin <crystalin@users.noreply.github.com>
2021-06-02 23:12:25 +0000

    deploy: d247c940d37f8d09409f5f881122d98fe4189456

--- moonbeam_cli_opt/sidebar-items.js
+++ moonbeam_cli_opt/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["EthApi",""],["Sealing","Block authoring scheme to be used by the dev service."]],"struct":[["RpcParams",""]]});
+initSidebarItems({"enum":[["EthApi",""],["Sealing","Block authoring scheme to be used by the dev service."]],"struct":[["RpcConfig",""]]});
