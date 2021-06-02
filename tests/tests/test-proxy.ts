538451c574d43ce7821c1b7211374014e1f09664
nanocryk <6422796+nanocryk@users.noreply.github.com>
2021-06-02 11:51:10 +0000

    independent tests

--- tests/tests/test-proxy.ts
+++ tests/tests/test-proxy.ts
@@ -8,194 +8,122 @@ import {
   CHARLETH_PRIVATE_KEY,
   CHARLETH_ADDRESS,
 } from "../util/constants";
-
-function delay(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
+import { blake2AsU8a } from "@polkadot/util-crypto";
 
 // In these tests Alith will allow Baltathar to perform calls on her behalf.
 // Charleth is used as a target account when making transfers.
+const keyring = new Keyring({ type: "ethereum" });
+const alith = keyring.addFromUri(ALITH_PRIVATE_KEY, null, "ethereum");
+const baltathar = keyring.addFromUri(BALTATHAR_PRIVATE_KEY, null, "ethereum");
+const charleth = keyring.addFromUri(CHARLETH_PRIVATE_KEY, null, "ethereum");
 
-describeDevMoonbeam("Pallet proxy", (context) => {
-  let alith;
-  let baltathar;
-  let charleth;
-  before("Setup: prepare keyring", async function () {
-    const keyring = new Keyring({ type: "ethereum" });
-    alith = await keyring.addFromUri(ALITH_PRIVATE_KEY, null, "ethereum");
-    baltathar = keyring.addFromUri(BALTATHAR_PRIVATE_KEY, null, "ethereum");
-    charleth = keyring.addFromUri(CHARLETH_PRIVATE_KEY, null, "ethereum");
+/// Sign and sand Substrate transaction then create a block.
+/// Will provide events emited by the transaction to check if they match what is expected.
+async function substrateTransaction(context, sender, polkadotCall, inBlockCallback) {
+  await new Promise(async (resolve) => {
+    const unsub = await polkadotCall.signAndSend(sender, ({ events = [], status }) => {
+      if (status.isInBlock) {
+        inBlockCallback(events);
+        unsub();
+        resolve(null);
+      }
+    });
+
+    await context.createBlock();
   });
+}
 
-  it("shouldn't accept unknown proxy", async function () {
-    const balance_before = await await context.web3.eth.getBalance(CHARLETH_ADDRESS);
+/// Fetch balance of provided account before and after the inner function is executed and
+/// check it matches expected difference.
+async function expectBalanceDifference(context, address, diff, inner) {
+  const balance_before = await context.web3.eth.getBalance(address);
 
-    // Proxy call
-    const unsub = await context.polkadotApi.tx.proxy
-      .proxy(alith.address, null, context.polkadotApi.tx.balances.transfer(charleth.address, 100))
-      .signAndSend(baltathar, ({ events = [], status }) => {
-        if (status.isInBlock) {
-          // Check proxy call failed.
+  await inner();
+
+  const balance_after = await context.web3.eth.getBalance(CHARLETH_ADDRESS);
+  expect(BigInt(balance_after)).to.be.eq(BigInt(balance_before) + BigInt(diff));
+}
+
+describeDevMoonbeam("Pallet proxy - shouldn't accept unknown proxy", (context) => {
+  it("shouldn't accept unknown proxy", async () => {
+    await expectBalanceDifference(context, CHARLETH_ADDRESS, 0, async () => {
+      await substrateTransaction(
+        context,
+        baltathar,
+        context.polkadotApi.tx.proxy.proxy(
+          alith.address,
+          null,
+          context.polkadotApi.tx.balances.transfer(charleth.address, 100)
+        ),
+        (events) => {
           expect(events[3].event.method).to.be.eq("ExtrinsicFailed");
-          unsub();
         }
-      });
-
-    await context.createBlock();
-    await delay(500);
-
-    const balance_after = await await context.web3.eth.getBalance(CHARLETH_ADDRESS);
-
-    // Check target balance didn't change.
-    expect(balance_after).to.be.eq(balance_before);
+      );
+    });
   });
+});
 
-  it("should accept known proxy", async function () {
-    const balance_before = await await context.web3.eth.getBalance(CHARLETH_ADDRESS);
+describeDevMoonbeam("Pallet proxy - should accept known proxy", (context) => {
+  it("should accept known proxy", async () => {
+    await expectBalanceDifference(context, CHARLETH_ADDRESS, 100, async () => {
+      await substrateTransaction(
+        context,
+        alith,
+        context.polkadotApi.tx.proxy.addProxy(baltathar.address, "Any", 0),
+        (events) => {
+          expect(events[4].event.method).to.be.eq("ExtrinsicSuccess");
+        }
+      );
 
-    // Allow proxy
-    await context.polkadotApi.tx.proxy.addProxy(baltathar.address, "Any", 0).signAndSend(alith);
-
-    // Proxy call
-    const unsub = await context.polkadotApi.tx.proxy
-      .proxy(alith.address, null, context.polkadotApi.tx.balances.transfer(charleth.address, 100))
-      .signAndSend(baltathar, ({ events = [], status }) => {
-        if (status.isInBlock) {
-          // Check proxy call succeeded.
+      await substrateTransaction(
+        context,
+        baltathar,
+        context.polkadotApi.tx.proxy.proxy(
+          alith.address,
+          null,
+          context.polkadotApi.tx.balances.transfer(charleth.address, 100)
+        ),
+        (events) => {
           expect(events[3].event.method).to.be.eq("ExtrinsicSuccess");
-          unsub();
         }
-      });
-
-    await context.createBlock();
-    await delay(500);
-
-    const balance_after = await await context.web3.eth.getBalance(CHARLETH_ADDRESS);
-
-    // Check target balance changed with correct amount.
-    expect(BigInt(balance_after)).to.be.eq(BigInt(balance_before) + 100n);
+      );
+    });
   });
+});
 
-  it("should remove proxy", async function () {
-    const balance_before = await context.web3.eth.getBalance(CHARLETH_ADDRESS);
-
-    // Remove proxy
-    await context.polkadotApi.tx.proxy.removeProxy(baltathar.address, "Any", 0).signAndSend(alith);
-
-    // Proxy call
-    const unsub = await context.polkadotApi.tx.proxy
-      .proxy(alith.address, null, context.polkadotApi.tx.balances.transfer(charleth.address, 100))
-      .signAndSend(baltathar, ({ events = [], status }) => {
-        if (status.isInBlock) {
-          // Check proxy call failed.
-          expect(events[1].event.method).to.be.eq("ExtrinsicFailed");
-          unsub();
+describeDevMoonbeam("Pallet proxy - should accept removed proxy", (context) => {
+  it("should accept removed proxy", async () => {
+    await expectBalanceDifference(context, CHARLETH_ADDRESS, 0, async () => {
+      await substrateTransaction(
+        context,
+        alith,
+        context.polkadotApi.tx.proxy.addProxy(baltathar.address, "Any", 0),
+        (events) => {
+          expect(events[4].event.method).to.be.eq("ExtrinsicSuccess");
         }
-      });
+      );
 
-    await context.createBlock();
-    await delay(500);
-
-    const balance_after = await context.web3.eth.getBalance(CHARLETH_ADDRESS);
-
-    // Check target balance didn't change.
-    expect(balance_after).to.be.eq(balance_before);
-  });
-
-  it("should refuse direct call to known delayed proxy", async function () {
-    const balance_before = await context.web3.eth.getBalance(CHARLETH_ADDRESS);
-
-    // Allow proxy (with delay)
-    await context.polkadotApi.tx.proxy.addProxy(baltathar.address, "Any", 2).signAndSend(alith);
-
-    // Proxy call
-    const unsub = await context.polkadotApi.tx.proxy
-      .proxy(alith.address, null, context.polkadotApi.tx.balances.transfer(charleth.address, 100))
-      .signAndSend(baltathar, ({ events = [], status }) => {
-        if (status.isInBlock) {
-          // Check proxy call failed.
-          expect(events[1].event.method).to.be.eq("ExtrinsicFailed");
-          unsub();
+      await substrateTransaction(
+        context,
+        alith,
+        context.polkadotApi.tx.proxy.removeProxy(baltathar.address, "Any", 0),
+        (events) => {
+          expect(events[1].event.method).to.be.eq("ExtrinsicSuccess");
         }
-      });
+      );
 
-    await context.createBlock();
-    await delay(500);
-
-    const balance_after = await context.web3.eth.getBalance(CHARLETH_ADDRESS);
-
-    // Check target balance didn't change.
-    expect(balance_after).to.be.eq(balance_before);
-  });
-
-  let transfer_tx;
-  it("should be able to announce a call", async function () {
-    transfer_tx = await context.polkadotApi.tx.balances.transfer(charleth.address, 100);
-
-    // Proxy announcement
-    const unsub = await context.polkadotApi.tx.proxy
-      .announce(alith.address, transfer_tx.hash)
-      .signAndSend(baltathar, ({ events = [], status }) => {
-        if (status.isInBlock) {
-          // Check proxy call succeeded.
-          expect(events[1].event.method).to.be.eq("Announced");
-          expect(events[3].event.method).to.be.eq("ExtrinsicSuccess");
-          unsub();
+      await substrateTransaction(
+        context,
+        baltathar,
+        context.polkadotApi.tx.proxy.proxy(
+          alith.address,
+          null,
+          context.polkadotApi.tx.balances.transfer(charleth.address, 100)
+        ),
+        (events) => {
+          expect(events[3].event.method).to.be.eq("ExtrinsicFailed");
         }
-      });
-
-    await context.createBlock();
-    await delay(500);
-  });
-
-  it("should refuse early announced call", async function () {
-    const balance_before = await context.web3.eth.getBalance(CHARLETH_ADDRESS);
-
-    // Proxy call
-    const unsub = await context.polkadotApi.tx.proxy
-      .proxyAnnounced(alith.address, baltathar.address, null, transfer_tx)
-      .signAndSend(baltathar, ({ events = [], status }) => {
-        if (status.isInBlock) {
-          events.forEach((value) => console.log(value.event.method));
-          // Check proxy call failed.
-          expect(events[1].event.method).to.be.eq("ExtrinsicFailed");
-          unsub();
-        }
-      });
-
-    await context.createBlock();
-    await delay(500);
-
-    const balance_after = await context.web3.eth.getBalance(CHARLETH_ADDRESS);
-
-    // Check target balance didn't change.
-    expect(balance_after).to.be.eq(balance_before);
-  });
-
-  it("should accept on-time announced call", async function () {
-    const balance_before = await context.web3.eth.getBalance(CHARLETH_ADDRESS);
-
-    await context.createBlock();
-
-    // Proxy call
-    const unsub = await context.polkadotApi.tx.proxy
-      .proxyAnnounced(alith.address, baltathar.address, null, transfer_tx)
-      .signAndSend(baltathar, ({ events = [], status }) => {
-        if (status.isInBlock) {
-          events.forEach((value) => console.log(value.event.method));
-          // Check proxy call succeeded.
-          expect(events[1].event.method).to.be.eq("ExtrinsicFailed"); // should be ExtrinsicSucceed
-
-          unsub();
-        }
-      });
-
-    await context.createBlock();
-    await delay(500);
-
-    const balance_after = await context.web3.eth.getBalance(CHARLETH_ADDRESS);
-
-    // Check target balance didn't change.
-    expect(balance_after).to.be.eq(balance_before);
+      );
+    });
   });
 });
