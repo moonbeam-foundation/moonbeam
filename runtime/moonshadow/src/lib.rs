fdda5080fcb6e7fd5f6750a8ae233e5afb5af0e6
Joshy Orndorff <JoshOrndorff@users.noreply.github.com>
2021-06-02 11:48:56 -0400

    AuthorMapping: Track deposit amounts. (#461)
    Co-authored-by: Crystalin <alan@purestake.com>
    Co-authored-by: girazoki <gorka.irazoki@gmail.com>

--- runtime/moonshadow/src/lib.rs
+++ runtime/moonshadow/src/lib.rs
@@ -113,7 +113,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("moonshadow"),
 	impl_name: create_runtime_str!("moonshadow"),
 	authoring_version: 3,
-	spec_version: 43,
+	spec_version: 44,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 2,
