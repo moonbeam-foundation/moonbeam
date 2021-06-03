1d6d9288498afe26593b343258c3e791f816dd23
tgmichel <telmo@purestake.com>
2021-06-03 15:46:31 +0200

    Update `MaxScheduledPerBlock`

--- runtime/moonshadow/src/lib.rs
+++ runtime/moonshadow/src/lib.rs
@@ -323,6 +323,7 @@ impl pallet_evm::Config for Runtime {
 
 parameter_types! {
 	pub MaximumSchedulerWeight: Weight = Perbill::from_percent(80) * BlockWeights::get().max_block;
+	pub const MaxScheduledPerBlock: u32 = 50;
 }
 
 impl pallet_scheduler::Config for Runtime {
@@ -332,7 +333,7 @@ impl pallet_scheduler::Config for Runtime {
 	type Call = Call;
 	type MaximumWeight = MaximumSchedulerWeight;
 	type ScheduleOrigin = EnsureRoot<AccountId>;
-	type MaxScheduledPerBlock = ();
+	type MaxScheduledPerBlock = MaxScheduledPerBlock;
 	type WeightInfo = ();
 }
 
