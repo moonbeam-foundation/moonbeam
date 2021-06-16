99b63a9fe6693ada13364375f2eed66f156ea0e1
crystalin <crystalin@users.noreply.github.com>
2021-06-16 18:35:26 +0000

    deploy: 89a4e7cc42669e2b262b3021c237bdd0480eb9c2

--- moonriver_runtime/currency/sidebar-items.js
+++ moonriver_runtime/currency/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"constant":[["BYTE_FEE",""],["KILOMOVRS",""],["MICROMOVRS",""],["MILLIMOVRS",""],["MOVR",""],["NANOMOVRS",""]],"fn":[["deposit",""]]});
+initSidebarItems({"constant":[["BYTE_FEE",""],["GIGASED",""],["KILOMOVR",""],["KILOSED",""],["MEGASED",""],["MICROMOVR",""],["MILLIMOVR",""],["MOVR",""],["SED",""]],"fn":[["deposit",""]]});
