c9f1f9928d85990c5d825728715abd5514679869
Stephen Shelton <steve@brewcraft.org>
2021-06-24 15:42:55 -0600

    s/    /\t/

--- runtime/moonshadow/src/lib.rs
+++ runtime/moonshadow/src/lib.rs
@@ -324,10 +324,10 @@ impl FeeCalculator for FixedGasPrice {
 /// Parameterized slow adjusting fee updated based on
 /// https://w3f-research.readthedocs.io/en/latest/polkadot/Token%20Economics.html#-2.-slow-adjusting-mechanism
 pub type SlowAdjustingFeeUpdate<R> = TargetedFeeAdjustment<
-    R,
-    TargetBlockFullness,
-    AdjustmentVariable,
-    MinimumMultiplier
+	R,
+	TargetBlockFullness,
+	AdjustmentVariable,
+	MinimumMultiplier
 >;
 
 impl pallet_evm::Config for Runtime {
@@ -840,68 +840,68 @@ cumulus_pallet_parachain_system::register_validate_block!(
 
 #[cfg(test)]
 mod multiplier_tests {
-    use super::*;
-    use frame_support::{parameter_types, weights::{Weight, DispatchClass}};
-    use sp_core::H256;
-    use sp_runtime::{
-        testing::Header,
-        traits::{BlakeTwo256, IdentityLookup, Convert, One},
-        Perbill,
-    };
+	use super::*;
+	use frame_support::{parameter_types, weights::{Weight, DispatchClass}};
+	use sp_core::H256;
+	use sp_runtime::{
+		testing::Header,
+		traits::{BlakeTwo256, IdentityLookup, Convert, One},
+		Perbill,
+	};
 
-    type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Runtime>;
-    type Block = frame_system::mocking::MockBlock<Runtime>;
+	type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Runtime>;
+	type Block = frame_system::mocking::MockBlock<Runtime>;
 
-    frame_support::construct_runtime!(
-        pub enum Runtime where
-            Block = Block,
-            NodeBlock = Block,
-            UncheckedExtrinsic = UncheckedExtrinsic,
-        {
-            System: frame_system::{Pallet, Call, Config, Storage, Event<T>}
-        }
-    );
+	frame_support::construct_runtime!(
+		pub enum Runtime where
+			Block = Block,
+			NodeBlock = Block,
+			UncheckedExtrinsic = UncheckedExtrinsic,
+		{
+			System: frame_system::{Pallet, Call, Config, Storage, Event<T>}
+		}
+	);
 
-    parameter_types! {
-        pub const BlockHashCount: u64 = 250;
-        pub const AvailableBlockRatio: Perbill = Perbill::one();
-        pub BlockLength: frame_system::limits::BlockLength =
-            frame_system::limits::BlockLength::max(2 * 1024);
-        pub BlockWeights: frame_system::limits::BlockWeights =
-            frame_system::limits::BlockWeights::simple_max(1024);
-    }
+	parameter_types! {
+		pub const BlockHashCount: u64 = 250;
+		pub const AvailableBlockRatio: Perbill = Perbill::one();
+		pub BlockLength: frame_system::limits::BlockLength =
+			frame_system::limits::BlockLength::max(2 * 1024);
+		pub BlockWeights: frame_system::limits::BlockWeights =
+			frame_system::limits::BlockWeights::simple_max(1024);
+	}
 
-    impl frame_system::Config for Runtime {
-        type BaseCallFilter = ();
-        type BlockWeights = BlockWeights;
-        type BlockLength = ();
-        type DbWeight = ();
-        type Origin = Origin;
-        type Index = u64;
-        type BlockNumber = u64;
-        type Call = Call;
-        type Hash = H256;
-        type Hashing = BlakeTwo256;
-        type AccountId = u64;
-        type Lookup = IdentityLookup<Self::AccountId>;
-        type Header = Header;
-        type Event = Event;
-        type BlockHashCount = BlockHashCount;
-        type Version = ();
-        type PalletInfo = PalletInfo;
-        type AccountData = ();
-        type OnNewAccount = ();
-        type OnKilledAccount = ();
-        type SystemWeightInfo = ();
-        type SS58Prefix = ();
-        type OnSetCode = ();
-    }
+	impl frame_system::Config for Runtime {
+		type BaseCallFilter = ();
+		type BlockWeights = BlockWeights;
+		type BlockLength = ();
+		type DbWeight = ();
+		type Origin = Origin;
+		type Index = u64;
+		type BlockNumber = u64;
+		type Call = Call;
+		type Hash = H256;
+		type Hashing = BlakeTwo256;
+		type AccountId = u64;
+		type Lookup = IdentityLookup<Self::AccountId>;
+		type Header = Header;
+		type Event = Event;
+		type BlockHashCount = BlockHashCount;
+		type Version = ();
+		type PalletInfo = PalletInfo;
+		type AccountData = ();
+		type OnNewAccount = ();
+		type OnKilledAccount = ();
+		type SystemWeightInfo = ();
+		type SS58Prefix = ();
+		type OnSetCode = ();
+	}
 
-    fn run_with_system_weight<F>(w: Weight, mut assertions: F) where F: FnMut() -> () {
-        let mut t: sp_io::TestExternalities =
-            frame_system::GenesisConfig::default().build_storage::<Runtime>().unwrap().into();
-        t.execute_with(|| {
-            System::set_block_consumed_resources(w, 0);
+	fn run_with_system_weight<F>(w: Weight, mut assertions: F) where F: FnMut() -> () {
+		let mut t: sp_io::TestExternalities =
+			frame_system::GenesisConfig::default().build_storage::<Runtime>().unwrap().into();
+		t.execute_with(|| {
+			System::set_block_consumed_resources(w, 0);
 			assertions()
 		});
 	}
