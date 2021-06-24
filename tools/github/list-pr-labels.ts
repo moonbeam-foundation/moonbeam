d2843f0ca1b74d240fdd9c1f7320724f28f01af4
Crystalin <alan@purestake.com>
2021-06-24 00:49:10 +0000

    Adds print version bump script + action

--- tools/github/list-pr-labels.ts
+++ tools/github/list-pr-labels.ts
@@ -1,57 +1,29 @@
-import yargs from "yargs";
 import { Octokit } from "octokit";
+import yargs from "yargs";
+import { getCommitAndLabels } from "./github-utils";
 
-const octokit = new Octokit();
-const labels = {};
-
-// Typescript 4 will support it natively, but not yet :(
-type Await<T> = T extends PromiseLike<infer U> ? U : T;
-type Commits = Await<ReturnType<Octokit["rest"]["repos"]["compareCommits"]>>["data"]["commits"];
-
-async function getLabels(repo: string, previousTag: string, newTag: string, filters: string[]) {
-  let commits: Commits = [];
-  let more = true;
-  let page = 0;
-  while (more) {
-    const compare = await octokit.rest.repos.compareCommits({
-      owner: repo.split("/")[0],
-      repo: repo.split("/")[1],
-      base: previousTag,
-      head: newTag,
-      per_page: 200,
-      page,
-    });
-    commits = commits.concat(compare.data.commits);
-    more = compare.data.commits.length == 200;
-    page++;
-  }
-
-  console.log(
-    `found ${commits.length} total commits in https://github.com/${repo}/compare/${previousTag}...${newTag}`
-  );
-
-  for (const commit of commits) {
-    const prs = await octokit.rest.repos.listPullRequestsAssociatedWithCommit({
-      owner: repo.split("/")[0],
-      repo: repo.split("/")[1],
-      commit_sha: commit.sha,
-    });
-    for (const pr of prs.data) {
-      for (const label of pr.labels) {
-        labels[label.name] = labels[label.name] || [];
-        labels[label.name].push(`(${repo}#${pr.number}) ${pr.title}`);
-      }
-    }
-  }
-
+async function listPrByLabels(
+  octokit: Octokit,
+  owner: string,
+  repo: string,
+  previousTag: string,
+  newTag: string,
+  filters: string[],
+  excludeList: string[]
+) {
+  const { prByLabels } = await getCommitAndLabels(octokit, owner, repo, previousTag, newTag);
   const filterRegs = filters && filters.map((f) => new RegExp(f));
-  for (const labelName of Object.keys(labels).sort().reverse()) {
+  const excludeRegs = excludeList && excludeList.map((f) => new RegExp(f));
+  for (const labelName of Object.keys(prByLabels).sort().reverse()) {
     if (filterRegs && !filterRegs.some((f) => f.test(labelName))) {
       continue;
     }
+    if (excludeRegs && excludeRegs.some((f) => f.test(labelName))) {
+      continue;
+    }
     console.log(`===== ${labelName}`);
-    for (const label of labels[labelName]) {
-      console.log(`  ${label}`);
+    for (const pr of prByLabels[labelName]) {
+      console.log(`  ${`(${repo}#${pr.number}) ${pr.title}`}`);
     }
   }
 }
@@ -76,13 +48,30 @@ async function main() {
       },
       "only-label": {
         type: "array",
-        describe: "filter specific labels (using grep)",
+        describe: "keep only specific labels (using grep)",
+      },
+      "exclude-label": {
+        type: "array",
+        alias: "e",
+        describe: "exclude specific labels (using grep)",
       },
     })
     .demandOption(["from", "to", "repo"])
     .help().argv;
 
-  getLabels(argv.repo, argv.from, argv.to, argv["only-label"] as string[]);
+  const octokit = new Octokit({
+    auth: process.env.GITHUB_TOKEN || undefined,
+  });
+
+  listPrByLabels(
+    octokit,
+    argv.repo.split("/")[0],
+    argv.repo.split("/")[1],
+    argv.from,
+    argv.to,
+    argv["only-label"] as string[],
+    argv["exclude-label"] as string[]
+  );
 }
 
 main();
