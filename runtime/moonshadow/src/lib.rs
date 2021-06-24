962ac9bc24135d062e04a4bbe241e1ddea77a0b3
Joshy Orndorff <admin@joshyorndorff.com>
2021-06-24 14:13:49 -0400

    spec versions

--- runtime/moonshadow/src/lib.rs
+++ runtime/moonshadow/src/lib.rs
@@ -123,7 +123,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("moonshadow"),
 	impl_name: create_runtime_str!("moonshadow"),
 	authoring_version: 3,
-	spec_version: 53,
+	spec_version: 54,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 2,
