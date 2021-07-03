2a54c78c694062a8a14a432ec328b51179b49696
Glenn Field <glenn@purestake.com>
2021-07-03 11:44:24 +0000

    Update fast tracking tests

--- runtime/moonbase/tests/integration_test.rs
+++ runtime/moonbase/tests/integration_test.rs
@@ -36,8 +36,8 @@ use sp_core::{Public, H160, U256};
 use sp_runtime::DispatchError;
 
 #[test]
-fn fast_track_unavailable() {
-	assert!(!<moonbase_runtime::Runtime as pallet_democracy::Config>::InstantAllowed::get());
+fn fast_track_available() {
+	assert!(<moonbase_runtime::Runtime as pallet_democracy::Config>::InstantAllowed::get());
 }
 
 #[test]
