83638603be110e5872df86216c8bd6d7fd1407e7
librelois <c@elo.tf>
2021-07-07 19:53:18 +0200

    tmp: upgrade runtimes spec version to 156

--- runtime/moonriver/src/lib.rs
+++ runtime/moonriver/src/lib.rs
@@ -131,7 +131,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("moonriver"),
 	impl_name: create_runtime_str!("moonriver"),
 	authoring_version: 3,
-	spec_version: 0155,
+	spec_version: 0156,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 2,
