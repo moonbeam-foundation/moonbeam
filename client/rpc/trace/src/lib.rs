f18d484b2bcb49776e3f9534df820ee0fef8bb62
tgmichel <telmo@purestake.com>
2021-07-19 17:55:53 +0200

    Runtime Api versioning (#614)
    * `DebugRuntimeApi` versioning
    
    * Pin cumulus `a53c106`
    
    * Pin new cumulus (again) `23d06b7`

--- client/rpc/trace/src/lib.rs
+++ client/rpc/trace/src/lib.rs
@@ -40,7 +40,7 @@ use tracing::{instrument, Instrument};
 
 use jsonrpc_core::Result;
 use sc_client_api::backend::Backend;
-use sp_api::{BlockId, HeaderT, ProvideRuntimeApi};
+use sp_api::{ApiExt, BlockId, HeaderT, ProvideRuntimeApi};
 use sp_block_builder::BlockBuilder;
 use sp_blockchain::{
 	Backend as BlockchainBackend, Error as BlockChainError, HeaderBackend, HeaderMetadata,
@@ -798,6 +798,17 @@ where
 		let height = *block_header.number();
 		let substrate_parent_id = BlockId::<B>::Hash(*block_header.parent_hash());
 
+		// Get `DebugRuntimeApi` version.
+		let api_version = api
+			.api_version::<dyn DebugRuntimeApi<B>>(&substrate_parent_id)
+			.map_err(|e| internal_err(format!("Runtime api access error: {:?}", e)))?
+			.ok_or_else(|| {
+				internal_err(format!(
+					"Could not find `DebugRuntimeApi` at {:?}.",
+					substrate_parent_id
+				))
+			})?;
+
 		// Get Ethereum block data.
 		let (eth_block, _, eth_transactions) = api
 			.current_all(&BlockId::Hash(substrate_hash))
@@ -839,24 +850,46 @@ where
 
 		// Trace the block.
 		let f = || {
-			api.trace_block(&substrate_parent_id, &block_header, extrinsics)
-				.map_err(|e| {
-					internal_err(format!(
-						"Blockchain error when replaying block {} : {:?}",
-						height, e
-					))
-				})?
-				.map_err(|e| {
-					tracing::warn!(
-						"Internal runtime error when replaying block {} : {:?}",
-						height,
-						e
-					);
-					internal_err(format!(
-						"Internal runtime error when replaying block {} : {:?}",
-						height, e
-					))
-				})
+			if api_version >= 2 {
+				api.trace_block(&substrate_parent_id, &block_header, extrinsics)
+					.map_err(|e| {
+						internal_err(format!(
+							"Blockchain error when replaying block {} : {:?}",
+							height, e
+						))
+					})?
+					.map_err(|e| {
+						tracing::warn!(
+							"Internal runtime error when replaying block {} : {:?}",
+							height,
+							e
+						);
+						internal_err(format!(
+							"Internal runtime error when replaying block {} : {:?}",
+							height, e
+						))
+					})
+			} else {
+				#[allow(deprecated)]
+				api.trace_block_before_version_2(&substrate_parent_id, extrinsics)
+					.map_err(|e| {
+						internal_err(format!(
+							"Blockchain error when replaying block {} : {:?}",
+							height, e
+						))
+					})?
+					.map_err(|e| {
+						tracing::warn!(
+							"Internal runtime error when replaying block {} : {:?}",
+							height,
+							e
+						);
+						internal_err(format!(
+							"Internal runtime error when replaying block {} : {:?}",
+							height, e
+						))
+					})
+			}
 		};
 
 		let mut proxy = proxy::CallListProxy::new();
