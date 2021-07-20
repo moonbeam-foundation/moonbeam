353288606bc8af085cb33a6f4f03944dc45aa1c8
Alan Sapede <alan.sapede@gmail.com>
2021-07-20 09:24:19 -0400

    Split release process in 2 (binary/runtime) (#572)
    * Split release process in 2 (binary/runtime)
    
    * Fix typo and comments
    
    * Adds missing github utils
    
    * Update tools/github/github-utils.ts
    
    Co-authored-by: Joshy Orndorff <JoshOrndorff@users.noreply.github.com>
    
    * Update .github/workflows/publish-runtime.yml
    
    Co-authored-by: Éloïs <c@elo.tf>
    
    * Update .github/workflows/publish-binary.yml
    
    Co-authored-by: Éloïs <c@elo.tf>
    
    * Update .github/workflows/publish-binary.yml
    
    Co-authored-by: Éloïs <c@elo.tf>
    
    * Apply suggestions from code review
    
    Co-authored-by: Éloïs <c@elo.tf>
    
    Co-authored-by: Joshy Orndorff <JoshOrndorff@users.noreply.github.com>
    Co-authored-by: Éloïs <c@elo.tf>

--- tools/github/generate-release-body.ts
+++ tools/github/generate-release-body.ts
@@ -1,45 +1,9 @@
 import { execSync } from "child_process";
-import { readFileSync } from "fs";
 import yargs from "yargs";
-import path from "path";
+import { getCompareLink } from "./github-utils";
 
 function capitalize(s) {
   return s[0].toUpperCase() + s.slice(1);
-}
-
-function getCompareLink(packageName: string, previousTag: string, newTag: string) {
-  const previousPackage = execSync(
-    `git show ${previousTag}:../Cargo.lock | grep ${packageName}? | head -1 | grep -o '".*"'`
-  ).toString();
-  const previosCommit = /#([0-9a-f]*)/g.exec(previousPackage)[1].slice(0, 8);
-  const previousRepo = /(https:\/\/.*)\?/g.exec(previousPackage)[1];
-
-  const newPackage = execSync(
-    `git show ${newTag}:../Cargo.lock | grep ${packageName}? | head -1 | grep -o '".*"'`
-  ).toString();
-  const newCommit = /#([0-9a-f]*)/g.exec(newPackage)[1].slice(0, 8);
-  const newRepo = /(https:\/\/.*)\?/g.exec(newPackage)[1];
-  const newRepoOrganization = /github.com\/([^\/]*)/g.exec(newRepo)[1];
-
-  const diffLink =
-    previousRepo !== newRepo
-      ? `${previousRepo}/compare/${previosCommit}...${newRepoOrganization}:${newCommit}`
-      : `${previousRepo}/compare/${previosCommit}...${newCommit}`;
-
-  return diffLink;
-}
-
-function getRuntimeInfo(srtoolReportFolder: string, runtimeName: string) {
-  const specVersion = execSync(
-    `cat ../runtime/${runtimeName}/src/lib.rs | grep 'spec_version: [0-9]*' | tail -1`
-  ).toString();
-  return {
-    name: runtimeName,
-    version: /:\s?([0-9A-z\-]*)/.exec(specVersion)[1],
-    srtool: JSON.parse(
-      readFileSync(path.join(srtoolReportFolder, `./${runtimeName}-srtool-digest.json`)).toString()
-    ),
-  };
 }
 
 const main = () => {
@@ -47,37 +11,23 @@ const main = () => {
     .usage("Usage: npm run ts-node github/generate-release-body.ts [args]")
     .version("1.0.0")
     .options({
-      "srtool-report-folder": {
+      from: {
         type: "string",
-        describe: "folder which contains <runtime>-srtool-digest.json",
+        describe: "previous tag to retrieve commits from",
         required: true,
       },
-      tag: {
+      to: {
         type: "string",
-        describe: "current tag to draft",
+        describe: "current tag being drafted",
         required: true,
       },
     })
-    .demandOption(["srtool-report-folder"])
+    .demandOption(["from", "to"])
     .help().argv;
 
-  const command =
-    `git tag | grep "^v[0-9]*.[0-9]*.[0-9]*$" |` +
-    `sort -t "." -k1,1n -k2,2n -k3,3n | grep -B 1 "${argv.tag.replace(".", ".")}"`;
+  const previousTag = argv.from;
+  const newTag = argv.to;
 
-  const lastTags = execSync(command).toString().split("\n");
-
-  const previousTag = lastTags[0];
-  const newTag = lastTags[1];
-
-  if (!previousTag || !newTag) {
-    console.log(`Couldn't retrieve tags from`, execSync(command).toString().replace(/\n/g, ", "));
-    process.exit(1);
-  }
-
-  const runtimes = ["moonbase", "moonshadow", "moonriver", "moonbeam"].map((runtimeName) =>
-    getRuntimeInfo(argv["srtool-report-folder"], runtimeName)
-  );
   const moduleLinks = ["substrate", "polkadot", "cumulus", "frontier"].map((repoName) => ({
     name: repoName,
     link: getCompareLink(repoName, previousTag, newTag),
@@ -88,24 +38,6 @@ const main = () => {
     .filter((l) => !!l);
 
   const template = `
-## Runtimes
-
-${runtimes
-  .map(
-    (runtime) => `### ${capitalize(runtime.name)}
-
-* spec_version: ${runtime.version}
-* sha256: ${runtime.srtool.runtimes.compact.sha256}
-* size: ${runtime.srtool.runtimes.compact.size}
-* proposal: ${runtime.srtool.runtimes.compact.prop}
-`
-  )
-  .join(`\n\n`)}
-
-## Build information
-
-WASM runtime built using \`${runtimes[0]?.srtool.info.rustc}\`
-
 ## Changes
 
 ${commits.map((commit) => `* ${commit}`).join("\n")}
