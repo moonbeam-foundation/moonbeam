1f044ee64e93b680be8f9e7ff40408f73a5d29fc
gorka <gorka.irazoki@gmail.com>
2021-07-26 10:45:56 +0200

    Fix comment

--- precompiles/relay-encoder/RelayEncoder.sol
+++ precompiles/relay-encoder/RelayEncoder.sol
@@ -2,7 +2,7 @@
 pragma solidity >=0.8.0;
 
 /// @author The Moonbeam Team
-/// @title The interface through which solidity contracts will interact with Crowdloan Rewards
+/// @title The interface through which solidity contracts will interact with Relay Encoder
 /// We follow this same interface including four-byte function selectors, in the precompile that
 /// wraps the pallet
 interface RelayEncoder {
