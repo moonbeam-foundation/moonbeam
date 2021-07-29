89d17d5b8dfe6c374e1a7ae67bf68532c98d2505
Joshy Orndorff <admin@joshyorndorff.com>
2021-07-29 12:21:50 -0400

    remove sudo from moonriver

--- runtime/moonriver/src/lib.rs
+++ runtime/moonriver/src/lib.rs
@@ -284,11 +284,6 @@ impl pallet_transaction_payment::Config for Runtime {
 	type FeeMultiplierUpdate = SlowAdjustingFeeUpdate<Runtime>;
 }
 
-impl pallet_sudo::Config for Runtime {
-	type Call = Call;
-	type Event = Event;
-}
-
 impl pallet_ethereum_chain_id::Config for Runtime {}
 
 impl pallet_randomness_collective_flip::Config for Runtime {}
@@ -805,8 +800,7 @@ construct_runtime! {
 		Utility: pallet_utility::{Pallet, Call, Event} = 30,
 		Proxy: pallet_proxy::{Pallet, Call, Storage, Event<T>} = 31,
 
-		// Sudo
-		Sudo: pallet_sudo::{Pallet, Call, Config<T>, Storage, Event<T>} = 40,
+		// Sudo was previously index 40
 
 		// Ethereum compatibility
 		EthereumChainId: pallet_ethereum_chain_id::{Pallet, Storage, Config} = 50,
