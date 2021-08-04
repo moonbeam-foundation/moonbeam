4ee815a5a4e93cdc5f7875a5806e31df9d5c7aaa
Joshy Orndorff <admin@joshyorndorff.com>
2021-08-04 14:01:39 -0400

    Finally a working blanket impl thanks to @librelois

--- precompiles/utils/src/data.rs
+++ precompiles/utils/src/data.rs
@@ -18,7 +18,7 @@ use super::{error, EvmResult};
 use core::ops::Range;
 use sp_core::{H160, H256, U256};
 use sp_std::{
-	convert::TryInto,
+	convert::{TryFrom, TryInto},
 	vec,
 	vec::Vec,
 };
@@ -226,45 +226,18 @@ impl EvmData for Address {
 	}
 }
 
-//TODO @nanocryk I thought I could change this to a blanket impl like
-// impl<T: TryFrom<U256>> EvmData for T
-//
-// But the compiler gives me this error
-//
-// error[E0119]: conflicting implementations of trait `data::EvmData` for type `sp_core::H256`:
-//    --> precompiles/utils/src/data.rs:217:1
-//     |
-// 183 | impl EvmData for H256 {
-//     | --------------------- first implementation here
-// ...
-// 217 | impl<T: TryFrom<U256>> EvmData for T {
-//     | ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ conflicting implementation for `sp_core::H256`
-//     |
-//     = note: upstream crates may add a new impl of trait `std::convert::From<sp_core::U256>` for type `sp_core::H256` in future versions
-//
-// Why should I care what they add in future versions? I should just be able to modify my code when I update the library?
-// Anyway, I needed an impl for u32 and u128. The code is very repetitive. Is this how you would have proceeded?
-impl EvmData for U256 {
-	fn read(reader: &mut EvmDataReader) -> EvmResult<Self> {
-		let range = reader.move_cursor(32)?;
+/// A local marker trait for types that are TryFrom<U256> so we can have a blanket impl
+/// This is necessary to avoide duplicate trait implementation errors.
+/// See https://github.com/PureStake/moonbeam/pull/518#discussion_r682493750 for details
+pub trait EvmDataTryFromU256Marker: TryFrom<U256> + Into<U256> {}
+impl EvmDataTryFromU256Marker for u8 {}
+impl EvmDataTryFromU256Marker for u16 {}
+impl EvmDataTryFromU256Marker for u32 {}
+impl EvmDataTryFromU256Marker for u64 {}
+impl EvmDataTryFromU256Marker for u128 {}
+impl EvmDataTryFromU256Marker for U256 {}
 
-		let data = reader
-			.input
-			.get(range)
-			.ok_or_else(|| error("tried to parse U256 out of bounds"))?;
-
-		Ok(U256::from_big_endian(data))
-	}
-
-	fn write(writer: &mut EvmDataWriter, value: Self) {
-		let mut buffer = [0u8; 32];
-		value.to_big_endian(&mut buffer);
-		writer.data.extend_from_slice(&buffer);
-	}
-}
-
-// TODO tests for this.
-impl EvmData for u8 {
+impl<T: EvmDataTryFromU256Marker> EvmData for T {
 	fn read(reader: &mut EvmDataReader) -> EvmResult<Self> {
 		let range = reader.move_cursor(32)?;
 
@@ -275,53 +248,7 @@ impl EvmData for u8 {
 
 		U256::from_big_endian(data)
 			.try_into()
-			.map_err(|_| error("Value is too large for u32"))
-	}
-
-	fn write(writer: &mut EvmDataWriter, value: Self) {
-		let mut buffer = [0u8; 32];
-		let u256_value: U256 = value.into();
-		u256_value.to_big_endian(&mut buffer);
-		writer.data.extend_from_slice(&buffer);
-	}
-}
-
-// TODO tests for this.
-impl EvmData for u32 {
-	fn read(reader: &mut EvmDataReader) -> EvmResult<Self> {
-		let range = reader.move_cursor(32)?;
-
-		let data = reader
-			.input
-			.get(range)
-			.ok_or_else(|| error("tried to parse U256 out of bounds"))?;
-
-		U256::from_big_endian(data)
-			.try_into()
-			.map_err(|_| error("Value is too large for u32"))
-	}
-
-	fn write(writer: &mut EvmDataWriter, value: Self) {
-		let mut buffer = [0u8; 32];
-		let u256_value: U256 = value.into();
-		u256_value.to_big_endian(&mut buffer);
-		writer.data.extend_from_slice(&buffer);
-	}
-}
-
-// TODO tests for this.
-impl EvmData for u128 {
-	fn read(reader: &mut EvmDataReader) -> EvmResult<Self> {
-		let range = reader.move_cursor(32)?;
-
-		let data = reader
-			.input
-			.get(range)
-			.ok_or_else(|| error("tried to parse U256 out of bounds"))?;
-
-		U256::from_big_endian(data)
-			.try_into()
-			.map_err(|_| error("Value is too large for u32"))
+			.map_err(|_| error("Value is too large for provided type"))
 	}
 
 	fn write(writer: &mut EvmDataWriter, value: Self) {
