7ffbbec6e3b1ff76c2e742ab766252689b9ea5bc
gorka <gorka.irazoki@gmail.com>
2021-08-09 10:37:33 +0200

    Cleanup currencies lib.rs

--- primitives/currencies/src/lib.rs
+++ primitives/currencies/src/lib.rs
@@ -1,15 +1,19 @@
+// Copyright 2019-2021 PureStake Inc.
+// This file is part of Moonbeam.
+
+// Moonbeam is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Moonbeam is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Moonbeam.  If not, see <http://www.gnu.org/licenses/>.
 #![cfg_attr(not(feature = "std"), no_std)]
 pub mod currencies;
 
-use codec::{Decode, Encode};
-use sp_runtime::{
-	generic,
-	traits::{BlakeTwo256, IdentifyAccount, Verify},
-	MultiSignature, RuntimeDebug,
-};
-use sp_std::{convert::Into, prelude::*};
-
 pub use currencies::{CurrencyId, TokenSymbol};
-
-#[cfg(feature = "std")]
-use serde::{Deserialize, Serialize};
