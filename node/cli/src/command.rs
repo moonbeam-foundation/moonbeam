5448bc92325efaaa2bb671b4c31161538537180d
librelois <c@elo.tf>
2021-08-25 23:03:33 +0200

    westend-native is needed for chain spec "westend_moonbase_relay_testnet"

--- node/cli/src/command.rs
+++ node/cli/src/command.rs
@@ -23,6 +23,7 @@ use cumulus_primitives_core::ParaId;
 use log::info;
 use parity_scale_codec::Encode;
 use polkadot_parachain::primitives::AccountIdConversion;
+#[cfg(feature = "westend-native")]
 use polkadot_service::WestendChainSpec;
 use sc_cli::{
 	ChainSpec, CliConfiguration, DefaultConfigurationValues, ImportParams, KeystoreParams,
@@ -184,6 +185,7 @@ impl SubstrateCli for RelayChainCli {
 
 	fn load_spec(&self, id: &str) -> std::result::Result<Box<dyn sc_service::ChainSpec>, String> {
 		match id {
+			#[cfg(feature = "westend-native")]
 			"westend_moonbase_relay_testnet" => Ok(Box::new(WestendChainSpec::from_json_bytes(
 				&include_bytes!("../../../specs/alphanet/rococo-embedded-specs-v8.json")[..],
 			)?)),
