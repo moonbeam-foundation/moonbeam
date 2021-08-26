0634ad8a45798617c57ba26e340e3aa32813e2c6
Joshy Orndorff <admin@joshyorndorff.com>
2021-08-26 07:19:39 -0400

    child_storage_keys_iter impl

--- node/service/src/client.rs
+++ node/service/src/client.rs
@@ -347,6 +347,20 @@ impl sc_client_api::StorageProvider<Block, crate::FullBackend> for Client {
 		}
 	}
 
+	fn child_storage_keys_iter<'a>(
+		&self, 
+		id: &BlockId<Block>, 
+		child_info: ChildInfo, 
+		prefix: Option<&'a StorageKey>, 
+		start_key: Option<&StorageKey>
+	) -> sp_blockchain::Result<KeyIterator<'a, <crate::FullBackend as sc_client_api::Backend<Block>>::State, Block>> {
+		match self {
+			Self::Moonbeam(client) => client.child_storage_keys_iter(id, child_info, prefix, start_key),
+			Self::Moonriver(client) => client.child_storage_keys_iter(id, child_info, prefix, start_key),
+			Self::Moonbase(client) => client.child_storage_keys_iter(id, child_info, prefix, start_key),
+		}
+	}
+
 	fn child_storage_hash(
 		&self,
 		id: &BlockId<Block>,
