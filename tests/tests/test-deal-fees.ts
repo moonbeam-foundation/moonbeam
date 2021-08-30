a4f1a73936f1d348d478794da2f5fa9b3677ab4c
librelois <c@elo.tf>
2021-08-30 13:19:58 +0200

    prettier

--- tests/tests/test-deal-fees.ts
+++ tests/tests/test-deal-fees.ts
@@ -9,14 +9,13 @@ describeDevMoonbeam("20% of the fees should go to treasury", (context) => {
   const keyring = new Keyring({ type: "ethereum" });
 
   it("20% of the fees should go to treasury", async () => {
-
     // Treasury account should be initially empty
     expect(await context.web3.eth.getBalance(TREASURY_ACCOUNT, 0)).to.equal(0n.toString());
-    
+
     // We make an ethereum transaction, 20% of the fees should go to treasury.
     await context.createBlock({
       transactions: [await createTransfer(context.web3, TEST_ACCOUNT, 128)],
-    }); 
+    });
     expect(await context.web3.eth.getBalance(TREASURY_ACCOUNT, 1)).to.equal("4200000000000");
   });
 });
