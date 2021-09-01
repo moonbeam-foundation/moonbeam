adea03ae77f41418e00bb851a5c348433c90bc51
gorka <gorka.irazoki@gmail.com>
2021-09-01 19:17:49 +0200

    Add threshold

--- runtime/moonbase/src/lib.rs
+++ runtime/moonbase/src/lib.rs
@@ -649,6 +649,7 @@ parameter_types! {
 	pub const MinimumReward: Balance = 0;
 	pub const Initialized: bool = false;
 	pub const InitializationPayment: Perbill = Perbill::from_percent(30);
+	pub const RelaySignaturesThreshold: Perbill = Perbill::from_percent(67);
 	pub const MaxInitContributorsBatchSizes: u32 = 500;
 }
 
@@ -660,6 +661,7 @@ impl pallet_crowdloan_rewards::Config for Runtime {
 	type MinimumReward = MinimumReward;
 	type RewardCurrency = Balances;
 	type RelayChainAccountId = AccountId32;
+	type RewardAddressRelayVoteThreshold = RelaySignaturesThreshold;
 	type WeightInfo = pallet_crowdloan_rewards::weights::SubstrateWeight<Runtime>;
 }
 
