b9056dd0e24b81285b6f2c0db7ceed23507c9b9c
Alan Sapede <alan.sapede@gmail.com>
2021-09-02 11:04:27 -0400

    Runtime 600 (#773)
    * Upgrade to runtime 600 and client v0.11.3

--- runtime/moonbeam/src/lib.rs
+++ runtime/moonbeam/src/lib.rs
@@ -134,7 +134,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("moonbeam"),
 	impl_name: create_runtime_str!("moonbeam"),
 	authoring_version: 3,
-	spec_version: 0400,
+	spec_version: 0600,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 2,
