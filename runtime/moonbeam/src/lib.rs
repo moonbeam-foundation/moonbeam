99179545bf4b15cdd5a5e8e3e44ac8fc2821b323
gorka <gorka.irazoki@gmail.com>
2021-09-03 13:48:58 +0200

    Add to moonbeam too

--- runtime/moonbeam/src/lib.rs
+++ runtime/moonbeam/src/lib.rs
@@ -642,6 +642,7 @@ parameter_types! {
 	pub const MinimumReward: Balance = 0;
 	pub const Initialized: bool = false;
 	pub const InitializationPayment: Perbill = Perbill::from_percent(30);
+	pub const RelaySignaturesThreshold: Perbill = Perbill::from_percent(67);
 	pub const MaxInitContributorsBatchSizes: u32 = 500;
 }
 
@@ -653,6 +654,7 @@ impl pallet_crowdloan_rewards::Config for Runtime {
 	type MinimumReward = MinimumReward;
 	type RewardCurrency = Balances;
 	type RelayChainAccountId = AccountId32;
+	type RewardAddressRelayVoteThreshold = RelaySignaturesThreshold;
 	type WeightInfo = pallet_crowdloan_rewards::weights::SubstrateWeight<Runtime>;
 }
 
