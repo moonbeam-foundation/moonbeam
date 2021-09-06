b47cedb633f27d057250ffd99e58389b49b8d56e
Crystalin <alan@purestake.com>
2021-09-06 12:19:42 +0000

    Adds support for web3 custom request in tools

--- tools/utils/transactions.ts
+++ tools/utils/transactions.ts
@@ -1,4 +1,7 @@
+import Web3 from "web3";
+
 import { SubmittableExtrinsic } from "@polkadot/api/promise/types";
+import { JsonRpcResponse } from "web3-core-helpers";
 
 export const sendAllAndWaitLast = async (extrinsics: SubmittableExtrinsic[]) => {
   return new Promise(async (resolve, reject) => {
@@ -25,3 +28,30 @@ export const sendAllAndWaitLast = async (extrinsics: SubmittableExtrinsic[]) => 
     console.log(`Waiting for last extrinsic...`);
   });
 };
+
+let globalId = 10000;
+export async function customWeb3Request(web3: Web3, method: string, params: any[]) {
+  return new Promise<JsonRpcResponse>((resolve, reject) => {
+    const id = globalId++;
+    console.log(`Sending ${id}`);
+    (web3.currentProvider as any).send(
+      {
+        jsonrpc: "2.0",
+        id,
+        method,
+        params,
+      },
+      (error: Error | null, result?: JsonRpcResponse) => {
+        console.log(`Receiving ${id}`, error);
+        if (error) {
+          reject(
+            `Failed to send custom request (${method} (${params.join(",")})): ${
+              error.message || error.toString()
+            }`
+          );
+        }
+        resolve(result);
+      }
+    );
+  });
+}
