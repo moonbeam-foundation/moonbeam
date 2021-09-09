dba0c23f3c445f56c9634bcad0a672948e638a24
Joshy Orndorff <admin@joshyorndorff.com>
2021-09-09 11:35:56 -0400

    try to install it in the moonbase runtime

--- runtime/moonbase/src/precompiles.rs
+++ runtime/moonbase/src/precompiles.rs
@@ -19,6 +19,7 @@
 use crowdloan_rewards_precompiles::CrowdloanRewardsWrapper;
 use evm::{executor::PrecompileOutput, Context, ExitError};
 use frame_support::dispatch::{Dispatchable, GetDispatchInfo, PostDispatchInfo};
+use pallet_democracy_precompiles::DemocracyWrapper;
 use pallet_evm::{Precompile, PrecompileSet};
 use pallet_evm_precompile_bn128::{Bn128Add, Bn128Mul, Bn128Pairing};
 use pallet_evm_precompile_dispatch::Dispatch;
@@ -103,6 +104,7 @@ where
 			a if a == hash(2049) => Some(CrowdloanRewardsWrapper::<R>::execute(
 				input, target_gas, context,
 			)),
+			a if a == hash(2051) => Some(DemocracyWrapper::<R>::execute(input, target_gas.context)),
 			_ => None,
 		}
 	}
