6ea1caf9f3c72197613d75df22106341c41e6a99
Joshy Orndorff <admin@joshyorndorff.com>
2021-09-09 15:56:33 -0400

    Revert "bump moonbase spec version for manual testing"
    This reverts commit 6a892706d0fabb15a04cb2a47fce3e9c35252b72.

--- runtime/moonbase/src/lib.rs
+++ runtime/moonbase/src/lib.rs
@@ -133,8 +133,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("moonbase"),
 	impl_name: create_runtime_str!("moonbase"),
 	authoring_version: 3,
-	//Joshy changed this for local testing purposes.
-	spec_version: 1600,
+	spec_version: 0600,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 2,
