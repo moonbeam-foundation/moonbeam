18549d30670ef1a2a39faed6341c599ba10012ef
nanocryk <6422796+nanocryk@users.noreply.github.com>
2021-09-10 13:43:59 +0000

    backport tracing to runtime 501

--- primitives/ext/src/lib.rs
+++ primitives/ext/src/lib.rs
@@ -28,54 +28,47 @@ use sp_runtime_interface::runtime_interface;
 use codec::Decode;
 use sp_std::vec::Vec;
 
-use ethereum_types::U256;
-use moonbeam_rpc_primitives_debug::{
-	proxy::types::{EvmEvent, GasometerEvent, RuntimeEvent},
-	proxy::v1::Event as EventV1,
-	proxy::v2::Event as EventV2,
-	single::{Call, RawStepLog},
+use moonbeam_rpc_primitives_debug::events::{
+	Event as EventV2, EvmEvent, GasometerEvent, RuntimeEvent,
 };
 
 #[runtime_interface]
 pub trait MoonbeamExt {
-	// Old format to be deprecated.
-	fn raw_step(&mut self, data: Vec<u8>) {
-		let data: RawStepLog = Decode::decode(&mut &data[..]).unwrap();
-		EventV1::RawStep(data).emit();
-	}
-	fn raw_gas(&mut self, data: Vec<u8>) {
-		let data: U256 = Decode::decode(&mut &data[..]).unwrap();
-		EventV1::RawGas(data).emit();
-	}
-	fn raw_return_value(&mut self, data: Vec<u8>) {
-		EventV1::RawReturnValue(data).emit();
-	}
-	fn call_list_entry(&mut self, index: u32, value: Vec<u8>) {
-		let value: Call = Decode::decode(&mut &value[..]).unwrap();
-		EventV1::CallListEntry((index, value)).emit();
-	}
-	fn call_list_new(&mut self) {
-		EventV1::CallListNew().emit();
-	}
+	fn raw_step(&mut self, _data: Vec<u8>) {}
+
+	fn raw_gas(&mut self, _data: Vec<u8>) {}
+
+	fn raw_return_value(&mut self, _data: Vec<u8>) {}
+
+	fn call_list_entry(&mut self, _index: u32, _value: Vec<u8>) {}
+
+	fn call_list_new(&mut self) {}
+
 	// New design, proxy events.
 	/// An `Evm` event proxied by the Moonbeam runtime to this host function.
 	/// evm -> moonbeam_runtime -> host.
 	fn evm_event(&mut self, event: Vec<u8>) {
-		let event: EvmEvent = Decode::decode(&mut &event[..]).unwrap();
-		EventV2::Evm(event).emit();
+		if let Ok(event) = EvmEvent::decode(&mut &event[..]) {
+			EventV2::Evm(event).emit();
+		}
 	}
+
 	/// A `Gasometer` event proxied by the Moonbeam runtime to this host function.
 	/// evm_gasometer -> moonbeam_runtime -> host.
 	fn gasometer_event(&mut self, event: Vec<u8>) {
-		let event: GasometerEvent = Decode::decode(&mut &event[..]).unwrap();
-		EventV2::Gasometer(event).emit();
+		if let Ok(event) = GasometerEvent::decode(&mut &event[..]) {
+			EventV2::Gasometer(event).emit();
+		}
 	}
+
 	/// A `Runtime` event proxied by the Moonbeam runtime to this host function.
 	/// evm_runtime -> moonbeam_runtime -> host.
 	fn runtime_event(&mut self, event: Vec<u8>) {
-		let event: RuntimeEvent = Decode::decode(&mut &event[..]).unwrap();
-		EventV2::Runtime(event).emit();
+		if let Ok(event) = RuntimeEvent::decode(&mut &event[..]) {
+			EventV2::Runtime(event).emit();
+		}
 	}
+
 	/// An event to create a new CallList (currently a new transaction when tracing a block).
 	#[version(2)]
 	fn call_list_new(&mut self) {
