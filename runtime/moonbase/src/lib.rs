0ff048ed2ea0a7ee1154e8f8fe9376ff53c45ae7
Crystalin <alan@purestake.com>
2021-09-19 19:06:37 +0000

    Upgrade to runtime 701

--- runtime/moonbase/src/lib.rs
+++ runtime/moonbase/src/lib.rs
@@ -162,7 +162,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("moonbase"),
 	impl_name: create_runtime_str!("moonbase"),
 	authoring_version: 3,
-	spec_version: 0700,
+	spec_version: 0701,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 2,
--- runtime/moonbeam/src/lib.rs
+++ runtime/moonbeam/src/lib.rs
@@ -138,7 +138,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("moonbeam"),
 	impl_name: create_runtime_str!("moonbeam"),
 	authoring_version: 3,
-	spec_version: 0700,
+	spec_version: 0701,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 2,
