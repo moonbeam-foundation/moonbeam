33e64a215fe2961c43e8dd0a618a2a13f89001c9
librelois <c@elo.tf>
2021-09-23 15:07:36 +0200

    replace hash by original string

--- precompiles/balances-erc20/src/lib.rs
+++ precompiles/balances-erc20/src/lib.rs
@@ -49,12 +49,10 @@ mod mock;
 mod tests;
 
 /// Solidity selector of the Transfer log, which is the Keccak of the Log signature.
-pub const SELECTOR_LOG_TRANSFER: [u8; 32] =
-	keccak256!("0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef");
+pub const SELECTOR_LOG_TRANSFER: [u8; 32] = keccak256!("Transfer(address,address,uint256)");
 
 /// Solidity selector of the Approval log, which is the Keccak of the Log signature.
-pub const SELECTOR_LOG_APPROVAL: [u8; 32] =
-	keccak256!("0x8c5be1e5ebec7d5bd14f71427d1e84f3dd0314c0f7b2291e5b200ac8c7c3b925");
+pub const SELECTOR_LOG_APPROVAL: [u8; 32] = keccak256!("Approval(address,address,uint256)");
 
 /// Associates pallet Instance to a prefix used for the Approves storage.
 /// This trait is implemented for () and the 16 substrate Instance.
