3b2d9a8a7f58962a0d5826246ae0bc0c70c31184
gorka <gorka.irazoki@gmail.com>
2021-09-27 17:34:52 +0200

    Comment precompile address revert code insertion

--- runtime/moonbase/src/lib.rs
+++ runtime/moonbase/src/lib.rs
@@ -1154,11 +1154,12 @@ impl pallet_asset_manager::AssetRegistrar<Runtime> for AssetRegistrar {
 		)?;
 
 		// The asset has been created. Let's put the revert code in the precompile address
-		let precompile_address = Runtime::asset_id_to_account(asset);
+		// TODO uncomment when we feel comfortable
+        /* let precompile_address = Runtime::asset_id_to_account(asset);
 		pallet_evm::AccountCodes::<Runtime>::insert(
 			precompile_address,
 			vec![0x60, 0x00, 0x60, 0x00, 0xfd],
-		);
+		);*/
 
 		// Lastly, the metadata
 		Assets::force_set_metadata(
