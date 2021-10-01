cd2271c791c096893c7db65951f7315c07bc3de7
gorka <gorka.irazoki@gmail.com>
2021-10-01 20:05:51 +0200

    fix tests

--- runtime/moonriver/tests/integration_test.rs
+++ runtime/moonriver/tests/integration_test.rs
@@ -596,7 +596,7 @@ fn initialize_crowdloan_address_and_change_with_relay_key_sig() {
 
 			// signature is new_account || previous_account
 			let mut message = pallet_crowdloan_rewards::WRAPPED_BYTES_PREFIX.to_vec();
-			message.append(&mut AccountId::from(CHARLIE).encode());
+			message.append(&mut AccountId::from(DAVE).encode());
 			message.append(&mut AccountId::from(CHARLIE).encode());
 			message.append(&mut pallet_crowdloan_rewards::WRAPPED_BYTES_POSTFIX.to_vec());
 			let signature1 = pair1.sign(&message);
