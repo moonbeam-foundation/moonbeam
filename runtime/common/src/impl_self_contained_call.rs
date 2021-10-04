8451498c75ae016393524e9c0cdf82b9ad98ffe8
librelois <c@elo.tf>
2021-10-04 20:38:31 +0200

    fix recursive call

--- runtime/common/src/impl_self_contained_call.rs
+++ runtime/common/src/impl_self_contained_call.rs
@@ -81,9 +81,9 @@ macro_rules! impl_self_contained_call {
 				)?;
 				// Then, do the controls defined by the ethereum pallet.
 				use fp_self_contained::SelfContainedCall as _;
-				let self_contained_validation = call.validate_self_contained(signed_info).ok_or(
-					TransactionValidityError::Invalid(InvalidTransaction::BadProof
-				))??;
+				let self_contained_validation = eth_call
+					.validate_self_contained(signed_info)
+					.ok_or(TransactionValidityError::Invalid(InvalidTransaction::BadProof))??;
 
 				Ok(extra_validation.combine_with(self_contained_validation))
 			} else {
