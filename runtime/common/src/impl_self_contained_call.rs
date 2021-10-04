0ae66ab853c67c58936a71999dc7c8567301d430
librelois <c@elo.tf>
2021-10-04 16:37:41 +0200

    refactor(runtimes): move SelfContainedCall impl in a common macro

---
+++ runtime/common/src/impl_self_contained_call.rs
@@ -0,0 +1,67 @@
+// Copyright 2019-2021 PureStake Inc.
+// This file is part of Moonbeam.
+
+// Moonbeam is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Moonbeam is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Moonbeam.  If not, see <http://www.gnu.org/licenses/>.
+
+#[macro_export]
+macro_rules! impl_self_contained_call {
+	{} => {
+		impl fp_self_contained::SelfContainedCall for Call {
+			type SignedInfo = H160;
+
+			fn is_self_contained(&self) -> bool {
+				match self {
+					Call::Ethereum(call) => call.is_self_contained(),
+					_ => false,
+				}
+			}
+
+			fn check_self_contained(&self) -> Option<Result<Self::SignedInfo, TransactionValidityError>> {
+				match self {
+					Call::Ethereum(call) => call.check_self_contained(),
+					_ => None,
+				}
+			}
+
+			fn validate_self_contained(&self, info: &Self::SignedInfo) -> Option<TransactionValidity> {
+				match self {
+					Call::Ethereum(call) => {
+						if let pallet_ethereum::Call::transact(ref eth_tx) = call {
+							if let Err(error) = frame_system::CheckWeight::<Runtime>::do_validate(
+								&self.get_dispatch_info(),
+								eth_tx.input.len(),
+							) {
+								return Some(Err(error));
+							}
+						}
+						call.validate_self_contained(info)
+					}
+					_ => None,
+				}
+			}
+
+			fn apply_self_contained(
+				self,
+				info: Self::SignedInfo,
+			) -> Option<sp_runtime::DispatchResultWithInfo<PostDispatchInfoOf<Self>>> {
+				match self {
+					call @ Call::Ethereum(pallet_ethereum::Call::transact(_)) => Some(call.dispatch(
+						Origin::from(pallet_ethereum::RawOrigin::EthereumTransaction(info)),
+					)),
+					_ => None,
+				}
+			}
+		}
+	}
+}
--- runtime/moonbeam/src/lib.rs
+++ runtime/moonbeam/src/lib.rs
@@ -1083,39 +1083,4 @@ cumulus_pallet_parachain_system::register_validate_block!(
 	CheckInherents = CheckInherents,
 );
 
-impl fp_self_contained::SelfContainedCall for Call {
-	type SignedInfo = H160;
-
-	fn is_self_contained(&self) -> bool {
-		match self {
-			Call::Ethereum(call) => call.is_self_contained(),
-			_ => false,
-		}
-	}
-
-	fn check_self_contained(&self) -> Option<Result<Self::SignedInfo, TransactionValidityError>> {
-		match self {
-			Call::Ethereum(call) => call.check_self_contained(),
-			_ => None,
-		}
-	}
-
-	fn validate_self_contained(&self, info: &Self::SignedInfo) -> Option<TransactionValidity> {
-		match self {
-			Call::Ethereum(call) => call.validate_self_contained(info),
-			_ => None,
-		}
-	}
-
-	fn apply_self_contained(
-		self,
-		info: Self::SignedInfo,
-	) -> Option<sp_runtime::DispatchResultWithInfo<PostDispatchInfoOf<Self>>> {
-		match self {
-			call @ Call::Ethereum(pallet_ethereum::Call::transact(_)) => Some(call.dispatch(
-				Origin::from(pallet_ethereum::RawOrigin::EthereumTransaction(info)),
-			)),
-			_ => None,
-		}
-	}
-}
+runtime_common::impl_self_contained_call!();
