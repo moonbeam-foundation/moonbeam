2f258634803b707100dc619bc1f50a45db43a48c
gorka <gorka.irazoki@gmail.com>
2021-10-13 12:38:57 +0200

    Take ABI from compiled xtokens instance

--- tests/tests/test-precompile-xtokens.ts
+++ tests/tests/test-precompile-xtokens.ts
@@ -2,113 +2,22 @@ import { expect } from "chai";
 import { describeDevMoonbeam } from "../util/setup-dev-tests";
 import { customWeb3Request } from "../util/providers";
 import { ethers } from "ethers";
+import { getCompiled } from "../util/contracts";
 
 import { GENESIS_ACCOUNT } from "../util/constants";
 
 const ADDRESS_XTOKENS = "0x0000000000000000000000000000000000000804";
 const BALANCES_ADDRESS = "0x0000000000000000000000000000000000000802";
 
-// ABI obtained when compiling ./precompiles/xtokens/Xtokens.sol
-var contractAbi = [
-  {
-    inputs: [
-      {
-        internalType: "address",
-        name: "currency_address",
-        type: "address",
-      },
-      {
-        internalType: "uint256",
-        name: "amount",
-        type: "uint256",
-      },
-      {
-        components: [
-          {
-            internalType: "uint8",
-            name: "parents",
-            type: "uint8",
-          },
-          {
-            internalType: "bytes[]",
-            name: "interior",
-            type: "bytes[]",
-          },
-        ],
-        internalType: "struct Xtokens.Multilocation",
-        name: "destination",
-        type: "tuple",
-      },
-      {
-        internalType: "uint64",
-        name: "weight",
-        type: "uint64",
-      },
-    ],
-    name: "transfer",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [
-      {
-        components: [
-          {
-            internalType: "uint8",
-            name: "parents",
-            type: "uint8",
-          },
-          {
-            internalType: "bytes[]",
-            name: "interior",
-            type: "bytes[]",
-          },
-        ],
-        internalType: "struct Xtokens.Multilocation",
-        name: "asset",
-        type: "tuple",
-      },
-      {
-        internalType: "uint256",
-        name: "amount",
-        type: "uint256",
-      },
-      {
-        components: [
-          {
-            internalType: "uint8",
-            name: "parents",
-            type: "uint8",
-          },
-          {
-            internalType: "bytes[]",
-            name: "interior",
-            type: "bytes[]",
-          },
-        ],
-        internalType: "struct Xtokens.Multilocation",
-        name: "destination",
-        type: "tuple",
-      },
-      {
-        internalType: "uint64",
-        name: "weight",
-        type: "uint64",
-      },
-    ],
-    name: "transfer_multiasset",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-];
 const GAS_PRICE = "0x" + (1_000_000_000).toString(16);
 
 describeDevMoonbeam("Precompiles - xtokens", (context) => {
   let iFace;
+  let contractData; 
+
   before("Deploy contract", async () => {
-    iFace = new ethers.utils.Interface(contractAbi);
+    contractData = await getCompiled("XtokensInstance");
+    iFace = new ethers.utils.Interface(contractData.contract.abi);
   });
 
   it("allows to issue transfer xtokens", async function () {
