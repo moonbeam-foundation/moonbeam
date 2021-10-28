6ac4d77f8e56472f2ff31227e50a8f8150ce6819
Joshy Orndorff <admin@joshyorndorff.com>
2021-10-28 12:01:40 -0400

    One working test, two more sketched

--- pallets/proxy-genesis-companion/src/tests.rs
+++ pallets/proxy-genesis-companion/src/tests.rs
@@ -15,95 +15,23 @@
 // along with Moonbeam.  If not, see <http://www.gnu.org/licenses/>.
 
 //! Unit testing
-use crate::mock::{events, Call as OuterCall, ExtBuilder, Origin, Test};
-use frame_support::{assert_noop, assert_ok, dispatch::Dispatchable};
+use crate::mock::{Call as OuterCall, ExtBuilder, Origin, Test};
+use frame_support::{assert_noop, assert_ok, dispatch::Dispatchable, storage::IterableStorageMap};
 use sp_runtime::DispatchError;
 
 #[test]
-fn can_remark_during_normal_operation() {
-	ExtBuilder::default().build().execute_with(|| {
-		let call: OuterCall = frame_system::Call::remark { remark: vec![] }.into();
-		assert_ok!(call.dispatch(Origin::signed(1)));
-	})
-}
-
-#[test]
-fn cannot_remark_during_maintenance_mode() {
+fn empty_genesis_works() {
 	ExtBuilder::default()
-		.with_maintenance_mode(true)
 		.build()
-		.execute_with(|| {
-			let call: OuterCall = frame_system::Call::remark { remark: vec![] }.into();
-			assert_noop!(call.dispatch(Origin::signed(1)), DispatchError::BadOrigin);
-		})
+		.execute_with(|| assert_eq!(pallet_proxy::Proxies::<Test>::iter().count(), 0))
 }
 
 #[test]
-fn can_enter_maintenance_mode() {
-	ExtBuilder::default().build().execute_with(|| {
-		let call: OuterCall = Call::enter_maintenance_mode {}.into();
-		assert_ok!(call.dispatch(Origin::root()));
-
-		assert_eq!(events(), vec![Event::EnteredMaintenanceMode,]);
-	})
+fn non_empty_genesis_works() {
+	ExtBuilder::default().build().execute_with(|| todo!())
 }
 
 #[test]
-fn cannot_enter_maintenance_mode_from_wrong_origin() {
-	ExtBuilder::default()
-		.with_maintenance_mode(true)
-		.build()
-		.execute_with(|| {
-			let call: OuterCall = Call::enter_maintenance_mode {}.into();
-			assert_noop!(call.dispatch(Origin::signed(1)), DispatchError::BadOrigin);
-		})
-}
-
-#[test]
-fn cannot_enter_maintenance_mode_when_already_in_it() {
-	ExtBuilder::default()
-		.with_maintenance_mode(true)
-		.build()
-		.execute_with(|| {
-			let call: OuterCall = Call::enter_maintenance_mode {}.into();
-			assert_noop!(
-				call.dispatch(Origin::root()),
-				Error::<Test>::AlreadyInMaintenanceMode
-			);
-		})
-}
-
-#[test]
-fn can_resume_normal_operation() {
-	ExtBuilder::default()
-		.with_maintenance_mode(true)
-		.build()
-		.execute_with(|| {
-			let call: OuterCall = Call::resume_normal_operation {}.into();
-			assert_ok!(call.dispatch(Origin::root()));
-
-			assert_eq!(events(), vec![Event::NormalOperationResumed,]);
-		})
-}
-
-#[test]
-fn cannot_resume_normal_operation_from_wrong_origin() {
-	ExtBuilder::default()
-		.with_maintenance_mode(true)
-		.build()
-		.execute_with(|| {
-			let call: OuterCall = Call::resume_normal_operation {}.into();
-			assert_noop!(call.dispatch(Origin::signed(1)), DispatchError::BadOrigin);
-		})
-}
-
-#[test]
-fn cannot_resume_normal_operation_while_already_operating_normally() {
-	ExtBuilder::default().build().execute_with(|| {
-		let call: OuterCall = Call::resume_normal_operation {}.into();
-		assert_noop!(
-			call.dispatch(Origin::root()),
-			Error::<Test>::NotInMaintenanceMode
-		);
-	})
+fn genesis_proxies_reserve_deposits() {
+	ExtBuilder::default().build().execute_with(|| todo!())
 }
