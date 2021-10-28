c53289e46d093d288f4dc30f1e1f0977e42a1b02
nanocryk <6422796+nanocryk@users.noreply.github.com>
2021-10-28 10:27:45 +0000

    separate test

--- runtime/moonbeam/tests/integration_test.rs
+++ runtime/moonbeam/tests/integration_test.rs
@@ -190,7 +190,10 @@ fn verify_pallet_prefixes() {
 			max_size: Some(1),
 		},]
 	);
+}
 
+#[test]
+fn test_collectives_storage_item_prefixes() {
 	for StorageInfo { pallet_name, .. } in
 		<moonbeam_runtime::CouncilCollective as StorageInfoTrait>::storage_info()
 	{
