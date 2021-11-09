e13bf961715213112ef0f61d31b4343dd73b4959
gorka <gorka.irazoki@gmail.com>
2021-11-09 21:08:26 +0100

    Revert "Try hardcoding"
    This reverts commit 96ad4cdcadcc57f3992a392031cf66dde478f6f9.

--- node/service/src/chain_spec/moonbase.rs
+++ node/service/src/chain_spec/moonbase.rs
@@ -274,9 +274,7 @@ pub fn testnet_genesis(
 			start_in_maintenance_mode: false,
 		},
 		// This should initialize it to whatever we have set in the pallet
-		polkadot_xcm: PolkadotXcmConfig {
-			safe_xcm_version: Some(2),
-		},
+		polkadot_xcm: PolkadotXcmConfig::default(),
 	}
 }
 
