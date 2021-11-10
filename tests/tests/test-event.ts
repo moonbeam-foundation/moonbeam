de58a7e5c3fc65cafe08206a1f6d5282479a338f
estienne.antoine@gmail.com <estienne.antoine@gmail.com>
2021-11-10 15:32:43 +0100

    remove more todos

--- tests/tests/test-event.ts
+++ tests/tests/test-event.ts
@@ -14,7 +14,6 @@ describeDevMoonbeam("Event - Contract", (context) => {
     expect(receipt.logs.length).to.be.eq(1);
     expect(
       "0x" + receipt.logs[0].topics[1].substring(26, receipt.logs[0].topics[1].length + 1)
-    ).to.be.eq(GENESIS_ACCOUNT.toLowerCase()); //TODO: verify that web3.eth.getTransactionReceipt
-    // should not return checksummed address
+    ).to.be.eq(GENESIS_ACCOUNT.toLowerCase()); // web3 doesn't checksum
   });
 });
--- tests/tests/test-filter-api-polling.ts
+++ tests/tests/test-filter-api-polling.ts
@@ -77,7 +77,7 @@ describeDevMoonbeam("Filter Block API - Polling", (context) => {
     ]);
 
     expect(poll.result.length).to.be.eq(1);
-    // TODO check web3.eth.getTransactionReceipt and eth_getFilterChanges
+    // web3 doesn't checksum
     expect(poll.result[0].address).to.be.eq(receipt.contractAddress.toLowerCase());
     expect(poll.result[0].topics).to.be.deep.eq(receipt.logs[0].topics);
   });
