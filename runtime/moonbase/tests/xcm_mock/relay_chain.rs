a75f424484cd117a57f9ed4eaf2f4fe6b7e37e95
Stephen Shelton <steve@brewcraft.org>
2021-11-10 12:50:27 -0700

    Point xcm mock to TestWeightInfo

--- runtime/moonbase/tests/xcm_mock/relay_chain.rs
+++ runtime/moonbase/tests/xcm_mock/relay_chain.rs
@@ -95,7 +95,7 @@ impl pallet_utility::Config for Runtime {
 impl shared::Config for Runtime {}
 
 impl configuration::Config for Runtime {
-	type WeightInfo = configuration::weights::WeightInfo<Runtime>;
+	type WeightInfo = configuration::TestWeightInfo;
 }
 
 parameter_types! {
