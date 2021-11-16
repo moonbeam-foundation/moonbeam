a8d10aa3f1d054aa428257d406bdb5d42171b06c
Joshy Orndorff <admin@joshyorndorff.com>
2021-11-16 14:20:14 -0500

    attempt at block method compiles

--- client/rpc/finality/src/lib.rs
+++ client/rpc/finality/src/lib.rs
@@ -13,58 +13,88 @@
 
 // You should have received a copy of the GNU General Public License
 // along with Moonbeam.  If not, see <http://www.gnu.org/licenses/>.
+use fc_rpc::{
+	frontier_backend_client::{self, is_canon},
+	internal_err,
+};
 use futures::{future::BoxFuture, FutureExt as _};
 use jsonrpc_core::Result as RpcResult;
 use jsonrpc_derive::rpc;
-use std::sync::Arc;
-
-use parity_scale_codec::Encode;
+use sp_core::H256;
+use std::{marker::PhantomData, sync::Arc};
+//TODO ideally we wouldn't depend on BlockId here. Can we change frontier
+// so it's load_hash helper returns an H256 instead of wrapping it in a BlockId?
+use fc_db::Backend as FrontierBackend;
+use sp_api::BlockId;
+use sp_blockchain::HeaderBackend;
+use sp_runtime::traits::Block;
 
 /// An RPC endpoint to check for finality of blocks and transactions in Moonbeam
 #[rpc(server)]
 pub trait MoonbeamFinalityApi {
 	/// Reports whether a Moonbeam or Ethereum block is finalized.
 	/// Errors if the block is not found. //TODO reevaluate design later. Could just return false.
+	/// TODO Should I be generic over the hash type? Probably; ChainApi is. Although chain api isn't moonbeam specific so...
 	#[rpc(name = "moon_isBlockFinalized")]
-	fn is_block_finalized(&self, block_hash: Vec<u8>) -> BoxFuture<'static, RpcResult<bool>>;
+	fn is_block_finalized(&self, block_hash: H256) -> BoxFuture<'static, RpcResult<bool>>;
 
 	/// Reports whether a Moonbeam or Ethereum transaction is finalized.
 	#[rpc(name = "moon_isTxFinalized")]
-	fn is_tx_finalized(&self, tx_hash: Vec<u8>) -> BoxFuture<'static, RpcResult<bool>>;
+	fn is_tx_finalized(&self, tx_hash: H256) -> BoxFuture<'static, RpcResult<bool>>;
 }
 
-pub struct MoonbeamFinality<C> {
+pub struct MoonbeamFinality<B: Block, C> {
+	pub backend: Arc<FrontierBackend<B>>,
 	pub client: Arc<C>,
+	_phdata: PhantomData<B>,
 }
 
-impl<C> MoonbeamFinalityApi for MoonbeamFinality<C>
+impl<B, C> MoonbeamFinalityApi for MoonbeamFinality<B, C>
 where
 	C: Send + Sync + 'static,
+	B: Block<Hash = H256>,
+	C: HeaderBackend<B>,
 {
-	fn is_block_finalized(&self, block_hash: Vec<u8>) -> BoxFuture<'static, RpcResult<bool>> {
+	fn is_block_finalized(&self, raw_hash: H256) -> BoxFuture<'static, RpcResult<bool>> {
+		let backend = self.backend.clone();
+		let client = self.client.clone();
 		async move {
-			//TODO actually check the finality XD
+			let substrate_hash =
+				match frontier_backend_client::load_hash::<B>(backend.as_ref(), raw_hash)
+					.expect("frontier lookup should not error?")
+				{
+					// If we find this hash in the frontier data base, we know it is an eth hash
+					Some(BlockId::Hash(hash)) => hash,
+					Some(BlockId::Number(_)) => panic!("is_canon test only works with hashes."),
+					// Otherwise, we assume this is a Substrate hash.
+					None => raw_hash,
+				};
 
-			Ok(false)
+			// Confirm that this block is in the best chain
+			if !is_canon(client.as_ref(), substrate_hash) {
+				return Ok(false);
+			}
+
+			// At this point we know the block in question is in the current best chain.
+			// It's just a question of whether it is in the finalized prefix or not
+			let query_height = client
+				.number(substrate_hash)
+				.expect("Blocks in best chain should have number")
+				.expect("uhhhh");
+			let finalized_height = client.info().finalized_number;
+
+			Ok(query_height <= finalized_height)
 		}
 		.boxed()
 	}
 
-	fn is_tx_finalized(&self, tx_hash: Vec<u8>) -> BoxFuture<'static, RpcResult<bool>> {
+	fn is_tx_finalized(&self, tx_hash: H256) -> BoxFuture<'static, RpcResult<bool>> {
 		async move {
-			//TODO actually check the finality XD
+			// Our goal here is to get a Substrate block, and then follow the same approach as
+			// we did for the block check
 
 			Ok(false)
 		}
 		.boxed()
-	}
-}
-
-// This bit cribbed from frontier.
-pub fn internal_err<T: ToString>(message: T) -> jsonrpc_core::Error {
-	jsonrpc_core::Error {
-		code: jsonrpc_core::ErrorCode::InternalError,
-		message: message.to_string(),
-		data: None,
 	}
 }
