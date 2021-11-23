ff02bc73d7ab20f3103214d6ba2db47eef259360
tgmichel <telmo@purestake.com>
2021-11-23 10:56:04 +0100

    Add pallet_utility PalletOrigin assoc type

--- runtime/moonriver/src/lib.rs
+++ runtime/moonriver/src/lib.rs
@@ -216,6 +216,7 @@ impl frame_system::Config for Runtime {
 impl pallet_utility::Config for Runtime {
 	type Event = Event;
 	type Call = Call;
+	type PalletsOrigin = OriginCaller;
 	type WeightInfo = pallet_utility::weights::SubstrateWeight<Runtime>;
 }
 
