7d54b222b480cdaf347a4aba35e0453d720106b1
Éloïs <c@elo.tf>
2021-11-28 21:08:22 +0100

    add nimbus to version bump info (#1027)
    * add nimbus to version bump info
    
    * prettier

--- tools/github/print-version-bump-info.ts
+++ tools/github/print-version-bump-info.ts
@@ -3,11 +3,17 @@ import yargs from "yargs";
 import { getCommitAndLabels } from "./github-utils";
 
 async function printInfo(octokit: Octokit, previousVersion: string, nextVersion: string) {
-  const owner = "paritytech";
+  const owners = {
+    substrate: "paritytech",
+    polkadot: "paritytech",
+    cumulus: "paritytech",
+    nimbus: "purestake",
+  };
   const prefixes = {
     substrate: "polkadot-",
     polkadot: "release-",
     cumulus: "polkadot-",
+    nimbus: "moonbeam-polkadot-",
   };
   console.log(`# Description\n`);
   console.log(`This ticket is automatically generated using\n`);
@@ -21,22 +27,22 @@ async function printInfo(octokit: Octokit, previousVersion: string, nextVersion:
     const nextTag = `${prefixes[repo]}${nextVersion}`;
 
     const previousCommit = await octokit.rest.git.getCommit({
-      owner,
+      owner: owners[repo],
       repo,
       commit_sha: (
         await octokit.rest.git.getTree({
-          owner,
+          owner: owners[repo],
           repo,
           tree_sha: previousTag,
         })
       ).data.sha,
     });
     const nextCommit = await octokit.rest.git.getCommit({
-      owner,
+      owner: owners[repo],
       repo,
       commit_sha: (
         await octokit.rest.git.getTree({
-          owner,
+          owner: owners[repo],
           repo,
           tree_sha: nextTag,
         })
@@ -50,12 +56,12 @@ async function printInfo(octokit: Octokit, previousVersion: string, nextVersion:
     );
     const { commits, prByLabels } = await getCommitAndLabels(
       octokit,
-      owner,
+      owners[repo],
       repo,
       previousTag,
       nextTag
     );
-    console.log(`https://github.com/${owner}/${repo}/compare/${previousTag}...${nextTag}`);
+    console.log(`https://github.com/${owners[repo]}/${repo}/compare/${previousTag}...${nextTag}`);
     console.log("```");
     console.log(`    from: ${previousCommit.data.sha}`);
     console.log(`      to: ${nextCommit.data.sha}`);
@@ -65,7 +71,7 @@ async function printInfo(octokit: Octokit, previousVersion: string, nextVersion:
     for (const label of Object.keys(prByLabels)) {
       prInfoByLabels[label] = (prInfoByLabels[label] || []).concat(
         prByLabels[label].map((pr) => {
-          return `  ${`(${owner}/${repo}#${pr.number}) ${pr.title}`}`;
+          return `  ${`(${owners[repo]}/${repo}#${pr.number}) ${pr.title}`}`;
         })
       );
     }
