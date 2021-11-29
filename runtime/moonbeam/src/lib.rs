e9c6062da9556c6b912d08b26271aadc4296390d
gorka <gorka.irazoki@gmail.com>
2021-11-29 12:18:20 +0100

    Add feature in all runtimes as one of them already incorporates it

--- runtime/moonbeam/src/lib.rs
+++ runtime/moonbeam/src/lib.rs
@@ -919,6 +919,10 @@ impl pallet_maintenance_mode::Config for Runtime {
 	type MaintenanceCallFilter = MaintenanceFilter;
 	type MaintenanceOrigin =
 		pallet_collective::EnsureProportionAtLeast<_2, _3, AccountId, TechCommitteeInstance>;
+	type NormalDmpHandler = ();
+	type MaintenanceDmpHandler = ();
+	type NormalXcmpHandler = ();
+	type MaintenanceXcmpHandler = ();
 	type NormalOnRuntimeUpgrade = AllPallets;
 	type MaintenanceOnRuntimeUpgrade = AllPallets;
 	type NormalOnInitialize = AllPallets;
