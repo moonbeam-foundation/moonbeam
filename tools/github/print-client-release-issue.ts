1d3d665306e21c8f4b6adbbd73b8273adbf6689f
librelois <c@elo.tf>
2021-12-02 13:25:20 +0100

    editorconfig

--- tools/github/print-client-release-issue.ts
+++ tools/github/print-client-release-issue.ts
@@ -23,15 +23,19 @@ async function main() {
   const newVersion = argv.to;
 
   const template = `
-  - [ ] Create a PR with v${newVersion} (see README.md last section or last v${previousVersion} PR to see the required changed)
+  - [ ] Create a PR with v${newVersion} (see README.md last section or last v${previousVersion} PR
+    to see the required changed)
   - [ ] Get that PR approved and merged
   - [ ] Tag master with v${newVersion} and push to github
   - [ ] Start the github action Publish Binary Draft with v${previousVersion} => v${newVersion}
   - [ ] Review the generated Draft and clean a bit the messages if needed (keep it draft)
-  - [ ] Update moonbeam-networks stagenet/moonsama config.json to include sha-xxxxx (matching your v${newVersion} tag) and increase the config version + 1
+  - [ ] Update moonbeam-networks stagenet/moonsama config.json to include sha-xxxxx (matching your
+    v${newVersion} tag) and increase the config version + 1
   - [ ] Test the new client on stagenet and moonsama
-  - [ ] When everything is ok, publish the new docker image: start github action Publish Docker with v${newVersion}
-  - [ ] Publish the new tracing image: on repo moonbeam-runtime-overrides, start github action Publish Docker with v${newVersion} and master
+  - [ ] When everything is ok, publish the new docker image: start github action Publish Docker
+  with v${newVersion}
+  - [ ] Publish the new tracing image: on repo moonbeam-runtime-overrides, start github action
+  Publish Docker with v${newVersion} and master
   `;
 
   console.log(template);
