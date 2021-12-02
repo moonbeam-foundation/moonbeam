4dac56e7499f784ccde99c07ed3323855f83ccb1
girazoki <girazoki@users.noreply.github.com>
2021-12-02 09:49:39 +0000

    deploy: 6327ae812e775ef117d32815d0b60aad220431c3

---
+++ implementors/polkadot_parachain/primitives/trait.DmpMessageHandler.js
@@ -0,0 +1,4 @@
+(function() {var implementors = {};
+implementors["moonbase_runtime"] = [{"text":"impl DmpMessageHandler for <a class=\"struct\" href=\"moonbase_runtime/struct.MaintenanceDmpHandler.html\" title=\"struct moonbase_runtime::MaintenanceDmpHandler\">MaintenanceDmpHandler</a>","synthetic":false,"types":["moonbase_runtime::MaintenanceDmpHandler"]}];
+implementors["pallet_maintenance_mode"] = [{"text":"impl&lt;T:&nbsp;<a class=\"trait\" href=\"pallet_maintenance_mode/pallet/trait.Config.html\" title=\"trait pallet_maintenance_mode::pallet::Config\">Config</a>&gt; DmpMessageHandler for <a class=\"struct\" href=\"pallet_maintenance_mode/pallet/struct.Pallet.html\" title=\"struct pallet_maintenance_mode::pallet::Pallet\">Pallet</a>&lt;T&gt;","synthetic":false,"types":["pallet_maintenance_mode::pallet::Pallet"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
