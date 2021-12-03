1ccc867032f2375a2d566f7dbc6e514a4188a75d
Albertov19 <albertoviera@gmail.com>
2021-12-03 13:48:07 +0100

    fixed crowdloan rewards

--- precompiles/crowdloan-rewards/CrowdloanInterface.sol
+++ precompiles/crowdloan-rewards/CrowdloanInterface.sol
@@ -6,8 +6,8 @@ pragma solidity >=0.8.0;
  * @title The interface through which solidity contracts will interact with Crowdloan Rewards
  * We follow this same interface including four-byte function selectors, in the precompile that
  * wraps the pallet
- * Moonbase address :    0x0000000000000000000000000000000000000802
- * Moonriver address :   0x0000000000000000000000000000000000000802
+ * Moonbase address :    0x0000000000000000000000000000000000000801
+ * Moonriver address :   0x0000000000000000000000000000000000000801
  */
  
 interface CrowdloanRewards {
