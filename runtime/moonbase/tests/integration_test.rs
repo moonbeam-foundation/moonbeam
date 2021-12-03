57909839daadad5bba9ef8521f616526917ff033
Alan Sapede <alan.sapede@gmail.com>
2021-12-02 22:09:16 -0500

    Increases staking delays (#1030)
    * Increases staking delays
    
    * Fix comments
    
    * Update runtime rust tests to reflect MinCollatorStk changes
    
    * fix rust tests (2x longer rounds)
    
    * fix moonbeam and moonriver tests
    
    * Fix genesis chain specs (#1046)
    
    * fix genesis chain_specs so round inflation is set based on default blocks per round configured
    
    * fix build
    
    * use gorka suggestion to use associated type instead of storage item
    
    * fix units
    
    * export ParachainStakingConfigTrait from runtime to use in chainspecs
    
    * fix
    
    * revert export use direct const getter
    
    * unused import
    
    * try TS inflation genesis config
    
    * fix arithmetic
    
    * fix annual to round inflation in genesis
    
    * fix
    
    * fix
    
    * pub mod inflation
    
    * compiles still TS tests off
    
    * fix ts
    
    Co-authored-by: Stephen Shelton <steve@brewcraft.org>
    Co-authored-by: nanocryk <6422796+nanocryk@users.noreply.github.com>
    Co-authored-by: librelois <c@elo.tf>
    Co-authored-by: Amar Singh <asinghchrony@protonmail.com>

--- runtime/moonbase/tests/integration_test.rs
+++ runtime/moonbase/tests/integration_test.rs
@@ -417,7 +417,7 @@ fn reward_block_authors() {
 		.build()
 		.execute_with(|| {
 			set_parachain_inherent_data();
-			for x in 2..599 {
+			for x in 2..1199 {
 				set_author(NimbusId::from_slice(&ALICE_NIMBUS));
 				run_to_block(x);
 			}
@@ -425,7 +425,7 @@ fn reward_block_authors() {
 			assert_eq!(Balances::free_balance(AccountId::from(ALICE)), 1_000 * UNIT,);
 			assert_eq!(Balances::free_balance(AccountId::from(BOB)), 500 * UNIT,);
 			set_author(NimbusId::from_slice(&ALICE_NIMBUS));
-			run_to_block(600);
+			run_to_block(1200);
 			// rewards minted and distributed
 			assert_eq!(
 				Balances::free_balance(AccountId::from(ALICE)),
@@ -464,7 +464,7 @@ fn reward_block_authors_with_parachain_bond_reserved() {
 				root_origin(),
 				AccountId::from(CHARLIE),
 			),);
-			for x in 2..599 {
+			for x in 2..1199 {
 				set_author(NimbusId::from_slice(&ALICE_NIMBUS));
 				run_to_block(x);
 			}
@@ -473,7 +473,7 @@ fn reward_block_authors_with_parachain_bond_reserved() {
 			assert_eq!(Balances::free_balance(AccountId::from(BOB)), 500 * UNIT,);
 			assert_eq!(Balances::free_balance(AccountId::from(CHARLIE)), UNIT,);
 			set_author(NimbusId::from_slice(&ALICE_NIMBUS));
-			run_to_block(600);
+			run_to_block(1200);
 			// rewards minted and distributed
 			assert_eq!(
 				Balances::free_balance(AccountId::from(ALICE)),
