f4f731e761d7b31dba46af0db3464fbbf6c03e20
librelois <c@elo.tf>
2021-12-06 16:41:13 +0100

    Add CI workflow "Create runtime release ticket"

---
+++ tools/github/print-runtime-release-issue.ts
@@ -0,0 +1,50 @@
+import yargs from "yargs";
+
+async function main() {
+  const argv = yargs(process.argv.slice(2))
+    .usage("Usage: npm run ts-node github/generate-gh-issue-runtime-release.ts [args]")
+    .version("1.0.0")
+    .options({
+      from: {
+        type: "string",
+        describe: "previous runtime version",
+        required: true,
+      },
+      to: {
+        type: "string",
+        describe: "next runtime version",
+        required: true,
+      },
+      client: {
+        type: "string",
+        describe: "current client version",
+        required: true,
+      },
+    })
+    .demandOption(["from", "to"])
+    .help().argv;
+
+  const previousVersion = argv.from;
+  const newVersion = argv.to;
+  const lastClientVersion = argv.client;
+
+  const template = `
+  - [ ] Create a PR that increment spec version (like #1051)
+  - [ ] Get that PR approved and merged
+  - [ ] Tag master with runtime-${newVersion} and push to github
+  - [ ] Start the github action Publish Runtime Draft
+  with runtime-${previousVersion} => runtime-${newVersion}
+  - [ ] Review the generated Draft and clean a bit the messages if needed (keep it draft)
+  - [ ] Create the tracing runtime on moonbeam-runtime-overrides
+  (see https://github.com/PureStake/moonbeam-runtime-overrides/blob/master/README.md)
+  - [ ] Upgrade stagenet
+  - [ ] Upgrade alphanet
+  - [ ] Create new tracing image for partners: start the github action Publish Docker
+  with v${lastClientVersion} and master
+  - [ ] When everything is ok, publish the draft release
+  `;
+
+  console.log(template);
+}
+
+main();
