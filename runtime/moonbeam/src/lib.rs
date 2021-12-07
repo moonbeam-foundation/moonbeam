a2846430b0c2da917f9a35e5790de1b08c1d82f6
Crystalin <alan@purestake.com>
2021-12-07 13:40:07 +0000

    fmt

--- runtime/moonbeam/src/lib.rs
+++ runtime/moonbeam/src/lib.rs
@@ -1144,7 +1144,6 @@ runtime_common::impl_self_contained_call!();
 #[cfg(test)]
 mod tests {
 	use super::{currency::*, *};
-	use sp_runtime::traits::Zero;
 
 	#[test]
 	fn currency_constants_are_correct() {
@@ -1174,10 +1173,7 @@ mod tests {
 
 		// staking minimums
 		assert_eq!(MinCollatorStk::get(), Balance::from(100 * KILOGLMR));
-		assert_eq!(
-			MinCandidateStk::get(),
-			Balance::from(100 * KILOGLMR)
-		);
+		assert_eq!(MinCandidateStk::get(), Balance::from(100 * KILOGLMR));
 		assert_eq!(MinDelegatorStk::get(), Balance::from(500 * GLMR));
 
 		// crowdloan min reward
@@ -1191,10 +1187,7 @@ mod tests {
 			ProxyDepositBase::get(),
 			Balance::from(100 * GLMR + 80 * MILLIGLMR)
 		);
-		assert_eq!(
-			ProxyDepositFactor::get(),
-			Balance::from(210 * MILLIGLMR)
-		);
+		assert_eq!(ProxyDepositFactor::get(), Balance::from(210 * MILLIGLMR));
 		assert_eq!(
 			AnnouncementDepositBase::get(),
 			Balance::from(100 * GLMR + 80 * MILLIGLMR)
