a3f1626eab18f13f6fc76acd925b0f408184ed87
gorka <gorka.irazoki@gmail.com>
2021-12-13 15:16:10 +0100

    Last update to benchmark

--- pallets/asset-manager/src/benchmarks.rs
+++ pallets/asset-manager/src/benchmarks.rs
@@ -39,7 +39,7 @@ benchmarks! {
 		let metadata = T::AssetRegistrarMetadata::default();
 		let amount = 1u32.into();
 		let asset_id: T::AssetId = asset_type.clone().into();
-		Pallet::<T>::register_asset(RawOrigin::Root.into(), asset_type.clone(), metadata, amount)?;
+		Pallet::<T>::register_asset(RawOrigin::Root.into(), asset_type.clone(), metadata, amount, true)?;
 
 	}: _(RawOrigin::Root, asset_id, 1)
 	verify {
