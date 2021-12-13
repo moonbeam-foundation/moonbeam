5540615eef23928bc2710c053f665c783050bd12
Éloïs <c@elo.tf>
2021-12-13 16:38:24 +0100

    Update scripts/toml-sort.sh
    Co-authored-by: Joshy Orndorff <JoshOrndorff@users.noreply.github.com>

--- scripts/toml-sort.sh
+++ scripts/toml-sort.sh
@@ -6,7 +6,7 @@
 # ./scripts/toml-sort.sh --check
 # to only check the formatting.
 
-if ! type "toml-sort2" > /dev/null; then
+if ! type "toml-sort" > /dev/null; then
   echo "Please install toml-sort with command 'cargo install --git https://github.com/PureStake/toml_sort'"
 else
   find . -name "Cargo.toml" -not -path "*/target/*" -exec toml-sort {} $@ \;
