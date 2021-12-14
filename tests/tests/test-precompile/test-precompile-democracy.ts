0eb7c774e381d788be757670ffae66d3fae033f8
estienne.antoine@gmail.com <estienne.antoine@gmail.com>
2021-12-14 15:47:35 +0100

    finish tests using iFace

--- tests/tests/test-precompile/test-precompile-democracy.ts
+++ tests/tests/test-precompile/test-precompile-democracy.ts
@@ -10,14 +10,23 @@ import {
   PROPOSAL_AMOUNT,
   VOTE_AMOUNT,
   ZERO_ADDRESS,
-} from "../util/constants";
-import { describeDevMoonbeam, DevTestContext } from "../util/setup-dev-tests";
+  GAS_PRICE,
+} from "../../util/constants";
+import { describeDevMoonbeam, DevTestContext } from "../../util/setup-dev-tests";
 import { ApiTypes, SubmittableExtrinsic } from "@polkadot/api/types";
 
 import { blake2AsHex } from "@polkadot/util-crypto";
-import { createBlockWithExtrinsic } from "../util/substrate-rpc";
-import { callPrecompile, sendPrecompileTx } from "../util/transactions";
+import { createBlockWithExtrinsic } from "../../util/substrate-rpc";
+import {
+  callPrecompile,
+  createContract,
+  createTransaction,
+  sendPrecompileTx,
+} from "../../util/transactions";
 import { numberToHex } from "@polkadot/util";
+import { getCompiled } from "../../util/contracts";
+import { ethers } from "ethers";
+import { Interface } from "ethers/lib/utils";
 
 const ADDRESS_DEMO_PRECOMPILE = "0x0000000000000000000000000000000000000803";
 // Function selector reference
@@ -43,82 +52,108 @@ const SELECTORS = {
   note_preimage: "200881f5",
 };
 
+export const deployAndInterfaceContract = async (
+  context: DevTestContext,
+  contractName: string
+): Promise<Interface> => {
+  // deploy contract
+  const { rawTx } = await createContract(context.web3, contractName);
+  await context.createBlock({ transactions: [rawTx] });
+  // Instantiate interface
+  const contractData = await getCompiled(contractName);
+  return new ethers.utils.Interface(contractData.contract.abi);
+};
+
 export const notePreimagePrecompile = async <
   Call extends SubmittableExtrinsic<ApiType>,
   ApiType extends ApiTypes
 >(
   context: DevTestContext,
+  iFace: Interface,
   proposal: Call
 ): Promise<`0x${string}`> => {
-  const encodedProposal = proposal.method.toHex()
-  console.log("encodedProposal", encodedProposal);
-  // await context.polkadotApi.tx.democracy.notePreimage(encodedProposal).signAndSend(account);
-  await sendPrecompileTx(
-    context,
-    ADDRESS_DEMO_PRECOMPILE,
-    SELECTORS,
-    GENESIS_ACCOUNT,
-    GENESIS_ACCOUNT_PRIVATE_KEY,
+  const encodedProposal = proposal.method.toHex();
+
+  const data = iFace.encodeFunctionData(
+    // action
     "note_preimage",
     [encodedProposal]
   );
-  await context.createBlock();
+
+  const tx = await createTransaction(context.web3, {
+    from: GENESIS_ACCOUNT,
+    privateKey: GENESIS_ACCOUNT_PRIVATE_KEY,
+    value: "0x0",
+    gas: "0x200000",
+    gasPrice: GAS_PRICE,
+    to: ADDRESS_DEMO_PRECOMPILE,
+    data,
+  });
+
+  await context.createBlock({
+    transactions: [tx],
+  });
   // return encodedHash
   return blake2AsHex(encodedProposal);
 };
 
 describeDevMoonbeam("Democracy - genesis and preimage", (context) => {
   let genesisAccount: KeyringPair;
+  let iFace: Interface;
 
   before("Setup genesis account for substrate", async () => {
     const keyring = new Keyring({ type: "ethereum" });
     genesisAccount = await keyring.addFromUri(GENESIS_ACCOUNT_PRIVATE_KEY, null, "ethereum");
+    iFace = await deployAndInterfaceContract(context, "Democracy");
   });
-  //   it("should check initial state - no referendum", async function () {
-  //     // referendumCount
-  //     const referendumCount = await context.polkadotApi.query.democracy.referendumCount();
-  //     expect(referendumCount.toHuman()).to.equal("0");
-  //   });
+  it("should check initial state - no referendum", async function () {
+    // referendumCount
+    const referendumCount = await context.polkadotApi.query.democracy.referendumCount();
+    expect(referendumCount.toHuman()).to.equal("0");
+  });
   it("should check initial state - 0x0 ParachainBondAccount", async function () {
     // referendumCount
     const parachainBondInfo = await context.polkadotApi.query.parachainStaking.parachainBondInfo();
     expect(parachainBondInfo.toHuman()["account"]).to.equal(ZERO_ADDRESS);
   });
-    it.only("notePreimage", async function () {
-      // notePreimage
+  it("notePreimage", async function () {
+    // notePreimage
     const encodedHash = await notePreimagePrecompile(
       context,
+      iFace,
       context.polkadotApi.tx.parachainStaking.setParachainBondAccount(GENESIS_ACCOUNT)
     );
-      const preimageStatus = await context.polkadotApi.query.democracy.preimages(encodedHash);
-      console.log("preimageStatus.toHuman()",preimageStatus.toHuman(),(await context.polkadotApi.query.democracy.preimages(null)).toHuman())
-      expect((preimageStatus.toHuman() as any).Available.provider).to.equal(GENESIS_ACCOUNT);
-      expect((preimageStatus.toHuman() as any).Available.deposit).to.equal("2.2000 mUNIT");
-    });
+
+    const preimageStatus = await context.polkadotApi.query.democracy.preimages(encodedHash);
+    expect(
+      preimageStatus.unwrap().isAvailable && preimageStatus.unwrap().asAvailable.provider.toString()
+    ).to.equal(GENESIS_ACCOUNT);
+    expect(
+      preimageStatus.unwrap().isAvailable && preimageStatus.unwrap().asAvailable.deposit.toString()
+    ).to.equal("2200000000000000");
+  });
 });
 
 describeDevMoonbeam("Democracy - propose", (context) => {
   let genesisAccount: KeyringPair;
   let encodedHash: `0x${string}`;
+  let iFace: Interface;
 
   before("Setup genesis account for substrate", async () => {
     const keyring = new Keyring({ type: "ethereum" });
     genesisAccount = await keyring.addFromUri(GENESIS_ACCOUNT_PRIVATE_KEY, null, "ethereum");
+    iFace = await deployAndInterfaceContract(context, "Democracy");
 
     // encodedHash
     encodedHash = await notePreimagePrecompile(
       context,
+      iFace,
       context.polkadotApi.tx.parachainStaking.setParachainBondAccount(GENESIS_ACCOUNT)
-      //genesisAccount
     );
   });
   it("propose", async function () {
     // propose
-    // await context.polkadotApi.tx.democracy
-    //   .propose(encodedHash, PROPOSAL_AMOUNT)
-    //   .signAndSend(genesisAccount);
-    // await context.createBlock();
-    const block = await sendPrecompileTx(
+    await sendPrecompileTx(
       context,
       ADDRESS_DEMO_PRECOMPILE,
       SELECTORS,
@@ -127,7 +162,6 @@ describeDevMoonbeam("Democracy - propose", (context) => {
       "propose",
       [encodedHash, numberToHex(Number(PROPOSAL_AMOUNT))]
     );
-    // console.log(block);
 
     // referendumCount
     const referendumCount = await context.polkadotApi.query.democracy.referendumCount();
@@ -145,40 +179,33 @@ describeDevMoonbeam("Democracy - propose", (context) => {
     expect((publicProps.toHuman() as any)[0][2]).to.equal(GENESIS_ACCOUNT);
     // depositOf
     const depositOf = await context.polkadotApi.query.democracy.depositOf(0);
-    expect((depositOf.toHuman() as any)[1]).to.equal("1.0000 kUNIT");
+    expect((depositOf.toHuman() as any)[1]).to.equal("1,000,000,000,000,000,000,000");
   });
 });
 
 describeDevMoonbeam("Democracy - second proposal", (context) => {
   let genesisAccount: KeyringPair, alith: KeyringPair;
-  let encodedHash:  `0x${string}`;
+  let encodedHash: `0x${string}`;
   let launchPeriod;
+  let iFace: Interface;
 
   before("Setup genesis account for substrate", async () => {
     const keyring = new Keyring({ type: "ethereum" });
     genesisAccount = await keyring.addFromUri(GENESIS_ACCOUNT_PRIVATE_KEY, null, "ethereum");
     alith = await keyring.addFromUri(ALITH_PRIV_KEY, null, "ethereum");
+    iFace = await deployAndInterfaceContract(context, "Democracy");
 
     //launchPeriod
     launchPeriod = await context.polkadotApi.consts.democracy.launchPeriod;
 
     // notePreimage
-    // encodedHash = await notePreimage(
-    //   context,
-    //   context.polkadotApi.tx.parachainStaking.setParachainBondAccount(GENESIS_ACCOUNT),
-    //   genesisAccount
-    // );
-    // encodedHash
     encodedHash = await notePreimagePrecompile(
       context,
+      iFace,
       context.polkadotApi.tx.parachainStaking.setParachainBondAccount(GENESIS_ACCOUNT)
     );
 
     // propose
-    // await context.polkadotApi.tx.democracy
-    //   .propose(encodedHash, PROPOSAL_AMOUNT)
-    //   .signAndSend(genesisAccount);
-    // await context.createBlock();
     await sendPrecompileTx(
       context,
       ADDRESS_DEMO_PRECOMPILE,
@@ -190,8 +217,6 @@ describeDevMoonbeam("Democracy - second proposal", (context) => {
     );
 
     // second
-    // await context.polkadotApi.tx.democracy.second(0, 1000).signAndSend(alith);
-    // await context.createBlock();
     await sendPrecompileTx(
       context,
       ADDRESS_DEMO_PRECOMPILE,
@@ -213,7 +238,7 @@ describeDevMoonbeam("Democracy - second proposal", (context) => {
 
     // depositOf
     const depositOf = await context.polkadotApi.query.democracy.depositOf(0);
-    expect((depositOf.toHuman() as any)[1]).to.equal("1.0000 kUNIT");
+    expect((depositOf.toHuman() as any)[1]).to.equal("1,000,000,000,000,000,000,000");
     expect((depositOf.toHuman() as any)[0][1]).to.equal(ALITH);
   });
   it("check launch period", async function () {
@@ -243,13 +268,16 @@ describeDevMoonbeam("Democracy - second proposal", (context) => {
 
 describeDevMoonbeam("Democracy - vote on referendum", (context) => {
   let genesisAccount: KeyringPair, alith: KeyringPair;
-  let encodedHash:  `0x${string}`;
+  let encodedHash: `0x${string}`;
   let enactmentPeriod, votingPeriod;
+  let iFace: Interface;
 
   before("Setup genesis account for substrate", async () => {
     const keyring = new Keyring({ type: "ethereum" });
     genesisAccount = await keyring.addFromUri(GENESIS_ACCOUNT_PRIVATE_KEY, null, "ethereum");
     alith = await keyring.addFromUri(ALITH_PRIV_KEY, null, "ethereum");
+
+    iFace = await deployAndInterfaceContract(context, "Democracy");
 
     // enactmentPeriod
     enactmentPeriod = await context.polkadotApi.consts.democracy.enactmentPeriod;
@@ -259,6 +287,7 @@ describeDevMoonbeam("Democracy - vote on referendum", (context) => {
     // encodedHash
     encodedHash = await notePreimagePrecompile(
       context,
+      iFace,
       context.polkadotApi.tx.parachainStaking.setParachainBondAccount(GENESIS_ACCOUNT)
     );
 
@@ -272,8 +301,7 @@ describeDevMoonbeam("Democracy - vote on referendum", (context) => {
       "propose",
       [encodedHash, numberToHex(Number(PROPOSAL_AMOUNT))]
     );
-    console.log("numberToHex(0)", numberToHex(0));
-    console.log("numberToHex(1)", numberToHex(1));
+
     // second
     await sendPrecompileTx(
       context,
@@ -301,12 +329,6 @@ describeDevMoonbeam("Democracy - vote on referendum", (context) => {
       await context.createBlock();
     }
     // vote
-    // await context.polkadotApi.tx.democracy
-    //   .vote(0, {
-    //     Standard: { balance: VOTE_AMOUNT, vote: { aye: true, conviction: 1 } },
-    //   })
-    //   .signAndSend(alith);
-    // await context.createBlock();
     await sendPrecompileTx(
       context,
       ADDRESS_DEMO_PRECOMPILE,
@@ -331,7 +353,6 @@ describeDevMoonbeam("Democracy - vote on referendum", (context) => {
     // let votePeriod + enactmentPeriod elapse to turn the proposal into a referendum
     for (let i = 0; i < Number(votingPeriod) + Number(enactmentPeriod) + 10; i++) {
       await context.createBlock();
-      console.log(i);
     }
     let parachainBondInfo = await context.polkadotApi.query.parachainStaking.parachainBondInfo();
     expect(parachainBondInfo.toHuman()["account"]).to.equal(GENESIS_ACCOUNT);
@@ -345,12 +366,14 @@ describeDevMoonbeam("Democracy - forget notePreimage", (context) => {
   let genesisAccount: KeyringPair, alith: KeyringPair;
   let encodedHash: string;
   let enactmentPeriod, votingPeriod;
+  let iFace: Interface;
 
   before("Setup genesis account for substrate", async () => {
     const keyring = new Keyring({ type: "ethereum" });
     genesisAccount = await keyring.addFromUri(GENESIS_ACCOUNT_PRIVATE_KEY, null, "ethereum");
     alith = await keyring.addFromUri(ALITH_PRIV_KEY, null, "ethereum");
 
+    iFace = await deployAndInterfaceContract(context, "Democracy");
     // notePreimage
     // compute proposal hash but don't submit it
     const encodedProposal =
@@ -368,6 +391,7 @@ describeDevMoonbeam("Democracy - forget notePreimage", (context) => {
       genesisAccount,
       context.polkadotApi.tx.democracy.propose(encodedHash, PROPOSAL_AMOUNT)
     );
+    expect(eventsPropose[2].toHuman().method).to.eq("Proposed");
     expect(eventsPropose[5].toHuman().method).to.eq("ExtrinsicSuccess");
     await context.createBlock();
     // second
@@ -376,7 +400,8 @@ describeDevMoonbeam("Democracy - forget notePreimage", (context) => {
       alith,
       context.polkadotApi.tx.democracy.second(0, 1000)
     );
-    expect(eventsSecond[2].toHuman().method).to.eq("ExtrinsicSuccess");
+    expect(eventsSecond[2].toHuman().method).to.eq("Seconded");
+    expect(eventsSecond[5].toHuman().method).to.eq("ExtrinsicSuccess");
     // let Launchperiod elapse to turn the proposal into a referendum
     // launchPeriod minus the 3 blocks that already elapsed
     for (let i = 0; i < 7200; i++) {
@@ -395,13 +420,19 @@ describeDevMoonbeam("Democracy - forget notePreimage", (context) => {
         Standard: { balance: VOTE_AMOUNT, vote: { aye: true, conviction: 1 } },
       })
     );
-    expect(eventsVote[1].toHuman().method).to.eq("ExtrinsicSuccess");
+
+    expect(eventsVote[1].toHuman().method).to.eq("Voted");
+    expect(eventsVote[4].toHuman().method).to.eq("ExtrinsicSuccess");
 
     // referendumInfoOf
     const referendumInfoOf = await context.polkadotApi.query.democracy.referendumInfoOf(0);
     expect((referendumInfoOf.toHuman() as any).Ongoing.proposalHash).to.equal(encodedHash);
-    expect((referendumInfoOf.toHuman() as any).Ongoing.tally.ayes).to.equal("10.0000 UNIT");
-    expect((referendumInfoOf.toHuman() as any).Ongoing.tally.turnout).to.equal("10.0000 UNIT");
+    expect((referendumInfoOf.toHuman() as any).Ongoing.tally.ayes).to.equal(
+      "10,000,000,000,000,000,000"
+    );
+    expect((referendumInfoOf.toHuman() as any).Ongoing.tally.turnout).to.equal(
+      "10,000,000,000,000,000,000"
+    );
 
     // let votePeriod + enactmentPeriod elapse to turn the proposal into a referendum
     for (let i = 0; i < Number(votingPeriod) + Number(enactmentPeriod); i++) {
