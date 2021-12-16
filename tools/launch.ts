13ba2386f74af129eb1f9fdc3f0615613e3a6789
librelois <c@elo.tf>
2021-12-16 16:38:03 +0100

    Increase client version to v0.17.0

--- tools/launch.ts
+++ tools/launch.ts
@@ -108,6 +108,11 @@ const parachains: { [name: string]: ParachainConfig } = {
     chain: "moonbase-local",
     docker: "purestake/moonbeam:v0.16.0",
   },
+  "moonbase-0.17.0": {
+    relay: "rococo-9106",
+    chain: "moonbase-local",
+    docker: "purestake/moonbeam:v0.17.0",
+  },
   local: {
     relay: "rococo-9111",
     chain: "moonbase-local",
@@ -159,6 +164,10 @@ const relays: { [name: string]: NetworkConfig } = {
   },
   "rococo-9130": {
     docker: "purestake/moonbase-relay-testnet:sha-45c0f1f3",
+    chain: "rococo-local",
+  },
+  "rococo-9106": {
+    docker: "purestake/moonbase-relay-testnet:sha-415ce0db",
     chain: "rococo-local",
   },
   "westend-9030": {
