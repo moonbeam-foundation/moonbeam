99a3f8708c8120321032fb076a4c7c2bc2fff058
nanocryk <nanocryk@users.noreply.github.com>
2021-12-22 10:25:29 +0000

    deploy: 69c52eaa6857869bae995e008786a7d69dedfdf3

---
+++ implementors/sp_runtime_interface/pass_by/trait.PassBy.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["evm_tracing_events"] = [{"text":"impl PassBy for <a class=\"struct\" href=\"evm_tracing_events/struct.StepEventFilter.html\" title=\"struct evm_tracing_events::StepEventFilter\">StepEventFilter</a>","synthetic":false,"types":["evm_tracing_events::StepEventFilter"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
