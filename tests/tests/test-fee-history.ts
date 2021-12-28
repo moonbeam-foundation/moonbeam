418754f4ca8901cc1363abadacc7e395c3ced9ab
tgmichel <telmo@purestake.com>
2021-12-28 15:25:56 +0100

    Prettier

--- tests/tests/test-fee-history.ts
+++ tests/tests/test-fee-history.ts
@@ -19,22 +19,23 @@ describeDevMoonbeamAllEthTxTypes("Fee History", (context) => {
   }
 
   function get_percentile(percentile, array) {
-    array.sort(function (a, b) { return a - b; });
-    let index = ((percentile/100) * (array.length))-1;
+    array.sort(function (a, b) {
+      return a - b;
+    });
+    let index = (percentile / 100) * array.length - 1;
     if (Math.floor(index) == index) {
       return array[index];
-    }
-    else {
-      return Math.ceil((array[Math.floor(index)] + array[Math.ceil(index)])/2);
+    } else {
+      return Math.ceil((array[Math.floor(index)] + array[Math.ceil(index)]) / 2);
     }
   }
 
   async function createBlocks(block_count, reward_percentiles, priority_fees) {
     const contractData = await getCompiled("TestContract");
     let nonce = await context.web3.eth.getTransactionCount(GENESIS_ACCOUNT);
-    for(var b = 0; b < block_count; b++) {
-      for(var p = 0; p < priority_fees.length; p++) {
-                await sendTransaction(context, {
+    for (var b = 0; b < block_count; b++) {
+      for (var p = 0; p < priority_fees.length; p++) {
+        await sendTransaction(context, {
           from: GENESIS_ACCOUNT,
           data: contractData.byteCode,
           value: "0x00",
@@ -43,7 +44,7 @@ describeDevMoonbeamAllEthTxTypes("Fee History", (context) => {
           accessList: [],
           nonce: nonce,
           gasLimit: "0x100000",
-          chainId: 1281
+          chainId: 1281,
         });
         nonce++;
       }
@@ -54,14 +55,12 @@ describeDevMoonbeamAllEthTxTypes("Fee History", (context) => {
   it("result length should match spec", async function () {
     this.timeout(100000);
     let block_count = 2;
-    let reward_percentiles = [20,50,70];
+    let reward_percentiles = [20, 50, 70];
     let priority_fees = [1, 2, 3];
     await createBlocks(block_count, reward_percentiles, priority_fees);
-    let result = (await customWeb3Request(
-      context.web3,
-      "eth_feeHistory",
-      ["0x2", "latest", reward_percentiles]
-    )).result;
+    let result = (
+      await customWeb3Request(context.web3, "eth_feeHistory", ["0x2", "latest", reward_percentiles])
+    ).result;
 
     // baseFeePerGas is always the requested block range + 1 (the next derived base fee).
     expect(result.baseFeePerGas.length).to.be.eq(block_count + 1);
@@ -70,7 +69,7 @@ describeDevMoonbeamAllEthTxTypes("Fee History", (context) => {
     // two-dimensional reward list for the requested block range.
     expect(result.reward.length).to.be.eq(block_count);
     // each block has a reward list which's size is the requested percentile list.
-    for(let i = 0; i < block_count; i++) {
+    for (let i = 0; i < block_count; i++) {
       expect(result.reward[i].length).to.be.eq(reward_percentiles.length);
     }
   });
@@ -78,27 +77,23 @@ describeDevMoonbeamAllEthTxTypes("Fee History", (context) => {
   it("should calculate percentiles", async function () {
     this.timeout(100000);
     let block_count = 11;
-    let reward_percentiles = [20,50,70,85,100];
+    let reward_percentiles = [20, 50, 70, 85, 100];
     let priority_fees = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     await createBlocks(block_count, reward_percentiles, priority_fees);
-    let result = (await customWeb3Request(
-      context.web3,
-      "eth_feeHistory",
-      ["0xA", "latest", reward_percentiles]
-    )).result;
-    
+    let result = (
+      await customWeb3Request(context.web3, "eth_feeHistory", ["0xA", "latest", reward_percentiles])
+    ).result;
+
     // Calculate the percentiles in javascript.
     let local_rewards = [];
-    for(let i = 0; i < reward_percentiles.length; i++) {
+    for (let i = 0; i < reward_percentiles.length; i++) {
       local_rewards.push(get_percentile(reward_percentiles[i], priority_fees));
     }
     // Compare the rpc result with the javascript percentiles.
-    for(let i = 0; i < result.reward.length; i++) {
+    for (let i = 0; i < result.reward.length; i++) {
       expect(result.reward[i].length).to.be.eq(local_rewards.length);
-      for(let j = 0; j < local_rewards.length; j++) {
-        expect(context.web3.utils.hexToNumber(
-          result.reward[i][j]
-        )).to.be.eq(local_rewards[j]);
+      for (let j = 0; j < local_rewards.length; j++) {
+        expect(context.web3.utils.hexToNumber(result.reward[i][j])).to.be.eq(local_rewards[j]);
       }
     }
   });
