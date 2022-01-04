26ccd4dfa74d282c180a04d8015aeb45203baa53
girazoki <gorka.irazoki@gmail.com>
2022-01-04 09:32:15 +0100

    Girazoki author mapping precompile (#1129)
    * Initial draft for author-mapping precompiles
    
    * Add it to moonbase and rust integration tests
    
    * Rypescript tests
    
    * Prettier
    
    * Toml sort
    
    * Prettier
    
    * editorconfig
    
    * Dead code removal
    
    * Update Cargo lock

---
+++ precompiles/author-mapping/src/mock.rs
@@ -0,0 +1,340 @@
+// Copyright 2019-2021 PureStake Inc.
+// This file is part of Moonbeam.
+
+// Moonbeam is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Moonbeam is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Moonbeam.  If not, see <http://www.gnu.org/licenses/>.
+
+//! Test utilities
+use super::*;
+use codec::{Decode, Encode, MaxEncodedLen};
+use frame_support::{
+	construct_runtime, parameter_types,
+	traits::{EqualPrivilegeOnly, Everything},
+};
+use frame_system::EnsureRoot;
+use pallet_evm::{
+	AddressMapping, EnsureAddressNever, EnsureAddressRoot, PrecompileSet, SubstrateBlockHashMapping,
+};
+use scale_info::TypeInfo;
+use serde::{Deserialize, Serialize};
+use sp_core::{H160, H256};
+use sp_io;
+use sp_runtime::{
+	testing::Header,
+	traits::{BlakeTwo256, IdentityLookup},
+};
+
+pub type AccountId = TestAccount;
+pub type Balance = u128;
+pub type BlockNumber = u64;
+
+type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Runtime>;
+type Block = frame_system::mocking::MockBlock<Runtime>;
+
+pub const PRECOMPILE_ADDRESS: u64 = 1;
+
+#[derive(
+	Eq,
+	PartialEq,
+	Ord,
+	PartialOrd,
+	Clone,
+	Encode,
+	Decode,
+	Debug,
+	MaxEncodedLen,
+	Serialize,
+	Deserialize,
+	derive_more::Display,
+	TypeInfo,
+)]
+pub enum TestAccount {
+	Alice,
+	Bob,
+	Charlie,
+	Bogus,
+	Precompile,
+}
+
+impl Default for TestAccount {
+	fn default() -> Self {
+		Self::Bogus
+	}
+}
+
+impl AddressMapping<TestAccount> for TestAccount {
+	fn into_account_id(h160_account: H160) -> TestAccount {
+		match h160_account {
+			a if a == H160::repeat_byte(0xAA) => Self::Alice,
+			a if a == H160::repeat_byte(0xBB) => Self::Bob,
+			a if a == H160::repeat_byte(0xCC) => Self::Charlie,
+			a if a == H160::from_low_u64_be(PRECOMPILE_ADDRESS) => Self::Precompile,
+			_ => Self::Bogus,
+		}
+	}
+}
+
+impl From<H160> for TestAccount {
+	fn from(x: H160) -> TestAccount {
+		TestAccount::into_account_id(x)
+	}
+}
+
+impl From<TestAccount> for H160 {
+	fn from(value: TestAccount) -> H160 {
+		match value {
+			TestAccount::Alice => H160::repeat_byte(0xAA),
+			TestAccount::Bob => H160::repeat_byte(0xBB),
+			TestAccount::Charlie => H160::repeat_byte(0xCC),
+			TestAccount::Precompile => H160::from_low_u64_be(PRECOMPILE_ADDRESS),
+			TestAccount::Bogus => Default::default(),
+		}
+	}
+}
+
+// Configure a mock runtime to test the pallet.
+construct_runtime!(
+	pub enum Runtime where
+		Block = Block,
+		NodeBlock = Block,
+		UncheckedExtrinsic = UncheckedExtrinsic,
+	{
+		System: frame_system::{Pallet, Call, Config, Storage, Event<T>},
+		Balances: pallet_balances::{Pallet, Call, Storage, Config<T>, Event<T>},
+		Evm: pallet_evm::{Pallet, Config, Call, Storage, Event<T>},
+		Timestamp: pallet_timestamp::{Pallet, Call, Storage, Inherent},
+		AuthorMapping: pallet_author_mapping::{Pallet, Storage, Config<T>, Event<T>, Call},
+		Scheduler: pallet_scheduler::{Pallet, Call, Storage, Config, Event<T>},
+	}
+);
+
+parameter_types! {
+	pub const BlockHashCount: u64 = 250;
+	pub const SS58Prefix: u8 = 42;
+}
+impl frame_system::Config for Runtime {
+	type BaseCallFilter = Everything;
+	type DbWeight = ();
+	type Origin = Origin;
+	type Index = u64;
+	type BlockNumber = BlockNumber;
+	type Call = Call;
+	type Hash = H256;
+	type Hashing = BlakeTwo256;
+	type AccountId = TestAccount;
+	type Lookup = IdentityLookup<Self::AccountId>;
+	type Header = Header;
+	type Event = Event;
+	type BlockHashCount = BlockHashCount;
+	type Version = ();
+	type PalletInfo = PalletInfo;
+	type AccountData = pallet_balances::AccountData<Balance>;
+	type OnNewAccount = ();
+	type OnKilledAccount = ();
+	type SystemWeightInfo = ();
+	type BlockWeights = ();
+	type BlockLength = ();
+	type SS58Prefix = SS58Prefix;
+	type OnSetCode = ();
+}
+parameter_types! {
+	pub const ExistentialDeposit: u128 = 0;
+}
+impl pallet_balances::Config for Runtime {
+	type MaxReserves = ();
+	type ReserveIdentifier = ();
+	type MaxLocks = ();
+	type Balance = Balance;
+	type Event = Event;
+	type DustRemoval = ();
+	type ExistentialDeposit = ExistentialDeposit;
+	type AccountStore = System;
+	type WeightInfo = ();
+}
+
+parameter_types! {
+	pub const PrecompilesValue: Precompiles<Runtime> = Precompiles(PhantomData);
+}
+
+impl pallet_evm::Config for Runtime {
+	type FeeCalculator = ();
+	type GasWeightMapping = ();
+	type CallOrigin = EnsureAddressRoot<TestAccount>;
+	type WithdrawOrigin = EnsureAddressNever<TestAccount>;
+	type AddressMapping = TestAccount;
+	type Currency = Balances;
+	type Event = Event;
+	type Runner = pallet_evm::runner::stack::Runner<Self>;
+	type PrecompilesType = Precompiles<Self>;
+	type PrecompilesValue = PrecompilesValue;
+	type ChainId = ();
+	type OnChargeTransaction = ();
+	type BlockGasLimit = ();
+	type BlockHashMapping = SubstrateBlockHashMapping<Self>;
+	type FindAuthor = ();
+}
+
+parameter_types! {
+	pub const MinimumPeriod: u64 = 5;
+}
+impl pallet_timestamp::Config for Runtime {
+	type Moment = u64;
+	type OnTimestampSet = ();
+	type MinimumPeriod = MinimumPeriod;
+	type WeightInfo = ();
+}
+
+parameter_types! {
+	pub const DepositAmount: Balance = 10;
+}
+
+impl pallet_author_mapping::Config for Runtime {
+	type Event = Event;
+	type DepositCurrency = Balances;
+	type DepositAmount = DepositAmount;
+	type WeightInfo = ();
+}
+
+impl pallet_scheduler::Config for Runtime {
+	type Event = Event;
+	type Origin = Origin;
+	type PalletsOrigin = OriginCaller;
+	type Call = Call;
+	type MaximumWeight = ();
+	type ScheduleOrigin = EnsureRoot<TestAccount>;
+	type MaxScheduledPerBlock = ();
+	type WeightInfo = ();
+	type OriginPrivilegeCmp = EqualPrivilegeOnly; // TODO : Simplest type, maybe there is better ?
+}
+
+#[derive(Default)]
+pub struct Precompiles<R>(PhantomData<R>);
+
+impl<R> PrecompileSet for Precompiles<R>
+where
+	AuthorMappingWrapper<R>: Precompile,
+{
+	fn execute(
+		&self,
+		address: H160,
+		input: &[u8],
+		target_gas: Option<u64>,
+		context: &Context,
+		is_static: bool,
+	) -> Option<EvmResult<PrecompileOutput>> {
+		match address {
+			a if a == hash(PRECOMPILE_ADDRESS) => Some(AuthorMappingWrapper::<R>::execute(
+				input, target_gas, context, is_static,
+			)),
+			_ => None,
+		}
+	}
+
+	fn is_precompile(&self, address: H160) -> bool {
+		address == hash(PRECOMPILE_ADDRESS)
+	}
+}
+
+fn hash(a: u64) -> H160 {
+	H160::from_low_u64_be(a)
+}
+
+pub(crate) struct ExtBuilder {
+	// endowed accounts with balances
+	balances: Vec<(AccountId, Balance)>,
+}
+
+impl Default for ExtBuilder {
+	fn default() -> ExtBuilder {
+		ExtBuilder { balances: vec![] }
+	}
+}
+
+impl ExtBuilder {
+	pub(crate) fn with_balances(mut self, balances: Vec<(AccountId, Balance)>) -> Self {
+		self.balances = balances;
+		self
+	}
+
+	pub(crate) fn build(self) -> sp_io::TestExternalities {
+		let mut t = frame_system::GenesisConfig::default()
+			.build_storage::<Runtime>()
+			.expect("Frame system builds valid default genesis config");
+
+		pallet_balances::GenesisConfig::<Runtime> {
+			balances: self.balances,
+		}
+		.assimilate_storage(&mut t)
+		.expect("Pallet balances storage can be assimilated");
+
+		let mut ext = sp_io::TestExternalities::new(t);
+		ext.execute_with(|| System::set_block_number(1));
+		ext
+	}
+}
+
+pub(crate) fn events() -> Vec<Event> {
+	System::events()
+		.into_iter()
+		.map(|r| r.event)
+		.collect::<Vec<_>>()
+}
+
+// Helper function to give a simple evm context suitable for tests.
+// We can remove this once https://github.com/rust-blockchain/evm/pull/35
+// is in our dependency graph.
+pub fn evm_test_context() -> fp_evm::Context {
+	fp_evm::Context {
+		address: Default::default(),
+		caller: Default::default(),
+		apparent_value: From::from(0),
+	}
+}
+
+#[test]
+fn test_account_id_mapping_works() {
+	// Bidirectional conversions for normal accounts
+	assert_eq!(
+		TestAccount::Alice,
+		TestAccount::into_account_id(TestAccount::Alice.into())
+	);
+	assert_eq!(
+		TestAccount::Bob,
+		TestAccount::into_account_id(TestAccount::Bob.into())
+	);
+	assert_eq!(
+		TestAccount::Charlie,
+		TestAccount::into_account_id(TestAccount::Charlie.into())
+	);
+
+	// Bidirectional conversion between bogus and default H160
+	assert_eq!(
+		TestAccount::Bogus,
+		TestAccount::into_account_id(H160::default())
+	);
+	assert_eq!(H160::default(), TestAccount::Bogus.into());
+
+	// All other H160s map to bogus
+	assert_eq!(
+		TestAccount::Bogus,
+		TestAccount::into_account_id(H160::zero())
+	);
+	assert_eq!(
+		TestAccount::Bogus,
+		TestAccount::into_account_id(H160::repeat_byte(0x12))
+	);
+	assert_eq!(
+		TestAccount::Bogus,
+		TestAccount::into_account_id(H160::repeat_byte(0xFF))
+	);
+}
