3dfa0db1376d401c57eb7d9b560ce4344d91c170
librelois <c@elo.tf>
2022-01-04 20:15:37 +0100

    git branch must be get in a different way in github action context

--- scripts/build-last-tracing-runtime.sh
+++ scripts/build-last-tracing-runtime.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-CURRENT_GIT_BRANCH="$(git symbolic-ref HEAD 2>/dev/null)"
-CURRENT_GIT_BRANCH=${CURRENT_GIT_BRANCH##refs/heads/}
-
-echo "CURRENT_GIT_BRANCH=$CURRENT_GIT_BRANCH"
+LOCAL_GIT_BRANCH="$(git symbolic-ref HEAD 2>/dev/null)"
 
 rm -rf build/{moonbeam-runtime-overrides,wasm}
 mkdir -p build/wasm
 git clone --depth 1 -b master-without-wasm https://github.com/PureStake/moonbeam-runtime-overrides build/moonbeam-runtime-overrides
 
 cd build/moonbeam-runtime-overrides
-./scripts/import-tracing-runtime.sh local $CURRENT_GIT_BRANCH
+./scripts/import-tracing-runtime.sh local ${1:-$LOCAL_GIT_BRANCH##refs/heads/}
 cd tracing/local && cargo update -p evm && cd ../..
 ./scripts/build-tracing-runtime.sh local moonbase
 mv wasm/moonbase-runtime-local-substitute-tracing.wasm ../wasm/moonbase-runtime-local-substitute-tracing.wasm
