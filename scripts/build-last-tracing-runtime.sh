f051144a0299cff5cdd5fcdfbafd69f3b4016477
librelois <c@elo.tf>
2022-01-05 14:34:22 +0100

    add script to help running tracing tests locally

--- scripts/build-last-tracing-runtime.sh
+++ scripts/build-last-tracing-runtime.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 LOCAL_GIT_BRANCH="$(git symbolic-ref HEAD 2>/dev/null)"
+LOCAL_GIT_BRANCH=${LOCAL_GIT_BRANCH##refs/heads/}
+
+echo ${1:-"$LOCAL_GIT_BRANCH"}
 
 rm -rf build/{moonbeam-runtime-overrides,wasm}
 mkdir -p build/wasm
 git clone --depth 1 -b master-without-wasm https://github.com/PureStake/moonbeam-runtime-overrides build/moonbeam-runtime-overrides
 
 cd build/moonbeam-runtime-overrides
-./scripts/import-tracing-runtime.sh local ${1:-$LOCAL_GIT_BRANCH##refs/heads/}
+./scripts/import-tracing-runtime.sh local ${1:-"$LOCAL_GIT_BRANCH"}
 cd tracing/local && cargo update -p evm && cd ../..
 ./scripts/build-tracing-runtime.sh local moonbase
 mv wasm/moonbase-runtime-local-substitute-tracing.wasm ../wasm/moonbase-runtime-local-substitute-tracing.wasm
