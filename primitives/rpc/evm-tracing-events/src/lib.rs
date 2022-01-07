157bb90842de547036fe89610b09e6f7d9a93efc
tgmichel <telmo@purestake.com>
2022-01-07 18:14:54 +0100

    Update copyright 2022 (#1146)
    * Update copyright 2022
    
    * Update copyright 2022

--- primitives/rpc/evm-tracing-events/src/lib.rs
+++ primitives/rpc/evm-tracing-events/src/lib.rs
@@ -1,4 +1,4 @@
-// Copyright 2019-2021 PureStake Inc.
+// Copyright 2019-2022 PureStake Inc.
 // This file is part of Moonbeam.
 
 // Moonbeam is free software: you can redistribute it and/or modify
