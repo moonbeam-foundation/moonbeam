bdeaebf086357458427704d391a20bdff61a708b
gorka <gorka.irazoki@gmail.com>
2022-01-11 16:34:20 +0100

    Update benchmark

--- pallets/asset-manager/src/benchmarks.rs
+++ pallets/asset-manager/src/benchmarks.rs
@@ -45,6 +45,19 @@ benchmarks! {
 	verify {
 		assert_eq!(Pallet::<T>::asset_id_units_per_second(asset_id), Some(1));
 	}
+
+	change_existing_asset_type {
+		// does not really matter what we register
+		let asset_type = T::AssetType::default();
+		let metadata = T::AssetRegistrarMetadata::default();
+		let amount = 1u32.into();
+		let asset_id: T::AssetId = asset_type.clone().into();
+		Pallet::<T>::register_asset(RawOrigin::Root.into(), asset_type.clone(), metadata, amount, true)?;
+
+	}: _(RawOrigin::Root, asset_id, asset_type.clone())
+	verify {
+		assert_eq!(Pallet::<T>::asset_id_type(asset_id), Some(asset_type));
+	}
 }
 
 #[cfg(test)]
