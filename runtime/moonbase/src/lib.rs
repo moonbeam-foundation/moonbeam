e6d5f1b139a0fb277ffd179c37cfa1c26bc1a71d
librelois <c@elo.tf>
2022-01-13 15:47:53 +0100

    increase spec_version to 1102

--- runtime/moonbase/src/lib.rs
+++ runtime/moonbase/src/lib.rs
@@ -173,7 +173,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("moonbase"),
 	impl_name: create_runtime_str!("moonbase"),
 	authoring_version: 3,
-	spec_version: 1101,
+	spec_version: 1102,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 2,
