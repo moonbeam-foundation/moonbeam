6a29c50c00fa6d8957a53c7248516a861f8fd5f4
gorka <gorka.irazoki@gmail.com>
2022-01-13 13:29:51 +0100

    Include the dmp and xcmp queues in cumulus-parachain system

--- runtime/moonriver/src/lib.rs
+++ runtime/moonriver/src/lib.rs
@@ -663,10 +663,10 @@ impl cumulus_pallet_parachain_system::Config for Runtime {
 	type Event = Event;
 	type OnValidationData = ();
 	type SelfParaId = ParachainInfo;
-	type DmpMessageHandler = ();
-	type ReservedDmpWeight = ();
-	type OutboundXcmpMessageSource = ();
-	type XcmpMessageHandler = ();
+	type DmpMessageHandler = MaintenanceMode;
+	type ReservedDmpWeight = ReservedDmpWeight;
+	type OutboundXcmpMessageSource = XcmpQueue;
+	type XcmpMessageHandler = MaintenanceMode;
 	type ReservedXcmpWeight = ReservedXcmpWeight;
 }
 
