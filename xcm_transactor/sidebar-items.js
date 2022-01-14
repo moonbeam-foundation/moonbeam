b8831c91d6dcf35a50cedea198544cccf959e40e
girazoki <girazoki@users.noreply.github.com>
2022-01-14 18:23:52 +0000

    deploy: ffb44f0f576024d19ffd211c05f34d562a7b01e0

--- xcm_transactor/sidebar-items.js
+++ xcm_transactor/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"mod":[["pallet","The module that hosts all the FRAME types needed to add this pallet to a runtime."]]});
+initSidebarItems({"mod":[["migrations",""],["pallet","The module that hosts all the FRAME types needed to add this pallet to a runtime."]]});
