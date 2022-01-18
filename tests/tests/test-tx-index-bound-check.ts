e55db0f55781df48f492dfce12c315daebd2ecc2
tgmichel <telmo@purestake.com>
2022-01-18 18:05:28 +0100

    assert successful request as failure + move state change to `before` fn

--- tests/tests/test-tx-index-bound-check.ts
+++ tests/tests/test-tx-index-bound-check.ts
@@ -3,13 +3,15 @@ import { createTransfer } from "../util/transactions";
 import { describeDevMoonbeam } from "../util/setup-dev-tests";
 
 describeDevMoonbeam("Transaction Index", (context) => {
-  it("should get transaction by index", async function () {
-    const block = 1;
-    const index = 0;
+  before("Setup: Create block with transaction", async () => {
     const testAccount = "0x1111111111111111111111111111111111111111";
     await context.createBlock({
       transactions: [await createTransfer(context, testAccount, 0)],
     });
+  });
+  it("should get transaction by index", async function () {
+    const block = 1;
+    const index = 0;
     let result = await context.web3.eth.getTransactionFromBlock(block, index);
     expect(result.transactionIndex).to.equal(index);
   });
@@ -18,6 +20,9 @@ describeDevMoonbeam("Transaction Index", (context) => {
     const index = 0;
     await context.web3.eth
       .getTransactionFromBlock(block, index)
+      .then((err) => {
+        throw new Error(`Not expected to succeed`);
+      })
       .catch((err) => expect(err.message).to.equal(`Returned error: ${index} is out of bounds`));
   });
 });
