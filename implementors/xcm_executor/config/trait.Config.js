add5df1eaa71cced068b31e4e85085d609038767
girazoki <girazoki@users.noreply.github.com>
2022-01-19 09:39:35 +0000

    deploy: 0a66dd4b31894c008ba5a16cbd4a156b6d63a67a

--- implementors/xcm_executor/config/trait.Config.js
+++ implementors/xcm_executor/config/trait.Config.js
@@ -1,4 +1,5 @@
 (function() {var implementors = {};
 implementors["moonbase_runtime"] = [{"text":"impl Config for <a class=\"struct\" href=\"moonbase_runtime/struct.XcmExecutorConfig.html\" title=\"struct moonbase_runtime::XcmExecutorConfig\">XcmExecutorConfig</a>","synthetic":false,"types":["moonbase_runtime::XcmExecutorConfig"]}];
+implementors["moonbeam_runtime"] = [{"text":"impl Config for <a class=\"struct\" href=\"moonbeam_runtime/struct.XcmExecutorConfig.html\" title=\"struct moonbeam_runtime::XcmExecutorConfig\">XcmExecutorConfig</a>","synthetic":false,"types":["moonbeam_runtime::XcmExecutorConfig"]}];
 implementors["moonriver_runtime"] = [{"text":"impl Config for <a class=\"struct\" href=\"moonriver_runtime/struct.XcmExecutorConfig.html\" title=\"struct moonriver_runtime::XcmExecutorConfig\">XcmExecutorConfig</a>","synthetic":false,"types":["moonriver_runtime::XcmExecutorConfig"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
