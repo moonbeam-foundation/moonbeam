e1d641124b64599b94830172a0e317373e481bf2
tgmichel <telmo@purestake.com>
2022-01-28 18:43:16 +0100

    `DebugRuntimeApi` version 4

--- client/rpc/debug/src/lib.rs
+++ client/rpc/debug/src/lib.rs
@@ -475,7 +475,7 @@ where
 					api.initialize_block(&parent_block_id, &header)
 						.map_err(|e| internal_err(format!("Runtime api access error: {:?}", e)))?;
 
-					if trace_api_version >= 2 {
+					if trace_api_version >= 4 {
 						let _result = api
 							.trace_transaction(&parent_block_id, ext, &transaction)
 							.map_err(|e| {
@@ -491,7 +491,7 @@ where
 							ethereum::TransactionV2::Legacy(tx) =>
 							{
 								#[allow(deprecated)]
-								api.trace_transaction_before_version_2(&parent_block_id, ext, &tx)
+								api.trace_transaction_before_version_4(&parent_block_id, ext, &tx)
 									.map_err(|e| {
 										internal_err(format!(
 											"Runtime api access error (legacy): {:?}",
