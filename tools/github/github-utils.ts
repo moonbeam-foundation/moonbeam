e0b69fb34069d968cf06bd5c947264a476e81356
librelois <c@elo.tf>
2022-02-11 18:26:33 +0100

    prettier

--- tools/github/github-utils.ts
+++ tools/github/github-utils.ts
@@ -52,7 +52,7 @@ export async function getCommitAndLabels(
 
   const prByLabels = {};
   for (const commit of commits) {
-    const foundPrsNumbers = commit.commit.message.split('\n')[0].match(/\(#([0-9]+)\)/);
+    const foundPrsNumbers = commit.commit.message.split("\n")[0].match(/\(#([0-9]+)\)/);
     if (foundPrsNumbers && foundPrsNumbers.length > 1) {
       const pr = await octokit.rest.pulls.get({
         owner,
