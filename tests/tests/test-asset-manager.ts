66d90402ac0f11f840b2525ea458b69733e7543b
gorka <gorka.irazoki@gmail.com>
2022-02-18 16:31:12 +0100

    add remove supported fee typescript test

--- tests/tests/test-asset-manager.ts
+++ tests/tests/test-asset-manager.ts
@@ -103,7 +103,7 @@ describeDevMoonbeam("XCM - asset manager - register asset", (context) => {
     await verifyLatestBlockFees(context, expect);
   });
 
-  it.only("should query asset balance", async function () {
+  it("should change the asset Id", async function () {
     // ChangeAssetType
     await createBlockWithExtrinsic(
       context,
@@ -136,5 +136,78 @@ describeDevMoonbeam("XCM - asset manager - register asset", (context) => {
     expect(assetType.toString()).to.eq(JSON.stringify(newSourceLocation).toLowerCase());
     expect(bnToHex(id)).to.eq(assetId);
     expect(supportedAssets[0].toString()).to.eq(JSON.stringify(newSourceLocation).toLowerCase());
+  });
+});
+
+describeDevMoonbeam("XCM - asset manager - register asset", (context) => {
+  let assetId: string;
+  let alith: KeyringPair;
+  before("should be able to change existing asset type", async function () {
+    const keyringEth = new Keyring({ type: "ethereum" });
+    alith = keyringEth.addFromUri(ALITH_PRIV_KEY, null, "ethereum");
+
+    const parachainOne = context.polkadotApi;
+    // registerAsset
+    const { events: eventsRegister } = await createBlockWithExtrinsic(
+      context,
+      alith,
+      parachainOne.tx.sudo.sudo(
+        parachainOne.tx.assetManager.registerAsset(sourceLocation, assetMetadata, new BN(1), true)
+      )
+    );
+
+    eventsRegister.forEach((e) => {
+      if (e.section.toString() === "assetManager") {
+        assetId = e.data[0].toHex();
+      }
+    });
+    assetId = assetId.replace(/,/g, "");
+
+    // setAssetUnitsPerSecond
+    const { events } = await createBlockWithExtrinsic(
+      context,
+      alith,
+      parachainOne.tx.sudo.sudo(
+        parachainOne.tx.assetManager.setAssetUnitsPerSecond(sourceLocation, 1, 0)
+      )
+    );
+    expect(events[1].method.toString()).to.eq("UnitsPerSecondChanged");
+    expect(events[4].method.toString()).to.eq("ExtrinsicSuccess");
+
+    // check asset in storage
+    const registeredAsset = ((await parachainOne.query.assets.asset(assetId)) as any).unwrap();
+    expect(registeredAsset.owner.toString()).to.eq(palletId);
+
+    await verifyLatestBlockFees(context, expect);
+  });
+
+  it("should remove an asset from our supported fee payments", async function () {
+    // ChangeAssetType
+    await createBlockWithExtrinsic(
+      context,
+      alith,
+      context.polkadotApi.tx.sudo.sudo(
+        context.polkadotApi.tx.assetManager.removeSupportedAsset(sourceLocation, 1)
+      )
+    );
+
+    // assetId
+    let id = (
+      (await context.polkadotApi.query.assetManager.assetTypeId(sourceLocation)) as any
+    ).unwrap();
+
+    // asset units per second removed
+    let assetUnitsPerSecond = (await context.polkadotApi.query.assetManager.assetTypeUnitsPerSecond(
+      sourceLocation
+    )) as any;
+
+    // Supported assets should be 0
+    let supportedAssets =
+      (await context.polkadotApi.query.assetManager.supportedFeePaymentAssets()) as any;
+
+    expect(assetUnitsPerSecond.isNone).to.eq(true);
+    expect(bnToHex(id)).to.eq(assetId);
+    // the asset should not be supported
+    expect(supportedAssets.length).to.eq(0);
   });
 });
