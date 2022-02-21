dd31346898abd151da41897991f668786aa2f9cc
librelois <c@elo.tf>
2022-02-22 00:14:52 +0100

    tracing tests: sort json fields (not sorted anymore by the server)

--- tests/tracing-tests/test-trace.ts
+++ tests/tracing-tests/test-trace.ts
@@ -384,7 +384,7 @@ describeDevMoonbeam("Trace", (context) => {
     ]);
     let res = traceTx.result;
     // Fields
-    expect(Object.keys(res)).to.deep.equal([
+    expect(Object.keys(res).sort()).to.deep.equal([
       "calls",
       "from",
       "gas",
@@ -425,7 +425,7 @@ describeDevMoonbeam("Trace", (context) => {
 
     let res = traceTx.result;
     // Fields
-    expect(Object.keys(res)).to.deep.equal([
+    expect(Object.keys(res).sort()).to.deep.equal([
       "from",
       "gas",
       "gasUsed",
@@ -475,7 +475,7 @@ describeDevMoonbeam("Trace", (context) => {
     expect(block.transactions.length).to.be.equal(traceTx.result.length);
     traceTx.result.forEach((trace) => {
       expect(trace.calls.length).to.be.equal(1);
-      expect(Object.keys(trace)).to.deep.equal([
+      expect(Object.keys(trace).sort()).to.deep.equal([
         "calls",
         "from",
         "gas",
@@ -495,7 +495,7 @@ describeDevMoonbeam("Trace", (context) => {
     expect(block.transactions.length).to.be.equal(traceTx.result.length);
     traceTx.result.forEach((trace) => {
       expect(trace.calls.length).to.be.equal(1);
-      expect(Object.keys(trace)).to.deep.equal([
+      expect(Object.keys(trace).sort()).to.deep.equal([
         "calls",
         "from",
         "gas",
