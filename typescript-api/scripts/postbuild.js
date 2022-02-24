a52ec91cfa38cf4683f03b5789a06c446954f214
Elmar Kenigs <elmar@purestake.com>
2022-02-24 08:16:44 -0800

    Formatting for scripts folder

--- typescript-api/scripts/postbuild.js
+++ typescript-api/scripts/postbuild.js
@@ -1,10 +1,10 @@
-const { writeFileSync, copyFileSync } = require('fs');
-const pck = require('../package.json');
+const { writeFileSync, copyFileSync } = require("fs");
+const pck = require("../package.json");
 
 pck.scripts = {};
 pck.private = false;
-pck.type = 'module';
-pck.files = ['**/*', '!**/tsconfig.tsbuildinfo', '!**/*.tgz'];
+pck.type = "module";
+pck.files = ["**/*", "!**/tsconfig.tsbuildinfo", "!**/*.tgz"];
 
 writeFileSync(`${process.env.PWD}/build/package.json`, JSON.stringify(pck, null, 2));
-copyFileSync('README.md', `${process.env.PWD}/build/README.md`);
+copyFileSync("README.md", `${process.env.PWD}/build/README.md`);
