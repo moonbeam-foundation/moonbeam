08a69788782722a794ba98a21e9882066516b747
tgmichel <telmo@purestake.com>
2022-03-23 13:53:57 +0100

    Test `max_fee_per_gas` and `max_priority_fee_per_gas` fixes

--- tests/tests/test-balance/test-balance-transfer.ts
+++ tests/tests/test-balance/test-balance-transfer.ts
@@ -1,9 +1,16 @@
 import { expect } from "chai";
+import { BN } from "@polkadot/util";
 import { verifyLatestBlockFees } from "../../util/block";
-import { GENESIS_ACCOUNT, GENESIS_ACCOUNT_BALANCE } from "../../util/constants";
+import {
+  GENESIS_ACCOUNT,
+  GENESIS_ACCOUNT_BALANCE,
+  ALITH,
+  ALITH_PRIV_KEY,
+  TEST_ACCOUNT,
+} from "../../util/constants";
 
 import { describeDevMoonbeam, describeDevMoonbeamAllEthTxTypes } from "../../util/setup-dev-tests";
-import { createTransfer } from "../../util/transactions";
+import { createTransfer, createTransaction } from "../../util/transactions";
 import { customWeb3Request } from "../../util/providers";
 
 describeDevMoonbeamAllEthTxTypes("Balance transfer cost", (context) => {
@@ -65,3 +72,61 @@ describeDevMoonbeamAllEthTxTypes("Balance transfer - fees", (context) => {
     await verifyLatestBlockFees(context, expect, BigInt(512));
   });
 });
+
+describeDevMoonbeam("Balance transfer - EIP1559 fees", (context) => {
+  it("should handle max_fee_per_gas", async function () {
+
+    const preBalance = BigInt(await context.web3.eth.getBalance(ALITH));
+    // With this configuration no priority fee will be used, as the max_fee_per_gas is exactly the
+    // base fee. Expect the balances to reflect this case.
+    const maxFeePerGas = 1_000_000_000;
+    const tx = await createTransaction(context, {
+      from: ALITH,
+      privateKey: ALITH_PRIV_KEY,
+      value: "0x0",
+      gas: "0x5208",
+      maxFeePerGas: maxFeePerGas,
+      maxPriorityFeePerGas: "0xBEBC200", // 0.2GWEI
+      to: TEST_ACCOUNT,
+      data: "0x",
+    });
+  
+    const block = await context.createBlock({
+      transactions: [tx],
+    });
+    const postBalance = BigInt(await context.web3.eth.getBalance(ALITH));
+    const fee = BigInt(21_000 * maxFeePerGas);
+    const actualPostBalance = preBalance - fee;
+
+    expect(postBalance).to.be.eq(actualPostBalance);
+  });
+}, "EIP1559");
+
+describeDevMoonbeam("Balance transfer - EIP1559 fees", (context) => {
+  it("should use partial max_priority_fee_per_gas", async function () {
+
+    const preBalance = BigInt(await context.web3.eth.getBalance(ALITH));
+    // With this configuration only half of the priority fee will be used, as the max_fee_per_gas is
+    // 2GWEI and the base fee is 1GWEI.
+    const maxFeePerGas = 1_000_000_000 * 2;
+    const tx = await createTransaction(context, {
+      from: ALITH,
+      privateKey: ALITH_PRIV_KEY,
+      value: "0x0",
+      gas: "0x5208",
+      maxFeePerGas: maxFeePerGas,
+      maxPriorityFeePerGas: maxFeePerGas,
+      to: TEST_ACCOUNT,
+      data: "0x",
+    });
+  
+    const block = await context.createBlock({
+      transactions: [tx],
+    });
+    const postBalance = BigInt(await context.web3.eth.getBalance(ALITH));
+    const fee = BigInt(21_000 * maxFeePerGas);
+    const actualPostBalance = preBalance - fee;
+
+    expect(postBalance).to.be.eq(actualPostBalance);
+  });
+}, "EIP1559");
--- tests/tests/test-receipt/test-receipt.ts
+++ tests/tests/test-receipt/test-receipt.ts
@@ -1,7 +1,13 @@
 import { expect } from "chai";
-import { describeDevMoonbeamAllEthTxTypes } from "../../util/setup-dev-tests";
-import { createContract } from "../../util/transactions";
-import { GENESIS_ACCOUNT } from "../../util/constants";
+import { describeDevMoonbeam, describeDevMoonbeamAllEthTxTypes } from "../../util/setup-dev-tests";
+import { createContract, createTransaction } from "../../util/transactions";
+import {
+  GENESIS_ACCOUNT,
+  ALITH,
+  ALITH_PRIV_KEY,
+  TEST_ACCOUNT,
+} from "../../util/constants";
+
 
 describeDevMoonbeamAllEthTxTypes("Receipt - Contract", (context) => {
   let txHash;
@@ -27,3 +33,32 @@ describeDevMoonbeamAllEthTxTypes("Receipt - Contract", (context) => {
     expect(receipt.logs[0].blockHash).to.be.eq(block.hash);
   });
 });
+
+describeDevMoonbeam("Receipt - EIP1559", (context) => {
+  it("should calculate effective gas price", async function () {
+
+    const preBalance = BigInt(await context.web3.eth.getBalance(ALITH));
+    // With this configuration only half of the priority fee will be used, as the max_fee_per_gas is
+    // 2GWEI and the base fee is 1GWEI.
+    const maxFeePerGas = 1_000_000_000 * 2;
+    const tx = await createTransaction(context, {
+      from: ALITH,
+      privateKey: ALITH_PRIV_KEY,
+      value: "0x0",
+      gas: "0x5208",
+      maxFeePerGas: maxFeePerGas,
+      maxPriorityFeePerGas: maxFeePerGas,
+      to: TEST_ACCOUNT,
+      data: "0x",
+    });
+  
+    await context.createBlock({
+      transactions: [tx],
+    });
+
+    const block = await context.web3.eth.getBlock("latest");
+    const receipt = await context.web3.eth.getTransactionReceipt(block.transactions[0]);
+    // The receipt should contain an effective gas price of 2GWEI.
+    expect(receipt.effectiveGasPrice).to.be.eq(maxFeePerGas);
+  });
+}, "EIP1559");
