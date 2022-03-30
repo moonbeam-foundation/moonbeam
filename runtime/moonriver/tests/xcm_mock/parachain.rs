f74096a8448dfa7eda5c94ef5d70c47e59dd703b
Stephen Shelton <steve@brewcraft.org>
2022-03-30 17:35:13 +0000

    Add some WeightInfo, remove some WeightInfo

--- runtime/moonriver/tests/xcm_mock/parachain.rs
+++ runtime/moonriver/tests/xcm_mock/parachain.rs
@@ -777,7 +777,6 @@ impl pallet_evm::Config for Runtime {
 	type OnChargeTransaction = ();
 	type BlockHashMapping = pallet_evm::SubstrateBlockHashMapping<Self>;
 	type FindAuthor = ();
-	type WeightInfo = ();
 }
 
 pub struct NormalFilter;
