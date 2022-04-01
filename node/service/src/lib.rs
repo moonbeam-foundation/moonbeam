eed5f310a68370a63a5fcf8960e70d6b63e9c44a
Stephen Shelton <steve@brewcraft.org>
2022-04-01 14:08:55 +0000

    Chop long line

--- node/service/src/lib.rs
+++ node/service/src/lib.rs
@@ -646,7 +646,8 @@ where
 			spawner,
 			parachain_consensus,
 			import_queue,
-			collator_key: collator_key.ok_or(sc_service::error::Error::Other("Collator Key is None".to_string()))?,
+			collator_key: collator_key
+				.ok_or(sc_service::error::Error::Other("Collator Key is None".to_string()))?,
 			relay_chain_slot_duration,
 		};
 
