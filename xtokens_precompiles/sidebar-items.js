0aae657a9a74b66e6029a05ab63d2e8083ba3a2f
girazoki <girazoki@users.noreply.github.com>
2022-04-02 08:22:52 +0000

    deploy: 78484f01a82253c586a8e404f07d4ca4d80d0369

--- xtokens_precompiles/sidebar-items.js
+++ xtokens_precompiles/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["Action",""]],"struct":[["XtokensWrapper","A precompile to wrap the functionality from xtokens"]],"type":[["CurrencyIdOf",""],["XBalanceOf",""]]});
+initSidebarItems({"enum":[["Action",""]],"struct":[["Currency",""],["EvmMultiAsset",""],["XtokensWrapper","A precompile to wrap the functionality from xtokens"]],"type":[["CurrencyIdOf",""],["MaxAssetsForTransfer",""],["XBalanceOf",""]]});
