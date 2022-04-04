7986e1b8d0f152c98f740ac5245fc71f1219a2d7
girazoki <girazoki@users.noreply.github.com>
2022-04-04 09:46:48 +0000

    deploy: 01b01b1ae3c02fb5af67429dd6492507e63a9c44

---
+++ moonriver_runtime/asset_config/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["ApprovalDeposit",""],["AssetDeposit",""],["AssetRegistrar",""],["AssetRegistrarMetadata",""],["AssetsStringLimit",""],["LocalAssetIdCreator",""],["MetadataDepositBase",""],["MetadataDepositPerByte",""]],"type":[["AssetsForceOrigin","We allow root and Chain council to execute privileged asset operations."]]});
