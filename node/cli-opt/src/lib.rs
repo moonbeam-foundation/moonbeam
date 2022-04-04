b66e5dc1d0743f9a9808a7f1c7aa74cd756c1ed1
Éloïs <c@elo.tf>
2022-04-04 17:10:27 +0200

    plumb cli option  `--relay-chain-rpc-url` (#1388)
    * fix: plumb cli option relay_chain_rpc_url
    
    * Update node/service/src/lib.rs
    
    Co-authored-by: tgmichel <telmo@purestake.com>
    
    Co-authored-by: tgmichel <telmo@purestake.com>

--- node/cli-opt/src/lib.rs
+++ node/cli-opt/src/lib.rs
@@ -76,6 +76,7 @@ pub struct RpcConfig {
 	pub ethapi_trace_cache_duration: u64,
 	pub eth_log_block_cache: usize,
 	pub eth_statuses_cache: usize,
-	pub max_past_logs: u32,
 	pub fee_history_limit: u64,
+	pub max_past_logs: u32,
+	pub relay_chain_rpc_url: Option<url::Url>,
 }
