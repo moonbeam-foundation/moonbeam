cd88f7d576eb8c70353fa26b47b5922b83e8eb1f
Alan Sapede <alan@purestake.com>
2022-04-15 08:11:21 -0400

    Adds smoke-test suite (#1409)
    * Adds smoke-test suite
    
    * Adds more doc to the test README
    
    * Adds better test for reserved balance
    
    * Updates package-lock for tests
    
    * Adds more doc to smoke test readme
    
    * Adds more smoke tests

--- tests/util/logging.ts
+++ tests/util/logging.ts
@@ -1,5 +1,16 @@
+import { ApiPromise } from "@polkadot/api";
+
 export function log(...msg: any[]) {
   if (process.argv && process.argv[2] && process.argv[2] === "--printlogs") {
     console.log(...msg);
   }
 }
+
+export const printTokens = (api: ApiPromise, tokens: bigint, decimals = 2, pad = 9) => {
+  return `${(
+    Math.ceil(Number(tokens / 10n ** BigInt(api.registry.chainDecimals[0] - decimals))) /
+    10 ** decimals
+  )
+    .toString()
+    .padStart(pad)} ${api.registry.chainTokens[0]}`;
+};
