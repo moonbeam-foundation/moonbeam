e51eb5fbc9a5d42a2ce5e9ca3da5d18c05df0bc3
nanocryk <6422796+nanocryk@users.noreply.github.com>
2022-04-27 12:47:12 +0000

    doc + test selectors

--- precompiles/assets-erc20/Permit.sol
+++ precompiles/assets-erc20/Permit.sol
@@ -6,17 +6,39 @@ pragma solidity ^0.8.0;
  * make a first approve transaction.
  */
 interface Permit {
+    /**
+     * @dev Consumes an approval permit.
+     * Anyone can call this function for a permit.
+     * Selector: 0xd505accf
+     * @param owner Owner of the tokens issuing the permit
+     * @param spender Address whose allowance will be increased.
+     * @param value Allowed value.
+     * @param deadline Timestamp after which the permit will no longer be valid.
+     * @param v V component of the signature.
+     * @param r R component of the signature.
+     * @param s S component of the signature.
+     */
     function permit(
         address owner,
         address spender,
-        uint value,
-        uint deadline,
+        uint256 value,
+        uint256 deadline,
         uint8 v,
         bytes32 r,
         bytes32 s
     ) external;
 
-    function nonces(address owner) external view returns (uint);
+    /**
+     * @dev Returns the current nonce for given owner.
+     * A permit must have this nonce to be consumed, which will
+     * increase the nonce by one.
+     * Selector: 0x7ecebe00
+     */
+    function nonces(address owner) external view returns (uint256);
 
+    /**
+     * @dev Returns the EIP712 domain separator.
+     * Selector: 0x3644e515
+     */
     function DOMAIN_SEPARATOR() external view returns (bytes32);
 }
--- precompiles/assets-erc20/src/tests.rs
+++ precompiles/assets-erc20/src/tests.rs
@@ -94,15 +94,18 @@ fn no_selector_exists_but_length_is_right() {
 
 #[test]
 fn selectors() {
-	assert_eq!(u32::from(Action::BalanceOf), 0x70a08231);
-	assert_eq!(u32::from(Action::TotalSupply), 0x18160ddd);
-	assert_eq!(u32::from(Action::Approve), 0x095ea7b3);
-	assert_eq!(u32::from(Action::Allowance), 0xdd62ed3e);
-	assert_eq!(u32::from(Action::Transfer), 0xa9059cbb);
-	assert_eq!(u32::from(Action::TransferFrom), 0x23b872dd);
+	assert_eq!(Action::BalanceOf as u32, 0x70a08231);
+	assert_eq!(Action::TotalSupply as u32, 0x18160ddd);
+	assert_eq!(Action::Approve as u32, 0x095ea7b3);
+	assert_eq!(Action::Allowance as u32, 0xdd62ed3e);
+	assert_eq!(Action::Transfer as u32, 0xa9059cbb);
+	assert_eq!(Action::TransferFrom as u32, 0x23b872dd);
 	assert_eq!(Action::Name as u32, 0x06fdde03);
 	assert_eq!(Action::Symbol as u32, 0x95d89b41);
 	assert_eq!(Action::Decimals as u32, 0x313ce567);
+	assert_eq!(Action::Eip2612Nonces as u32, 0x7ecebe00);
+	assert_eq!(Action::Eip2612Permit as u32, 0xd505accf);
+	assert_eq!(Action::Eip2612DomainSeparator as u32, 0x3644e515);
 
 	assert_eq!(
 		crate::SELECTOR_LOG_TRANSFER,
