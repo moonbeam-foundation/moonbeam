87f48bd68f8b70d4c47216ad4edbba9cfdee8045
librelois <librelois@users.noreply.github.com>
2022-04-28 14:49:50 +0000

    deploy: 2a5e7e82e01d187ea547c91e51df653f2c7047cb

---
+++ implementors/pallet_moonbeam_orbiters/weights/trait.WeightInfo.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["pallet_moonbeam_orbiters"] = [];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
