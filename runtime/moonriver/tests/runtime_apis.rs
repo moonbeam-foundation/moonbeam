b5ce527041fc4f602a301e874b5422467483ba95
Stephen Shelton <steve@brewcraft.org>
2022-04-29 13:22:03 +0000

    Fix tests

--- runtime/moonriver/tests/runtime_apis.rs
+++ runtime/moonriver/tests/runtime_apis.rs
@@ -20,7 +20,8 @@ mod common;
 use common::*;
 
 use nimbus_primitives::NimbusId;
-use pallet_evm::{Account as EVMAccount, AddressMapping, FeeCalculator, GenesisAccount};
+use pallet_evm::{Account as EVMAccount, AddressMapping, FeeCalculator};
+use fp_evm::GenesisAccount;
 use sp_core::{ByteArray, H160, H256, U256};
 
 use fp_rpc::runtime_decl_for_EthereumRuntimeRPCApi::EthereumRuntimeRPCApi;
