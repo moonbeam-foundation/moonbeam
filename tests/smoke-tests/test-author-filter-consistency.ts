e8c03f032a4855389cc5e5bf9c0371c96e0127f5
Crystalin <alan@purestake.com>
2022-05-02 18:37:13 -0400

    Improve author eligibility consistency test

--- tests/smoke-tests/test-author-filter-consistency.ts
+++ tests/smoke-tests/test-author-filter-consistency.ts
@@ -2,7 +2,6 @@ import "@moonbeam-network/api-augment";
 import { ApiDecoration } from "@polkadot/api/types";
 import type { FrameSystemAccountInfo } from "@polkadot/types/lookup";
 import { expect } from "chai";
-import { printTokens } from "../util/logging";
 import { describeSmokeSuite } from "../util/setup-smoke-tests";
 const debug = require("debug")("smoke:author");
 
@@ -13,20 +12,26 @@ describeSmokeSuite(`Verify author filter consistency`, { wssUrl }, (context) => 
 
   let atBlockNumber: number = 0;
   let apiAt: ApiDecoration<"promise"> = null;
+  let specVersion: number = 0;
 
   before("Setup api", async function () {
     atBlockNumber = (await context.polkadotApi.rpc.chain.getHeader()).number.toNumber();
     apiAt = await context.polkadotApi.at(
       await context.polkadotApi.rpc.chain.getBlockHash(atBlockNumber)
     );
+    specVersion = (await apiAt.query.system.lastRuntimeUpgrade()).unwrap().specVersion.toNumber();
   });
 
   it("should have eligibility > 0", async function () {
-    // Load data
-    const eligibilityRatio = await apiAt.query.authorFilter.eligibleRatio();
-
-    expect(eligibilityRatio.toBigInt() > 0n).to.be.true;
-
+    if (specVersion < 1500) {
+      const eligibilityRatio = await apiAt.query.authorFilter.eligibleRatio();
+      expect(eligibilityRatio.toBigInt() > 0n).to.be.true;
+    }
+    if (specVersion >= 1500) {
+      // TODO remove `as any` once api-augment is updated
+      const eligibilityCount = (await apiAt.query.authorFilter.eligibleCount()) as any;
+      expect(eligibilityCount.toNumber() > 0).to.be.true;
+    }
     debug(`Verified eligibility`);
   });
 });
