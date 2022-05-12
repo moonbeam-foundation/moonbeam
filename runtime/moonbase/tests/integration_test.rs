9874d9256927142008906267855ac05ae8ca8348
Stephen Shelton <steve@brewcraft.org>
2022-05-12 03:17:06 +0000

    Ascii art

--- runtime/moonbase/tests/integration_test.rs
+++ runtime/moonbase/tests/integration_test.rs
@@ -2333,14 +2333,17 @@ fn length_fee_is_sensible() {
 		};
 
 		// left: cost of length fee, right: size in bytes
-		assert_eq!(1_000_000_001, calc_fee(1));
-		assert_eq!(10_000_001_000, calc_fee(10));
-		assert_eq!(100_001_000_000, calc_fee(100));
-		assert_eq!(1_001_000_000_000, calc_fee(1_000));
-		assert_eq!(11_000_000_000_000, calc_fee(10_000));
-		assert_eq!(1_100_000_000_000_000, calc_fee(100_000));
-		assert_eq!(1_001_000_000_000_000_000, calc_fee(1_000_000));
-		assert_eq!(1_000_010_000_000_000_000_000, calc_fee(10_000_000));
+		assert_eq!(                    1_000_000_001, calc_fee(1));
+		//                             |           |
+		//                             |           \- exponential component: O(N ** 3)
+		//                             \------------- proportional component: O(N * 1B)
+		assert_eq!(                   10_000_001_000, calc_fee(10));
+		assert_eq!(                  100_001_000_000, calc_fee(100));
+		assert_eq!(                1_001_000_000_000, calc_fee(1_000));
+		assert_eq!(               11_000_000_000_000, calc_fee(10_000));
+		assert_eq!(            1_100_000_000_000_000, calc_fee(100_000));
+		assert_eq!(        1_001_000_000_000_000_000, calc_fee(1_000_000));
+		assert_eq!(    1_000_010_000_000_000_000_000, calc_fee(10_000_000));
 		assert_eq!(1_000_000_100_000_000_000_000_000, calc_fee(100_000_000));
 	});
 }
