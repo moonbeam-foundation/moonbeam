a2a21fead7443120c6401566a9d7e1273758a620
Amar Singh <asinghchrony@protonmail.com>
2022-05-12 12:19:40 -0400

    Add vrf key to session keys type (#1480)

--- runtime/moonbase/tests/integration_test.rs
+++ runtime/moonbase/tests/integration_test.rs
@@ -2584,7 +2584,11 @@ fn author_mapping_precompile_associate_update_and_clear() {
 			let author_mapping_precompile_address = H160::from_low_u64_be(2055);
 			let first_nimbus_id: NimbusId =
 				sp_core::sr25519::Public::unchecked_from([1u8; 32]).into();
+			let first_vrf_id: session_keys_primitives::VrfId =
+				sp_core::sr25519::Public::unchecked_from([1u8; 32]).into();
 			let second_nimbus_id: NimbusId =
+				sp_core::sr25519::Public::unchecked_from([2u8; 32]).into();
+			let second_vrf_id: session_keys_primitives::VrfId =
 				sp_core::sr25519::Public::unchecked_from([2u8; 32]).into();
 
 			let associate_expected_result = Some(Ok(PrecompileOutput {
@@ -2616,7 +2620,7 @@ fn author_mapping_precompile_associate_update_and_clear() {
 				Event::AuthorMapping(pallet_author_mapping::Event::AuthorRegistered {
 					author_id: first_nimbus_id.clone(),
 					account_id: AccountId::from(ALICE),
-					keys: first_nimbus_id.clone(),
+					keys: first_vrf_id.clone(),
 				});
 			assert_eq!(last_event(), expected_associate_event);
 
@@ -2650,7 +2654,7 @@ fn author_mapping_precompile_associate_update_and_clear() {
 				Event::AuthorMapping(pallet_author_mapping::Event::AuthorRotated {
 					new_author_id: second_nimbus_id.clone(),
 					account_id: AccountId::from(ALICE),
-					new_keys: second_nimbus_id.clone(),
+					new_keys: second_vrf_id.clone(),
 				});
 			assert_eq!(last_event(), expected_update_event);
 
@@ -2681,9 +2685,9 @@ fn author_mapping_precompile_associate_update_and_clear() {
 
 			let expected_clear_event =
 				Event::AuthorMapping(pallet_author_mapping::Event::AuthorDeRegistered {
-					author_id: second_nimbus_id.clone(),
+					author_id: second_nimbus_id,
 					account_id: AccountId::from(ALICE),
-					keys: second_nimbus_id,
+					keys: second_vrf_id,
 				});
 			assert_eq!(last_event(), expected_clear_event);
 		});
@@ -2698,11 +2702,11 @@ fn author_mapping_register_and_set_keys() {
 			let author_mapping_precompile_address = H160::from_low_u64_be(2055);
 			let first_nimbus_id: NimbusId =
 				sp_core::sr25519::Public::unchecked_from([1u8; 32]).into();
-			let first_vrf_key: NimbusId =
+			let first_vrf_key: session_keys_primitives::VrfId =
 				sp_core::sr25519::Public::unchecked_from([3u8; 32]).into();
 			let second_nimbus_id: NimbusId =
 				sp_core::sr25519::Public::unchecked_from([2u8; 32]).into();
-			let second_vrf_key: NimbusId =
+			let second_vrf_key: session_keys_primitives::VrfId =
 				sp_core::sr25519::Public::unchecked_from([4u8; 32]).into();
 
 			let associate_expected_result = Some(Ok(PrecompileOutput {
