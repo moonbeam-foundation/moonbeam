6a330d1dbee4053839bad50b204af8b4539eebfe
Crystalin <alan@purestake.com>
2022-05-14 12:49:48 -0400

    Adds expectation

--- tests/tests/test-gas/test-gas-staking-precompile.ts
+++ tests/tests/test-gas/test-gas-staking-precompile.ts
@@ -1,4 +1,4 @@
-import { use as chaiUse } from "chai";
+import { expect, use as chaiUse } from "chai";
 import chaiAsPromised from "chai-as-promised";
 
 import { describeDevMoonbeamAllEthTxTypes } from "../../util/setup-dev-tests";
@@ -11,12 +11,12 @@ describeDevMoonbeamAllEthTxTypes("Estimate Gas - Staking precompile", (context) 
   it("should estimate sufficient gas for revoke", async function () {
     const estimate = await context.web3.eth.estimateGas({
       from: GENESIS_ACCOUNT,
-      data: `0xe42366a6000000000000000000000000${ALITH.slice(
-        2
-      ).toLowerCase()}000000000000000000000000${ALITH.slice(2).toLowerCase()}`,
+      data: `0xe42366a6${ALITH.slice(2).toLowerCase().padStart(64, "0")}${ALITH.slice(2)
+        .toLowerCase()
+        .padStart(64, "0")}`,
     });
 
-    console.log(JSON.stringify(estimate, null, 2));
-    // TODO: add expectations
+    expect(estimate).to.be.at.least(55641);
+    expect(estimate).to.be.at.most(55641 * 1.1); // Allow 10% extra
   });
 });
