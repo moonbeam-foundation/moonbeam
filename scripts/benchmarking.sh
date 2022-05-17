2cc5ae91aa51a0ebb2ee44e775c2df85fddaf17d
Nisheeth Barthwal <nbaztec@gmail.com>
2022-05-17 13:17:00 +0200

    adapt benchmark command to new version

--- scripts/benchmarking.sh
+++ scripts/benchmarking.sh
@@ -43,12 +43,12 @@ function choose_and_bench {
 
 function bench {
     echo "benchmarking '${1}::${2}' --check=${3}"
-    if [[ "${check}" -eq 1 ]]; then
+    if [[ "${3}" -eq 1 ]]; then
         STEPS=16
         REPEAT=1
     fi
 
-    WASMTIME_BACKTRACE_DETAILS=1 ${BINARY} benchmark \
+    WASMTIME_BACKTRACE_DETAILS=1 ${BINARY} benchmark pallet \
         --chain dev \
         --execution=wasm \
         --wasm-execution=compiled \
