9766fd0161281d74ecacbfa2f121079b3af29e48
Stephen Shelton <steve@brewcraft.org>
2022-05-23 20:24:50 +0000

    fmt

--- runtime/moonbase/src/lib.rs
+++ runtime/moonbase/src/lib.rs
@@ -47,8 +47,8 @@ use frame_support::{
 	},
 	weights::{
 		constants::{RocksDbWeight, WEIGHT_PER_SECOND},
-		DispatchClass, GetDispatchInfo, Weight, WeightToFeeCoefficient,
-		WeightToFeeCoefficients, WeightToFeePolynomial,
+		DispatchClass, GetDispatchInfo, Weight, WeightToFeeCoefficient, WeightToFeeCoefficients,
+		WeightToFeePolynomial,
 	},
 	PalletId,
 };
