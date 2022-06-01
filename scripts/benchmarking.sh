1abcd8ed05ba441f920e01f18dfa45fcb4428146
Nisheeth Barthwal <nbaztec@gmail.com>
2022-06-01 12:50:38 +0200

    add --all to benchmarking script

--- scripts/benchmarking.sh
+++ scripts/benchmarking.sh
@@ -42,7 +42,10 @@ function choose_and_bench {
 }
 
 function bench {
-    echo "benchmarking '${1}::${2}' --check=${3}"
+    OUTPUT=${4:-weights.rs}
+    echo "benchmarking '${1}::${2}' --check=${3}, writing results to '${OUTPUT}'"
+
+    # Check enabled
     if [[ "${3}" -eq 1 ]]; then
         STEPS=16
         REPEAT=1
@@ -59,7 +62,7 @@ function bench {
         --template=./benchmarking/frame-weight-template.hbs \
         --record-proof \
         --json-file raw.json \
-        --output weights.rs
+        --output "${OUTPUT}"
 }
 
 if [[ "${@}" =~ "--help" ]]; then
@@ -71,7 +74,15 @@ else
         set -o noglob && set -- ${@/'--check'} && set +o noglob
     fi
 
-    if [[ $# -ne 2 ]]; then
+    ALL=0
+    if [[ "${@}" =~ "--all" ]]; then
+        ALL=1
+    fi
+
+    if [[ "${ALL}" -eq 1 ]]; then
+        mkdir -p weights/
+        bench '*' '*' "${CHECK}" "weights/"
+    elif [[ $# -ne 2 ]]; then
         choose_and_bench "${CHECK}"
     else
         bench "${1}" "${2}" "${CHECK}"
