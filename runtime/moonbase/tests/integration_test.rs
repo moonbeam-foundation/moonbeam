2b8a15b451049521868bee5d95da0bd83bf8b5a1
nanocryk <6422796+nanocryk@users.noreply.github.com>
2022-06-09 09:07:42 +0000

    author-mapping precompile

--- runtime/moonbase/tests/integration_test.rs
+++ runtime/moonbase/tests/integration_test.rs
@@ -42,8 +42,8 @@ use moonbase_runtime::{
 
 use crowdloan_rewards_precompiles::Action as CrowdloanAction;
 use nimbus_primitives::NimbusId;
-use pallet_author_mapping_precompiles::Action as AuthorMappingAction;
 use pallet_evm::PrecompileSet;
+use pallet_evm_precompile_author_mapping::Action as AuthorMappingAction;
 use pallet_evm_precompileset_assets_erc20::{
 	AccountIdAssetIdConversion, Action as AssetAction, SELECTOR_LOG_APPROVAL, SELECTOR_LOG_TRANSFER,
 };
