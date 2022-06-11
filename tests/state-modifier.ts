85b2d778b8bed1352c30a757efc797e7b47f2ba2
Crystalin <alan@purestake.com>
2022-06-11 17:58:41 -0400

    Better state modifier for forked states

--- tests/state-modifier.ts
+++ tests/state-modifier.ts
@@ -3,7 +3,7 @@ import readline from "readline";
 import chalk from "chalk";
 
 import { xxhashAsU8a, blake2AsU8a } from "@polkadot/util-crypto";
-import { u8aConcat, u8aToHex } from "@polkadot/util";
+import { u8aConcat, u8aToHex, hexToBigInt, nToHex } from "@polkadot/util";
 
 const storageKey = (module, name) => {
   return u8aToHex(u8aConcat(xxhashAsU8a(module, 128), xxhashAsU8a(name, 128)));
@@ -14,6 +14,7 @@ const storageBlake128MapKey = (module, name, key) => {
     u8aConcat(xxhashAsU8a(module, 128), xxhashAsU8a(name, 128), blake2AsU8a(key, 128), key)
   );
 };
+
 /**
  * All module prefixes except those mentioned in the skippedModulesPrefix will be added to this by the script.
  * If you want to add any past module or part of a skipped module, add the prefix here manually.
@@ -34,8 +35,6 @@ const storageBlake128MapKey = (module, name, key) => {
 async function main(inputFile: string, outputFile?: string) {
   const ALITH = "0xf24FF3a9CF04c71Dbc94D0b566f7A27B94566cac";
   const ALITH_SESSION = "0xd43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d";
-  const BOB = "0x3Cd0A705a2DC65e5b1E1205896BaA2be8A07c6e0";
-  const BOB_SESSION = "0x8eaf04151687736326c9fea17e25fc5287613693c912909cb226aa4794f26a48";
 
   const in1Stream = fs.createReadStream(inputFile, "utf8");
   const rl1 = readline.createInterface({
@@ -62,14 +61,16 @@ async function main(inputFile: string, outputFile?: string) {
   const techCommitteeeLinePrefix = `        "${storageKey("TechCommitteeCollective", "Members")}`;
   const parachainIdPrefix = `        "${storageKey("ParachainInfo", "ParachainId")}`;
   const lastDmqMqcHeadPrefix = `        "${storageKey("ParachainSystem", "LastDmqMqcHead")}`;
-  const alithBalance = `        "${storageBlake128MapKey("System", "Account", ALITH)}`;
-  const bobBalance = `        "${storageBlake128MapKey("System", "Account", BOB)}`;
+  const alithBalancePrefix = `        "${storageBlake128MapKey("System", "Account", ALITH)}`;
+  const totalIssuanceBalancePrefix = `        "${storageKey("Balances", "TotalIssuance")}`;
 
   // List all the collator author mapping
-  const collatorAuthorMapping = {};
+  const authorMappingLines = {};
 
   // First pass
   let selectedCollator = null;
+  let totalIssuance: bigint;
+  let alithAccountData;
   for await (const line of rl1) {
     if (line.startsWith(collatorLinePrefix)) {
       // First collator we found, it will be our target for Alice
@@ -79,32 +80,54 @@ async function main(inputFile: string, outputFile?: string) {
       }
     }
     if (line.startsWith(authorLinePrefix)) {
-      // First collator we found, it will be our target for Alice
-      if (!selectedCollator) {
-        const collator = line.split('"')[3].slice(0, 42);
-        const collatorMappingKey = line.split('"')[1];
-        collatorAuthorMapping[collator] = collatorMappingKey;
-      }
+      const collator = line.split('"')[3].slice(0, 42);
+      authorMappingLines[collator] = line;
     }
     if (line.startsWith(revelentMessagingStatePrefix)) {
       messagingState = line.split('"')[3];
+    }
+    if (line.startsWith(totalIssuanceBalancePrefix)) {
+      totalIssuance = hexToBigInt(line.split('"')[3], { isLe: true });
+    }
+    if (line.startsWith(alithBalancePrefix)) {
+      alithAccountData = line.split('"')[3];
     }
   }
 
   // List all the collator author mapping
   console.log(
-    `Using account ${selectedCollator} as alice collator, session ${collatorAuthorMapping[selectedCollator]}`
+    `Using account ${selectedCollator} as alice collator, session ${
+      authorMappingLines[selectedCollator].split('"')[1]
+    }`
   );
 
   if (!selectedCollator) {
     console.log(`Couldn't find collator with prefix ${authorLinePrefix}`);
     return;
   }
-  const collatorMappingKey = collatorAuthorMapping[selectedCollator];
+  const selectedCollatorMappingKey = authorMappingLines[selectedCollator].split('"')[1];
+
+  console.log(selectedCollator, authorMappingLines[selectedCollator]);
 
   if (!messagingState) {
     console.log(`Couldn't find messaging state with prefix ${revelentMessagingStatePrefix}`);
     return;
+  }
+
+  // We add 1_000 tokens to alith if needed (for governance...)
+  // and so we need to add it to the totalIssuance to stay consistent;
+  const alithFreeBalance = hexToBigInt(alithAccountData.slice(34, 66), { isLe: true });
+  let newAlithAccountData = alithAccountData;
+  let newTotalIssuance = totalIssuance;
+  if (alithFreeBalance < 1000n * 10n ** 18n) {
+    newAlithAccountData = `${alithAccountData.slice(0, 34)}${nToHex(
+      alithFreeBalance + 1000n * 10n ** 18n,
+      {
+        isLe: true,
+        bitLength: 128,
+      }
+    ).slice(2)}${alithAccountData.slice(66)}`;
+    newTotalIssuance = totalIssuance + 1_000n * 10n ** 18n;
   }
 
   const in2Stream = fs.createReadStream(inputFile, "utf8");
@@ -114,80 +137,89 @@ async function main(inputFile: string, outputFile?: string) {
   });
   const outStream = fs.createWriteStream(destFile, { encoding: "utf8" });
 
-  const authorMappingPrefix = `        "${collatorMappingKey}"`;
+  const selectedAuthorMappingPrefix = `        "${selectedCollatorMappingKey}"`;
 
   for await (const line of rl2) {
     if (line.startsWith(`      "top"`)) {
       outStream.write(line);
       console.log("found top");
-      const newLine = `        "${storageBlake128MapKey(
+    } else if (line.startsWith(selectedAuthorMappingPrefix)) {
+      console.log(` ${chalk.red(`- AuthorMapping.MappingWithDeposit`)}\n\t${line}`);
+      let newLine = `        "${storageBlake128MapKey(
         "AuthorMapping",
         "MappingWithDeposit",
         ALITH_SESSION
-      )}": "${selectedCollator}000010632d5ec76b0500000000000000${ALITH_SESSION.slice(2)}",\n`;
-      console.log(` ${chalk.green(`+ Adding session`)}\n\t${newLine}`);
+      )}": "${authorMappingLines[selectedCollator]
+        .split('"')[3]
+        .slice(0, -64)}${ALITH_SESSION.slice(2)}",\n`;
+      console.log(
+        ` ${chalk.green(`+ Adding AuthorMapping.MappingWithDeposit: Alith`)}\n\t${newLine}`
+      );
       outStream.write(newLine);
+    } else if (line.startsWith(revelentMessagingStatePrefix)) {
+      console.log(` ${chalk.red(`- Removing  ParachainSystem.RelevantMessagingState`)}\n\t${line}`);
+      const newLine = `        "${storageKey(
+        "ParachainSystem",
+        "RelevantMessagingState"
+      )}": "0x${new Array(64).fill(0).join("")}${messagingState.slice(66)}",\n`;
+      console.log(
+        ` ${chalk.green(`+ Adding ParachainSystem.RelevantMessagingState`)}\n\t${newLine}`
+      );
+      outStream.write(newLine);
+    } else if (line.startsWith(authorEligibilityRatioPrefix)) {
+      console.log(` ${chalk.red(`- Removing AuthorFilter.EligibleRatio`)}\n\t${line}`);
+      const newLine = `        "${storageKey("AuthorFilter", "EligibleRatio")}": "0x64",\n`;
+      console.log(` ${chalk.green(`+ Adding AuthorFilter.EligibleRatio`)}\n\t${newLine}`);
+      outStream.write(newLine);
+    } else if (line.startsWith(authorEligibilityCountPrefix)) {
+      console.log(` ${chalk.red(`- Removing AuthorFilter.EligibleCount`)}\n\t${line}`);
 
-      // outStream.write(
-      //   `        "${storageBlake128MapKey(
-      //     "AuthorMapping",
-      //     "MappingWithDeposit",
-      //     BOB_SESSION
-      //   )}": "${selectedCollator}000010632d5ec76b0500000000000000${BOB_SESSION.slice(2)}",\n`
-      // );
-      // outStream.write(
-      //   `        "${storageKey(
-      //     "ParachainSystem",
-      //     "LastDmqMqcHead"
-      //   )}": "${collator}000010632d5ec76b0500000000000000",\n`
-      // );
-      outStream.write(
-        `        "${storageKey("ParachainSystem", "RelevantMessagingState")}": "0x${new Array(64)
-          .fill(0)
-          .join("")}${messagingState.slice(66)}",\n`
-      );
-      outStream.write(`        "${storageKey("AuthorFilter", "EligibleRatio")}": "0x64",\n`);
-      outStream.write(`        "${storageKey("AuthorFilter", "EligibleCount")}": "0x32000000",\n`);
-      outStream.write(
-        `        "${storageKey("CouncilCollective", "Members")}": "0x04${ALITH.slice(2)}",\n`
-      );
-      outStream.write(
-        `        "${storageKey("TechCommitteeCollective", "Members")}": "0x04${ALITH.slice(2)}",\n`
-      );
-      // outStream.write(
-      //   `        "${storageKey("ParachainInfo", "ParachainId")}": "0xe8030000",\n`
-      // );
-      outStream.write(
-        `        "${storageBlake128MapKey(
-          "System",
-          "Account",
-          ALITH
-        )}": "0x00000000000000000000000000000000d699d3ded12e14d6e701000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",\n`
-      );
-      outStream.write(
-        `        "${storageBlake128MapKey(
-          "System",
-          "Account",
-          BOB
-        )}": "0x00000000000000000000000000000000d699d3ded12e14d6e701000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",\n`
-      );
-    } else if (line.startsWith(authorMappingPrefix)) {
-      console.log(` ${chalk.red(`- Removing session`)}\n\t${line}`);
-    } else if (
-      !line.startsWith(revelentMessagingStatePrefix) &&
-      !line.startsWith(authorEligibilityRatioPrefix) &&
-      !line.startsWith(authorEligibilityCountPrefix) &&
-      !line.startsWith(councilLinePrefix) &&
-      !line.startsWith(techCommitteeeLinePrefix) &&
-      !line.startsWith(authorMappingPrefix) &&
-      // !line.startsWith(parachainIdPrefix) &&
-      !line.startsWith(lastDmqMqcHeadPrefix) &&
-      !line.startsWith(alithBalance) &&
-      !line.startsWith(bobBalance)
-    ) {
+      const newLine = `        "${storageKey("AuthorFilter", "EligibleCount")}": "0x32000000",\n`;
+      console.log(` ${chalk.green(`+ Adding AuthorFilter.EligibleCount`)}\n\t${newLine}`);
+      outStream.write(newLine);
+    } else if (line.startsWith(councilLinePrefix)) {
+      console.log(` ${chalk.red(`- Removing CouncilCollective.Members`)}\n\t${line}`);
+      const newLine = `        "${storageKey("CouncilCollective", "Members")}": "0x04${ALITH.slice(
+        2
+      )}",\n`;
+      console.log(` ${chalk.green(`+ Adding CouncilCollective.Members`)}\n\t${newLine}`);
+      outStream.write(newLine);
+    } else if (line.startsWith(techCommitteeeLinePrefix)) {
+      console.log(` ${chalk.red(`- Removing TechCommitteeCollective.Members`)}\n\t${line}`);
+      const newLine = `        "${storageKey(
+        "TechCommitteeCollective",
+        "Members"
+      )}": "0x04${ALITH.slice(2)}",\n`;
+      console.log(` ${chalk.green(`+ Adding TechCommitteeCollective.Members`)}\n\t${newLine}`);
+      outStream.write(newLine);
+    } else if (line.startsWith(lastDmqMqcHeadPrefix)) {
+      console.log(` ${chalk.red(`- Removing ParachainSystem.LastDmqMqcHead`)}\n\t${line}`);
+    } else if (line.startsWith(alithBalancePrefix)) {
+      console.log(` ${chalk.red(`- Removing System.Account: Alith`)}\n\t${line}`);
+      const newLine = `        "${storageBlake128MapKey(
+        "System",
+        "Account",
+        ALITH
+      )}": "${newAlithAccountData}",\n`;
+      console.log(` ${chalk.green(`+ Adding System.Account: Alith`)}\n\t${newLine}`);
+      outStream.write(newLine);
+    } else if (line.startsWith(totalIssuanceBalancePrefix)) {
+      console.log(` ${chalk.red(`- Removing Balances.TotalIssuance`)}\n\t${line}`);
+
+      const newLine = `        "${storageKey("Balances", "TotalIssuance")}": "${nToHex(
+        newTotalIssuance,
+        {
+          isLe: true,
+          bitLength: 128,
+        }
+      )}",\n`;
+      console.log(` ${chalk.green(`+ Balances.TotalIssuance`)}\n\t${newLine}`);
+      outStream.write(newLine);
+    } else {
       outStream.write(line);
       outStream.write("\n");
     }
+    // !line.startsWith(parachainIdPrefix)
   }
   // outStream.write("}\n")
   outStream.end();
