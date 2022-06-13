634a6451392c24bfae96a90eb112639ed0329e33
Crystalin <alan@purestake.com>
2022-06-13 19:16:50 -0400

    Better decoding

--- tests/smoke-tests/test-polkadot-decoding.ts
+++ tests/smoke-tests/test-polkadot-decoding.ts
@@ -1,5 +1,4 @@
 import { ApiDecoration } from "@polkadot/api/types";
-import { expect } from "chai";
 import chalk from "chalk";
 import { describeSmokeSuite } from "../util/setup-smoke-tests";
 
@@ -18,8 +17,11 @@ describeSmokeSuite("Polkadot API - Storage items", { wssUrl, relayWssUrl }, (con
     );
   });
 
+  // This test simply load all the storage items to make sure they can be loaded.
+  // It prevents issue where a storage item type is modified but the data is not correctly
+  // migrated.
   it("should be decodable", async function () {
-    this.timeout(3600000); // 1 hour should be enough
+    this.timeout(600000); // 1 hour should be enough
     const modules = Object.keys(context.polkadotApi.query);
     for (const moduleName of modules) {
       console.log(`  - ${moduleName}`);
@@ -30,13 +32,17 @@ describeSmokeSuite("Polkadot API - Storage items", { wssUrl, relayWssUrl }, (con
         continue;
       }
       for (const fn of fns) {
+        if (moduleName == "evm" && ["accountStorages", "accountCodes"].includes(fn)) {
+          // This is just H256 entries and quite big
+          continue;
+        }
         const keys = Object.keys(module[fn]);
         if (keys.includes("keysPaged")) {
           // Map item
           let startKey = "";
           let count = 0;
           while (true) {
-            let query = await apiAt.query.system.account.entriesPaged({
+            let query = await module[fn].entriesPaged({
               args: [],
               pageSize,
               startKey,
@@ -48,7 +54,9 @@ describeSmokeSuite("Polkadot API - Storage items", { wssUrl, relayWssUrl }, (con
             count += query.length;
             startKey = query[query.length - 1][0].toString();
           }
-          console.log(`     - ${fn}: ${count == 0 ? chalk.green(`✔`) : " "}${count} entries`);
+          console.log(
+            `     - ${fn}: ${count != 0 ? `${chalk.green(`✔`)} [${count} entries]` : "N/A"} `
+          );
         } else {
           await module[fn]();
           console.log(`     - ${fn}:  ${chalk.green(`✔`)}`);
