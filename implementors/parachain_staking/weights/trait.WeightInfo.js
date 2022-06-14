1cdf45d6ecb583a3b080b846ae2d9b777c02b21f
nanocryk <nanocryk@users.noreply.github.com>
2022-06-14 13:12:48 +0000

    deploy: c39a717e2501a3470ae589c52daaa51786189cce

--- implementors/parachain_staking/weights/trait.WeightInfo.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["parachain_staking"] = [];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
