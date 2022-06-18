295503f617fb26af50829bf2334d34599340de10
Crystalin <alan@purestake.com>
2022-06-18 17:34:14 -0400

    Adds rpc types to moonbeam-types-bundle

--- moonbeam-types-bundle/index.ts
+++ moonbeam-types-bundle/index.ts
@@ -475,6 +475,55 @@ export const moonbeamDefinitions = {
     {
       minmax: [900, undefined],
       types: TYPES_POST_900,
+      rpc: {
+        moon: {
+          isBlockFinalized: {
+            description: "Checks if an Ethereum block is finalized",
+            params: [
+              {
+                name: "blockHash",
+                type: "Hash",
+              },
+            ],
+            type: "bool",
+          },
+          isTxFinalized: {
+            description: "Checks if an Ethereum transaction is finalized",
+            params: [
+              {
+                name: "transactionHash",
+                type: "Hash",
+              },
+            ],
+            type: "bool",
+          },
+        },
+        eth: {
+          feeHistory: {
+            description: "Returns Ethereum fee history",
+            params: [
+              {
+                name: "blockCount",
+                type: "U256",
+              },
+              {
+                name: "newestBlock",
+                type: "BlockNumber",
+              },
+              {
+                name: "rewardPercentiles",
+                type: "Option<Vec<f64>>",
+              },
+            ],
+            type: "FeeHistory",
+          },
+          maxPriorityFeePerGas: {
+            description: "Returns Ethereum max priority fee per gas",
+            params: [],
+            type: "U256",
+          },
+        },
+      },
     },
   ],
 } as OverrideBundleDefinition;
@@ -507,3 +556,6 @@ export const typesBundleDeprecated = {
     moonriver: moonbeamDefinitionsDeprecated,
   },
 } as OverrideBundleType;
+
+// default types to use
+export const types = typesBundlePre900;
