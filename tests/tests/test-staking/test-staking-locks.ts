858ab8c686924d686b083f8189e825a29a00cb31
Crystalin <alan@purestake.com>
2022-06-26 17:10:31 -0400

    Adds more tests

--- tests/tests/test-staking/test-staking-locks.ts
+++ tests/tests/test-staking/test-staking-locks.ts
@@ -1,23 +1,15 @@
 import "@moonbeam-network/api-augment";
 
-import { u128 } from "@polkadot/types";
-import {
-  FrameSupportWeightsDispatchInfo,
-  FrameSystemEventRecord,
-  SpRuntimeDispatchError,
-} from "@polkadot/types/lookup";
-import { IEvent } from "@polkadot/types/types";
 import { expect } from "chai";
 
-import { alith, baltathar, ethan, generateKeyingPair } from "../../util/accounts";
+import { alith, baltathar, generateKeyingPair } from "../../util/accounts";
 import {
-  DEFAULT_GENESIS_MAPPING,
-  DEFAULT_GENESIS_STAKING,
-  GLMR,
-  MIN_GLMR_DELEGATOR,
-  MIN_GLMR_STAKING,
-} from "../../util/constants";
-import { describeDevMoonbeam, DevTestContext } from "../../util/setup-dev-tests";
+  execCouncilProposal,
+  execTechnicalCommitteeProposal,
+  notePreimage,
+} from "../../util/governance";
+import { GLMR } from "../../util/constants";
+import { describeDevMoonbeam } from "../../util/setup-dev-tests";
 
 describeDevMoonbeam("Staking - Locks", (context) => {
   const randomAccount = generateKeyingPair();
@@ -37,6 +29,7 @@ describeDevMoonbeam("Staking - Locks", (context) => {
     const locks = await context.polkadotApi.query.balances.locks(randomAccount.address);
     expect(result.successful).to.be.true;
     expect(locks.length).to.be.equal(1, "Missing lock");
+    expect(locks[0].id.toHuman().toString()).to.be.equal("DelStake");
   });
 });
 
@@ -61,5 +54,47 @@ describeDevMoonbeam("Staking - Locks", (context) => {
         .signAsync(randomAccount)
     );
     expect(result.error.name.toString()).to.be.equal("InsufficientBalance");
+  });
+});
+
+describeDevMoonbeam("Staking - Locks", (context) => {
+  const randomAccount = generateKeyingPair();
+  const STAKING_AMOUNT = 100n * GLMR;
+
+  before("Setup account balance & staking", async function () {
+    await context.createBlock(
+      context.polkadotApi.tx.balances.transfer(randomAccount.address, STAKING_AMOUNT + 1n * GLMR)
+    );
+    await context.createBlock(
+      context.polkadotApi.tx.parachainStaking
+        .delegate(alith.address, STAKING_AMOUNT, 10, 10)
+        .signAsync(randomAccount)
+    );
+
+    const proposalHash = await notePreimage(
+      context,
+      context.polkadotApi.tx.parachainStaking.setParachainBondAccount(alith.address),
+      alith
+    );
+    await execCouncilProposal(
+      context,
+      context.polkadotApi.tx.democracy.externalProposeMajority(proposalHash)
+    );
+    await execTechnicalCommitteeProposal(
+      context,
+      context.polkadotApi.tx.democracy.fastTrack(proposalHash, 100, 1)
+    );
+  });
+
+  it("should be usable for democracy vote", async function () {
+    const { result } = await context.createBlock(
+      context.polkadotApi.tx.democracy
+        .vote(0, {
+          Standard: { balance: STAKING_AMOUNT, vote: { aye: true, conviction: 1 } },
+        })
+        .signAsync(randomAccount)
+    );
+    expect(result.successful).to.be.true;
+    expect(result.events.find(({ event: { method } }) => method == "Voted")).to.not.be.undefined;
   });
 });
