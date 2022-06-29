bc0a784aa7bad8e88456cb7fe81d7e15d5dc2316
tgmichel <telmo@purestake.com>
2022-06-29 15:55:11 +0200

    Update frontier

--- runtime/moonriver/src/lib.rs
+++ runtime/moonriver/src/lib.rs
@@ -75,7 +75,7 @@ use sp_runtime::{
 	create_runtime_str, generic, impl_opaque_keys,
 	traits::{
 		BlakeTwo256, Block as BlockT, DispatchInfoOf, Dispatchable, IdentityLookup,
-		PostDispatchInfoOf,
+		PostDispatchInfoOf, UniqueSaturatedInto,
 	},
 	transaction_validity::{
 		InvalidTransaction, TransactionSource, TransactionValidity, TransactionValidityError,
