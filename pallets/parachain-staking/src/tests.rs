46b2b6498e1ddfb9fff07ce221e275313ce3854f
notlesh <steve@brewcraft.org>
2022-06-30 18:06:09 +0000

    Test that bumping because of top delegator insertion leaves no reserve

--- pallets/parachain-staking/src/tests.rs
+++ pallets/parachain-staking/src/tests.rs
@@ -9156,7 +9156,7 @@ mod jit_migrate_reserve_to_locks_tests {
 	}
 
 	#[test]
-	fn test_bumping_bottom_delegator_doesnt_leave_reserve() {
+	fn test_bumping_bottom_delegator_from_add_bottom_doesnt_leave_reserve() {
 		use crate::DelegatorState;
 
 		ExtBuilder::default()
@@ -9214,8 +9214,78 @@ mod jit_migrate_reserve_to_locks_tests {
 				ensure_delegator_unmigrated(8, 17);
 				assert!(<DelegatorState<Test>>::get(8).is_some());
 
-				// delegate with delegator 9 and kick delegator 8 out
+				// delegate with delegator 9 (to the bottom) and kick delegator 8 out
 				assert_ok!(ParachainStaking::delegate(Origin::signed(9), 100, 18, 8, 0));
+
+				// 18 should have no reserved balance and should not be delegating
+				assert!(<DelegatorState<Test>>::get(8).is_none());
+				assert_eq!(Balances::reserved_balance(8), 0);
+			});
+	}
+
+	#[test]
+	fn test_bumping_bottom_delegator_from_add_top_doesnt_leave_reserve() {
+		use crate::DelegatorState;
+
+		// same test as above, but we bump by inserting into the top instead of the bottom
+
+		ExtBuilder::default()
+			.with_balances(vec![
+				(100, 100),
+				(1, 100),
+				(2, 100),
+				(3, 100),
+				(4, 100),
+				(5, 100),
+				(6, 100),
+				(7, 100),
+				(8, 100),
+				(9, 100),
+			])
+			.with_candidates(vec![(100, 20)])
+			// delegate candidate 100 enough to fill top and bottom delegations
+			.with_delegations(vec![
+				(1, 100, 25),
+				(2, 100, 24),
+				(3, 100, 23),
+				(4, 100, 22),
+				(5, 100, 21),
+				(6, 100, 20),
+				(7, 100, 19),
+				(8, 100, 17),
+			])
+			.build()
+			.execute_with(|| {
+				assert_eq!(
+					crate::mock::MaxTopDelegationsPerCandidate::get(),
+					4u32,
+					"Test will fail if MaxTopDelegationsPerCandidate is changed"
+				);
+				assert_eq!(
+					crate::mock::MaxBottomDelegationsPerCandidate::get(),
+					4u32,
+					"Test will fail if MaxBottomDelegationsPerCandidate is changed"
+				);
+
+				let top = <TopDelegations<Test>>::get(100).expect("100 is collating");
+				assert_eq!(
+					top.delegations.len() as u32,
+					crate::mock::MaxTopDelegationsPerCandidate::get(),
+				);
+
+				let bottom = <BottomDelegations<Test>>::get(100).expect("100 is collating");
+				assert_eq!(
+					bottom.delegations.len() as u32,
+					crate::mock::MaxBottomDelegationsPerCandidate::get(),
+				);
+
+				// top and bottom delegations are full. now make sure the bottommost is unmigrated
+				crate::mock::unmigrate_delegator_from_lock_to_reserve(8);
+				ensure_delegator_unmigrated(8, 17);
+				assert!(<DelegatorState<Test>>::get(8).is_some());
+
+				// delegate with delegator 9 (to the top) and kick delegator 8 out
+				assert_ok!(ParachainStaking::delegate(Origin::signed(9), 100, 25, 8, 0));
 
 				// 18 should have no reserved balance and should not be delegating
 				assert!(<DelegatorState<Test>>::get(8).is_none());
