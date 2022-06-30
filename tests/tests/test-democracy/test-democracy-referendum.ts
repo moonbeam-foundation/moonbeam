29159828bd1931c37fba71888a1b710c2748978f
notlesh <steve@brewcraft.org>
2022-06-30 22:11:27 +0000

    WIP: test that we can both vote while staked in ts test

--- tests/tests/test-democracy/test-democracy-referendum.ts
+++ tests/tests/test-democracy/test-democracy-referendum.ts
@@ -2,7 +2,7 @@ import "@moonbeam-network/api-augment";
 
 import { expect } from "chai";
 
-import { alith } from "../../util/accounts";
+import { alith, generateKeyingPair } from "../../util/accounts";
 import { GLMR, VOTE_AMOUNT, ZERO_ADDRESS } from "../../util/constants";
 import { instantFastTrack, notePreimage } from "../../util/governance";
 import { describeDevMoonbeam } from "../../util/setup-dev-tests";
@@ -99,5 +99,44 @@ describeDevMoonbeam("Democracy - Referendum", (context) => {
 
     let parachainBondInfo = await context.polkadotApi.query.parachainStaking.parachainBondInfo();
     expect(parachainBondInfo.account.toString()).to.equal(ZERO_ADDRESS);
+  });
+
+  it("should be votable while staked", async function () {
+
+    // transfer funds into a new account so we know there will be no locks/reserves/etc
+    const randomAccount = generateKeyingPair();
+
+    await context.createBlock(
+      context.polkadotApi.tx.balances.transfer(randomAccount.address, 100_000_000_000_000_000_000n)
+    );
+
+    expect(
+      (await context.polkadotApi.query.system.account(randomAccount.address)).data.free.toBigInt()
+    ).to.equal(100_000_000_000_000_000_000n);
+
+    await context.polkadotApi.tx.parachainStaking.delegate(
+      alith.address,
+      90_000_000_000_000_000_000n,
+      0,
+      0,
+    )
+    .signAndSend(randomAccount);
+
+    await context.polkadotApi.tx.democracy.vote(0, {
+      Standard: { balance: 90_000_000_000_000_000_000n, vote: { aye: false, conviction: 1 } },
+    })
+    .signAndSend(randomAccount);
+
+    await context.createBlock();
+
+    // TODO: ensure both voting and staking occurred (e.g. through their respective pallets)
+
+    // ensure we have both locks
+    const locks = await context.polkadotApi.query.balances.locks(randomAccount.address);
+    expect(locks.length).to.be.equal(2, "Failed to incur two locks");
+    expect(locks[0].amount.toBigInt()).to.be.equal(90_000_000_000_000_000_000n);
+    expect(locks[0].id.toHuman().toString()).to.be.equal("stkngdel");
+    expect(locks[1].amount.toBigInt()).to.be.equal(90_000_000_000_000_000_000n);
+    expect(locks[1].id.toHuman().toString()).to.be.equal("fixme_something_about_voting");
   });
 });
