76495c71b63b3b0f4c9a60e9e10137f0b705037a
Nisheeth Barthwal <nbaztec@gmail.com>
2022-07-01 09:58:39 +0200

    fix doc

--- precompiles/author-mapping/AuthorMappingInterface.sol
+++ precompiles/author-mapping/AuthorMappingInterface.sol
@@ -6,13 +6,13 @@ pragma solidity >=0.8.0;
 /// The interface through which solidity contracts will interact with pallet-author.mapping
 /// Address :    0x0000000000000000000000000000000000000807
 interface AuthorMapping {
-    /// Add association
+    /// @dev Add association
     /// Selector: ef8b6cd8
     ///
     /// @param nimbusId The nimbusId to be associated
     function addAssociation(bytes32 nimbusId) external;
 
-    /// Update existing association
+    /// @dev Update existing association
     /// Selector: 25a39da5
     ///
     /// @param oldNimbusId The old nimbusId to be replaced
@@ -20,18 +20,18 @@ interface AuthorMapping {
     function updateAssociation(bytes32 oldNimbusId, bytes32 newNimbusId)
         external;
 
-    /// Clear existing association
+    /// @dev Clear existing association
     /// Selector: 448b54d6
     ///
     /// @param nimbusId The nimbusId to be cleared
     function clearAssociation(bytes32 nimbusId) external;
 
-    /// Remove keys
+    /// @dev Remove keys
     /// Selector: a36fee17
     ///
     function removeKeys() external;
 
-    /// Set keys
+    /// @dev Set keys
     /// Selector: f1ec919c
     ///
     /// @param keys The new session keys
