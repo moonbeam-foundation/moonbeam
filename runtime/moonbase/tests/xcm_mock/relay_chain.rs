db1f064cd1b63102dc228e164a2e2898d502f45a
girazoki <gorka.irazoki@gmail.com>
2022-07-05 10:55:16 +0200

    Ethereum Call Xcm (#1641)
    * upgrade all dependencies to polkadot v0.9.22
    
    * fix runtime compilation
    
    * migrate moonbeam client to jsonrpsee
    
    * remove temporary  chain spec file
    
    * use bip32 fork
    
    * fix rust tests
    
    * ignore export_current_state
    
    * fix ts tests: the logger is not initialized at startup
    
    * update frontier
    
    * upgrade to polkadot-v0.9.23
    
    * fix rust tests
    
    * fix rust tests
    
    * fix ts tests
    
    * fix ts tests
    
    * wip update tomls
    
    * runtime common
    
    * adapt to latest frontier changes
    
    * refactor xcm primitives
    
    * bring ethereum xcm types
    
    * remove previous leftover from xcm-transact in pallet-ethereum
    
    * bring pallet-ethereum-xcm
    
    * macro implementing moonbeam xcm call
    
    * Add `pallet-ethereum-xcm` to runtimes
    
    * fix pallet tests
    
    * XCM rust tests
    
    * typescript tests
    
    * Switch to simplified `Auto` base fee
    
    * Update frontier
    
    * Update frontier
    
    * Fix minor stuff
    
    * remove renamed dependency
    
    * FMT
    
    * Re-pin frontier
    
    * Fix tests, block gas limit within boundaries
    
    * Add mock weight to gas mapping
    
    * Do not add pallet-ethereum to moonbeam
    
    * Remove ethereum-xcm pallet from Moonriver
    
    * update nimbus
    
    * fix tests
    
    * Fixes missing init logs
    
    * More test fixes
    
    * Re-pin frontier
    
    * Remove traces and pin to frontier struct
    
    * Cleanup
    
    * FMT
    
    * sort
    
    * remove new pallet from generic Cargo.toml
    
    * sanitize cargo.lock
    
    * re-update lock
    
    * Fix pallet-ethereum tests
    
    * keep working on fixing tests
    
    * add ethereum-xcm to benchmark
    
    * toml sort
    
    * Add hardware benchmark telemetry
    
    * Add gas estimation on tests
    
    * better comment
    
    * remove log
    
    * Fixes call permit precompile dependencies
    
    * Re-pin Frontier
    
    * More ts tests fixing
    
    * prettier
    
    * Remove `WeightInfo`
    
    * upgrade GH action yogevbd/enforce-label-action
    
    * t
    
    * editorconfig
    
    * Contract
    
    * Add `ReservedXcmpWeight` to `pallet-ethereum-xcm`
    
    * fix ethereum-xcm mock
    
    Co-authored-by: librelois <c@elo.tf>
    Co-authored-by: tgmichel <telmo@purestake.com>
    Co-authored-by: Crystalin <alan@purestake.com>

--- runtime/moonbase/tests/xcm_mock/relay_chain.rs
+++ runtime/moonbase/tests/xcm_mock/relay_chain.rs
@@ -159,6 +159,7 @@ impl Config for XcmConfig {
 	type AssetTrap = XcmPallet;
 	type AssetClaims = XcmPallet;
 	type SubscriptionService = XcmPallet;
+	type CallDispatcher = Call;
 }
 
 pub type LocalOriginToLocation = SignedToAccountId32<Origin, AccountId, KusamaNetwork>;
