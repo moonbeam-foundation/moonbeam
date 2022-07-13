6135b67722cba1e8df2a519c9e4ef2aee3376a57
gorka <gorka.irazoki@gmail.com>
2022-07-13 14:28:08 +0200

    First startlottery tests working

--- tests/tests/test-randomness/test-randomness-vrf-lottery.ts
+++ tests/tests/test-randomness/test-randomness-vrf-lottery.ts
@@ -6,10 +6,13 @@ import { Contract } from "web3-eth-contract";
 
 import {
   alith,
+  ALITH_ADDRESS,
   ALITH_PRIVATE_KEY,
   baltathar,
+  BALTATHAR_ADDRESS,
   BALTATHAR_PRIVATE_KEY,
   charleth,
+  CHARLETH_ADDRESS,
   CHARLETH_PRIVATE_KEY,
 } from "../../util/accounts";
 import { jumpToRound } from "../../util/block";
@@ -22,10 +25,13 @@ import {
   createContract,
   createContractExecution,
   createTransaction,
+  TRANSACTION_TEMPLATE
 } from "../../util/transactions";
 import { deployContract } from "../test-precompile/test-precompile-democracy";
+import { web3EthCall } from "../../util/providers";
 
 const LOTTERY_CONTRACT = getCompiled("RandomnessLotteryDemo");
+const LOTTERY_INTERFACE = new ethers.utils.Interface(LOTTERY_CONTRACT.contract.abi);
 
 describeDevMoonbeam("Randomness VRF - Lottery Demo", (context) => {
   let lotteryContract: Contract;
@@ -35,19 +41,34 @@ describeDevMoonbeam("Randomness VRF - Lottery Demo", (context) => {
     await context.createBlock(rawTx);
 
     // Adds participants
-    for (const privateKey of [ALITH_PRIVATE_KEY, BALTATHAR_PRIVATE_KEY, CHARLETH_PRIVATE_KEY]) {
+    for (const [privateKey, from] of [[ALITH_PRIVATE_KEY, ALITH_ADDRESS], [BALTATHAR_PRIVATE_KEY, BALTATHAR_ADDRESS], [CHARLETH_PRIVATE_KEY, CHARLETH_ADDRESS]]) {
       await context.createBlock(
-        createContractExecution(
-          context,
-          {
-            contract,
-            contractCall: contract.methods.participate(),
-          },
-          { privateKey, value: Web3.utils.toWei("1", "ether") }
-        )
+        createTransaction(context, {
+          ...TRANSACTION_TEMPLATE,
+          privateKey,
+          from,
+          to: lotteryContract.options.address,
+          data: LOTTERY_INTERFACE.encodeFunctionData("participate", []),
+          value:  Web3.utils.toWei("1", "ether")
+        })
       );
     }
   });
 
-  it("should be successful", async function () {});
+  it("should be successful", async function () {
+
+    const { result } =  await context.createBlock(
+      createTransaction(context, {
+        ...ALITH_TRANSACTION_TEMPLATE,
+        to: lotteryContract.options.address,
+        data: LOTTERY_INTERFACE.encodeFunctionData("startLottery", []),
+        value:  Web3.utils.toWei("1", "ether")
+      })
+    );
+
+    const receipt = await context.web3.eth.getTransactionReceipt(result.hash);
+
+    expect(receipt.status).to.equal(true);
+  });
+  
 });
