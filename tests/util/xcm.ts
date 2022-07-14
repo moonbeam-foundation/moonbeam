6a1ed2d364febd23b371b6a13b76fee0f124c774
gorka <gorka.irazoki@gmail.com>
2022-07-14 15:20:40 +0200

    Cleanup of util/xcm

--- tests/util/xcm.ts
+++ tests/util/xcm.ts
@@ -5,7 +5,7 @@ import { DevTestContext } from "./setup-dev-tests";
 
 import type { CumulusPalletParachainSystemRelayStateSnapshotMessagingStateSnapshot } from "@polkadot/types/lookup";
 
-// Creates and returns the tx that overrides the paraHRMP existance
+// Creates and returns the tx that overrides the paraHRMP existence
 // This needs to be inserted at every block in which you are willing to test
 // state changes
 // The reason is that set_validation_data inherent overrides it
@@ -55,10 +55,10 @@ export function mockHrmpChannelExistanceTx(
     ) as any;
 
   // Get keys to modify state
-  let module = xxhashAsU8a(new TextEncoder().encode("ParachainSystem"), 128);
-  let account_key = xxhashAsU8a(new TextEncoder().encode("RelevantMessagingState"), 128);
+  const module = xxhashAsU8a(new TextEncoder().encode("ParachainSystem"), 128);
+  const account_key = xxhashAsU8a(new TextEncoder().encode("RelevantMessagingState"), 128);
 
-  let overallKey = new Uint8Array([...module, ...account_key]);
+  const overallKey = new Uint8Array([...module, ...account_key]);
 
   return context.polkadotApi.tx.system.setStorage([
     [u8aToHex(overallKey), u8aToHex(stateToInsert.toU8a())],
