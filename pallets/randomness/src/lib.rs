d941fab3fa63cf975e1397dc527b57f6c905c3f6
4meta5 <asinghchrony@protonmail.com>
2022-07-14 13:59:24 -0400

    address review

--- pallets/randomness/src/lib.rs
+++ pallets/randomness/src/lib.rs
@@ -315,6 +315,7 @@ pub mod pallet {
 			let next_id = request_id
 				.checked_add(1u64)
 				.ok_or(Error::<T>::RequestCounterOverflowed)?;
+			let new_snapshot = existing_randomness_snapshot.increment_request_count::<T>()?;
 			// send deposit to reserve account
 			T::Currency::transfer(
 				&contract_address,
@@ -324,10 +325,7 @@ pub mod pallet {
 			)?;
 			let info_key: RequestType<T> = info.into();
 			if let Some(existing_randomness_snapshot) = <RandomnessResults<T>>::take(&info_key) {
-				<RandomnessResults<T>>::insert(
-					&info_key,
-					existing_randomness_snapshot.increment_request_count::<T>()?,
-				);
+				<RandomnessResults<T>>::insert(&info_key, new_snapshot);
 			} else {
 				<RandomnessResults<T>>::insert(&info_key, RandomnessResult::new());
 			}
