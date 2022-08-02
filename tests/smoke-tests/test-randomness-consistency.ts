3a94c9cd5d12005fdd0ec36e34a43e59c35fe61c
Stephen Shelton <steve@brewcraft.org>
2022-08-02 16:19:36 -0600

    Refactor to query distinct requestIds

--- tests/smoke-tests/test-randomness-consistency.ts
+++ tests/smoke-tests/test-randomness-consistency.ts
@@ -19,20 +19,74 @@ describeSmokeSuite(`Verify number of proxies per account`, { wssUrl, relayWssUrl
   let atBlockNumber: number = 0;
   let apiAt: ApiDecoration<"promise"> = null;
 
-  before("Setup API", async function () {
+  const requestIds: number[] = [];
+
+  before("Retrieve all requests", async function () {
+    // It takes time to load all the proxies.
+    // TEMPLATE: Adapt the timeout to be an over-estimate
+    this.timeout(30_000); // 30s
+
+    const limit = 1000;
+    let last_key = "";
+    let count = 0;
+
     atBlockNumber = process.env.BLOCK_NUMBER
       ? parseInt(process.env.BLOCK_NUMBER)
       : (await context.polkadotApi.rpc.chain.getHeader()).number.toNumber();
     apiAt = await context.polkadotApi.at(
       await context.polkadotApi.rpc.chain.getBlockHash(atBlockNumber)
     );
+
+    // TEMPLATE: query the data
+    while (true) {
+      let query = await apiAt.query.randomness.requests.entriesPaged({
+        args: [],
+        pageSize: limit,
+        startKey: last_key,
+      });
+
+      if (query.length == 0) {
+        break;
+      }
+      count += query.length;
+
+      // TEMPLATE: convert the data into the format you want (usually a dictionary per account)
+      for (const request of query) {
+        const key = request[0].toString()
+
+        // requestId will be the last 8 bytes (16) nibbles but we need endianness swap
+        expect(key.length >= 18, "storage key should be at least 64 bits"); // assumes "0x"
+        const subkey = key.slice(-16);
+        let idHex = "";
+        for (let i = 0; i < subkey.length; i += 2) {
+          idHex += subkey.charAt(i+1);
+          idHex += subkey.charAt(i);
+        }
+        // reverse
+        idHex = idHex.split("").reverse().join("");
+        const requestId = parseInt(idHex, 16);
+
+        requestIds.push(requestId);
+        last_key = key;
+      }
+
+      // Debug logs to make sure it keeps progressing
+      // TEMPLATE: Adapt log line
+      if (true || count % (10 * limit) == 0) {
+        debug(`Retrieved ${count} requests`);
+        debug(`Array: ${requestIds}`);
+      }
+    }
+
+    // TEMPLATE: Adapt proxies
+    debug(`Retrieved ${count} total proxies`);
   });
 
   it("should have fewer Requests than RequestCount", async function () {
     this.timeout(10000);
 
     const requestCount = (await apiAt.query.randomness.requestCount() as any).toNumber();
-    const numOutstandingRequests = (await apiAt.query.randomness.requests.entries()).length;
+    const numOutstandingRequests = requestIds.length;
 
     expect(numOutstandingRequests).to.be.lessThanOrEqual(requestCount);
   });
