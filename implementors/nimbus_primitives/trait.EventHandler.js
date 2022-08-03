bfb5b5387385130cbf4dc362503551936e9bf160
4meta5 <4meta5@users.noreply.github.com>
2022-08-03 14:14:43 +0000

    deploy: 9b774cf1ac6ec98bd48f49358da3015f98e7cbd0

--- implementors/nimbus_primitives/trait.EventHandler.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["pallet_parachain_staking"] = [{"text":"impl&lt;T:&nbsp;<a class=\"trait\" href=\"pallet_parachain_staking/pallet/trait.Config.html\" title=\"trait pallet_parachain_staking::pallet::Config\">Config</a>&gt; EventHandler&lt;&lt;T as Config&gt;::AccountId&gt; for <a class=\"struct\" href=\"pallet_parachain_staking/pallet/struct.Pallet.html\" title=\"struct pallet_parachain_staking::pallet::Pallet\">Pallet</a>&lt;T&gt;","synthetic":false,"types":["pallet_parachain_staking::pallet::Pallet"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
