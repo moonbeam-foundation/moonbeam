f4c211d1adcfcaa76eb473848176f1d1b49b5ddc
gorka <gorka.irazoki@gmail.com>
2022-08-05 09:46:54 +0200

    Instance modification to match new interface

--- tests/contracts/solidity/XCMTransactorInstance.sol
+++ tests/contracts/solidity/XCMTransactorInstance.sol
@@ -69,6 +69,27 @@ contract XcmTransactorInstance is XcmTransactor {
         );
     }
 
+    function transact_through_derivative_multilocation_custom_fee_and_weight(
+        uint8 transactor,
+        uint16 index,
+        Multilocation memory fee_asset,
+        uint64 transact_weight,
+        bytes memory inner_call,
+        uint256 fee_amount,
+        uint64 overall_weight
+    ) external override {
+        xcmtransactor
+            .transact_through_derivative_multilocation_custom_fee_and_weight(
+                transactor,
+                index,
+                fee_asset,
+                transact_weight,
+                inner_call,
+                fee_amount,
+                overall_weight
+            );
+    }
+
     function transact_through_derivative(
         uint8 transactor,
         uint16 index,
@@ -82,6 +103,26 @@ contract XcmTransactorInstance is XcmTransactor {
             currency_id,
             weight,
             inner_call
+        );
+    }
+
+    function transact_through_derivative_custom_fee_and_weight(
+        uint8 transactor,
+        uint16 index,
+        address currency_id,
+        uint64 transact_weight,
+        bytes memory inner_call,
+        uint256 fee_amount,
+        uint64 overall_weight
+    ) external override {
+        xcmtransactor.transact_through_derivative_custom_fee_and_weight(
+            transactor,
+            index,
+            currency_id,
+            transact_weight,
+            inner_call,
+            fee_amount,
+            overall_weight
         );
     }
 
@@ -99,6 +140,24 @@ contract XcmTransactorInstance is XcmTransactor {
         );
     }
 
+    function transact_through_signed_custom_fee_and_weight(
+        Multilocation memory dest,
+        address fee_location_address,
+        uint64 transact_weight,
+        bytes memory call,
+        uint256 fee_amount,
+        uint64 overall_weight
+    ) external override {
+        xcmtransactor.transact_through_signed_custom_fee_and_weight(
+            dest,
+            fee_location_address,
+            transact_weight,
+            call,
+            fee_amount,
+            overall_weight
+        );
+    }
+
     function transact_through_signed_multilocation(
         Multilocation memory dest,
         Multilocation memory fee_location,
@@ -111,5 +170,24 @@ contract XcmTransactorInstance is XcmTransactor {
             weight,
             call
         );
+    }
+
+    function transact_through_signed_multilocation_custom_fee_and_weight(
+        Multilocation memory dest,
+        Multilocation memory fee_location,
+        uint64 transact_weight,
+        bytes memory call,
+        uint256 fee_amount,
+        uint64 overall_weight
+    ) external override {
+        xcmtransactor
+            .transact_through_signed_multilocation_custom_fee_and_weight(
+                dest,
+                fee_location,
+                transact_weight,
+                call,
+                fee_amount,
+                overall_weight
+            );
     }
 }
