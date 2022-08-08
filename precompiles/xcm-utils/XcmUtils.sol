c4d6ccfda074bab009fef0b576e045f1b67ea0ae
gorka <gorka.irazoki@gmail.com>
2022-08-08 16:29:45 +0200

    Interface doc

--- precompiles/xcm-utils/XcmUtils.sol
+++ precompiles/xcm-utils/XcmUtils.sol
@@ -2,12 +2,12 @@
 pragma solidity >=0.8.0;
 
 /**
- * @title Xcm Transactor Interface
- * The interface through which solidity contracts will interact with xcm transactor pallet
- * Address :    0x0000000000000000000000000000000000000806
+ * @title Xcm Utils Interface
+ * The interface through which solidity contracts will interact with xcm utils pallet
+ * Address :    0x000000000000000000000000000000000000080B
  */
 
-interface XcmTransactor {
+interface XcmUtils {
 
     // A multilocation is defined by its number of parents and the encoded junctions (interior)
     struct Multilocation {
@@ -15,7 +15,13 @@ interface XcmTransactor {
         bytes [] interior;
     }
 
+     /** Retrieve the account associated to a given MultiLocation
+     *
+     * @param multilocation The multilocation that we want to know to which account maps to
+    * @return account The account the multilocation maps to in this chain
 
+     * Selector: 343b3e00
+     */
     function multilocationToAccount(Multilocation memory multilocation) external view returns(address account);
 
 }
