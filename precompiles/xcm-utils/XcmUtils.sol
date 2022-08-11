9501be1fb2b695938240b48dfb3a295467adbfd6
gorka <gorka.irazoki@gmail.com>
2022-08-11 18:26:28 +0200

    Update interface to standard

--- precompiles/xcm-utils/XcmUtils.sol
+++ precompiles/xcm-utils/XcmUtils.sol
@@ -1,11 +1,9 @@
 // SPDX-License-Identifier: GPL-3.0-only
 pragma solidity >=0.8.0;
 
-/**
- * @title Xcm Utils Interface
- * The interface through which solidity contracts will interact with xcm utils pallet
- * Address :    0x000000000000000000000000000000000000080B
- */
+/// @title Xcm Utils Interface
+/// The interface through which solidity contracts will interact with xcm utils pallet
+/// @custom:address 0x000000000000000000000000000000000000080B
 
 interface XcmUtils {
 
@@ -15,12 +13,9 @@ interface XcmUtils {
         bytes [] interior;
     }
 
-     /** Retrieve the account associated to a given MultiLocation
-     *
-     * @param multilocation The multilocation that we want to know to which account maps to
-    * @return account The account the multilocation maps to in this chain
-
-     * Selector: 343b3e00
-     */
+    /// Get retrieve the account associated to a given MultiLocation
+    /// @custom:selector 343b3e00
+    /// @param multilocation The multilocation that we want to know to which account maps to
+    /// @return account The account the multilocation maps to in this chain
     function multilocationToAddress(Multilocation memory multilocation) external view returns(address account);
 }
