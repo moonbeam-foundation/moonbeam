28f743853ec4343a7c9fd29ca658fc1fff44626f
Stephen Shelton <steve@brewcraft.org>
2022-08-11 09:06:22 -0600

    Tune extrinsic base fee (#1576)
    * Implement custom BlockWeights to tune extrinsic base fee
    
    * Fix some tests
    
    * Adjust EXTRINSIC_BASE_COST
    
    * Rework staking test that can't fit in one block now
    
    * prettier
    
    * Correct EXTRINSIC_BASE_COST in constant
    
    * Reflect latest change to EXTRINSIC_GAS_LIMIT
    
    * Reflect change to Delegator Leaving (right?)
    
    * Revert "Reflect change to Delegator Leaving (right?)"
    
    This reverts commit d9daf78013e4ebf449ba4624cce4984e63260c29.
    
    * Fix test

--- tests/tests/test-staking/test-staking-locks.ts
+++ tests/tests/test-staking/test-staking-locks.ts
@@ -447,13 +447,12 @@ describeDevMoonbeam("Staking - Locks - bottom delegator removed", (context) => {
   before("setup candidate & delegations", async function () {
     this.timeout(20000);
 
-    // Create the delegators to fill the lists
-    additionalDelegators = new Array(
+    const maxDelegations =
       context.polkadotApi.consts.parachainStaking.maxTopDelegationsPerCandidate.toNumber() +
-        context.polkadotApi.consts.parachainStaking.maxBottomDelegationsPerCandidate.toNumber()
-    )
-      .fill(0)
-      .map(() => generateKeyringPair());
+      context.polkadotApi.consts.parachainStaking.maxBottomDelegationsPerCandidate.toNumber();
+
+    // Create the delegators to fill the lists
+    additionalDelegators = new Array(maxDelegations).fill(0).map(() => generateKeyringPair());
 
     await expectOk(
       context.createBlock(
@@ -482,20 +481,29 @@ describeDevMoonbeam("Staking - Locks - bottom delegator removed", (context) => {
       `Unexpected number of locks: ${locks.map((l) => l.id.toHuman().toString()).join(` - `)}`
     );
 
-    await expectOk(
-      context.createBlock(
-        [...additionalDelegators].map((account, i) =>
-          context.polkadotApi.tx.parachainStaking
-            .delegate(alith.address, MIN_GLMR_DELEGATOR + GLMR, i + 1, 1)
-            .signAsync(account)
-        )
-      )
+    const txns = await [...additionalDelegators].map((account, i) =>
+      context.polkadotApi.tx.parachainStaking
+        .delegate(alith.address, MIN_GLMR_DELEGATOR + GLMR, i + 1, 1)
+        .signAsync(account)
     );
+
+    // this can no longer fit in one block
+    const batchSize = 100;
+    for (let i = 0; i < txns.length; i += batchSize) {
+      await expectOk(context.createBlock(txns.slice(i, i + batchSize)));
+    }
+
+    const alithCandidateInfo = (
+      (await context.polkadotApi.query.parachainStaking.candidateInfo(alith.address)) as any
+    ).unwrap();
+    expect(alithCandidateInfo.delegationCount.toNumber()).to.equal(additionalDelegators.length);
 
     const newLocks = await context.polkadotApi.query.balances.locks(randomAccount.address);
     expect(newLocks.length).to.be.equal(
       0,
-      "Lock should have been removed after getting removed from bottom delegations"
+      `Unexpected number of locks: ${newLocks
+        .map((l) => `${l.id.toHuman().toString()}: ${l.amount.toHuman().toString()}`)
+        .join(` - `)}`
     );
   });
 });
