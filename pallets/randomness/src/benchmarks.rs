e57dc4ba1c84e00d1907e97b2da07217637deef8
4meta5 <asinghchrony@protonmail.com>
2022-08-16 18:38:31 -0400

    improve

--- pallets/randomness/src/benchmarks.rs
+++ pallets/randomness/src/benchmarks.rs
@@ -18,7 +18,10 @@
 
 //! Benchmarking
 use crate::vrf::*;
-use crate::{BalanceOf, Config, LocalVrfOutput, Pallet, RandomnessResults, Request, RequestType};
+use crate::{
+	BalanceOf, Config, LocalVrfOutput, NotFirstBlock, Pallet, RandomnessResults, Request,
+	RequestType,
+};
 use frame_benchmarking::{account, benchmarks, impl_benchmark_test_suite, Zero};
 use frame_support::{
 	dispatch::DispatchResult,
@@ -36,7 +39,7 @@ use sp_core::{
 	sr25519, H160, H256,
 };
 use sp_runtime::traits::One;
-use sp_std::vec;
+use sp_std::{mem::size_of, vec};
 
 /// Create a funded user from the input
 fn fund_user<T: Config>(user: H160, fee: BalanceOf<T>) {
@@ -67,7 +70,9 @@ benchmarks! {
 		// 	]
 		// 	PreRuntime: [
 		// 	  rand
-		// 	  0x708859b63dd8284e829de2cbd90cb9b7de7eb9bec2d15ec45523e9039216fd362aacbc19ff00abbed38bac4be9acebef32e5d5ab4b1b3aa60bcbc7ca3b22880824f93050f4d51798a0ea41e8be7f0e1a541b60df46103f7785a9129e425da107
+		// 	  0x708859b63dd8284e829de2cbd90cb9b7de7eb9bec2d15ec45523e9039216fd362aacbc19ff00abbed38
+		//bac4be9acebef32e5d5ab4b1b3aa60bcbc7ca3b22880824f93050f4d51798a0ea41e8be7f0e1a541b60df4610
+		//3f7785a9129e425da107
 		// 	]
 		// VRFId 0x4ceed6a5aaa5377723234853ab0862d24de182b4cc66302f94229888f84adc7b
 		fn hex_decode(input: String) -> [u8; 32] {
@@ -78,16 +83,20 @@ benchmarks! {
 		}
 		fn predigest_decode(input: String) -> PreDigest {
 			let output = hex::decode(input).expect("expect to decode input");
-			let mut ret: [u8; 64] = [0u8; 64];
-			ret.copy_from_slice(&output[0..64]);
+			const PRE_DIGEST_BYTE_LEN: usize = size_of::<PreDigest>() as usize;
+			let mut ret: [u8; PRE_DIGEST_BYTE_LEN] = [0u8; PRE_DIGEST_BYTE_LEN];
+			ret.copy_from_slice(&output[0..PRE_DIGEST_BYTE_LEN]);
 			Decode::decode(&mut ret.as_slice()).expect("expect to decode predigest")
 		}
+		// TODO: put raw inputs in own variables
+		// TODO: get inputs from TS script instead of manually
 		let nimbus_id: NimbusId = sr25519::Public::unchecked_from(hex_decode("4a3017130aa08a05121b6e1b23f9db471e32da06acddba1bfa0be25c2748bb52".to_string())).into();
 		let vrf_id: VrfId = sr25519::Public::unchecked_from(hex_decode("4ceed6a5aaa5377723234853ab0862d24de182b4cc66302f94229888f84adc7b".to_string())).into();
 		let vrf_input: [u8; 32] = hex_decode("7d2b74fab7f37c93344abcc282d428985ddee49e494a8950c76df0342bfe6f02".to_string());
 		let vrf_pre_digest = predigest_decode("708859b63dd8284e829de2cbd90cb9b7de7eb9bec2d15ec45523e9039216fd362aacbc19ff00abbed38bac4be9acebef32e5d5ab4b1b3aa60bcbc7ca3b22880824f93050f4d51798a0ea41e8be7f0e1a541b60df46103f7785a9129e425da107".to_string());
 		let last_vrf_output: T::Hash = Decode::decode(&mut vrf_input.as_slice()).ok().expect("decode into same type");
 		LocalVrfOutput::<T>::put(Some(last_vrf_output));
+		NotFirstBlock::<T>::put(());
 		let transcript = make_transcript::<T::Hash>(LocalVrfOutput::<T>::get().unwrap_or_default());
 
 		let nimbus_digest_item = NimbusDigest::nimbus_pre_digest(nimbus_id.clone());
@@ -98,6 +107,7 @@ benchmarks! {
 		// set keys in author mapping
 		T::KeySetter::benchmark_set_keys(nimbus_id, account("key", 0u32, 0u32), vrf_id.clone());
 	}: {
+		// verification failing, try different set of inputs
 		Pallet::<T>::on_initialize(T::BlockNumber::default());
 	}
 	verify {
