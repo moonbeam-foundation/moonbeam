964fe353fdcc29975159996d5811948c297f0fa8
Nisheeth Barthwal <nbaztec@gmail.com>
2022-08-17 12:17:11 +0200

    fix randomness precompile test

--- precompiles/randomness/src/lib.rs
+++ precompiles/randomness/src/lib.rs
@@ -166,7 +166,7 @@ where
 		let selector = handle.read_selector()?;
 
 		handle.check_function_modifier(match selector {
-			Action::RelayEpochIndex | Action::GetRequestStatus | Action::GetRequest => {
+			Action::RelayEpochIndex | Action::GetRequestStatus | Action::GetRequest | Action::RequiredDeposit => {
 				FunctionModifier::View
 			}
 			_ => FunctionModifier::NonPayable,
