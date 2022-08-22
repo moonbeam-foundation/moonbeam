7dd15edb932c11bad127f79d1cf32dec094afe89
nanocryk <6422796+nanocryk@users.noreply.github.com>
2022-08-22 12:52:01 +0000

    wip ts tests

---
+++ tests/tests/test-precompile/test-precompile-collective.ts
@@ -0,0 +1,520 @@
+import "@moonbeam-network/api-augment";
+
+import { expect } from "chai";
+import { ethers } from "ethers";
+
+import {
+  alith,
+  baltathar,
+  charleth,
+  CHARLETH_ADDRESS,
+  dorothy,
+  DOROTHY_ADDRESS,
+  ethan,
+} from "../../util/accounts";
+import { describeDevMoonbeam } from "../../util/setup-dev-tests";
+import { getCompiled } from "../../util/contracts";
+import {
+  ALITH_TRANSACTION_TEMPLATE,
+  BALTATHAR_TRANSACTION_TEMPLATE,
+  createContract,
+  createTransaction,
+  TRANSACTION_TEMPLATE,
+} from "../../util/transactions";
+import { expectEVMResult } from "../../util/eth-transactions";
+import {
+  PRECOMPILE_COUNCIL_ADDRESS,
+  PRECOMPILE_TREASURY_COUNCIL_ADDRESS,
+} from "../../util/constants";
+
+const COLLECTIVE_CONTRACT_JSON = getCompiled("CollectivePrecompile");
+const COLLECTIVE_INTERFACE = new ethers.utils.Interface(COLLECTIVE_CONTRACT_JSON.contract.abi);
+
+// Duplicate of treasury tests but with interaction with the collectives throught
+// precompiles.
+
+describeDevMoonbeam("Treasury council precompile #1", (context) => {
+  it("should not be able to be approved by a non-council member", async function () {
+    // Ethan submit a treasury proposal
+
+    await context.createBlock(
+      context.polkadotApi.tx.treasury.proposeSpend(10, baltathar.address).signAsync(ethan)
+    );
+
+    // Verify that the proposal is submitted
+    let proposalCount = await context.polkadotApi.query.treasury.proposalCount();
+    expect(proposalCount.toHuman() === "1").to.equal(true, "new proposal should have been added");
+
+    // Try to approve the proposal directly (must be fail)
+    let approvals = (await context.polkadotApi.query.treasury.approvals()) as any;
+    expect(approvals.length).to.equal(0, "No proposal must have been approved");
+  });
+});
+
+describeDevMoonbeam("Treasury council precompile #2", (context) => {
+  it("should not be able to be rejected by a non-council member", async function () {
+    // Ethan submit a treasury proposal
+
+    await context.createBlock(
+      context.polkadotApi.tx.treasury.proposeSpend(10, baltathar.address).signAsync(ethan)
+    );
+
+    // Verify that the proposal is submitted
+    let proposalCount = await context.polkadotApi.query.treasury.proposalCount();
+    expect(proposalCount.toHuman() === "1").to.equal(true, "new proposal should have been added");
+
+    // Try to reject the proposal directly (must be fail)
+    await context.polkadotApi.tx.treasury.rejectProposal(0).signAsync(ethan);
+    expect(await context.polkadotApi.query.treasury.proposals(0)).not.equal(
+      null,
+      "The proposal should not have been deleted"
+    );
+  });
+});
+
+describeDevMoonbeam("Treasury council precompile #3", (context) => {
+  // prettier-ignore
+  it(
+    "should be rejected if three-fifths of the treasury council did not vote in favor",
+    async function () {
+      // Ethan submit a treasury proposal
+
+      await context.createBlock(
+        context.polkadotApi.tx.treasury.proposeSpend(10, baltathar.address).signAsync(ethan)
+      );
+
+      // Verify that the proposal is submitted
+      let proposalCount = await context.polkadotApi.query.treasury.proposalCount();
+      expect(proposalCount.toHuman() === "1").to.equal(true, "new proposal should have been added");
+
+      // A council member attempts to approve the proposal on behalf of the council
+      const { result: evmResult } = await context.createBlock(
+        createTransaction(context, {
+          ...BALTATHAR_TRANSACTION_TEMPLATE,
+          from: CHARLETH_ADDRESS,
+          to: PRECOMPILE_TREASURY_COUNCIL_ADDRESS,
+          data: COLLECTIVE_INTERFACE.encodeFunctionData("propose", [
+            1,
+            context.polkadotApi.tx.treasury.approveProposal(0).toHex()
+          ]),          
+        })
+      );
+      expectEVMResult(evmResult.events, "Revert");
+
+      // Verify that the proposal is not deleted
+      expect(await context.polkadotApi.query.treasury.proposals(0)).not.equal(
+        null,
+        "The proposal must not have been deleted"
+      );
+    }
+  );
+});
+
+describeDevMoonbeam("Treasury council precompile #4", (context) => {
+  // prettier-ignore
+  it(
+    "should not be rejected by less than half of the members of the treasury council",
+    async function () {
+      // Ethan submit a treasury proposal
+
+      await context.createBlock(
+        context.polkadotApi.tx.treasury.proposeSpend(10, baltathar.address).signAsync(ethan)
+      );
+
+      // Verify that the proposal is submitted
+      let proposalCount = await context.polkadotApi.query.treasury.proposalCount();
+      expect(proposalCount.toHuman() === "1").to.equal(true, "new proposal should have been added");
+
+      // A council member attempts to reject the proposal on behalf of the council
+      // (must fail because there is not a quorum)
+      const { result: evmResult } = await context.createBlock(
+        createTransaction(context, {
+          ...BALTATHAR_TRANSACTION_TEMPLATE,
+          from: CHARLETH_ADDRESS,
+          to: PRECOMPILE_TREASURY_COUNCIL_ADDRESS,
+          data: COLLECTIVE_INTERFACE.encodeFunctionData("propose", [
+            1,
+            context.polkadotApi.tx.treasury.rejectProposal(0).toHex()
+          ]),          
+        })
+      );
+      expectEVMResult(evmResult.events, "Revert");
+
+      // Verify that the proposal is not approved
+      let approvals = (await context.polkadotApi.query.treasury.approvals()) as any;
+      expect(approvals.length).to.equal(0, "No proposal should have been approved");
+    }
+  );
+});
+
+describeDevMoonbeam("Treasury council precompile #5", (context) => {
+  it("should be approvable by root", async function () {
+    // Ethan submit a treasury proposal
+
+    await context.createBlock(
+      context.polkadotApi.tx.treasury.proposeSpend(10, baltathar.address).signAsync(ethan)
+    );
+
+    // Verify that the proposal is submitted
+    let proposalCount = await context.polkadotApi.query.treasury.proposalCount();
+    expect(proposalCount.toHuman() === "1").to.equal(true, "new proposal should have been added");
+
+    // Root approve the proposal directly
+    await context.createBlock(
+      context.polkadotApi.tx.sudo
+        .sudo(context.polkadotApi.tx.treasury.approveProposal(0))
+        .signAsync(alith)
+    );
+
+    // Verify that the proposal is approved
+    let approvals = (await context.polkadotApi.query.treasury.approvals()) as any;
+    expect(approvals.length).to.equal(1, "One proposal should have been approved");
+  });
+});
+
+describeDevMoonbeam("Treasury council precompile #6", (context) => {
+  it("should be rejectable by root", async function () {
+    // Ethan submit a treasury proposal
+
+    await context.createBlock(
+      context.polkadotApi.tx.treasury.proposeSpend(10, baltathar.address).signAsync(ethan)
+    );
+
+    // Verify that the proposal is submitted
+    let proposalCount = await context.polkadotApi.query.treasury.proposalCount();
+    expect(proposalCount.toHuman() === "1").to.equal(true, "new proposal should have been added");
+
+    // Root approve the proposal directly
+    await context.createBlock(
+      context.polkadotApi.tx.sudo
+        .sudo(context.polkadotApi.tx.treasury.rejectProposal(0))
+        .signAsync(alith)
+    );
+
+    // Verify that the proposal is deleted
+    expect(await (await context.polkadotApi.query.treasury.proposals(0)).toHuman()).to.equal(
+      null,
+      "The proposal must have been deleted"
+    );
+  });
+});
+
+describeDevMoonbeam("Treasury council precompile #7", (context) => {
+  // prettier-ignore
+  it(
+    "should NO LONGER be approved if the three fifths of the council voted for it",
+    async function () {
+      // To run this long scenarios, we have to go through a lot of steps, so anyway we won't be
+      // able to keep this tests within 5 seconds.
+      this.timeout(10_000);
+
+      // Ethan submit a treasury proposal
+
+      await context.createBlock(
+        context.polkadotApi.tx.treasury.proposeSpend(10, baltathar.address).signAsync(ethan)
+      );
+
+      // Verify that the proposal is submitted
+      let proposalCount = (await context.polkadotApi.query.treasury.proposalCount()) as any;
+      expect(proposalCount.toBigInt()).to.equal(1n, "new proposal should have been added");
+
+      // Charleth submit the proposal to the council
+      const proposal = context.polkadotApi.tx.treasury.approveProposal(0);
+      const proposalHash = proposal.hash;
+
+      {
+        let {result} = await context.createBlock(
+          createTransaction(context, {
+            ...BALTATHAR_TRANSACTION_TEMPLATE,
+            from: CHARLETH_ADDRESS,
+            to: PRECOMPILE_COUNCIL_ADDRESS,
+            gas: 1_000_000,
+            data: COLLECTIVE_INTERFACE.encodeFunctionData("propose", [
+              2,
+              proposal.toHex()
+            ]),          
+          })
+        );
+        console.log(result.events.toString());
+        expectEVMResult(result.events, "Succeed");
+      }
+
+      // Charleth & Dorothy vote for this proposal and close it
+      {
+        let {result} = await context.createBlock(
+          createTransaction(context, {
+            ...BALTATHAR_TRANSACTION_TEMPLATE,
+            from: CHARLETH_ADDRESS,
+            to: PRECOMPILE_COUNCIL_ADDRESS,
+            gas: 1_000_000,
+            data: COLLECTIVE_INTERFACE.encodeFunctionData("vote", [
+              proposalHash,
+              0,
+              true
+            ]),          
+          }),
+        );
+        expectEVMResult(result.events, "Succeed");
+      }
+
+      {
+        let {result} = await context.createBlock(
+          createTransaction(context, {
+            ...BALTATHAR_TRANSACTION_TEMPLATE,
+            from: DOROTHY_ADDRESS,
+            to: PRECOMPILE_COUNCIL_ADDRESS,
+            gas: 1_000_000,
+            nonce: 0,
+            data: COLLECTIVE_INTERFACE.encodeFunctionData("vote", [
+              proposalHash,
+              0,
+              true
+            ]),          
+          })
+        );
+        expectEVMResult(result.events, "Succeed");
+      }
+
+      {
+        let {result} = await context.createBlock(
+          createTransaction(context, {
+            ...BALTATHAR_TRANSACTION_TEMPLATE,
+            from: DOROTHY_ADDRESS,
+            to: PRECOMPILE_COUNCIL_ADDRESS,
+            gas: 1_000_000,
+            nonce: 1,
+            data: COLLECTIVE_INTERFACE.encodeFunctionData("close", [
+              proposalHash,
+              0,
+              800_000_000,
+              proposal.toHex().length / 2 - 1,
+            ]),          
+          })
+        );
+        expectEVMResult(result.events, "Succeed");
+        // council vote succeeds, proposal dispatch success is not
+        // taken into account.
+      }
+
+      // Verify that the proposal is not approved
+      let approvals = (await context.polkadotApi.query.treasury.approvals()) as any;
+      expect(approvals.length).to.equal(0, "No proposal should have been approved");
+    }
+  );
+});
+
+describeDevMoonbeam("Treasury council precompile #8", (context) => {
+  // prettier-ignore
+  it(
+    "should NO LONGER be rejected if the half of the council voted against it",
+    async function () {
+      // Ethan submit a treasury proposal
+
+      await context.createBlock(
+        context.polkadotApi.tx.treasury.proposeSpend(10, baltathar.address).signAsync(ethan)
+      );
+
+      // Verify that the proposal is submitted
+      let proposalCount = (await context.polkadotApi.query.treasury.proposalCount()) as any;
+      expect(proposalCount.toBigInt()).to.equal(1n, "new proposal should have been added");
+
+      // Charleth proposed that the council reject the treasury proposal
+      const proposal = context.polkadotApi.tx.treasury.rejectProposal(0);
+      const proposalHash = proposal.hash;
+
+      await context.createBlock(
+        createTransaction(context, {
+          ...BALTATHAR_TRANSACTION_TEMPLATE,
+          from: CHARLETH_ADDRESS,
+          to: PRECOMPILE_COUNCIL_ADDRESS,
+          data: COLLECTIVE_INTERFACE.encodeFunctionData("propose", [
+            2,
+            proposal.toHex()
+          ]),          
+        })
+      );
+
+      // Charleth & Dorothy vote for against proposal and close it
+      await context.createBlock(
+        [
+          createTransaction(context, {
+            ...BALTATHAR_TRANSACTION_TEMPLATE,
+            from: CHARLETH_ADDRESS,
+            to: PRECOMPILE_COUNCIL_ADDRESS,
+            data: COLLECTIVE_INTERFACE.encodeFunctionData("vote", [
+              proposalHash,
+              0,
+              true
+            ]),          
+          }),
+        
+          createTransaction(context, {
+            ...BALTATHAR_TRANSACTION_TEMPLATE,
+            from: DOROTHY_ADDRESS,
+            to: PRECOMPILE_COUNCIL_ADDRESS,
+            nonce: 0,
+            data: COLLECTIVE_INTERFACE.encodeFunctionData("vote", [
+              proposalHash,
+              0,
+              true
+            ]),          
+          }),
+
+          createTransaction(context, {
+            ...BALTATHAR_TRANSACTION_TEMPLATE,
+            from: DOROTHY_ADDRESS,
+            to: PRECOMPILE_COUNCIL_ADDRESS,
+            nonce: 1,
+            data: COLLECTIVE_INTERFACE.encodeFunctionData("close", [
+              proposalHash,
+              0,
+              800_000_000,
+              proposal.toHex().length / 2 - 1,
+            ]),          
+          })
+        ]
+      );
+
+      // Verify that the proposal is not deleted
+      expect(await context.polkadotApi.query.treasury.proposals(0)).not.equal(
+        null,
+        "The proposal must not have been deleted"
+      );
+    }
+  );
+});
+
+describeDevMoonbeam("Treasury council precompile #9", (context) => {
+  // prettier-ignore
+  it(
+    "should be approved if the three fifths of the treasury council voted for it",
+    async function () {
+      // To run this long scenarios, we have to go through a lot of steps, so anyway we won't be
+      // able to keep this tests within 5 seconds.
+      this.timeout(10_000);
+
+      // Ethan submit a treasury proposal
+
+      await context.createBlock(
+        context.polkadotApi.tx.treasury.proposeSpend(10, baltathar.address).signAsync(ethan)
+      );
+
+      // Verify that the proposal is submitted
+      let proposalCount = (await context.polkadotApi.query.treasury.proposalCount()) as any;
+      expect(proposalCount.toBigInt()).to.equal(1n, "new proposal should have been added");
+
+      const proposal = context.polkadotApi.tx.treasury.approveProposal(0);
+      const proposalHash = proposal.hash;
+
+      let {result} = await context.createBlock(
+        createTransaction(context, {
+          ...BALTATHAR_TRANSACTION_TEMPLATE,
+          from: CHARLETH_ADDRESS,
+          to: PRECOMPILE_TREASURY_COUNCIL_ADDRESS,
+          data: COLLECTIVE_INTERFACE.encodeFunctionData("propose", [
+            2,
+            proposal.toHex()
+          ]),          
+        })
+      );
+      expectEVMResult(result.events, "Succeed");
+
+      // Charleth & Dorothy vote for this proposal and close it
+      await context.createBlock(
+        [
+          createTransaction(context, {
+            ...BALTATHAR_TRANSACTION_TEMPLATE,
+            from: CHARLETH_ADDRESS,
+            to: PRECOMPILE_TREASURY_COUNCIL_ADDRESS,
+            data: COLLECTIVE_INTERFACE.encodeFunctionData("vote", [
+              proposalHash,
+              0,
+              true
+            ]),          
+          }),
+        
+          createTransaction(context, {
+            ...BALTATHAR_TRANSACTION_TEMPLATE,
+            from: DOROTHY_ADDRESS,
+            to: PRECOMPILE_TREASURY_COUNCIL_ADDRESS,
+            nonce: 0,
+            data: COLLECTIVE_INTERFACE.encodeFunctionData("vote", [
+              proposalHash,
+              0,
+              true
+            ]),          
+          }),
+
+          createTransaction(context, {
+            ...BALTATHAR_TRANSACTION_TEMPLATE,
+            from: DOROTHY_ADDRESS,
+            to: PRECOMPILE_TREASURY_COUNCIL_ADDRESS,
+            nonce: 1,
+            data: COLLECTIVE_INTERFACE.encodeFunctionData("close", [
+              proposalHash,
+              0,
+              800_000_000,
+              proposal.toHex().length / 2 - 1,
+            ]),          
+          })
+        ]
+      );
+
+      // Verify that the proposal is approved
+      let approvals = (await context.polkadotApi.query.treasury.approvals()) as any;
+      expect(approvals.length).to.equal(1, "one proposal should have been approved");
+    }
+  );
+});
+
+describeDevMoonbeam("Treasury council precompile #10", (context) => {
+  it("should be rejected if the half of the treasury council voted against it", async function () {
+    // Ethan submit a treasury proposal
+
+    await context.createBlock(
+      context.polkadotApi.tx.treasury.proposeSpend(10, baltathar.address).signAsync(ethan)
+    );
+
+    // Verify that the proposal is submitted
+    let proposalCount = (await context.polkadotApi.query.treasury.proposalCount()) as any;
+    expect(proposalCount.toBigInt()).to.equal(1n, "new proposal should have been added");
+
+    // Charleth proposed that the council reject the treasury proposal
+    // (and therefore implicitly votes for)
+    const {
+      result: { events: rejectEvents },
+    } = await context.createBlock(
+      context.polkadotApi.tx.treasuryCouncilCollective
+        .propose(2, context.polkadotApi.tx.treasury.rejectProposal(0), 1_000)
+        .signAsync(charleth)
+    );
+    const councilProposalHash = rejectEvents
+      .find(({ event: { method } }) => method.toString() == "Proposed")
+      .event.data[2].toHex() as string;
+
+    // Charleth & Dorothy vote for against proposal and close it
+    await context.createBlock([
+      context.polkadotApi.tx.treasuryCouncilCollective
+        .vote(councilProposalHash, 0, true)
+        .signAsync(charleth),
+      context.polkadotApi.tx.treasuryCouncilCollective
+        .vote(councilProposalHash, 0, true)
+        .signAsync(dorothy),
+    ]);
+    const {
+      result: { events: closeEvents },
+    } = await context.createBlock(
+      context.polkadotApi.tx.treasuryCouncilCollective
+        .close(councilProposalHash, 0, 800_000_000, 1_000)
+        .signAsync(dorothy)
+    );
+    // method: 'Rejected', section: 'treasury', index: '0x1103',
+    expect(closeEvents.map(({ event }) => event.index.toHuman())).to.contain("0x1103");
+
+    // Verify that the proposal is deleted
+    expect((await context.polkadotApi.query.treasury.proposals(0)).toHuman()).to.equal(
+      null,
+      "The proposal must have been deleted"
+    );
+  });
+});
