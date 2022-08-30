ffa9a8f474c68a7f7bbf75d3db3b0541f740c41d
gorka <gorka.irazoki@gmail.com>
2022-08-30 12:28:43 +0200

    Default transactors

--- runtime/moonriver/src/xcm_config.rs
+++ runtime/moonriver/src/xcm_config.rs
@@ -475,8 +475,9 @@ impl orml_xtokens::Config for Runtime {
 // For now we only allow to transact in the relay, although this might change in the future
 // Transactors just defines the chains in which we allow transactions to be issued through
 // xcm
-#[derive(Clone, Eq, Debug, PartialEq, Ord, PartialOrd, Encode, Decode, TypeInfo)]
+#[derive(Clone, Eq, Debug, Default, PartialEq, Ord, PartialOrd, Encode, Decode, TypeInfo)]
 pub enum Transactors {
+	#[default]
 	Relay,
 }
 
