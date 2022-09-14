6f01037e43bab08c55721e372b0749968eddb819
gorka <gorka.irazoki@gmail.com>
2022-09-14 11:39:00 +0200

    Verify weight used is independent from gas limit

--- tests/tests/test-xcm/test-mock-hrmp-transact-ethereum.ts
+++ tests/tests/test-xcm/test-mock-hrmp-transact-ethereum.ts
@@ -19,138 +19,6 @@ import { createContract } from "../../util/transactions";
 
 import { expectOk } from "../../util/expect";
 
-describeDevMoonbeam("Mock XCM - receive horizontal transact ETHEREUM (transfer)", (context) => {
-  let transferredBalance;
-  let sendingAddress;
-  let descendAddress;
-  let random: KeyringPair;
-
-  before("should receive transact action with DescendOrigin", async function () {
-    const { originAddress, descendOriginAddress } = descendOriginFromAddress(context);
-    sendingAddress = originAddress;
-    descendAddress = descendOriginAddress;
-    random = generateKeyringPair();
-    transferredBalance = 10_000_000_000_000_000_000n;
-
-    // We first fund parachain 2000 sovreign account
-    await expectOk(
-      context.createBlock(
-        context.polkadotApi.tx.balances.transfer(descendOriginAddress, transferredBalance)
-      )
-    );
-    const balance = (
-      (await context.polkadotApi.query.system.account(descendOriginAddress)) as any
-    ).data.free.toBigInt();
-    expect(balance).to.eq(transferredBalance);
-  });
-
-  it("should receive transact and should be able to execute", async function () {
-    // Get Pallet balances index
-    const metadata = await context.polkadotApi.rpc.state.getMetadata();
-    const balancesPalletIndex = (metadata.asLatest.toHuman().pallets as Array<any>).find(
-      (pallet) => {
-        return pallet.name === "Balances";
-      }
-    ).index;
-
-    const amountToTransfer = transferredBalance / 10n;
-
-    const xcmTransactions = [
-      {
-        V1: {
-          gas_limit: 21000,
-          fee_payment: {
-            Auto: {
-              Low: null,
-            },
-          },
-          action: {
-            Call: random.address,
-          },
-          value: amountToTransfer,
-          input: [],
-          access_list: null,
-        },
-      },
-      {
-        V2: {
-          gas_limit: 21000,
-          action: {
-            Call: random.address,
-          },
-          value: amountToTransfer,
-          input: [],
-          access_list: null,
-        },
-      },
-    ];
-
-    let expectedTransferredAmount = 0n;
-    let expectedTransferredAmountPlusFees = 0n;
-
-    const targetXcmWeight = 1_325_000_000n;
-    const targetXcmFee = targetXcmWeight * 50_000n;
-
-    for (const xcmTransaction of xcmTransactions) {
-      expectedTransferredAmount += amountToTransfer;
-      expectedTransferredAmountPlusFees += amountToTransfer + targetXcmFee;
-      // TODO need to update lookup types for xcm ethereum transaction V2
-      const transferCall = context.polkadotApi.tx.ethereumXcm.transact(xcmTransaction as any);
-      const transferCallEncoded = transferCall?.method.toHex();
-
-      // We are going to test that we can receive a transact operation from parachain 1
-      // using descendOrigin first
-      const xcmMessage = new XcmFragment({
-        fees: {
-          multilocation: [
-            {
-              parents: 0,
-              interior: {
-                X1: { PalletInstance: balancesPalletIndex },
-              },
-            },
-          ],
-          fungible: targetXcmFee,
-        },
-        weight_limit: new BN(targetXcmWeight.toString()),
-        descend_origin: sendingAddress,
-      })
-        .descend_origin()
-        .withdraw_asset()
-        .buy_execution()
-        .push_any({
-          Transact: {
-            originType: "SovereignAccount",
-            requireWeightAtMost: new BN(525_000_000), // 21_000 gas limit
-            call: {
-              encoded: transferCallEncoded,
-            },
-          },
-        })
-        .as_v2();
-
-      // Send an XCM and create block to execute it
-      await injectHrmpMessageAndSeal(context, 1, {
-        type: "XcmVersionedXcm",
-        payload: xcmMessage,
-      } as RawXcmMessage);
-
-      // Make sure the state has ALITH's foreign parachain tokens
-      const testAccountBalance = (
-        await context.polkadotApi.query.system.account(random.address)
-      ).data.free.toBigInt();
-      expect(testAccountBalance).to.eq(expectedTransferredAmount);
-
-      // Make sure ALITH has been deducted fees once (in xcm-executor) and balance has been
-      // transfered through evm.
-      const alithAccountBalance = await context.web3.eth.getBalance(descendAddress);
-      expect(BigInt(alithAccountBalance)).to.eq(
-        transferredBalance - expectedTransferredAmountPlusFees
-      );
-    }
-  });
-});
-
 describeDevMoonbeam("Mock XCM - receive horizontal transact ETHEREUM (call)", (context) => {
   let transferredBalance;
   let sendingAddress;
@@ -877,6 +745,134 @@ describeDevMoonbeam("Mock XCM - receive horizontal transact ETHEREUM (proxy)", (
       // Make sure derived / descended account nonce still zero.
       const derivedAccountNonce = await context.web3.eth.getTransactionCount(descendAddress);
       expect(derivedAccountNonce).to.eq(0);
+    }
+  });
+});
+
+describeDevMoonbeam("Mock XCM - receive horizontal transact ETHEREUM (transfer)", (context) => {
+  let transferredBalance;
+  let sendingAddress;
+  let descendAddress;
+  let random: KeyringPair;
+
+  before("should receive ethereum transact and account weight used", async function () {
+    const { originAddress, descendOriginAddress } = descendOriginFromAddress(context);
+    sendingAddress = originAddress;
+    descendAddress = descendOriginAddress;
+    random = generateKeyringPair();
+    transferredBalance = 10_000_000_000_000_000_000n;
+
+    // We first fund parachain 2000 sovreign account
+    await expectOk(
+      context.createBlock(
+        context.polkadotApi.tx.balances.transfer(descendOriginAddress, transferredBalance)
+      )
+    );
+    const balance = (
+      (await context.polkadotApi.query.system.account(descendOriginAddress)) as any
+    ).data.free.toBigInt();
+    expect(balance).to.eq(transferredBalance);
+  });
+
+  it("should receive transact and should use less weight than gas limit", async function () {
+    // Get Pallet balances index
+    const metadata = await context.polkadotApi.rpc.state.getMetadata();
+    const balancesPalletIndex = (metadata.asLatest.toHuman().pallets as Array<any>).find(
+      (pallet) => {
+        return pallet.name === "Balances";
+      }
+    ).index;
+
+    const amountToTransfer = transferredBalance / 10n;
+
+    // We will put a very high gas limit. However, the weight accounted
+    // for the block should only
+    const xcmTransactions = [
+      {
+        V1: {
+          gas_limit: 500_000,
+          fee_payment: {
+            Auto: {
+              Low: null,
+            },
+          },
+          action: {
+            Call: random.address,
+          },
+          value: amountToTransfer,
+          input: [],
+          access_list: null,
+        },
+      },
+    ];
+
+    let expectedTransferredAmount = 0n;
+    let expectedTransferredAmountPlusFees = 0n;
+
+    const targetXcmWeight = 500_000n * 25000n + 800000000n;
+    const targetXcmFee = targetXcmWeight * 50_000n;
+
+    for (const xcmTransaction of xcmTransactions) {
+      expectedTransferredAmount += amountToTransfer;
+      expectedTransferredAmountPlusFees += amountToTransfer + targetXcmFee;
+      // TODO need to update lookup types for xcm ethereum transaction V2
+      const transferCall = context.polkadotApi.tx.ethereumXcm.transact(xcmTransaction as any);
+      const transferCallEncoded = transferCall?.method.toHex();
+
+      // We are going to test that we can receive a transact operation from parachain 1
+      // using descendOrigin first
+      const xcmMessage = new XcmFragment({
+        fees: {
+          multilocation: [
+            {
+              parents: 0,
+              interior: {
+                X1: { PalletInstance: balancesPalletIndex },
+              },
+            },
+          ],
+          fungible: targetXcmFee,
+        },
+        weight_limit: new BN(targetXcmWeight.toString()),
+        descend_origin: sendingAddress,
+      })
+        .descend_origin()
+        .withdraw_asset()
+        .buy_execution()
+        .push_any({
+          Transact: {
+            originType: "SovereignAccount",
+            requireWeightAtMost: new BN(12_500_000_000), // 500_000 gas limit
+            call: {
+              encoded: transferCallEncoded,
+            },
+          },
+        })
+        .as_v2();
+
+      // Send an XCM and create block to execute it
+      await injectHrmpMessageAndSeal(context, 1, {
+        type: "XcmVersionedXcm",
+        payload: xcmMessage,
+      } as RawXcmMessage);
+
+      // Make sure the state has ALITH's foreign parachain tokens
+      const testAccountBalance = (
+        await context.polkadotApi.query.system.account(random.address)
+      ).data.free.toBigInt();
+      expect(testAccountBalance).to.eq(expectedTransferredAmount);
+
+      // Make sure ALITH has been deducted fees once (in xcm-executor) and balance has been
+      // transfered through evm.
+      const alithAccountBalance = await context.web3.eth.getBalance(descendAddress);
+      expect(BigInt(alithAccountBalance)).to.eq(
+        transferredBalance - expectedTransferredAmountPlusFees
+      );
+
+      const weightBlock = await context.polkadotApi.query.system.blockWeight();
+      // Make sure the system block weight corresponds to gas used and not gas limit
+      // It should be sufficient to verify that we used less than what was marked
+      expect(12_500_000_000n - weightBlock.mandatory.toBigInt() > 0n).to.be.true;
     }
   });
 });
