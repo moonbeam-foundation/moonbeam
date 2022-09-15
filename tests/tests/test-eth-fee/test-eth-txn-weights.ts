72b09c8e008e60359fd452ccef284d353a864f42
Stephen Shelton <steve@brewcraft.org>
2022-09-15 10:11:02 -0600

    Improve frontier weight test (#1808)

--- tests/tests/test-eth-fee/test-eth-txn-weights.ts
+++ tests/tests/test-eth-fee/test-eth-txn-weights.ts
@@ -1,44 +1,45 @@
 import "@moonbeam-network/api-augment";
 
 import { expect } from "chai";
-import { ethers } from "ethers";
 
-import { alith, ALITH_PRIVATE_KEY } from "../../util/accounts";
+import { alith, ALITH_PRIVATE_KEY, baltathar } from "../../util/accounts";
 import { getCompiled } from "../../util/contracts";
 import { customWeb3Request } from "../../util/providers";
-import { describeDevMoonbeamAllEthTxTypes, DevTestContext } from "../../util/setup-dev-tests";
-import { EXTRINSIC_GAS_LIMIT } from "../../util/constants";
+import { describeDevMoonbeam, DevTestContext } from "../../util/setup-dev-tests";
+import { EXTRINSIC_GAS_LIMIT, EXTRINSIC_BASE_WEIGHT, WEIGHT_PER_GAS } from "../../util/constants";
+import { createTransaction, ALITH_TRANSACTION_TEMPLATE } from "../../util/transactions";
 
 // This tests an issue where pallet Ethereum in Frontier does not properly account for weight after
 // transaction application. Specifically, it accounts for weight before a transaction by multiplying
 // GasToWeight by gas_price, but does not adjust this afterwards. This leads to accounting for too
 // much weight in a block.
-describeDevMoonbeamAllEthTxTypes("Ethereum Weight Accounting", (context) => {
+describeDevMoonbeam("Ethereum Weight Accounting", (context) => {
   it("should account for weight used", async function () {
     this.timeout(10000);
 
-    let signer = new ethers.Wallet(ALITH_PRIVATE_KEY, context.ethers);
+    const { block, result } = await context.createBlock(
+      createTransaction(context, {
+        ...ALITH_TRANSACTION_TEMPLATE,
+        gas: EXTRINSIC_GAS_LIMIT,
+        maxFeePerGas: 1_000_000_000,
+        maxPriorityFeePerGas: 0,
+        to: baltathar.address,
+        data: null,
+      })
+    );
 
-    let tx = await signer.signTransaction({
-      from: alith.address,
-      to: null,
-      value: "0x0",
-      gasLimit: EXTRINSIC_GAS_LIMIT,
-      gasPrice: 1_000_000_000,
-      data: null,
-      nonce: await context.web3.eth.getTransactionCount(alith.address),
-    });
+    const EXPECTED_GAS_USED = 21_000n;
+    const EXPECTED_WEIGHT = EXPECTED_GAS_USED * WEIGHT_PER_GAS + BigInt(EXTRINSIC_BASE_WEIGHT);
 
-    // TODO: tweak: this includes some priority fee. it would be more clear without this.
-    const expected_weight = 1_575_000_000;
+    const receipt = await context.web3.eth.getTransactionReceipt(result.hash);
+    expect(BigInt(receipt.gasUsed)).to.equal(EXPECTED_GAS_USED);
 
-    const { block } = await context.createBlock(tx);
-
+    // query the block's weight, whose normal portion should reflect only this txn
     const apiAt = await context.polkadotApi.at(block.hash);
 
     let blockWeightsUsed = await apiAt.query.system.blockWeight();
     let normalWeight = blockWeightsUsed.normal;
 
-    expect(normalWeight.toNumber()).to.equal(expected_weight);
+    expect(normalWeight.toBigInt()).to.equal(EXPECTED_WEIGHT);
   });
 });
