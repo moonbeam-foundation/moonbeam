f756a9f3a5688e994554e7f7b98e56041769dc91
4meta5 <asinghchrony@protonmail.com>
2022-09-20 22:45:34 -0400

    clean

--- runtime/moonbase/tests/gov_tests.rs
+++ runtime/moonbase/tests/gov_tests.rs
@@ -1,3 +1,19 @@
+// Copyright 2019-2022 PureStake Inc.
+// This file is part of Moonbeam.
+
+// Moonbeam is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Moonbeam is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Moonbeam.  If not, see <http://www.gnu.org/licenses/>.
+
 //! TODO: move to integration tests if any of these are worth keeping
 
 mod common;
@@ -58,6 +74,8 @@ fn referenda_times_out_if_inaction() {
 			};
 			assert!(is_ongoing);
 			// TODO: bring back once scheduler fixed
+			// TODO: check state in scheduler to see when expected to schedule
+			// and try to figure out why not scheduled
 			// run_to_block(15, NimbusId::from_slice(&ALICE_NIMBUS).ok());
 			// // Timed out - ended.
 			// let is_timed_out = match pallet_referenda::ReferendumInfoFor::<Runtime>::get(0) {
