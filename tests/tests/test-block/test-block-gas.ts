21d8888c2ec2ab672dffb8e879d223844ba0ca85
Crystalin <alan@purestake.com>
2022-10-03 16:24:13 -0400

    adds test for paying fees

--- tests/tests/test-block/test-block-gas.ts
+++ tests/tests/test-block/test-block-gas.ts
@@ -27,7 +27,7 @@ describeDevMoonbeamAllEthTxTypes("Block Gas - Limit", (context) => {
     expect(
       ((await customWeb3Request(context.web3, "eth_sendRawTransaction", [rawTx])).error as any)
         .message
-    ).to.equal("exceeds block gas limit");
+    ).to.equal("gas limit reached");
   });
 });
 
