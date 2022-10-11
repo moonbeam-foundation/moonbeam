638288841d8b9a619ee7398b1589424aa4f8c233
notlesh <steve@brewcraft.org>
2022-10-11 02:52:03 +0000

    Fix length fee tests

--- tests/tests/test-fees/test-length-fees.ts
+++ tests/tests/test-fees/test-length-fees.ts
@@ -12,7 +12,7 @@ describeDevMoonbeam(
   (context) => {
     it("should have low balance transfer fees", async () => {
       const fee = await testBalanceTransfer(context);
-      expect(fee).to.equal(20958001520875n);
+      expect(fee).to.equal(79359001520875n);
     });
   },
   "Legacy",
@@ -24,7 +24,7 @@ describeDevMoonbeam(
   (context) => {
     it("should have expensive runtime-upgrade fees", async () => {
       const fee = await testRuntimeUpgrade(context);
-      expect(fee).to.equal(9226801315723667008n);
+      expect(fee).to.equal(9226801665723667008n);
     });
   },
   "Legacy",
@@ -36,7 +36,7 @@ describeDevMoonbeam(
   (context) => {
     it("should have low balance transfer fees", async () => {
       const fee = await testBalanceTransfer(context);
-      expect(fee).to.equal(12772901520875n);
+      expect(fee).to.equal(71173901520875n);
     });
   },
   "Legacy",
@@ -48,7 +48,7 @@ describeDevMoonbeam(
   (context) => {
     it("should have expensive runtime-upgrade fees", async () => {
       const fee = await testRuntimeUpgrade(context);
-      expect(fee).to.equal(9226793130623667008n);
+      expect(fee).to.equal(9226793480623667008n);
     });
   },
   "Legacy",
@@ -60,7 +60,7 @@ describeDevMoonbeam(
   (context) => {
     it("should have low balance transfer fees", async () => {
       const fee = await testBalanceTransfer(context);
-      expect(fee).to.equal(1277290152087500n);
+      expect(fee).to.equal(7117390152087500n);
     });
   },
   "Legacy",
@@ -72,7 +72,7 @@ describeDevMoonbeam(
   (context) => {
     it("should have expensive runtime-upgrade fees", async () => {
       const fee = await testRuntimeUpgrade(context);
-      expect(fee).to.equal(922679313062366700800n);
+      expect(fee).to.equal(922679348062366700800n);
     });
   },
   "Legacy",
