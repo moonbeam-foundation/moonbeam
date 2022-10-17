6290ef419c50d609963889efa1ed98e790e15bc1
timbrinded <timbrinded@gmail.com>
2022-10-17 14:05:53 +0100

    Calculated non-ext weights

--- tests/smoke-tests/test-block-weight.ts
+++ tests/smoke-tests/test-block-weight.ts
@@ -12,54 +12,48 @@ const wssUrl = process.env.WSS_URL || null;
 const relayWssUrl = process.env.RELAY_WSS_URL || null;
 const ethRpcUrl = process.env.ETH_URL || null;
 
-interface BlockWeights {
-  hash: string;
-  weights: BlockLimits;
-}
 
-interface BlockLimits {
-  normal: BN;
-  operational: BN;
-}
 
 describeSmokeSuite(
   `Verify weights of blocks being produced`,
   { wssUrl, relayWssUrl, ethRpcUrl },
   (context) => {
-
     const limiter = new Bottleneck({ maxConcurrent: 5 });
-    let firstBlockNumber: number
-    let lastBlockNumber: number
+    let blockArray: number[];
 
     before("Retrieve past hour's worth of blocks", async function () {
       const signedBlock = await context.polkadotApi.rpc.chain.getBlock(
         await context.polkadotApi.rpc.chain.getFinalizedHead()
       );
 
-      lastBlockNumber = signedBlock.block.header.number.toNumber();
+      const lastBlockNumber = signedBlock.block.header.number.toNumber();
       const lastBlockTime = getBlockTime(signedBlock);
 
       // Target time here is set to be 1 hours, possible parameterize this in future
-      const firstBlockTime = lastBlockTime - 5 * 60 * 60 * 1000;
+      const firstBlockTime = lastBlockTime - 60 * 60 * 1000;
       debug(`Searching for the block at: ${new Date(firstBlockTime)}`);
-      firstBlockNumber = (await limiter.wrap(fetchHistoricBlockNum)(
+      const firstBlockNumber = (await limiter.wrap(fetchHistoricBlockNum)(
         context.polkadotApi,
         lastBlockNumber,
         firstBlockTime
       )) as number;
+
+      const length = lastBlockNumber - firstBlockNumber;
+      blockArray = Array.from({ length }, (_, i) => firstBlockNumber + i);
     });
 
-
-    // Despite being a naive test, this will flag up any egregiously heavy blocks in prod for 
+    // Despite being a naive test, this will flag up any egregiously heavy blocks in prod for
     // further inspection
     it("should roughly have a block weight mostly composed of transactions", async function () {
       this.timeout(120000);
       debug(
-        `Checking if #${firstBlockNumber} - #${lastBlockNumber} block weights.`
+        `Checking #${blockArray[0]} - #${
+          blockArray[blockArray.length - 1]
+        } block weight proportions.`
       );
 
       const checkBlockWeight = async (blockNum: number) => {
-        /////// Check that if a Block is heavy, that it is because of extrinsics
+        /////// Check that if a Block is heavy, that it is because of eth transactions
         const apiAt = await context.polkadotApi.at(
           await context.polkadotApi.rpc.chain.getBlockHash(blockNum)
         );
@@ -82,10 +76,7 @@ describeSmokeSuite(
         }
       };
 
-      const promises = (() => {
-        const length = lastBlockNumber - firstBlockNumber;
-        return Array.from({ length }, (_, i) => firstBlockNumber + i);
-      })().map((num) => limiter.schedule(() => checkBlockWeight(num)));
+      const promises = blockArray.map((num) => limiter.schedule(() => checkBlockWeight(num)));
 
       const results = await Promise.all(promises);
       const nonTxnHeavyBlocks = results.filter((a) => a && a.nonTxn > 0.2);
@@ -96,10 +87,51 @@ describeSmokeSuite(
           .join(", ")}`
       ).to.be.empty;
     });
+
+    /// TODO: Write new test for looking at sum of weights by looking at events extrinsic success
+    /// and fails compared to normal block weight
+
+    it("should have a total weight charged matching sum of all extrinsics", async function () {
+      this.timeout(120000);
+      debug(
+        `Checking if #${blockArray[0]} - #${
+          blockArray[blockArray.length - 1]
+        } extrinsic weights sum up.`
+      );
+
+      const BASE_BLOCK_WEIGHT = context.polkadotApi.consts.system.blockWeights.baseBlock.toNumber()
+
+      const checkNonExtWeight = async (blockNum: number) => {
+        const hash = await context.polkadotApi.rpc.chain.getBlockHash(blockNum);
+        const apiAt = await context.polkadotApi.at(hash);
+        const events = await apiAt.query.system.events();
+
+        const extTotal = events
+          .filter(
+            (a) => a.event.method == "ExtrinsicSuccess" || a.event.method == "ExtrinsicFailed"
+          )
+          .reduce((acc, curr) => acc + (curr.event.data as any).dispatchInfo.weight.toNumber(), 0);
+
+        const xcmpTotal = events
+          .filter((a) => a.event.section == "xcmpQueue" && a.event.method == "Success")
+          .reduce((acc, curr) => acc + (curr.event.data as any).weight.toNumber(), 0);
+
+        const blockWeight = (await apiAt.query.system.blockWeight())
+          .toArray()
+          .reduce((total, current) => total + Number(current.toString()), 0);
+        const ext = extTotal + xcmpTotal;
+        const nonExt = blockWeight - extTotal - xcmpTotal - BASE_BLOCK_WEIGHT;
+        debug(
+          `Block #${blockNum} weights - non-ext: ${nonExt}, ext: ${ext}, total: ${blockWeight}.`
+        );
+        return { blockNum, ext, nonExt, blockWeight };
+      };
+
+      const promises = blockArray.map((num) => limiter.schedule(() => checkNonExtWeight(num)));
+
+      const results = await Promise.all(promises);
+      const heavyNonExts = results.filter(a=> a && (a.nonExt)> 0)
+      console.log(heavyNonExts)
+    });
   }
-
-  /// TODO: Write new test for looking at sum of weights by looking at events extrinsic success
-  /// and fails compared to normal block weight
-
-
 );
