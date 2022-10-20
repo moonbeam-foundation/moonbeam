d4e067e487a48cda713747196f6e200473a00b3b
notlesh <steve@brewcraft.org>
2022-10-20 16:24:08 +0000

    Increase gas limit in test to account for extra weight headroom

--- tests/tests/test-eth-tx/test-eth-tx-size.ts
+++ tests/tests/test-eth-tx/test-eth-tx-size.ts
@@ -27,7 +27,7 @@ describeDevMoonbeam("Ethereum Transaction - Large Transaction", (context) => {
   };
 
   // TODO: I'm not sure where this 2000 came from...
-  const max_size = Math.floor((EXTRINSIC_GAS_LIMIT - 21000) / 16) - 2000;
+  const max_size = Math.floor(21000 / 16) - 2000;
 
   it("should accept txns up to known size", async function () {
     expect(max_size).to.equal(808562); // our max Ethereum TXN size in bytes
