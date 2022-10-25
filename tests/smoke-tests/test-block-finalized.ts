5447aca5d3183886f4ac73013adca2ef7517dd9c
timbrinded <timbrinded@gmail.com>
2022-10-25 15:27:49 +0100

    lint

--- tests/smoke-tests/test-block-finalized.ts
+++ tests/smoke-tests/test-block-finalized.ts
@@ -17,32 +17,32 @@ describeSmokeSuite(`Parachain blocks should be finalized..`, { wssUrl, relayWssU
     expect(diff).to.be.lessThanOrEqual(10 * 60 * 1000); // 10 minutes in milliseconds
   });
 
-    it("should have a recently finalized eth block", async function () {
-      const specVersion = context.polkadotApi.consts.system.version.specVersion.toNumber();
-      if (specVersion < 1900) {
-        debug(`ChainSpec ${specVersion} does not support Finalized BlockTag, skipping test`);
-        this.skip();
-      }
-      const timestamp = (await context.ethers.getBlock("finalized")).timestamp;
-      const diff = Date.now() - timestamp * 1000;
-      debug(`Last finalized block was ${diff / 1000} seconds ago`);
-      expect(diff).to.be.lessThanOrEqual(10 * 60 * 1000);
-    });
+  it("should have a recently finalized eth block", async function () {
+    const specVersion = context.polkadotApi.consts.system.version.specVersion.toNumber();
+    if (specVersion < 1900) {
+      debug(`ChainSpec ${specVersion} does not support Finalized BlockTag, skipping test`);
+      this.skip();
+    }
+    const timestamp = (await context.ethers.getBlock("finalized")).timestamp;
+    const diff = Date.now() - timestamp * 1000;
+    debug(`Last finalized block was ${diff / 1000} seconds ago`);
+    expect(diff).to.be.lessThanOrEqual(10 * 60 * 1000);
+  });
 
-    it(`should have only finalized blocks in the past ${(timePeriod / (1000 * 60 * 60)).toFixed(
-      2
-    )} hours`, async function () {
-      this.timeout(120000);
-      const signedBlock = await context.polkadotApi.rpc.chain.getBlock(
-        await context.polkadotApi.rpc.chain.getFinalizedHead()
-      );
+  it(`should have only finalized blocks in the past ${(timePeriod / (1000 * 60 * 60)).toFixed(
+    2
+  )} hours`, async function () {
+    this.timeout(120000);
+    const signedBlock = await context.polkadotApi.rpc.chain.getBlock(
+      await context.polkadotApi.rpc.chain.getFinalizedHead()
+    );
 
     const lastBlockNumber = signedBlock.block.header.number.toNumber();
     const lastBlockTime = getBlockTime(signedBlock);
     const limiter = new Bottleneck({ maxConcurrent: 5 });
 
-      const firstBlockTime = lastBlockTime - timePeriod;
-      debug(`Searching for the block at: ${new Date(firstBlockTime)}`);
+    const firstBlockTime = lastBlockTime - timePeriod;
+    debug(`Searching for the block at: ${new Date(firstBlockTime)}`);
 
     const firstBlockNumber = (await limiter.wrap(fetchHistoricBlockNum)(
       context.polkadotApi,
