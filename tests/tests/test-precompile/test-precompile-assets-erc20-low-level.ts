aeb067938a04ced2af214d635087a28b848e39ad
timbrinded <timbrinded@gmail.com>
2022-11-03 14:15:49 +0000

    Fixed up tests

--- tests/tests/test-precompile/test-precompile-assets-erc20-low-level.ts
+++ tests/tests/test-precompile/test-precompile-assets-erc20-low-level.ts
@@ -1,41 +1,28 @@
 import "@moonbeam-network/api-augment";
 
 import { u128 } from "@polkadot/types";
-import { BN, bnToHex, numberToHex, stringToHex } from "@polkadot/util";
+import { BN, bnToHex } from "@polkadot/util";
 import { expect } from "chai";
 import { ethers } from "ethers";
-
-import { alith, ALITH_ADDRESS, baltathar, charleth } from "../../util/accounts";
+import { web3EthCall } from "../../util/providers";
+import { alith, baltathar, charleth } from "../../util/accounts";
 import { mockAssetBalance } from "../../util/assets";
 import { getCompiled } from "../../util/contracts";
-import { web3EthCall } from "../../util/providers";
 import { describeDevMoonbeamAllEthTxTypes } from "../../util/setup-dev-tests";
 import type { PalletAssetsAssetAccount, PalletAssetsAssetDetails } from "@polkadot/types/lookup";
 
 import {
   ALITH_TRANSACTION_TEMPLATE,
-  BALTATHAR_TRANSACTION_TEMPLATE,
   CHARLETH_TRANSACTION_TEMPLATE,
   createContract,
   createTransaction,
 } from "../../util/transactions";
-import { createImportSpecifier } from "typescript";
 
-const ADDRESS_ERC20 = "0xFfFFfFff1FcaCBd218EDc0EbA20Fc2308C778080";
 const ASSET_ID = new BN("42259045809535163221576417993425387648");
-const SELECTORS = {
-  balanceOf: "70a08231",
-  totalSupply: "18160ddd",
-  approve: "095ea7b3",
-  allowance: "dd62ed3e",
-  transfer: "a9059cbb",
-  transferFrom: "23b872dd",
-  logApprove: "0x8c5be1e5ebec7d5bd14f71427d1e84f3dd0314c0f7b2291e5b200ac8c7c3b925",
-  logTransfer: "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef",
-};
-
+const ADDRESS_ERC20 = "0xFfFFfFff1FcaCBd218EDc0EbA20Fc2308C778080";
 const ERC20_CONTRACT = getCompiled("ERC20Instance");
 const ERC20_INTERFACE = new ethers.utils.Interface(ERC20_CONTRACT.contract.abi);
+const MAX_SUPPLY = 100000000000000;
 
 describeDevMoonbeamAllEthTxTypes(
   "Precompiles - Low Level Transactions",
@@ -46,7 +33,7 @@ describeDevMoonbeamAllEthTxTypes(
     before("Setup contract and mock balance", async () => {
       // We need to mint units with sudo.setStorage, as we dont have xcm mocker yet
       // And we need relay tokens for issuing a transaction to be executed in the relay
-      const balance = context.polkadotApi.createType("Balance", 100000000000000);
+      const balance = context.polkadotApi.createType("Balance", MAX_SUPPLY);
       const assetBalance: PalletAssetsAssetAccount = context.polkadotApi.createType(
         "PalletAssetsAssetAccount",
         {
@@ -85,6 +72,36 @@ describeDevMoonbeamAllEthTxTypes(
         alith.address,
         true
       );
+    });
+
+
+    it("can make static calls to view functions", async function () {
+      expect(
+        (
+          await web3EthCall(context.web3, {
+            to: contractInstanceAddress,
+            data: ERC20_INTERFACE.encodeFunctionData("totalSupply_static", []),
+          })
+        ).result
+      ).equals(bnToHex(new BN(MAX_SUPPLY), { bitLength: 256 }));
+    });
+
+    it("can make static calls to view functions and transact", async function () {
+      await context.createBlock(
+        createTransaction(context, {
+          ...ALITH_TRANSACTION_TEMPLATE,
+          to: contractInstanceAddress,
+          data: ERC20_INTERFACE.encodeFunctionData("approve_max_supply", [charleth.address]),
+        })
+      );
+
+      const approvals = await context.polkadotApi.query.assets.approvals(
+        assetId.toU8a(),
+        contractInstanceAddress,
+        charleth.address
+      );
+
+      expect(approvals.unwrap().amount.toNumber()).to.equal(MAX_SUPPLY);
     });
 
     it("has unchanged state when submitting static call", async function () {
