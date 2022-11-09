05768de1186f9f2e9a9916ece7647760751606dc
timbrinded <timbrinded@gmail.com>
2022-11-09 15:57:23 +0000

    added get txn test too

--- tests/smoke-tests/test-historic-compatibility.ts
+++ tests/smoke-tests/test-historic-compatibility.ts
@@ -5,11 +5,11 @@ import { NetworkTestArtifact, tracingTxns } from "../util/tracing-txns";
 import Bottleneck from "bottleneck";
 import { providers } from "ethers";
 
-const debug = require("debug")("smoke:tracing-compatibility");
+const debug = require("debug")("smoke:historic-compatibility");
 const limiter = new Bottleneck({ maxConcurrent: 10, minTime: 100 });
 const httpEndpoint = process.env.HTTP_URL;
 
-describeSmokeSuite(`Verifying tracing compatibility...`, async (context) => {
+describeSmokeSuite(`Verifying historic compatibility...`, async (context) => {
   let traceStatic: NetworkTestArtifact;
   before("Loading tracing static data", async function () {
     const chainId = (await context.polkadotApi.query.ethereumChainId.chainId()).toString();
@@ -20,18 +20,15 @@ describeSmokeSuite(`Verifying tracing compatibility...`, async (context) => {
       debug(`No test data available for ChainId ${chainId}, skipping test.`);
       this.skip();
     }
+  });
 
+  it("can debugTrace for all previous runtimes", async function () {
     if (httpEndpoint == null) {
       debug(`No HTTP_URL provided, skipping test.`);
       this.skip();
     }
-  });
-
-  //
-  it("can debugTrace for all previous runtimes", async function () {
     this.timeout(300000);
     const provider = new providers.JsonRpcProvider(httpEndpoint);
-
     const promises = traceStatic.testData.map(async (a) => {
       try {
         const result = await limiter.schedule(() =>
@@ -50,7 +47,37 @@ describeSmokeSuite(`Verifying tracing compatibility...`, async (context) => {
     const results = await Promise.all(promises.flatMap((a) => a));
     const failures = results.filter((a) => {
       if (a.error === true) {
-        debug(`Failure tracing in runtime ${a.runtime} blocknumber ${a.blockNumber} with `);
+        debug(
+          `Failure tracing in runtime ${a.runtime}, blocknumber ${a.blockNumber} ` + `: ${a.result}`
+        );
+        return true;
+      }
+    });
+    expect(failures).to.be.empty;
+  });
+
+  it("can get receipt for historic transactions", async function () {
+    this.timeout(300000);
+    const promises = traceStatic.testData.map(async (a) => {
+      try {
+        const result = await limiter.schedule(() =>
+          context.ethers.send("eth_getTransactionReceipt", [a.txHash])
+        );
+        debug(`Successful response from runtime ${a.runtime} in block #${a.blockNumber}.`);
+        const error = result == null;
+        return { runtime: a.runtime, blockNumber: a.blockNumber, error, result };
+      } catch (e) {
+        return { runtime: a.runtime, blockNumber: a.blockNumber, error: true, result: e };
+      }
+    });
+
+    const results = await Promise.all(promises.flatMap((a) => a));
+    const failures = results.filter((a) => {
+      if (a.error === true) {
+        debug(
+          `Failure fetching txn receipt on runtime ${a.runtime}, blocknumber ${a.blockNumber}` +
+            ` and result: ${JSON.stringify(a.result)}`
+        );
         return true;
       }
     });
