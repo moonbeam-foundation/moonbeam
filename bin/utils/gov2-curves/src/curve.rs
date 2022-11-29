e926e8825861de4128d8b2fe43487d5c5f0132f7
4meta5 <asinghchrony@protonmail.com>
2022-11-29 00:23:40 -0500

    fix copyrights

--- bin/utils/gov2-curves/src/curve.rs
+++ bin/utils/gov2-curves/src/curve.rs
@@ -1,27 +1,57 @@
-// Copyright 2022 Parity Technologies (UK) Ltd.
-// This file is part of Polkadot.
+// Copyright 2019-2022 PureStake Inc.
+// This file is part of Moonbeam.
 
-// Polkadot is free software: you can redistribute it and/or modify
+// Moonbeam is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 
-// Polkadot is distributed in the hope that it will be useful,
+// Moonbeam is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 
 // You should have received a copy of the GNU General Public License
-// along with Polkadot. If not, see <http://www.gnu.org/licenses/>.
+// along with Moonbeam.  If not, see <http://www.gnu.org/licenses/>.
 
 //! Temporary curve threshold until substrate patch is made and pulled in
 
 use pallet_referenda::Curve;
+use plotters::prelude::*;
 use sp_arithmetic::{Rounding::*, SignedRounding::*};
 use sp_runtime::{FixedI64, Perbill};
 
+pub fn plot_curve(curve: &Curve, curve_name: String, days: u32) {
+	let file_name = format!("plots/{}.png", curve_name);
+	let grid = BitMapBackend::new(&file_name, (600, 400)).into_drawing_area();
+
+	grid.fill(&WHITE).unwrap();
+
+	let mut plot = ChartBuilder::on(&grid)
+		.caption(&curve_name, ("Arial", 30))
+		// Y axis size is 40 px
+		.set_label_area_size(LabelAreaPosition::Left, 40)
+		// X axis size is 40 px
+		.set_label_area_size(LabelAreaPosition::Bottom, 40)
+		.build_cartesian_2d(0..days, 0..100)
+		.unwrap();
+
+	plot.draw_series(LineSeries::new(
+		(0..days).map(|x| {
+			(
+				x,
+				perbill_to_percent_coordinate(threshold(curve, Perbill::from_rational(x, days))),
+			)
+		}),
+		&GREEN,
+	))
+	.unwrap();
+
+	plot.configure_mesh().draw().unwrap();
+}
+
 /// Input Perbill, output i32 between 0 and 100
-pub fn perbill_to_percent_coordinate(input: Perbill) -> i32 {
+fn perbill_to_percent_coordinate(input: Perbill) -> i32 {
 	(input.deconstruct() / (Perbill::one().deconstruct() / 100))
 		.try_into()
 		.expect("failed conversion")
@@ -37,7 +67,7 @@ fn perbill_to_i32_percent_conversion() {
 
 // TODO: expose in substrate
 /// Determine the `y` value for the given `x` value.
-pub fn threshold(curve: &Curve, x: Perbill) -> Perbill {
+fn threshold(curve: &Curve, x: Perbill) -> Perbill {
 	match curve {
 		Curve::LinearDecreasing {
 			length,
--- bin/utils/gov2-curves/src/main.rs
+++ bin/utils/gov2-curves/src/main.rs
@@ -1,55 +1,24 @@
-// Copyright 2022 Parity Technologies (UK) Ltd.
-// This file is part of Polkadot.
+// Copyright 2019-2022 PureStake Inc.
+// This file is part of Moonbeam.
 
-// Polkadot is free software: you can redistribute it and/or modify
+// Moonbeam is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 
-// Polkadot is distributed in the hope that it will be useful,
+// Moonbeam is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 
 // You should have received a copy of the GNU General Public License
-// along with Polkadot. If not, see <http://www.gnu.org/licenses/>.
+// along with Moonbeam.  If not, see <http://www.gnu.org/licenses/>.
 
 use moonbase_runtime::{governance::TracksInfo as MoonbaseTracks, Balance, BlockNumber, DAYS};
-use pallet_referenda::{Curve, TracksInfo};
-use plotters::prelude::*;
-use sp_runtime::Perbill;
+use pallet_referenda::TracksInfo;
 
 pub mod curve;
 use curve::*;
-
-fn plot_curve(curve: &Curve, curve_name: String, days: u32) {
-	let file_name = format!("plots/{}.png", curve_name);
-	let grid = BitMapBackend::new(&file_name, (600, 400)).into_drawing_area();
-
-	grid.fill(&WHITE).unwrap();
-
-	let mut plot = ChartBuilder::on(&grid)
-		.caption(&curve_name, ("Arial", 30))
-		// Y axis size is 40 px
-		.set_label_area_size(LabelAreaPosition::Left, 40)
-		// X axis size is 40 px
-		.set_label_area_size(LabelAreaPosition::Bottom, 40)
-		.build_cartesian_2d(0..days, 0..100)
-		.unwrap();
-
-	plot.draw_series(LineSeries::new(
-		(0..days).map(|x| {
-			(
-				x,
-				perbill_to_percent_coordinate(threshold(curve, Perbill::from_rational(x, days))),
-			)
-		}),
-		&GREEN,
-	))
-	.unwrap();
-
-	plot.configure_mesh().draw().unwrap();
-}
 
 fn print_moonbase_track_info() {
 	for (track_id, track) in <MoonbaseTracks as TracksInfo<Balance, BlockNumber>>::tracks() {
