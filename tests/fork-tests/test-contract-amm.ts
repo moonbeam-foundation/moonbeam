1561f3d69fa4a8983f1915dae94c8536dc6dc1bf
timbrinded <timbrinded@gmail.com>
2022-12-08 13:50:20 +0000

    Added test cases

--- tests/fork-tests/test-contract-amm.ts
+++ tests/fork-tests/test-contract-amm.ts
@@ -1,13 +1,19 @@
 import child_process from "child_process";
 import { alith, ALITH_PRIVATE_KEY } from "../util/accounts";
 import { expect } from "chai";
-import { BigNumber, ethers } from "ethers";
+import { BigNumber, Contract, ethers } from "ethers";
 import {
+  ammRouterAbi,
+  ammRouterAddress,
+  glmrDotPoolAbi,
   glmrDotPoolAddress,
   usdcGlmrPoolAbi,
   usdcGlmrPoolAddress,
   usdcwhAbi,
   usdcwhAddress,
+  usdtAddress,
+  wethAbi,
+  wethAddress,
   xcdotAddress,
   xcTokenAbi,
 } from "./staticData";
@@ -15,10 +21,12 @@ import {
 import { describeParachain } from "../util/setup-para-tests";
 
 import { xxhashAsU8a, blake2AsU8a } from "@polkadot/util-crypto";
-import { u8aConcat, u8aToHex, bnToHex} from "@polkadot/util";
-import {u128 } from "@polkadot/types"
+import { u8aConcat, u8aToHex, bnToHex } from "@polkadot/util";
+import { u128 } from "@polkadot/types";
 import { hashMessage } from "ethers/lib/utils";
+import { createImportSpecifier } from "typescript";
 
+const debug = require("debug")("contract-sim:amm");
 const RUNTIME_NAME = process.env.RUNTIME_NAME as "moonbeam" | "moonbase" | "moonriver";
 const SPEC_FILE = process.env.SPEC_FILE;
 const PARA_ID = process.env.PARA_ID && parseInt(process.env.PARA_ID);
@@ -39,7 +47,7 @@ if (!PARA_ID) {
 }
 
 describeParachain(
-  `AMM Contract-Sim testss on forked ${RUNTIME_NAME}`,
+  `When interacting with AMM contracts on forked ${RUNTIME_NAME} network...`,
   {
     parachain: {
       spec: SPEC_FILE,
@@ -53,41 +61,70 @@ describeParachain(
   (context) => {
     const signer = new ethers.Wallet(ALITH_PRIVATE_KEY, context.ethers);
 
+    let usdtContract: Contract,
+      poolContract: Contract,
+      routerContract: Contract,
+      dotContract: Contract,
+      wglmrContract: Contract;
+
     before(async () => {
-      console.log(signer.address);
-      const dotContract = new ethers.Contract(xcdotAddress, xcTokenAbi, context.ethers);
-      const dotDps = await dotContract.functions.decimals();
-      const poolBal = await dotContract.functions.balanceOf(glmrDotPoolAddress);
-      const signerBal = await dotContract.functions.balanceOf(signer.address);
+      dotContract = new ethers.Contract(xcdotAddress, xcTokenAbi, context.ethers);
+      usdtContract = new ethers.Contract(usdtAddress, xcTokenAbi, context.ethers);
+      wglmrContract = new ethers.Contract(wethAddress, wethAbi, context.ethers);
+      poolContract = new ethers.Contract(glmrDotPoolAddress, glmrDotPoolAbi, context.ethers);
+      routerContract = new ethers.Contract(ammRouterAddress, ammRouterAbi, context.ethers);
 
-      console.log("pool balance is  " + poolBal[0].div(BigNumber.from(dotDps[0])));
-      console.log("signer balance is  " + signerBal);
+      const alithUsdtBalance = (await usdtContract.functions.balanceOf(signer.address))[0];
+      const alithDotBalance = (await dotContract.functions.balanceOf(signer.address))[0];
+      const alithGlmrBalance = await context.ethers.getBalance(signer.address);
+      debug(
+        `Alith Balances - USDT:${alithUsdtBalance.div(
+          BigNumber.from((await usdtContract.functions.decimals())[0] ** 10)
+        )} DOT:${alithDotBalance.div(
+          BigNumber.from((await dotContract.functions.decimals())[0] ** 10)
+        )} GLMR:${ethers.utils.formatEther(alithGlmrBalance)}`
+      );
 
-      // Function for giving yourself tokens
-
-      const storageBlake128MapKey = (module, name, key) => {
-        return u8aToHex(
-          u8aConcat(xxhashAsU8a(module, 128), xxhashAsU8a(name, 128), blake2AsU8a(key, 128), key)
-        );
-      };
-
-      const storageBlake128DoubleMapKey = (module, name, [key1, key2]) => {
-        return u8aToHex(
-          u8aConcat(xxhashAsU8a(module, 128), xxhashAsU8a(name, 128), blake2AsU8a(key1, 128), key1, blake2AsU8a(key2, 128), key2)
-        );
-      };
-
-      const hash = storageBlake128MapKey("System", "Account","0xf24FF3a9CF04c71Dbc94D0b566f7A27B94566cac");
-      console.log(hash);
-
-      const number = BigInt("42259045809535163221576417993425387648")
-      const leHex = bnToHex(number,{isLe: true})
-      const hash2 = storageBlake128DoubleMapKey("Assets", "Account",[leHex,"0xf24FF3a9CF04c71Dbc94D0b566f7A27B94566cac"]);
-      console.log(hash2);
+      expect(
+        [alithUsdtBalance.isZero(), alithDotBalance.isZero(), alithGlmrBalance.isZero()],
+        "❌ Balances not injected into Alith Account"
+      ).to.not.include(true);
     });
 
-    it("pool should have USDT/GLMR balances", async function () {
-      expect(true).to.be.true;
+    it("...should have DOT/WGLMR token balances in pool", async function () {
+      const dotPoolBalance = await dotContract.functions.balanceOf(poolContract.address);
+      const wglmrPoolBalance = await wglmrContract.functions.balanceOf(poolContract.address);
+      expect([dotPoolBalance[0].isZero(), wglmrPoolBalance[0].isZero()]).to.not.include(true);
     });
+
+    it("...should have expected token addresses for pool", async function () {
+      const token0Address = await poolContract.functions.token0();
+      const token1Address = await poolContract.functions.token1();
+      expect([
+        token0Address[0].toLowerCase() === wglmrContract.address.toLowerCase(),
+        token1Address[0].toLowerCase() === dotContract.address.toLowerCase(),
+      ]).to.not.include(false);
+    });
+
+    it("...should have matching WETH address for router", async function () {
+      expect(
+        (await routerContract.functions.WETH())[0].toLowerCase() === wethAddress.toLowerCase(),
+        `❌ WETH address is not match, is this the right network? (moonbeam)`
+      ).to.be.true;
+    });
+
+    // TODO: can calculate the amounts in and out of pool (2x test cases)
+
+    // TODO: write test to swap some tokens
+
+    // TODO: write test to add liquidity
+
+    // TODO: write test to remove liquidity
+
+    // TODO: Deposit LP token to farm
+
+    // TODO: Harvest rewards from farm
+
+    // TODO: Withdraw LP token from farm
   }
 );
