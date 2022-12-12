2d5515c61257ac51e21489373196b262768d3ed3
girazoki <girazoki@users.noreply.github.com>
2022-12-12 15:35:04 +0000

    deploy: 85771c621c59a14cc54199327b28603eeb1a95a4

--- implementors/pallet_base_fee/trait.BaseFeeThreshold.js
+++
@@ -1,5 +0,0 @@
-(function() {var implementors = {
-"moonbase_runtime":[["impl BaseFeeThreshold for <a class=\"struct\" href=\"moonbase_runtime/struct.BaseFeeThreshold.html\" title=\"struct moonbase_runtime::BaseFeeThreshold\">BaseFeeThreshold</a>"]],
-"moonbeam_runtime":[["impl BaseFeeThreshold for <a class=\"struct\" href=\"moonbeam_runtime/struct.BaseFeeThreshold.html\" title=\"struct moonbeam_runtime::BaseFeeThreshold\">BaseFeeThreshold</a>"]],
-"moonriver_runtime":[["impl BaseFeeThreshold for <a class=\"struct\" href=\"moonriver_runtime/struct.BaseFeeThreshold.html\" title=\"struct moonriver_runtime::BaseFeeThreshold\">BaseFeeThreshold</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
