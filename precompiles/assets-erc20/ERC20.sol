c378623c3d28fd1d1343a67723794d7d5b3aa888
girazoki <gorka.irazoki@gmail.com>
2022-12-22 09:32:33 +0100

    Moves role accesors to its own solidity file

--- precompiles/assets-erc20/ERC20.sol
+++ precompiles/assets-erc20/ERC20.sol
@@ -37,41 +37,6 @@ interface IERC20 {
         view
         returns (uint256);
 
-    /// @dev Function to check the owner of the asset
-    /// @custom:selector 8da5cb5b
-    /// @return the address of the owner.
-    function owner()
-        external
-        view
-        returns (address);
-
-    /// @dev Function to check the freezer of the asset
-    /// @dev Freezer: the account that can freeze an asset
-    /// @custom:selector 92716054
-    /// @return the address of the freezer.
-    function freezer()
-        external
-        view
-        returns (address);
-
-    /// @dev Function to check the issuer of the asset
-    /// @dev Issuer: the account that can issue tokens for an asset
-    /// @custom:selector 1d143848
-    /// @return the address of the issuer.
-    function issuer()
-        external
-        view
-        returns (address);
-
-    /// @dev Function to check the admin of the asset
-    /// @dev Admin: the account that can unfreeze and force transfer
-    /// @custom:selector f851a440
-    /// @return the address of the admin.
-    function admin()
-        external
-        view
-        returns (address);
-
     /// @dev Transfer token for a specified address
     /// @custom:selector a9059cbb
     /// @param to The address to transfer to.
