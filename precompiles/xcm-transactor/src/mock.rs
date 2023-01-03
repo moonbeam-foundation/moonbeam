98f65a0bc4664206ff112e0f1a0367d6ea926deb
girazoki <gorka.irazoki@gmail.com>
2023-01-03 18:17:37 +0100

    solve mock

--- precompiles/xcm-transactor/src/mock.rs
+++ precompiles/xcm-transactor/src/mock.rs
@@ -304,6 +304,7 @@ impl pallet_xcm_transactor::Config for Runtime {
 	type AssetTransactor = DummyAssetTransactor;
 	type ReserveProvider = orml_traits::location::RelativeReserveProvider;
 	type WeightInfo = ();
+	type HrmpManipulatorOrigin = frame_system::EnsureRoot<AccountId>;
 }
 
 // We need to use the encoding from the relay mock runtime
