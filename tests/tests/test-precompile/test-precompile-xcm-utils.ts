e4e4e4e60a8b9ae863e6a866b11bd3f10d9f026f
girazoki <gorka.irazoki@gmail.com>
2023-01-10 10:37:43 +0100

    Add test showing for moonbeam does not work

--- tests/tests/test-precompile/test-precompile-xcm-utils.ts
+++ tests/tests/test-precompile/test-precompile-xcm-utils.ts
@@ -245,6 +245,52 @@ describeDevMoonbeam(
   "moonriver"
 );
 
+describeDevMoonbeam(
+  "Precompiles - xcm utils",
+  (context) => {
+    it("moonbeam does not allow to execute a custom xcm message", async function () {
+      let random = generateKeyringPair();
+
+      const transferCall = context.polkadotApi.tx.balances.transfer(random.address, 1n * GLMR);
+      const transferCallEncoded = transferCall?.method.toHex();
+
+      const xcmMessage = {
+        V2: [
+          {
+            Transact: {
+              originType: "SovereignAccount",
+              requireWeightAtMost: new BN(525_000_000).add(new BN(100_000_000)), // 21_000 gas limit
+              call: {
+                encoded: transferCallEncoded,
+              },
+            },
+          },
+        ],
+      };
+
+      const receivedMessage: XcmVersionedXcm = context.polkadotApi.createType(
+        "XcmVersionedXcm",
+        xcmMessage
+      ) as any;
+
+      const { result } = await context.createBlock(
+        createTransaction(context, {
+          ...ALITH_TRANSACTION_TEMPLATE,
+          gasPrice: 100_000_000_000,
+          to: PRECOMPILE_XCM_UTILS_ADDRESS,
+          data: XCM_UTILSTRANSACTOR_INTERFACE.encodeFunctionData("xcmExecute", [
+            receivedMessage.toU8a(),
+            2_000_000_000,
+          ]),
+        })
+      );
+      expectEVMResult(result.events, "Revert");
+    });
+  },
+  "Legacy",
+  "moonbeam"
+);
+
 describeDevMoonbeamAllEthTxTypes("Precompiles - xcm utils", (context) => {
   it("allows to execute a custom xcm evm to evm, but reentrancy forbids", async function () {
     let random = generateKeyringPair();
