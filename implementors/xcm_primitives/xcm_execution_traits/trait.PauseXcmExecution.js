43b84ea54f4b17afdaec35611746deb5dff4dd02
librelois <librelois@users.noreply.github.com>
2023-01-10 14:12:56 +0000

    deploy: bb7ce1a06abcf3fd865941084da5e7eb45d07139

---
+++ implementors/xcm_primitives/xcm_execution_traits/trait.PauseXcmExecution.js
@@ -0,0 +1,6 @@
+(function() {var implementors = {
+"moonbase_runtime":[["impl <a class=\"trait\" href=\"xcm_primitives/xcm_execution_traits/trait.PauseXcmExecution.html\" title=\"trait xcm_primitives::xcm_execution_traits::PauseXcmExecution\">PauseXcmExecution</a> for <a class=\"struct\" href=\"moonbase_runtime/struct.XcmExecutionManager.html\" title=\"struct moonbase_runtime::XcmExecutionManager\">XcmExecutionManager</a>"]],
+"moonbeam_runtime":[["impl <a class=\"trait\" href=\"xcm_primitives/xcm_execution_traits/trait.PauseXcmExecution.html\" title=\"trait xcm_primitives::xcm_execution_traits::PauseXcmExecution\">PauseXcmExecution</a> for <a class=\"struct\" href=\"moonbeam_runtime/struct.XcmExecutionManager.html\" title=\"struct moonbeam_runtime::XcmExecutionManager\">XcmExecutionManager</a>"]],
+"moonriver_runtime":[["impl <a class=\"trait\" href=\"xcm_primitives/xcm_execution_traits/trait.PauseXcmExecution.html\" title=\"trait xcm_primitives::xcm_execution_traits::PauseXcmExecution\">PauseXcmExecution</a> for <a class=\"struct\" href=\"moonriver_runtime/struct.XcmExecutionManager.html\" title=\"struct moonriver_runtime::XcmExecutionManager\">XcmExecutionManager</a>"]],
+"xcm_primitives":[]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
