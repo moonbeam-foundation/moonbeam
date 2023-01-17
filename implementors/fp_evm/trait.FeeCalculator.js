9f5d683acc7aeca742ab4afe0e5b318e73cfc6f2
notlesh <notlesh@users.noreply.github.com>
2023-01-17 17:42:39 +0000

    deploy: f90be0461dc35b6050c719ff7e3cf43997938efa

--- implementors/fp_evm/trait.FeeCalculator.js
+++ implementors/fp_evm/trait.FeeCalculator.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {
-"moonbase_runtime":[["impl FeeCalculator for <a class=\"struct\" href=\"moonbase_runtime/struct.FixedGasPrice.html\" title=\"struct moonbase_runtime::FixedGasPrice\">FixedGasPrice</a>"]],
+"moonbase_runtime":[["impl FeeCalculator for <a class=\"struct\" href=\"moonbase_runtime/struct.TransactionPaymentAsGasPrice.html\" title=\"struct moonbase_runtime::TransactionPaymentAsGasPrice\">TransactionPaymentAsGasPrice</a>"]],
 "moonbeam_runtime":[["impl FeeCalculator for <a class=\"struct\" href=\"moonbeam_runtime/struct.FixedGasPrice.html\" title=\"struct moonbeam_runtime::FixedGasPrice\">FixedGasPrice</a>"]],
 "moonriver_runtime":[["impl FeeCalculator for <a class=\"struct\" href=\"moonriver_runtime/struct.FixedGasPrice.html\" title=\"struct moonriver_runtime::FixedGasPrice\">FixedGasPrice</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
