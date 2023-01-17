14d9e811c8e05f17a750b1c9535eef40e8fdbcce
Nisheeth Barthwal <nbaztec@gmail.com>
2023-01-17 16:14:34 +0100

    Update runtime/moonriver/src/governance/councils.rs

--- runtime/moonriver/src/governance/councils.rs
+++ runtime/moonriver/src/governance/councils.rs
@@ -14,7 +14,6 @@
 // You should have received a copy of the GNU General Public License
 // along with Moonbeam.  If not, see <http://www.gnu.org/licenses/>.
 
-
 //! Councils for Gov1 and Gov2
 
 use super::*;
