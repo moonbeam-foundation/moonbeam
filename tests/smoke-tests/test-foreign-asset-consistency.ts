5ac51eb6db1d6a6cc5eacc052aca2f97bc86b503
Tim B <79199034+timbrinded@users.noreply.github.com>
2023-01-23 14:07:04 +0000

    Add Smoke test IDs (#2027)
    * Added rudimentary suite IDs to smoke tests
    
    * Added IDs to smoke tests
    
    * Lint
    
    * Lint again
    
    * unmuted kint
    
    * Tweaked dev-test node options
    
    * Updated IDs
    
    * Typo
    
    * Changed suite id order
    
    * rebase
    
    * Updated relay indices test
    
    * Lint

--- tests/smoke-tests/test-foreign-asset-consistency.ts
+++ tests/smoke-tests/test-foreign-asset-consistency.ts
@@ -5,8 +5,10 @@ import { describeSmokeSuite } from "../util/setup-smoke-tests";
 const debug = require("debug")("smoke:localAssets");
 
 describeSmokeSuite(
-  `Verifying foreign asset count, mapping, assetIds and deposits...`,
-  (context) => {
+  "S1000",
+  `Verifying foreign asset count, mapping, assetIds and deposits`,
+
+  (context, testIt) => {
     let atBlockNumber: number = 0;
     let apiAt: ApiDecoration<"promise"> = null;
     const foreignAssetIdType: { [assetId: string]: string } = {};
@@ -41,60 +43,73 @@ describeSmokeSuite(
       });
     });
 
-    it("should make sure xcm fee assets accepted is <=> than existing assets", async function () {
-      expect(
-        foreignXcmAcceptedAssets.length,
-        `Number of local asset deposits does not much number of local assets`
-      ).to.be.lessThanOrEqual(Object.keys(foreignAssetIdType).length);
+    testIt(
+      "C100",
+      `should make sure xcm fee assets accepted is <=> than existing assets`,
+      async function () {
+        expect(
+          foreignXcmAcceptedAssets.length,
+          `Number of local asset deposits does not much number of local assets`
+        ).to.be.lessThanOrEqual(Object.keys(foreignAssetIdType).length);
 
-      debug(
-        `Verified FOREIGN asset counter (${
-          Object.keys(foreignAssetIdType).length
-        }) >= xcm fee payment assets: (${foreignXcmAcceptedAssets.length})`
-      );
-    });
-
-    it("should make sure assetId->AssetType matches AssetType->AssetId mapping", async function () {
-      const failedAssetReserveMappings: { assetId: string }[] = [];
-
-      for (const assetId of Object.keys(foreignAssetIdType)) {
-        let assetType = foreignAssetIdType[assetId];
-        if (foreignAssetTypeId[assetType] != assetId) {
-          failedAssetReserveMappings.push({ assetId: assetId });
-        }
+        debug(
+          `Verified FOREIGN asset counter (${
+            Object.keys(foreignAssetIdType).length
+          }) >= xcm fee payment assets: (${foreignXcmAcceptedAssets.length})`
+        );
       }
+    );
 
-      expect(
-        failedAssetReserveMappings.length,
-        `Failed foreign asset entries: ${failedAssetReserveMappings
-          .map(({ assetId }) => `expected: ${assetId} to be present in localAssets `)
-          .join(`, `)}`
-      ).to.equal(0);
-      debug(
-        `Verified ${
-          Object.keys(foreignAssetIdType).length
-        } assetId<->AssetType entries (at #${atBlockNumber})`
-      );
-    });
+    testIt(
+      "C200`",
+      `should make sure assetId->AssetType matches AssetType->AssetId mapping`,
+      async function () {
+        const failedAssetReserveMappings: { assetId: string }[] = [];
 
-    it("should make sure existing xcm payment assets exist in mapping", async function () {
-      const failedXcmPaymentAssets: { assetType: string }[] = [];
-
-      for (const assetType of foreignXcmAcceptedAssets) {
-        if (!Object.keys(foreignAssetTypeId).includes(assetType)) {
-          failedXcmPaymentAssets.push({ assetType });
+        for (const assetId of Object.keys(foreignAssetIdType)) {
+          let assetType = foreignAssetIdType[assetId];
+          if (foreignAssetTypeId[assetType] != assetId) {
+            failedAssetReserveMappings.push({ assetId: assetId });
+          }
         }
-      }
 
-      expect(
-        failedXcmPaymentAssets.length,
-        `Failed xcm fee assets: ${failedXcmPaymentAssets
-          .map(({ assetType }) => `expected: ${assetType} to be present in localAssets `)
-          .join(`\n`)}`
-      ).to.equal(0);
-      debug(
-        `Verified ${foreignXcmAcceptedAssets.length} xcm fee payment assets (at #${atBlockNumber})`
-      );
-    });
+        expect(
+          failedAssetReserveMappings.length,
+          `Failed foreign asset entries: ${failedAssetReserveMappings
+            .map(({ assetId }) => `expected: ${assetId} to be present in localAssets `)
+            .join(`, `)}`
+        ).to.equal(0);
+        debug(
+          `Verified ${
+            Object.keys(foreignAssetIdType).length
+          } assetId<->AssetType entries (at #${atBlockNumber})`
+        );
+      }
+    );
+
+    testIt(
+      "C300",
+      `should make sure existing xcm payment assets exist in mapping`,
+      async function () {
+        const failedXcmPaymentAssets: { assetType: string }[] = [];
+
+        for (const assetType of foreignXcmAcceptedAssets) {
+          if (!Object.keys(foreignAssetTypeId).includes(assetType)) {
+            failedXcmPaymentAssets.push({ assetType });
+          }
+        }
+
+        expect(
+          failedXcmPaymentAssets.length,
+          `Failed xcm fee assets: ${failedXcmPaymentAssets
+            .map(({ assetType }) => `expected: ${assetType} to be present in localAssets `)
+            .join(`\n`)}`
+        ).to.equal(0);
+        debug(
+          `Verified ${foreignXcmAcceptedAssets.length} xcm ` +
+            `fee payment assets (at #${atBlockNumber})`
+        );
+      }
+    );
   }
 );
--- tests/smoke-tests/test-staking-rewards.ts
+++ tests/smoke-tests/test-staking-rewards.ts
@@ -19,7 +19,7 @@ import { FIVE_MINS, ONE_HOURS, TWO_HOURS } from "../util/constants";
 import { Perbill, Percent } from "../util/common";
 const debug = require("debug")("smoke:staking");
 
-describeSmokeSuite(`When verifying ParachainStaking rewards...`, function (context) {
+describeSmokeSuite("S2000", `When verifying ParachainStaking rewards`, function (context, testIt) {
   let atStakeSnapshot: [StorageKey<[u32, AccountId20]>, PalletParachainStakingCollatorSnapshot][];
   let apiAt: ApiDecoration<"promise">;
   let predecessorApiAt: ApiDecoration<"promise">;
@@ -61,7 +61,7 @@ describeSmokeSuite(`When verifying ParachainStaking rewards...`, function (conte
     atStakeSnapshot = await apiAt.query.parachainStaking.atStake.entries(nowRound);
   });
 
-  it("should snapshot the selected candidates for that round.", async function () {
+  testIt("C100", `should snapshot the selected candidates for that round`, async function () {
     const selectedCandidates = await apiAt.query.parachainStaking.selectedCandidates();
     const totalSelected = (await apiAt.query.parachainStaking.totalSelected()).toNumber();
     expect(atStakeSnapshot.length).to.be.lessThanOrEqual(totalSelected);
@@ -75,7 +75,7 @@ describeSmokeSuite(`When verifying ParachainStaking rewards...`, function (conte
     ).to.be.empty;
   });
 
-  it("should have accurate collator stats in snapshot.", async function () {
+  testIt("C200", `should have accurate collator stats in snapshot`, async function () {
     this.timeout(FIVE_MINS);
 
     const limiter = new Bottleneck({
@@ -123,8 +123,8 @@ describeSmokeSuite(`When verifying ParachainStaking rewards...`, function (conte
     ).to.be.empty;
   });
 
-  it("should snapshot candidate delegation amounts correctly.", async function () {
-    // This test is so slow due to rate limiting, this should be off until a better solution appears
+  testIt("C300", `should snapshot candidate delegation amounts correctly`, async function () {
+    // This test is slow due to rate limiting, and should be run ad-hoc only
     if (process.env.RUN_ATSTAKE_CONSISTENCY_TESTS != "true") {
       debug("Explicit RUN_ATSTAKE_CONSISTENCY_TESTS flag not set to 'true', skipping test");
       this.skip();
@@ -232,8 +232,8 @@ describeSmokeSuite(`When verifying ParachainStaking rewards...`, function (conte
     await new Promise((resolve) => setTimeout(resolve, 2000));
   });
 
-  it("should snapshot delegate autocompound preferences correctly.", async function () {
-    // This test is so slow due to rate limiting, this should be off until a better solution appears
+  testIt("C400", `should snapshot delegate autocompound preferences correctly`, async function () {
+    // This test is slow due to rate limiting, this should be off until a better solution appears
     if (process.env.RUN_ATSTAKE_CONSISTENCY_TESTS != "true") {
       debug("Explicit RUN_ATSTAKE_CONSISTENCY_TESTS flag not set to 'true', skipping test");
       this.skip();
@@ -319,7 +319,7 @@ describeSmokeSuite(`When verifying ParachainStaking rewards...`, function (conte
     await new Promise((resolve) => setTimeout(resolve, 2000));
   });
 
-  it("rewards are given as expected", async function () {
+  testIt("C500", `rewards are given as expected`, async function () {
     this.timeout(500000);
     const atBlockNumber = process.env.BLOCK_NUMBER
       ? parseInt(process.env.BLOCK_NUMBER)
