5f830a6f0eeb7a360cd4d4545d0f5d0f2efc23da
Éloïs <c@elo.tf>
2023-01-23 12:37:47 +0100

    bump runtimes spec version to 2200 (#2058)

--- runtime/moonbase/src/lib.rs
+++ runtime/moonbase/src/lib.rs
@@ -180,7 +180,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("moonbase"),
 	impl_name: create_runtime_str!("moonbase"),
 	authoring_version: 4,
-	spec_version: 2100,
+	spec_version: 2200,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 2,
