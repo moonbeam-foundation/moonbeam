bf0427828c7e3b715ea2722692db5e096f9232ac
nanocryk <6422796+nanocryk@users.noreply.github.com>
2023-01-26 13:44:34 +0000

    cargo fmt

--- runtime/moonbase/src/precompiles.rs
+++ runtime/moonbase/src/precompiles.rs
@@ -169,10 +169,10 @@ type MoonbasePrecompilesAt<R> = (
 		AddressU64<2059>,
 		ProxyPrecompile<R>,
 		(
-		    CallableByContract<OnlyIsProxy<R>>,
-		    SubcallWithMaxNesting<0>,
-		    // Batch is the only precompile allowed to call Proxy.
-		    CallableByPrecompile<OnlyFrom<AddressU64<2056>>>,
+			CallableByContract<OnlyIsProxy<R>>,
+			SubcallWithMaxNesting<0>,
+			// Batch is the only precompile allowed to call Proxy.
+			CallableByPrecompile<OnlyFrom<AddressU64<2056>>>,
 		),
 	>,
 	PrecompileAt<
