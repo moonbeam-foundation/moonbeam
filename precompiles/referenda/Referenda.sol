e0f31c813895ac05d85e4ed7b18040b87270e28d
nanocryk <6422796+nanocryk@users.noreply.github.com>
2023-02-08 10:03:35 +0100

    Fix precompile tuple encoding in return position (#2068)
    * fix returns tuple encoding
    
    * use struct in Referanda precompile to keep same encoding
    
    * fix typos

--- precompiles/referenda/Referenda.sol
+++ precompiles/referenda/Referenda.sol
@@ -12,6 +12,18 @@ Referenda constant REFERENDA_CONTRACT = Referenda(REFERENDA_ADDRESS);
 /// @title The interface through which solidity contracts will interact with the Referenda pallet
 /// @custom:address 0x0000000000000000000000000000000000000811
 interface Referenda {
+    struct TrackInfo {
+        string name;
+        uint256 maxDeciding;
+        uint256 decisionDeposit;
+        uint256 preparePeriod;
+        uint256 decisionPeriod;
+        uint256 confirmPeriod;
+        uint256 minEnactmentPeriod;
+        bytes minApproval;
+        bytes minSupport;
+    }
+
     /// Return the total referendum count
     /// @custom:selector 3a42ee31
     function referendumCount() external view returns (uint256);
@@ -33,20 +45,7 @@ interface Referenda {
     /// Return the governance parameters configured for the input TrackId
     /// @param trackId The track identifier
     /// @custom:selector 34038146
-    function trackInfo(uint16 trackId)
-        external
-        view
-        returns (
-            string memory,
-            uint256,
-            uint256,
-            uint256,
-            uint256,
-            uint256,
-            uint256,
-            bytes memory,
-            bytes memory
-        );
+    function trackInfo(uint16 trackId) external view returns (TrackInfo memory);
 
     /// @dev Submit a referenda
     /// @custom:selector 95f9ed68
