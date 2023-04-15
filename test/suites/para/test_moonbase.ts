e174f7a0d5c830d253fc4408f2f1ad98fe3e5842
timbrinded <79199034+timbrinded@users.noreply.github.com>
2023-04-15 21:20:53 +0100

    fix tests

--- test/suites/para/test_moonbase.ts
+++ test/suites/para/test_moonbase.ts
@@ -1,6 +1,8 @@
 import { expect, describeSuite, beforeAll, ApiPromise, MoonwallContext } from "@moonwall/cli";
+import { Signer, ethers } from "ethers";
 import fs from "node:fs";
 import "@moonbeam-network/api-augment";
+import { BALTATHAR_ADDRESS, charleth } from "@moonwall/util";
 
 describeSuite({
   id: "ZAN",
@@ -9,10 +11,12 @@ describeSuite({
   testCases: function ({ it, context, log }) {
     let paraApi: ApiPromise;
     let relayApi: ApiPromise;
+    let ethersSigner: Signer;
 
     beforeAll(async () => {
       paraApi = context.polkadotJs({ type: "moon" });
       relayApi = context.polkadotJs({ type: "polkadotJs" });
+      ethersSigner = context.ethersSigner();
 
       const relayNetwork = relayApi.consts.system.version.specName.toString();
       expect(relayNetwork, "Relay API incorrect").to.contain("rococo");
@@ -23,9 +27,69 @@ describeSuite({
       const currentBlock = (await paraApi.rpc.chain.getBlock()).block.header.number.toNumber();
       expect(currentBlock, "Parachain not producing blocks").to.be.greaterThan(0);
     }, 120000);
-
     it({
       id: "T01",
+      title: "Blocks are being produced on parachain",
+      test: async function () {
+        const blockNum = (await paraApi.rpc.chain.getBlock()).block.header.number.toNumber();
+        expect(blockNum).to.be.greaterThan(0);
+      },
+    });
+
+    it({
+      id: "T02",
+      title: "Can connect to parachain and execute a transaction",
+      timeout: 60000,
+      test: async function () {
+        const balBefore = (await paraApi.query.system.account(BALTATHAR_ADDRESS)).data.free;
+
+        log("Please wait, this will take at least 30s for transaction to complete");
+
+        await new Promise((resolve) => {
+          paraApi.tx.balances
+            .transfer(BALTATHAR_ADDRESS, ethers.parseEther("2"))
+            .signAndSend(charleth, ({ status, events }) => {
+              if (status.isInBlock) {
+                log("Transaction is in block");
+              }
+              if (status.isFinalized) {
+                log("Transaction is finalized!");
+                resolve(events);
+              }
+            });
+        });
+
+        const balAfter = (await paraApi.query.system.account(BALTATHAR_ADDRESS)).data.free;
+        expect(balBefore.lt(balAfter)).to.be.true;
+      },
+    });
+
+    it({
+      id: "T03",
+      title: "Tags are present on emulated Ethereum blocks",
+      test: async function () {
+        expect(
+          (await ethersSigner.provider.getBlock("safe")).number,
+          "Safe tag is not present"
+        ).to.be.greaterThan(0);
+        expect(
+          (await ethersSigner.provider.getBlock("finalized")).number,
+          "Finalized tag is not present"
+        ).to.be.greaterThan(0);
+        expect(
+          (await ethersSigner.provider.getBlock("latest")).number,
+          "Latest tag is not present"
+        ).to.be.greaterThan(0);
+        // log(await ethersSigner.provider.getTransactionCount(ALITH_ADDRESS, "latest"));
+        // await context
+        //   .ethersSigner()
+        //   .sendTransaction({ to: BALTATHAR_ADDRESS, value: ethers.parseEther("1") });
+        // log(await ethersSigner.provider.getTransactionCount(ALITH_ADDRESS, "pending"));
+      },
+    });
+
+    it({
+      id: "T04",
       title: "Chain can be upgraded",
       timeout: 600000,
       test: async function () {
