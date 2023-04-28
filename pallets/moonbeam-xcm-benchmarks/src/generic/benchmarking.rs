887ca497d19daec45eb53bbecb3ec72aab0b9507
Francisco Gamundi <francisco@purestake.com>
2023-04-28 15:18:19 +0200

    Increase XCM benchmark overrides to max weight

--- pallets/moonbeam-xcm-benchmarks/src/generic/benchmarking.rs
+++ pallets/moonbeam-xcm-benchmarks/src/generic/benchmarking.rs
@@ -45,51 +45,37 @@ benchmarks! {
 
 	exchange_asset {
 	} : {
-		Err(BenchmarkError::Override(
-			BenchmarkResult::from_weight(T::BlockWeights::get().max_block)
-		))?;
+		Err(BenchmarkError::Override(BenchmarkResult::from_weight(Weight::MAX)))?;
 	}
 
 	export_message {
 	} : {
-		Err(BenchmarkError::Override(
-			BenchmarkResult::from_weight(T::BlockWeights::get().max_block)
-		))?;
+		Err(BenchmarkError::Override(BenchmarkResult::from_weight(Weight::MAX)))?;
 	}
 
 	lock_asset {
 	} : {
-		Err(BenchmarkError::Override(
-			BenchmarkResult::from_weight(T::BlockWeights::get().max_block)
-		))?;
+		Err(BenchmarkError::Override(BenchmarkResult::from_weight(Weight::MAX)))?;
 	}
 
 	unlock_asset {
 	} : {
-		Err(BenchmarkError::Override(
-			BenchmarkResult::from_weight(T::BlockWeights::get().max_block)
-		))?;
+		Err(BenchmarkError::Override(BenchmarkResult::from_weight(Weight::MAX)))?;
 	}
 
 	note_unlockable {
 	} : {
-		Err(BenchmarkError::Override(
-			BenchmarkResult::from_weight(T::BlockWeights::get().max_block)
-		))?;
+		Err(BenchmarkError::Override(BenchmarkResult::from_weight(Weight::MAX)))?;
 	}
 
 	request_unlock {
 	} : {
-		Err(BenchmarkError::Override(
-			BenchmarkResult::from_weight(T::BlockWeights::get().max_block)
-		))?;
+		Err(BenchmarkError::Override(BenchmarkResult::from_weight(Weight::MAX)))?;
 	}
 
 	universal_origin {
 	} : {
-		Err(BenchmarkError::Override(
-			BenchmarkResult::from_weight(T::BlockWeights::get().max_block)
-		))?;
+		Err(BenchmarkError::Override(BenchmarkResult::from_weight(Weight::MAX)))?;
 	}
 
 	impl_benchmark_test_suite!(
