2f849ee6be0a1ad39da4721c5a7db010736b488d
Nisheeth Barthwal <nbaztec@gmail.com>
2023-05-08 16:25:50 +0200

    detect taplo formatted multiline entries for check-reentrancy

--- scripts/check-forbid-evm-reentrancy.sh
+++ scripts/check-forbid-evm-reentrancy.sh
@@ -3,13 +3,22 @@
 while IFS= read -r path
 do
     echo "$path"
-    while IFS= read -r line
-    do
-        matches=$(echo "$line" | awk '(/^pallet-evm = / || /^pallet-ethereum = /) && !/reentrancy/');
-        if [ ! -z "$matches" ]; then
-            echo "Check fails: $line"
-            echo "Please add 'forbid-evm-reentrancy' feature to 'pallet-evm' and 'pallet-ethereum'."
-            exit 1
-        fi
-    done < "$path"
+    # 
+    matches=$(awk 'flag{
+      if (buf ~ /}/ && buf !~ /reentrancy/) { 
+        printf "(line %d) %s", line, buf; flag=0; buf=""        # single line declaration
+      } else  {
+          buf = buf $0 ORS;
+          if (flag && /}/ && buf !~ /reentrancy/) {             # multiline declaration
+            printf "(line %d) %s", line, buf; flag=0; buf="" 
+          }
+      }
+    }
+    /pallet-(ethereum|evm) = /{buf = $0 ORS; flag=1;line=NR}' "${path}") 
+
+    if [[ -n "$matches" ]]; then
+      echo "Check failed. Please add 'forbid-evm-reentrancy' feature to 'pallet-evm' and 'pallet-ethereum'."
+      echo "${matches}"
+      exit 1
+    fi
 done < <(find . -name "Cargo.toml" -not -path "*/target/*" -not -path "*/build/*")
