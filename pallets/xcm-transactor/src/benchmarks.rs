1e1ea1731c0b21ec54cdb2b037e939abc49d0a84
Agus <agus@purestake.com>
2023-05-08 22:03:58 -0300

    fix benchmarks

--- pallets/xcm-transactor/src/benchmarks.rs
+++ pallets/xcm-transactor/src/benchmarks.rs
@@ -137,7 +137,8 @@ benchmarks! {
 				transact_required_weight_at_most: dest_weight,
 				// This involves a db Read, hence the None is worst case
 				overall_weight: None
-			}
+			},
+			false
 		);
 
 		// It's expected that the error comes from the fact that the asset is not known
@@ -189,7 +190,8 @@ benchmarks! {
 				transact_required_weight_at_most: dest_weight,
 				// This involves a db Read, hence the None is worst case
 				overall_weight: None
-			}
+			},
+			false
 		);
 
 		// It's expected that the error comes from the fact that the asset is not known
