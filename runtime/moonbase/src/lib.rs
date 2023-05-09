57215887c0c239a2dd90dfe34b8829ed8a34c15d
Agus <agus@purestake.com>
2023-05-09 12:46:57 -0300

    fix depositFactor

--- runtime/moonbase/src/lib.rs
+++ runtime/moonbase/src/lib.rs
@@ -1256,7 +1256,7 @@ parameter_types! {
 	// One storage item; key size is 32; value is size 4+4+16+20 bytes = 44 bytes.
 	pub const DepositBase: Balance = currency::deposit(1, 76);
 	// Additional storage item size of 32 bytes.
-	pub const DepositFactor: Balance = currency::deposit(0, 20);
+	pub const DepositFactor: Balance = currency::deposit(0, 32);
 	pub const MaxSignatories: u32 = 100;
 }
 
