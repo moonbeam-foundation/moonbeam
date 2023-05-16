2f193baaba974998ccdb4eec5fc04d94b51b8a17
timbrinded <79199034+timbrinded@users.noreply.github.com>
2023-05-16 09:53:54 +0100

    added typesafe viem helpers

---
+++ test/helpers/viem.ts
@@ -0,0 +1,102 @@
+// TODO: Refactor these into moonwall util once they have matured
+
+import { DevModeContext } from "@moonwall/cli";
+import { ALITH_PRIVATE_KEY } from "@moonwall/util";
+import { TransactionSerializable } from "viem";
+import { privateKeyToAccount } from "viem/accounts";
+
+type InputAmountFormats = number | bigint | string | `0x${string}`;
+
+type DeepPartial<T> = {
+  [P in keyof T]?: T[P] extends (infer U)[]
+    ? DeepPartial<U>[]
+    : T[P] extends ReadonlyArray<infer U>
+    ? ReadonlyArray<DeepPartial<U>>
+    : DeepPartial<T[P]>;
+};
+
+type TransferOptions =
+  | (Omit<TransactionSerializable, "to" | "value"> & {
+      privateKey: `0x${string}`;
+    })
+  | undefined;
+
+type TransactionOptions =
+  | TransactionSerializable & {
+      privateKey: `0x${string}`;
+    };
+
+export const TransactionTypes = ["eip1559", "eip2930", "legacy"] as const;
+export type TransactionType = (typeof TransactionTypes)[number];
+
+export async function createRawTransfer<TOptions extends TransferOptions>(
+  context: DevModeContext,
+  to: `0x${string}`,
+  value: InputAmountFormats,
+  options?: TOptions
+): Promise<string> {
+  const transferAmount = typeof value === "bigint" ? value : BigInt(value);
+  return await createRawTransaction(context, { ...options, to, value: transferAmount });
+}
+
+export async function createRawTransaction<TOptions extends DeepPartial<TransactionOptions>>(
+  context: DevModeContext,
+  options: TOptions
+): Promise<string> {
+  const type = !!options && !!options.type ? options.type : "eip1559";
+  const privateKey = !!options && !!options.privateKey ? options.privateKey : ALITH_PRIVATE_KEY;
+  const account = privateKeyToAccount(privateKey);
+  const value = options && options.value ? options.value : 0n;
+  const to = options && options.to ? options.to : "0x0000000000000000000000000000000000000000";
+  const chainId = await context.viemClient("public").getChainId();
+  const txnCount = await context
+    .viemClient("public")
+    .getTransactionCount({ address: account.address });
+  const gasPrice = await context.viemClient("public").getGasPrice();
+  const estimatedGas = await context.viemClient("public").estimateGas({ account, to, value });
+  const accessList = options && options.accessList ? options.accessList : [];
+  const data = options && options.data ? options.data : "0x";
+
+  const txnBlob: TransactionSerializable =
+    type === "eip1559"
+      ? {
+          to,
+          value,
+          maxFeePerGas: options && options.maxFeePerGas ? options.maxFeePerGas : gasPrice,
+          maxPriorityFeePerGas:
+            options && options.maxPriorityFeePerGas ? options.maxPriorityFeePerGas : gasPrice,
+          gas: options && options.gas ? options.gas : estimatedGas,
+          nonce: options && options.nonce ? options.nonce : txnCount,
+          data,
+          chainId,
+          type,
+        }
+      : type === "legacy"
+      ? {
+          to,
+          value,
+          gasPrice: options && options.gasPrice ? options.gasPrice : gasPrice,
+          gas: options && options.gas ? options.gas : estimatedGas,
+          nonce: options && options.nonce ? options.nonce : txnCount,
+          data,
+        }
+      : type === "eip2930"
+      ? {
+          to,
+          value,
+          gasPrice: options && options.gasPrice ? options.gasPrice : gasPrice,
+          gas: options && options.gas ? options.gas : estimatedGas,
+          nonce: options && options.nonce ? options.nonce : txnCount,
+          data,
+          chainId,
+          type,
+        }
+      : {};
+
+      if (type !== "legacy" && accessList.length > 0) {
+        // @ts-expect-error
+        txnBlob["accessList"] = accessList
+      }
+
+  return await account.signTransaction(txnBlob);
+}
--- test/suites/dev/test-balance/test-balance-existential/test-balance-existential.ts
+++ test/suites/dev/test-balance/test-balance-existential/test-balance-existential.ts
@@ -1,67 +1,19 @@
 import "@moonbeam-network/api-augment";
-import { expect, beforeAll, describeSuite, getDevChain } from "@moonwall/cli";
-import {
-  alith,
-  ALITH_ADDRESS,
-  baltathar,
-  GLMR,
-  MIN_GAS_PRICE,
-  RUNTIME_CONSTANTS,
-} from "@moonwall/util";
-import {
-  PrivateKeyAccount,
-  createPublicClient,
-  createWalletClient,
-  formatGwei,
-  http,
-  verifyMessage,
-  webSocket,
-} from "viem";
+import { expect, describeSuite, beforeEach } from "@moonwall/cli";
+import { alith, ALITH_ADDRESS, baltathar, GLMR, MIN_GAS_PRICE } from "@moonwall/util";
+import { PrivateKeyAccount } from "viem";
 import { privateKeyToAccount, generatePrivateKey } from "viem/accounts";
-import { localhost, moonbaseAlpha, moonbeam } from "viem/chains";
-import { getProviderPath, localViemNetworkDetails } from "../../../../helpers/common.js";
-import Keyring from "@polkadot/keyring";
-import { Wallet, ethers, Signer } from "ethers";
-import { createTransfer } from "../../../../helpers/transactions.js";
-// TODO: Sort out matrix tests with multi txn types, i.e. eip1550, legacy, etc.
-
-// describeDevMoonbeamAllEthTxTypes("Existential Deposit", (context) => {
-//   let randomWeb3Account: Account;
-//   before("setup accounts", async function () {
-//     randomWeb3Account = context.web3.eth.accounts.create("random");
-//     const { result, block } = await context.createBlock(
-//       createTransfer(context, randomWeb3Account.address, 10n * GLMR, {
-//         from: alith.address,
-//         gas: 21000,
-//       })
-//     );
-//     expect(result.successful, result.error?.name).to.be.true;
-//   });
-
-//   it("should be disabled (no reaped account on 0 balance)", async function () {
-//     const { block, result } = await context.createBlock(
-//       createTransfer(context, alith.address, 10n * GLMR - 21000n * MIN_GAS_PRICE, {
-//         from: randomWeb3Account.address,
-//         privateKey: randomWeb3Account.privateKey,
-//         gas: 21000,
-//         gasPrice: MIN_GAS_PRICE,
-//       })
-//     );
-//     expect(result.successful, result.error?.name).to.be.true;
-//     expect(parseInt(await context.web3.eth.getBalance(randomWeb3Account.address))).to.eq(0);
-//     expect(await context.web3.eth.getTransactionCount(randomWeb3Account.address)).to.eq(1);
-//   });
-// });
+import { TransactionTypes, createRawTransfer } from "../../../../helpers/viem.js";
 
 describeSuite({
   id: "D030101",
-  title: "Existential Deposit",
+  title: "Existential Deposit disabled",
   foundationMethods: "dev",
   testCases: ({ context, log, it }) => {
     let randomAccount: PrivateKeyAccount;
     let privateKey: `0x${string}`;
 
-    beforeAll(async function () {
+    beforeEach(async function () {
       privateKey = generatePrivateKey();
       randomAccount = privateKeyToAccount(privateKey);
       const { result, block } = await context.createBlock(
@@ -70,67 +22,68 @@ describeSuite({
       expect(result!.successful, result!.error?.name).to.be.true;
     });
 
+    for (const txnType of TransactionTypes) {
+      it({
+        id: `T0${TransactionTypes.indexOf(txnType) + 1}`,
+        title: `full ${txnType} transfer should not reap on 0 account balance`,
+        test: async function () {
+          const raw = await createRawTransfer(
+            context,
+            ALITH_ADDRESS,
+            10n * GLMR - 21000n * MIN_GAS_PRICE,
+            {
+              privateKey,
+              type: txnType,
+              gasPrice: MIN_GAS_PRICE,
+              gas: 21000n,
+              maxFeePerGas: MIN_GAS_PRICE,
+            }
+          );
+          const { result } = await context.createBlock(raw);
+
+          expect(
+            await context
+              .viemClient("public")
+              .getTransactionCount({ address: randomAccount.address })
+          ).toBe(1);
+          expect(result!.successful, result!.error?.name).toBe(true);
+          expect(
+            await context.viemClient("public").getBalance({ address: randomAccount.address })
+          ).toBe(0n);
+        },
+      });
+    }
+
     it({
-      id: "T01",
-      title: "should be disabled (no reaped account on 0 balance)",
+      id: "T04",
+      title: "should not reap on tiny balance",
       test: async function () {
-        const raw = await randomAccount.signTransaction({
-          to: ALITH_ADDRESS,
-          value: 10n * GLMR - 21000n * MIN_GAS_PRICE,
-          chainId: await context.viemClient("public").getChainId(),
-          type: "eip1559",
-          gas: 21000n,
-          maxFeePerGas: MIN_GAS_PRICE,
-          maxPriorityFeePerGas: MIN_GAS_PRICE,
-        });
-
-        const { block, result } = await context.createBlock(raw);
-
+        await context.createBlock(
+          createRawTransfer(context, baltathar.address, 10n * GLMR - 1n - 21000n * MIN_GAS_PRICE, {
+            privateKey,
+            type: "legacy",
+            gas: 21000n,
+            gasPrice: MIN_GAS_PRICE,
+          })
+        );
+        expect(
+          await context.viemClient("public").getBalance({ address: randomAccount.address })
+        ).toBe(1n);
         expect(
           await context.viemClient("public").getTransactionCount({ address: randomAccount.address })
         ).toBe(1);
-        expect(result!.successful, result!.error?.name).toBe(true);
-        expect(
-          await context.viemClient("public").getBalance({ address: randomAccount.address })
-        ).toBe(0n);
+      },
+    });
+
+    it({
+      id: "T05",
+      title: "runtime constant should be set to zero",
+      test: async function () {
+        const existentialDeposit = context
+          .polkadotJs()
+          .consts.balances.existentialDeposit.toBigInt();
+        expect(existentialDeposit).toBe(0n);
       },
     });
   },
 });
-
-// // run in legacy only -- this test requires that exactly its gas_price * gas_limit be deducted from
-// // the sender's account
-// describeDevMoonbeam("Existential Deposit", (context) => {
-//   let randomWeb3Account: Account;
-//   before("setup accounts", async function () {
-//     randomWeb3Account = context.web3.eth.accounts.create("random");
-//     await context.createBlock(
-//       createTransfer(context, randomWeb3Account.address, 10n * GLMR, {
-//         from: alith.address,
-//         gas: 21000,
-//         gasPrice: MIN_GAS_PRICE,
-//       })
-//     );
-//   });
-
-//   it("should be disabled (no reaped account on tiny balance - 1)", async function () {
-//     await context.createBlock(
-//       createTransfer(context, baltathar.address, 10n * GLMR - 1n - 21000n * MIN_GAS_PRICE, {
-//         from: randomWeb3Account.address,
-//         privateKey: randomWeb3Account.privateKey,
-//         gas: 21000,
-//         gasPrice: MIN_GAS_PRICE,
-//       })
-//     );
-//     expect(parseInt(await context.web3.eth.getBalance(randomWeb3Account.address))).to.eq(1);
-//     expect(await context.web3.eth.getTransactionCount(randomWeb3Account.address)).to.eq(1);
-//   });
-// });
-
-// describeDevMoonbeam("Existential Deposit", (context) => {
-//   it("checks that existantial deposit is set to zero", async function () {
-//     // Grab existential deposit
-//     let existentialDeposit = (await context.polkadotApi.consts.balances.existentialDeposit) as any;
-//     expect(existentialDeposit.toBigInt()).to.eq(0n);
-//   });
-// });
