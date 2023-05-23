25258985997544366486cf7ebcdff885911ae10d
Agustín Rodriguez <agus@purestake.com>
2023-05-23 13:00:30 -0300

    Adapt xcm-utils to XCM V3 (#2304)
    * first attempt
    
    * progress
    
    * added new instructions to fragment
    
    * add more instructions (draft)
    
    * add remaining xcmv3 instructions
    
    * split files and fmt
    
    * delete files
    
    * refactor of some files
    
    * add comments
    
    * refactor code and parameterize with new interfaces
    
    * add default case in expect
    
    * remove unused imports
    
    * add tests for existing v2 instructions
    
    ---------
    
    Co-authored-by: timbrinded <79199034+timbrinded@users.noreply.github.com>

--- tests/tests/test-xcm/test-xcm-erc20-v3.ts
+++ tests/tests/test-xcm/test-xcm-erc20-v3.ts
@@ -1,12 +1,11 @@
 import "@moonbeam-network/api-augment";
-import { bnToHex, stringToU8a } from "@polkadot/util";
+import { bnToHex } from "@polkadot/util";
 
 import { expect } from "chai";
 import { ethers } from "ethers";
 import { Contract } from "web3-eth-contract";
 
-import { ALITH_ADDRESS, BALTATHAR_ADDRESS, CHARLETH_ADDRESS } from "../../util/accounts";
-import { PRECOMPILE_XTOKENS_ADDRESS } from "../../util/constants";
+import { CHARLETH_ADDRESS } from "../../util/accounts";
 import { web3EthCall } from "../../util/providers";
 import { getCompiled } from "../../util/contracts";
 import { describeDevMoonbeam } from "../../util/setup-dev-tests";
@@ -14,7 +13,6 @@ import {
   ALITH_TRANSACTION_TEMPLATE,
   createTransaction,
   setupErc20Contract,
-  ERC20_TOTAL_SUPPLY,
 } from "../../util/transactions";
 import { expectEVMResult } from "../../util/eth-transactions";
 import {
@@ -27,75 +25,8 @@ import {
 
 const ERC20_CONTRACT = getCompiled("ERC20WithInitialSupply");
 const ERC20_INTERFACE = new ethers.utils.Interface(ERC20_CONTRACT.contract.abi);
-const XTOKENS_CONTRACT = getCompiled("XtokensInstance");
-const XTOKENS_INTERFACE = new ethers.utils.Interface(XTOKENS_CONTRACT.contract.abi);
 
-describeDevMoonbeam("Mock XCM - Send local erc20", (context) => {
-  let erc20Contract: Contract;
-  let erc20ContractAddress: string;
-
-  before("Should deploy erc20 contract", async function () {
-    const { contract, contractAddress } = await setupErc20Contract(context, "MyToken", "TKN");
-    erc20Contract = contract;
-    erc20ContractAddress = contractAddress;
-  });
-
-  it("Should be able to transfer ERC20 token through xcm with xtoken precomp", async function () {
-    const amountTransferred = 1000n;
-    // Destination as multilocation
-    const destination = [
-      // one parent
-      1,
-      // This represents X1(AccountKey20(BALTATHAR_ADDRESS, NetworkAny))
-      // AccountKey20 variant (03) + the 20 bytes account + Any network variant (00)
-      ["0x03" + BALTATHAR_ADDRESS.slice(2) + "00"],
-    ];
-    const data = XTOKENS_INTERFACE.encodeFunctionData(
-      // action
-      "transfer",
-      [
-        // address of the multiasset
-        erc20ContractAddress,
-        // amount
-        amountTransferred,
-        // Destination as multilocation
-        destination,
-        // weight
-        500_000_000n,
-      ]
-    );
-
-    const { result } = await context.createBlock(
-      createTransaction(context, {
-        ...ALITH_TRANSACTION_TEMPLATE,
-        to: PRECOMPILE_XTOKENS_ADDRESS,
-        data,
-      })
-    );
-    expectEVMResult(result.events, "Succeed");
-
-    const receipt = await context.web3.eth.getTransactionReceipt(result.hash);
-    const gasPrice = receipt.effectiveGasPrice;
-    const fees = BigInt(receipt.gasUsed) * BigInt(gasPrice);
-
-    // Fees should have been spent
-    expect(BigInt(await context.web3.eth.getBalance(ALITH_ADDRESS, 2))).to.equal(
-      BigInt(await context.web3.eth.getBalance(ALITH_ADDRESS, 1)) - fees
-    );
-
-    // Erc20 tokens should have been spent
-    expect(
-      (
-        await web3EthCall(context.web3, {
-          to: erc20ContractAddress,
-          data: ERC20_INTERFACE.encodeFunctionData("balanceOf", [ALITH_ADDRESS]),
-        })
-      ).result
-    ).equals(bnToHex(ERC20_TOTAL_SUPPLY - amountTransferred, { bitLength: 256 }));
-  });
-});
-
-describeDevMoonbeam("Mock XCM - Receive back erc20", (context) => {
+describeDevMoonbeam("Mock XCM V3 - Receive erc20 via XCM", (context) => {
   let erc20Contract: Contract;
   let erc20ContractAddress: string;
 
@@ -188,8 +119,8 @@ describeDevMoonbeam("Mock XCM - Receive back erc20", (context) => {
       .withdraw_asset()
       .clear_origin()
       .buy_execution()
-      .deposit_asset(2n)
-      .as_v2();
+      .deposit_asset_v3(2n)
+      .as_v3();
 
     // Mock the reception of the xcm message
     await injectHrmpMessage(context, paraId, {
