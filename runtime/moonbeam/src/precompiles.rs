c9938c598cf0484a96783336ead3e30addc6c6aa
Stephen Shelton <steve@brewcraft.org>
2023-06-06 14:44:28 +0000

    Add GMP precompile to Moonriver and Moonbeam runtimes (#2327)

--- runtime/moonbeam/src/precompiles.rs
+++ runtime/moonbeam/src/precompiles.rs
@@ -31,6 +31,7 @@ use pallet_evm_precompile_collective::CollectivePrecompile;
 use pallet_evm_precompile_conviction_voting::ConvictionVotingPrecompile;
 use pallet_evm_precompile_crowdloan_rewards::CrowdloanRewardsPrecompile;
 use pallet_evm_precompile_democracy::DemocracyPrecompile;
+use pallet_evm_precompile_gmp::GmpPrecompile;
 use pallet_evm_precompile_modexp::Modexp;
 use pallet_evm_precompile_parachain_staking::ParachainStakingPrecompile;
 use pallet_evm_precompile_preimage::PreimagePrecompile;
@@ -228,6 +229,7 @@ type MoonbeamPrecompilesAt<R> = (
 		PrecompileRegistry<R>,
 		(CallableByContract, CallableByPrecompile),
 	>,
+	PrecompileAt<AddressU64<2070>, GmpPrecompile<R>, SubcallWithMaxNesting<0>>,
 );
 
 /// The PrecompileSet installed in the Moonbeam runtime.
