45c14f7be578412ddd50768c193b0caf07499f2e
timbrinded <79199034+timbrinded@users.noreply.github.com>
2023-06-16 12:04:15 +0100

    fix CI

--- test/suites/para/test_moonbeam.ts
+++ test/suites/para/test_moonbeam.ts
@@ -15,8 +15,8 @@ describeSuite({
     let ethersSigner: Signer;
 
     beforeAll(async () => {
-      paraApi = context.polkadotJs({ type: "moon" });
-      relayApi = context.polkadotJs({ type: "polkadotJs" });
+      paraApi = context.polkadotJs({ apiName: "parachain" });
+      relayApi = context.polkadotJs({ apiName: "relaychain" });
       ethersSigner = context.ethers();
 
       const relayNetwork = relayApi.consts.system.version.specName.toString();
