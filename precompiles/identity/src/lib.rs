4e6f73aece906b3da9fd4d3bc82026ea257e1eb2
Nisheeth Barthwal <nbaztec@gmail.com>
2023-07-17 16:57:23 +0200

    add add_sub

--- precompiles/identity/src/lib.rs
+++ precompiles/identity/src/lib.rs
@@ -58,8 +58,13 @@ where
 	}
 
 	#[precompile::public("setIdentity()")]
-	fn set_identity(handle: &mut impl PrecompileHandle, info: IdentityInfo<Runtime::MaxAdditionalFields>) -> EvmResult {
-		let call = pallet_identity::Call::<Runtime>::set_identity { info: Self::input_to_identity(info)? };
+	fn set_identity(
+		handle: &mut impl PrecompileHandle,
+		info: IdentityInfo<Runtime::MaxAdditionalFields>,
+	) -> EvmResult {
+		let call = pallet_identity::Call::<Runtime>::set_identity {
+			info: Self::input_to_identity(info)?,
+		};
 
 		let origin = Runtime::AddressMapping::into_account_id(handle.context().caller);
 		RuntimeHelper::<Runtime>::try_dispatch(handle, Some(origin).into(), call)?;
@@ -67,11 +72,15 @@ where
 		Ok(())
 	}
 
-	#[precompile::public("setSubs()")]
+	#[precompile::public("setSubs((address,(bool,bytes))[])")]
 	fn set_subs(handle: &mut impl PrecompileHandle, subs: Vec<(Address, Data)>) -> EvmResult {
 		let mut call_subs = vec![];
-		for (addr, data) in subs {
-			call_subs.push();
+		for (i, (addr, data)) in subs.into_iter().enumerate() {
+			let addr = Runtime::AddressMapping::into_account_id(addr.into());
+			let data: pallet_identity::Data = data
+				.try_into()
+				.map_err(|e| RevertReason::custom(e).in_field(format!("index {i}")))?;
+			call_subs.push((addr, data));
 		}
 		let call = pallet_identity::Call::<Runtime>::set_subs { subs: call_subs };
 
@@ -81,33 +90,85 @@ where
 		Ok(())
 	}
 
-	fn input_to_identity(info: IdentityInfo<Runtime::MaxAdditionalFields>) -> MayRevert<Box<pallet_identity::IdentityInfo<Runtime::MaxAdditionalFields>>> {
-		// let additional: Vec<(pallet_identity::Data, pallet_identity::Data)> = info.additional.into();
-		let mut additional: sp_runtime::BoundedVec<(pallet_identity::Data, pallet_identity::Data), Runtime::MaxAdditionalFields> = Default::default();
-		let iter: Vec<_> = info.additional.into();
-		for (i, (k, v)) in iter.iter().enumerate() {
-			let k: pallet_identity::Data = k.clone().try_into().map_err(|e| RevertReason::custom(e).in_field(format!("additional.{i}.key")))?;
-			let v: pallet_identity::Data = v.clone().try_into().map_err(|e| RevertReason::custom(e).in_field(format!("additional.{i}.value")))?;
-			additional.try_push((k, v)).map_err(|_| RevertReason::custom("out of bounds").in_field(format!("additional")))?;
+	#[precompile::public("clear_identity()")]
+	fn clear_identity(handle: &mut impl PrecompileHandle, subs: Vec<(Address, Data)>) -> EvmResult {
+		let mut call_subs = vec![];
+		for (i, (addr, data)) in subs.into_iter().enumerate() {
+			let addr = Runtime::AddressMapping::into_account_id(addr.into());
+			let data: pallet_identity::Data = data
+				.try_into()
+				.map_err(|e| RevertReason::custom(e).in_field(format!("index {i}")))?;
+			call_subs.push((addr, data));
 		}
-		
+		let call = pallet_identity::Call::<Runtime>::set_subs { subs: call_subs };
+
+		let origin = Runtime::AddressMapping::into_account_id(handle.context().caller);
+		RuntimeHelper::<Runtime>::try_dispatch(handle, Some(origin).into(), call)?;
+
+		Ok(())
+	}
+
+	fn input_to_identity(
+		info: IdentityInfo<Runtime::MaxAdditionalFields>,
+	) -> MayRevert<Box<pallet_identity::IdentityInfo<Runtime::MaxAdditionalFields>>> {
+		// let additional: Vec<(pallet_identity::Data, pallet_identity::Data)> = info.additional.into();
+		let mut additional: sp_runtime::BoundedVec<
+			(pallet_identity::Data, pallet_identity::Data),
+			Runtime::MaxAdditionalFields,
+		> = Default::default();
+		let iter: Vec<_> = info.additional.into();
+		for (i, (k, v)) in iter.into_iter().enumerate() {
+			let k: pallet_identity::Data = k
+				.try_into()
+				.map_err(|e| RevertReason::custom(e).in_field(format!("additional.{i}.key")))?;
+			let v: pallet_identity::Data = v
+				.try_into()
+				.map_err(|e| RevertReason::custom(e).in_field(format!("additional.{i}.value")))?;
+			additional.try_push((k, v)).map_err(|_| {
+				RevertReason::custom("out of bounds").in_field(format!("additional"))
+			})?;
+		}
+
 		let pgp_fingerprint: Option<[u8; 20]> = if info.has_pgp_fingerprint {
 			let fingerprint: Vec<_> = info.pgp_fingerprint.into();
-			let fingerprint = fingerprint.try_into().map_err(|_| RevertReason::custom("pgp_fingerprint must be 20 bytes long"))?;
+			let fingerprint = fingerprint
+				.try_into()
+				.map_err(|_| RevertReason::custom("pgp_fingerprint must be 20 bytes long"))?;
 			Some(fingerprint)
 		} else {
 			None
 		};
 		let identity_info = pallet_identity::IdentityInfo::<Runtime::MaxAdditionalFields> {
 			additional,
-			display: info.display.try_into().map_err(|e| RevertReason::custom(e).in_field(format!("display")))?,
-			legal: info.legal.try_into().map_err(|e| RevertReason::custom(e).in_field(format!("legal")))?,
-			web: info.web.try_into().map_err(|e| RevertReason::custom(e).in_field(format!("web")))?,
-			riot: info.riot.try_into().map_err(|e| RevertReason::custom(e).in_field(format!("riot")))?,
-			email: info.email.try_into().map_err(|e| RevertReason::custom(e).in_field(format!("email")))?,
+			display: info
+				.display
+				.try_into()
+				.map_err(|e| RevertReason::custom(e).in_field(format!("display")))?,
+			legal: info
+				.legal
+				.try_into()
+				.map_err(|e| RevertReason::custom(e).in_field(format!("legal")))?,
+			web: info
+				.web
+				.try_into()
+				.map_err(|e| RevertReason::custom(e).in_field(format!("web")))?,
+			riot: info
+				.riot
+				.try_into()
+				.map_err(|e| RevertReason::custom(e).in_field(format!("riot")))?,
+			email: info
+				.email
+				.try_into()
+				.map_err(|e| RevertReason::custom(e).in_field(format!("email")))?,
 			pgp_fingerprint,
-			image: info.image.try_into().map_err(|e| RevertReason::custom(e).in_field(format!("image")))?,
-			twitter: info.twitter.try_into().map_err(|e| RevertReason::custom(e).in_field(format!("twitter")))?,
+			image: info
+				.image
+				.try_into()
+				.map_err(|e| RevertReason::custom(e).in_field(format!("image")))?,
+			twitter: info
+				.twitter
+				.try_into()
+				.map_err(|e| RevertReason::custom(e).in_field(format!("twitter")))?,
 		};
 
 		Ok(Box::new(identity_info))
@@ -134,7 +195,6 @@ pub struct IdentityInfo<FieldLimit> {
 	twitter: Data,
 }
 
-
 impl TryFrom<Data> for pallet_identity::Data {
 	type Error = &'static str;
 
@@ -144,7 +204,8 @@ impl TryFrom<Data> for pallet_identity::Data {
 		}
 
 		let value: Vec<_> = value.value.into();
-		let value: sp_runtime::BoundedVec<_, ConstU32<32>> = value.try_into().map_err(|_| "exceeded bounds")?;
+		let value: sp_runtime::BoundedVec<_, ConstU32<32>> =
+			value.try_into().map_err(|_| "exceeded bounds")?;
 		Ok(pallet_identity::Data::Raw(value))
 	}
 }
