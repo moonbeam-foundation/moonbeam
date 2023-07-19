38385249714a1703a71b1bbc24b683bf922a7026
agus <agusrodriguez2456@gmail.com>
2023-07-19 18:36:20 -0300

    status.isFrozen fix in precompile TS tests

--- tests/tests/test-precompile/test-precompile-local-assets-erc20.ts
+++ tests/tests/test-precompile/test-precompile-local-assets-erc20.ts
@@ -953,7 +953,7 @@ describeDevMoonbeamAllEthTxTypes(
 
       let alithFrozen = await context.polkadotApi.query.localAssets.account(assetId, alith.address);
 
-      expect(alithFrozen.unwrap().isFrozen).to.be.true;
+      expect(alithFrozen.unwrap().status.isFrozen).to.be.true;
     });
   },
   true
@@ -1002,7 +1002,7 @@ describeDevMoonbeamAllEthTxTypes(
         alith.address
       );
 
-      expect(baltatharFrozen.unwrap().isFrozen).to.be.false;
+      expect(baltatharFrozen.unwrap().status.isFrozen).to.be.false;
     });
   },
   true
