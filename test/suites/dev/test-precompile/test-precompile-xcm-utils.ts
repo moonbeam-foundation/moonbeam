e167882600db88e223bbf391cf8fdc4708692006
timbrinded <79199034+timbrinded@users.noreply.github.com>
2023-07-24 21:01:08 +0100

    Precompile tests done

--- test/suites/dev/test-precompile/test-precompile-xcm-utils.ts
+++ test/suites/dev/test-precompile/test-precompile-xcm-utils.ts
@@ -1,29 +1,12 @@
 import "@moonbeam-network/api-augment";
 import { describeSuite, expect } from "@moonwall/cli";
-import { bnToHex, u8aToHex } from "@polkadot/util";
-import { descendOriginFromAddress20 } from "../../../helpers/xcm.js";
 import { GLMR, generateKeyringPair } from "@moonwall/util";
 import { XcmVersionedXcm } from "@polkadot/types/lookup";
+import { u8aToHex } from "@polkadot/util";
 import { expectEVMResult } from "../../../helpers/eth-transactions.js";
-
-// import { u8aToHex, bnToHex } from "@polkadot/util";
-// import { expect } from "chai";
-// import { ethers } from "ethers";
-// import { GLMR, PRECOMPILE_XCM_UTILS_ADDRESS } from "../../util/constants";
-// import { getCompiled } from "../../util/contracts";
-// import { web3EthCall } from "../../util/providers";
-// import { describeSuite, describeDevMoonbeam } from "../../util/setup-dev-tests";
-// import { generateKeyringPair } from "../../util/accounts";
-// import { BN } from "@polkadot/util";
-// import type { XcmVersionedXcm } from "@polkadot/types/lookup";
-// import { descendOriginFromAddress20 } from "../../util/xcm";
-// import { ALITH_TRANSACTION_TEMPLATE, createTransaction } from "../../util/transactions";
-// import { expectEVMResult, extractRevertReason } from "../../util/eth-transactions";
+import { descendOriginFromAddress20 } from "../../../helpers/xcm.js";
 
 export const CLEAR_ORIGIN_WEIGHT = 5_194_000n;
-
-// const XCM_UTILS_CONTRACT = getCompiled("precompiles/xcm-utils/XcmUtils");
-// const XCM_UTILSTRANSACTOR_INTERFACE = new ethers.utils.Interface(XCM_UTILS_CONTRACT.contract.abi);
 
 describeSuite({
   id: "D2578",
@@ -218,6 +201,225 @@ describeSuite({
         expect(testAccountBalance).to.eq(1n * GLMR);
       },
     });
+
+    it({
+      id: "T07",
+      title: "allows to execute a custom xcm evm to evm, but reentrancy forbids",
+      test: async function () {
+        let random = generateKeyringPair();
+
+        const ethTx = {
+          V1: {
+            gas_limit: 21000,
+            fee_payment: {
+              Auto: {
+                Low: null,
+              },
+            },
+            action: {
+              Call: random.address,
+            },
+            value: 1n * GLMR,
+            input: [],
+            access_list: null,
+          },
+        };
+        const transferCall = context.polkadotJs().tx.ethereumXcm.transact(ethTx as any);
+        const transferCallEncoded = transferCall?.method.toHex();
+
+        const xcmMessage = {
+          V2: [
+            {
+              Transact: {
+                originType: "SovereignAccount",
+                requireWeightAtMost: 525_000_000n + 25_000_000n, // 21_000 gas limit
+                call: {
+                  encoded: transferCallEncoded,
+                },
+              },
+            },
+          ],
+        };
+
+        const receivedMessage: XcmVersionedXcm = context
+          .polkadotJs()
+          .createType("XcmVersionedXcm", xcmMessage);
+
+        const rawTxn = await context.writePrecompile!({
+          precompileName: "XcmUtils",
+          functionName: "xcmExecute",
+          args: [receivedMessage.toHex(), 4_000_000_000],
+          rawTxOnly: true,
+          gas: 5_000_000n,
+        });
+
+        const { result } = await context.createBlock(rawTxn);
+        expectEVMResult(result!.events, "Succeed");
+
+        // Tokens transferred
+        const testAccountBalance = (
+          await context.polkadotJs().query.system.account(random.address)
+        ).data.free.toBigInt();
+
+        expect(testAccountBalance, "Transfer went through, possible EVM re-entrancy").to.eq(0n);
+      },
+    });
+
+    it({
+      id: "T08",
+      title: "does not allow to self-send a custom xcm message",
+      test: async function () {
+        const ownParaId = (await context.polkadotJs().query.parachainInfo.parachainId()) as any;
+        const x1_parachain_asset_enum_selector = "0x00";
+        const x1_parachain_id = ownParaId.toHex().slice(2);
+
+        // Sending it here
+        // { parents:0, Here}
+        const destHere: [number, {}[]] = [
+          // one parents
+          0,
+          // Here
+          [],
+        ];
+
+        // Sending it with the representation of the para as seen by the relay
+        // { parents:0, parachain(0)}
+        const destParaRelayView: [number, {}[]] = [
+          // one parents
+          0,
+          // Parachain(0)
+          [x1_parachain_asset_enum_selector + x1_parachain_id],
+        ];
+
+        // Sending it with the representation of the para as seen by other paras
+        // { parents:1, parachain(0)}
+        const destParaOtherParaView: [number, {}[]] = [
+          // one parents
+          1,
+          // Parachain(0)
+          [x1_parachain_asset_enum_selector + x1_parachain_id],
+        ];
+
+        const xcmMessage = {
+          V2: [
+            {
+              ClearOrigin: null,
+            },
+          ],
+        };
+
+        const sentMessage: XcmVersionedXcm = context
+          .polkadotJs()
+          .createType("XcmVersionedXcm", xcmMessage) as any;
+
+        // Try sending it with local view
+        const localRawTxn = await context.writePrecompile!({
+          precompileName: "XcmUtils",
+          functionName: "xcmSend",
+          args: [destHere, sentMessage.toHex()],
+          rawTxOnly: true,
+          gas: 1_000_000n,
+        });
+
+        const { result: localResult } = await context.createBlock(localRawTxn);
+        expectEVMResult(localResult!.events, "Revert");
+        expect(
+          async () =>
+            await context.writePrecompile!({
+              precompileName: "XcmUtils",
+              functionName: "xcmSend",
+              args: [destHere, sentMessage.toHex()],
+            })
+        ).rejects.toThrowError(
+          "Dispatched call failed with error: Module(ModuleError " +
+            '{ index: 28, error: [0, 0, 0, 0], message: Some("Unreachable") })'
+        );
+
+        const paraRawTxn = await context.writePrecompile!({
+          precompileName: "XcmUtils",
+          functionName: "xcmSend",
+          args: [destParaRelayView, sentMessage.toHex()],
+          rawTxOnly: true,
+          gas: 1_000_000n,
+        });
+
+        const { result: paraResult } = await context.createBlock(paraRawTxn);
+
+        expectEVMResult(paraResult!.events, "Revert");
+        expect(
+          async () =>
+            await context.writePrecompile!({
+              precompileName: "XcmUtils",
+              functionName: "xcmSend",
+              args: [destParaRelayView, sentMessage.toHex()],
+            })
+        ).rejects.toThrowError(
+          "Dispatched call failed with error: Module(ModuleError " +
+            '{ index: 28, error: [0, 0, 0, 0], message: Some("Unreachable") })'
+        );
+
+        const paraRawTxn2 = await context.writePrecompile!({
+          precompileName: "XcmUtils",
+          functionName: "xcmSend",
+          args: [destParaOtherParaView, sentMessage.toHex()],
+          rawTxOnly: true,
+          gas: 1_000_000n,
+        });
+
+        const { result: paraResult2 } = await context.createBlock(paraRawTxn2);
+
+        expectEVMResult(paraResult2!.events, "Revert");
+        expect(
+          async () =>
+            await context.writePrecompile!({
+              precompileName: "XcmUtils",
+              functionName: "xcmSend",
+              args: [destParaOtherParaView, sentMessage.toHex()],
+            })
+        ).rejects.toThrowError(
+          "Dispatched call failed with error: Module(ModuleError " +
+            '{ index: 28, error: [1, 0, 0, 0], message: Some("SendFailure") })'
+        );
+      },
+    });
+
+    it({
+      id: "T09",
+      title: "allows to send a custom xcm message",
+      test: async function () {
+        // Sending it to the relay
+        // { parents:1, Here}
+        const dest = [
+          // one parents
+          1,
+          // Here
+          [],
+        ];
+
+        const xcmMessage = {
+          V2: [
+            {
+              ClearOrigin: null,
+            },
+          ],
+        };
+
+        const sentMessage: XcmVersionedXcm = context
+          .polkadotJs()
+          .createType("XcmVersionedXcm", xcmMessage);
+
+        const rawTxn = await context.writePrecompile!({
+          precompileName: "XcmUtils",
+          functionName: "xcmSend",
+          args: [dest, sentMessage.toHex()],
+          rawTxOnly: true,
+          gas: 1_000_000n,
+        });
+
+        const { result } = await context.createBlock(rawTxn);
+        expectEVMResult(result!.events, "Succeed");
+      },
+    });
   },
 });
 
@@ -327,221 +529,3 @@ describeSuite({
 //   "Legacy",
 //   "moonbeam"
 // );
-
-// describeSuite({id:"",title:"Precompiles - xcm utils",foundationMethods:"dev", testCases ({context, it ,log}) => {
-//   it({id:"",title:"allows to execute a custom xcm evm to evm, but reentrancy forbids", test: async function () {
-//     let random = generateKeyringPair();
-
-//     const ethTx = {
-//       V1: {
-//         gas_limit: 21000,
-//         fee_payment: {
-//           Auto: {
-//             Low: null,
-//           },
-//         },
-//         action: {
-//           Call: random.address,
-//         },
-//         value: 1n * GLMR,
-//         input: [],
-//         access_list: null,
-//       },
-//     };
-//     const transferCall = context.polkadotJs().tx.ethereumXcm.transact(ethTx as any);
-//     const transferCallEncoded = transferCall?.method.toHex();
-
-//     const xcmMessage = {
-//       V2: [
-//         {
-//           Transact: {
-//             originType: "SovereignAccount",
-//             requireWeightAtMost: new BN(525_000_000).add(new BN(25_000_000)), // 21_000 gas limit
-//             call: {
-//               encoded: transferCallEncoded,
-//             },
-//           },
-//         },
-//       ],
-//     };
-
-//     const receivedMessage: XcmVersionedXcm = context.polkadotJs().createType(
-//       "XcmVersionedXcm",
-//       xcmMessage
-//     ) as any;
-
-//     await context.createBlock(
-//       createTransaction(context, {
-//         ...ALITH_TRANSACTION_TEMPLATE,
-//         to: PRECOMPILE_XCM_UTILS_ADDRESS,
-//         data: XCM_UTILSTRANSACTOR_INTERFACE.encodeFunctionData("xcmExecute", [
-//           receivedMessage.toU8a(),
-//           4_000_000_000,
-//         ]),
-//       })
-//     );
-
-//     // Tokens transferred
-//     const testAccountBalance = (
-//       await context.polkadotJs().query.system.account(random.address)
-//     ).data.free.toBigInt();
-
-//     // Transfer did not go through, EVM reentrancy NOT POSSIBLE
-//     expect(testAccountBalance).to.eq(0n * GLMR);
-//   });
-// });
-
-// describeSuite({id:"",title:"Precompiles - xcm utils",foundationMethods:"dev", testCases ({context, it ,log}) => {
-//   it({id:"",title:"allows to send a custom xcm message", test: async function () {
-//     // Sending it to the relay
-//     // { parents:1, Here}
-//     const dest = [
-//       // one parents
-//       1,
-//       // Here
-//       [],
-//     ];
-
-//     const xcmMessage = {
-//       V2: [
-//         {
-//           ClearOrigin: null,
-//         },
-//       ],
-//     };
-
-//     const sentMessage: XcmVersionedXcm = context.polkadotJs().createType(
-//       "XcmVersionedXcm",
-//       xcmMessage
-//     ) as any;
-
-//     const { result } = await context.createBlock(
-//       createTransaction(context, {
-//         ...ALITH_TRANSACTION_TEMPLATE,
-//         to: PRECOMPILE_XCM_UTILS_ADDRESS,
-//         data: XCM_UTILSTRANSACTOR_INTERFACE.encodeFunctionData("xcmSend", [
-//           dest,
-//           sentMessage.toU8a(),
-//         ]),
-//       })
-//     );
-
-//     // Verify the result
-//     // Expect success
-//     expectEVMResult(result.events, "Succeed");
-//   });
-// });
-
-// describeSuite({id:"",title:"Precompiles - xcm utils",foundationMethods:"dev", testCases ({context, it ,log}) => {
-//   it({id:"",title:"does not allow to self-send a custom xcm message", test: async function () {
-//     const ownParaId = (await context.polkadotJs().query.parachainInfo.parachainId()) as any;
-
-//     const x1_parachain_asset_enum_selector = "0x00";
-
-//     const x1_parachain_id = ownParaId.toHex().slice(2);
-
-//     // Sending it here
-//     // { parents:0, Here}
-//     const destHere: [number, {}[]] = [
-//       // one parents
-//       0,
-//       // Here
-//       [],
-//     ];
-
-//     // Sending it with the representation of the para as seen by the relay
-//     // { parents:0, parachain(0)}
-//     const destParaRelayView: [number, {}[]] = [
-//       // one parents
-//       0,
-//       // Parachain(0)
-//       [x1_parachain_asset_enum_selector + x1_parachain_id],
-//     ];
-
-//     // Sending it with the representation of the para as seen by other paras
-//     // { parents:1, parachain(0)}
-//     const destParaOtherParaView: [number, {}[]] = [
-//       // one parents
-//       1,
-//       // Parachain(0)
-//       [x1_parachain_asset_enum_selector + x1_parachain_id],
-//     ];
-
-//     const xcmMessage = {
-//       V2: [
-//         {
-//           ClearOrigin: null,
-//         },
-//       ],
-//     };
-
-//     const sentMessage: XcmVersionedXcm = context.polkadotJs().createType(
-//       "XcmVersionedXcm",
-//       xcmMessage
-//     ) as any;
-
-//     // Try sending it with local view
-//     const { result: resultHere } = await context.createBlock(
-//       createTransaction(context, {
-//         ...ALITH_TRANSACTION_TEMPLATE,
-//         to: PRECOMPILE_XCM_UTILS_ADDRESS,
-//         data: XCM_UTILSTRANSACTOR_INTERFACE.encodeFunctionData("xcmSend", [
-//           destHere,
-//           sentMessage.toU8a(),
-//         ]),
-//       })
-//     );
-
-//     // Verify the result
-//     // Expect success
-//     expectEVMResult(resultHere.events, "Revert");
-//     const revertReason = await extractRevertReason(resultHere.hash, context.ethers);
-//     // Full error expected:
-//     // Dispatched call failed with error: Module(ModuleError { index: 28, error: [0, 0, 0, 0],
-//     // message: Some("Unreachable") })
-//     expect(revertReason).to.contain("Unreachable");
-
-//     // Try sending it with para relay view
-//     const { result: resultParaRelayView } = await context.createBlock(
-//       createTransaction(context, {
-//         ...ALITH_TRANSACTION_TEMPLATE,
-//         to: PRECOMPILE_XCM_UTILS_ADDRESS,
-//         data: XCM_UTILSTRANSACTOR_INTERFACE.encodeFunctionData("xcmSend", [
-//           destParaRelayView,
-//           sentMessage.toU8a(),
-//         ]),
-//       })
-//     );
-
-//     // Verify the result
-//     // Expect success
-//     expectEVMResult(resultParaRelayView.events, "Revert");
-//     const revertReason2 = await extractRevertReason(resultHere.hash, context.ethers);
-//     // Full error expected:
-//     // Dispatched call failed with error: Module(ModuleError { index: 28, error: [0, 0, 0, 0],
-//     // message: Some("Unreachable") })
-//     expect(revertReason2).to.contain("Unreachable");
-
-//     // Try sending it with another para view (parents 1)
-//     const { result: resultParaOtherParaView } = await context.createBlock(
-//       createTransaction(context, {
-//         ...ALITH_TRANSACTION_TEMPLATE,
-//         to: PRECOMPILE_XCM_UTILS_ADDRESS,
-//         data: XCM_UTILSTRANSACTOR_INTERFACE.encodeFunctionData("xcmSend", [
-//           destParaOtherParaView,
-//           sentMessage.toU8a(),
-//         ]),
-//       })
-//     );
-
-//     // Verify the result
-//     // Expect success
-//     expectEVMResult(resultParaOtherParaView.events, "Revert");
-
-//     const revertReason3 = await extractRevertReason(resultParaOtherParaView.hash, context.ethers);
-//     // Full error expected:
-//     // Dispatched call failed with error: Module(ModuleError { index: 28, error: [1, 0, 0, 0],
-//     // message: Some("SendFailure") })
-//     expect(revertReason3).to.contain("SendFailure");
-//   });
-// });
