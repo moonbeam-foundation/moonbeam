3ec95467264b34b9dcee61cb4e1710c08759af56
timbrinded <79199034+timbrinded@users.noreply.github.com>
2023-07-29 10:45:04 +0100

    more staking

---
+++ test/suites/dev/test-staking/test-set-auto-compound2.ts
@@ -0,0 +1,51 @@
+import "@moonbeam-network/api-augment";
+import { beforeAll, describeSuite, expect } from "@moonwall/cli";
+import {
+  MIN_GLMR_DELEGATOR,
+  MIN_GLMR_STAKING,
+  alith,
+  baltathar,
+  dorothy,
+  ethan,
+} from "@moonwall/util";
+
+describeSuite({
+  id: "D2969",
+  title: "Staking - Set Auto-Compound",
+  foundationMethods: "dev",
+  testCases: ({ context, it, log }) => {
+    beforeAll(async () => {
+      await context.createBlock(
+        context
+          .polkadotJs()
+          .tx.parachainStaking.delegateWithAutoCompound(
+            alith.address,
+            MIN_GLMR_DELEGATOR,
+            50,
+            0,
+            0,
+            0
+          )
+          .signAsync(ethan),
+        { allowFailures: false }
+      );
+    });
+
+    it({
+      id: "T01",
+      title: "wrong candidate auto-compounding delegation hint should fail",
+      test: async () => {
+        const block = await context.createBlock(
+          context
+            .polkadotJs()
+            .tx.parachainStaking.setAutoCompound(alith.address, 50, 0, 1)
+            .signAsync(ethan)
+        );
+        expect(block.result!.successful).to.be.false;
+        expect(block.result!.error!.name).to.equal(
+          "TooLowCandidateAutoCompoundingDelegationCountToAutoCompound"
+        );
+      },
+    });
+  },
+});
