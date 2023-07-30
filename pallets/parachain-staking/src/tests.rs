5a07bb731466a69b2ec7f3b90b828bbd56b1dad3
librelois <c@elo.tf>
2023-07-30 21:45:03 +0200

    fix rust tests compilation

--- pallets/parachain-staking/src/tests.rs
+++ pallets/parachain-staking/src/tests.rs
@@ -7788,7 +7788,7 @@ fn test_delegator_with_deprecated_status_leaving_can_schedule_leave_delegators_a
 		.build()
 		.execute_with(|| {
 			<DelegatorState<Test>>::mutate(2, |value| {
-				value.as_mut().map(|mut state| {
+				value.as_mut().map(|state| {
 					state.status = DelegatorStatus::Leaving(2);
 				})
 			});
@@ -7822,7 +7822,7 @@ fn test_delegator_with_deprecated_status_leaving_can_cancel_leave_delegators_as_
 		.build()
 		.execute_with(|| {
 			<DelegatorState<Test>>::mutate(2, |value| {
-				value.as_mut().map(|mut state| {
+				value.as_mut().map(|state| {
 					state.status = DelegatorStatus::Leaving(2);
 				})
 			});
@@ -7849,7 +7849,7 @@ fn test_delegator_with_deprecated_status_leaving_can_execute_leave_delegators_as
 		.build()
 		.execute_with(|| {
 			<DelegatorState<Test>>::mutate(2, |value| {
-				value.as_mut().map(|mut state| {
+				value.as_mut().map(|state| {
 					state.status = DelegatorStatus::Leaving(2);
 				})
 			});
@@ -7882,7 +7882,7 @@ fn test_delegator_with_deprecated_status_leaving_cannot_execute_leave_delegators
 		.build()
 		.execute_with(|| {
 			<DelegatorState<Test>>::mutate(2, |value| {
-				value.as_mut().map(|mut state| {
+				value.as_mut().map(|state| {
 					state.status = DelegatorStatus::Leaving(2);
 				})
 			});
@@ -8177,7 +8177,7 @@ fn test_execute_leave_delegators_with_deprecated_status_leaving_removes_auto_com
 			));
 
 			<DelegatorState<Test>>::mutate(2, |value| {
-				value.as_mut().map(|mut state| {
+				value.as_mut().map(|state| {
 					state.status = DelegatorStatus::Leaving(2);
 				})
 			});
