ae6cda6f90a4dfba1afbfda2295fd61027b0bb5d
timbrinded <79199034+timbrinded@users.noreply.github.com>
2023-07-31 09:09:14 +0100

    progress

---
+++ test/suites/dev/test-staking/test-staking-locks2.ts
@@ -0,0 +1,32 @@
+import "@moonbeam-network/api-augment";
+import { describeSuite, expect } from "@moonwall/cli";
+import { MIN_GLMR_STAKING, baltathar } from "@moonwall/util";
+import { fromBytes } from "viem";
+
+describeSuite({
+  id: "D2976",
+  title: "Staking - Locks - join candidates",
+  foundationMethods: "dev",
+  testCases: ({ context, it, log }) => {
+    it({
+      id: "T01",
+      title: 'should set "stkngcol" when when joining candidates',
+      test: async function () {
+        await context.createBlock(
+          context
+            .polkadotJs()
+            .tx.parachainStaking.joinCandidates(MIN_GLMR_STAKING, 1)
+            .signAsync(baltathar)
+        );
+
+        const locks = await context.polkadotJs().query.balances.locks(baltathar.address);
+        expect(locks.length).to.be.equal(
+          1,
+          `Unexpected number of locks: ${locks.map((l) => l.id.toHuman()).join(` - `)}`
+        );
+        expect(locks[0].amount.toBigInt()).to.be.equal(MIN_GLMR_STAKING);
+        expect(fromBytes(locks[0].id.toU8a(), "string")).to.be.equal("stkngcol");
+      },
+    });
+  },
+});
---
+++ test/suites/dev/test-staking/test-weights2.ts
@@ -0,0 +1,378 @@
+import "@moonbeam-network/api-augment";
+import { DevModeContext, afterEach, describeSuite, expect, beforeEach } from "@moonwall/cli";
+import {
+  GLMR,
+  KeyringPair,
+  MIN_GLMR_DELEGATOR,
+  MIN_GLMR_STAKING,
+  alith,
+  baltathar,
+  generateKeyringPair,
+} from "@moonwall/util";
+import chalk from "chalk";
+import { Debugger } from "debug";
+import { setTimeout } from "node:timers/promises";
+import { generatePrivateKey } from "viem/accounts";
+// import { expect } from "chai";
+// import { MIN_GLMR_STAKING, MIN_GLMR_DELEGATOR, GLMR } from "../../util/constants";
+// import { DevTestContext, describeDevMoonbeam } from "../../util/setup-dev-tests";
+// import { alith, ethan, generateKeyringPair } from "../../util/accounts";
+// import { expectOk } from "../../util/expect";
+// import { chunk } from "../../util/common";
+// import { KeyringPair } from "@substrate/txwrapper-core";
+// import chalk from "chalk";
+// import { jumpRounds } from "../../util/block";
+
+// const debug = require("debug")("test:staking-transaction-fit");
+
+describeSuite({
+  id: "D2988",
+  title: "Staking - Max Transaction Fit",
+  foundationMethods: "dev",
+  testCases: ({ context, it, log }) => {
+
+    it({
+      id: "T01",
+      title: "delegateWithAutoCompound",
+      timeout: 30000,
+      test: async () => {
+        const maxTransactions = 350;
+        const randomAccounts = await createAccounts(context, maxTransactions);
+
+        randomAccounts.forEach((account) => {
+          log(account.address);
+        });
+
+        await context.createBlock(
+          randomAccounts.map((account, index) =>
+            context
+              .polkadotJs()
+              .tx.parachainStaking.delegateWithAutoCompound(
+                alith.address,
+                MIN_GLMR_DELEGATOR,
+                100,
+                maxTransactions,
+                maxTransactions,
+                0
+              )
+              .signAsync(account)
+          ),
+          { allowFailures: false }
+        );
+
+        /// Boilerplate to get the number of transactions
+
+        const nameParts = expect.getState().currentTestName!.split(" ");
+        const methodName = nameParts[nameParts.length - 1];
+        const [numTransactions, weightUtil, proofUtil] = await countExtrinsics(
+          context,
+          methodName,
+          log
+        );
+
+        expect(numTransactions).to.be.greaterThanOrEqual(2);
+      },
+    });
+  },
+});
+
+
+// describeBenchmark("Staking - Max Transaction Fit", "delegatorBondMore", async (context) => {
+//   const maxTransactions = 350;
+//   const randomAccounts = await createAccounts(context, maxTransactions);
+//   for (const randomAccountsChunk of chunk(randomAccounts, 20)) {
+//     await expectOk(
+//       context.createBlock(
+//         randomAccountsChunk.map((account) =>
+//           context
+//             .polkadotJs()
+//             .tx.parachainStaking.delegateWithAutoCompound(
+//               alith.address,
+//               MIN_GLMR_DELEGATOR,
+//               100,
+//               maxTransactions,
+//               maxTransactions,
+//               0
+//             )
+//             .signAsync(account)
+//         )
+//       )
+//     );
+//   }
+
+//   expect((await context.polkadotJs().query.parachainStaking.delegatorState.keys()).length).to.equal(
+//     maxTransactions
+//   );
+
+//   await expectOk(
+//     context.createBlock(
+//       randomAccounts.map((account) =>
+//         context
+//           .polkadotJs()
+//           .tx.parachainStaking.delegatorBondMore(alith.address, 1000)
+//           .signAsync(account)
+//       )
+//     )
+//   );
+// });
+
+// describeBenchmark("Staking - Max Transaction Fit", "scheduleDelegatorBondLess", async (context) => {
+//   const maxTransactions = 350;
+//   const randomAccounts = await createAccounts(context, maxTransactions);
+//   for (const randomAccountsChunk of chunk(randomAccounts, 20)) {
+//     await expectOk(
+//       context.createBlock(
+//         randomAccountsChunk.map((account) =>
+//           context
+//             .polkadotJs()
+//             .tx.parachainStaking.delegateWithAutoCompound(
+//               alith.address,
+//               MIN_GLMR_DELEGATOR + 1000n,
+//               100,
+//               maxTransactions,
+//               maxTransactions,
+//               0
+//             )
+//             .signAsync(account)
+//         )
+//       )
+//     );
+//   }
+
+//   expect((await context.polkadotJs().query.parachainStaking.delegatorState.keys()).length).to.equal(
+//     maxTransactions
+//   );
+
+//   await expectOk(
+//     context.createBlock(
+//       randomAccounts.map((account) =>
+//         context
+//           .polkadotJs()
+//           .tx.parachainStaking.scheduleDelegatorBondLess(alith.address, 1000)
+//           .signAsync(account)
+//       )
+//     )
+//   );
+// });
+
+// describeBenchmark("Staking - Max Transaction Fit", "scheduleRevokeDelegation", async (context) => {
+//   const maxTransactions = 350;
+//   const randomAccounts = await createAccounts(context, maxTransactions);
+//   for (const randomAccountsChunk of chunk(randomAccounts, 20)) {
+//     await expectOk(
+//       context.createBlock(
+//         randomAccountsChunk.map((account) =>
+//           context
+//             .polkadotJs()
+//             .tx.parachainStaking.delegateWithAutoCompound(
+//               alith.address,
+//               MIN_GLMR_DELEGATOR,
+//               100,
+//               maxTransactions,
+//               maxTransactions,
+//               0
+//             )
+//             .signAsync(account)
+//         )
+//       )
+//     );
+//   }
+
+//   expect((await context.polkadotJs().query.parachainStaking.delegatorState.keys()).length).to.equal(
+//     maxTransactions
+//   );
+
+//   await expectOk(
+//     context.createBlock(
+//       randomAccounts.map((account) =>
+//         context
+//           .polkadotJs()
+//           .tx.parachainStaking.scheduleRevokeDelegation(alith.address)
+//           .signAsync(account)
+//       )
+//     )
+//   );
+// });
+
+// describeBenchmark("Staking - Max Transaction Fit", "scheduleLeaveDelegators", async (context) => {
+//   const maxTransactions = 350;
+//   const randomAccounts = await createAccounts(context, maxTransactions);
+//   for (const randomAccountsChunk of chunk(randomAccounts, 20)) {
+//     await expectOk(
+//       context.createBlock(
+//         randomAccountsChunk.map((account) =>
+//           context
+//             .polkadotJs()
+//             .tx.parachainStaking.delegateWithAutoCompound(
+//               alith.address,
+//               MIN_GLMR_DELEGATOR,
+//               100,
+//               maxTransactions,
+//               maxTransactions,
+//               0
+//             )
+//             .signAsync(account)
+//         )
+//       )
+//     );
+//   }
+
+//   expect((await context.polkadotJs().query.parachainStaking.delegatorState.keys()).length).to.equal(
+//     maxTransactions
+//   );
+
+//   await expectOk(
+//     context.createBlock(
+//       randomAccounts.map((account) =>
+//         context.polkadotJs().tx.parachainStaking.scheduleLeaveDelegators().signAsync(account)
+//       )
+//     )
+//   );
+// });
+
+// describeBenchmark("Staking - Max Transaction Fit", "executeLeaveDelegators", async (context) => {
+//   const maxTransactions = 350;
+//   const randomAccounts = await createAccounts(context, maxTransactions);
+
+//   await expectOk(
+//     context.createBlock(
+//       context
+//         .polkadotJs()
+//         .tx.sudo.sudo(context.polkadotJs().tx.parachainStaking.setBlocksPerRound(10))
+//         .signAsync(alith)
+//     )
+//   );
+
+//   for (const randomAccountsChunk of chunk(randomAccounts, 20)) {
+//     await expectOk(
+//       context.createBlock(
+//         randomAccountsChunk.map((account) =>
+//           context
+//             .polkadotJs()
+//             .tx.parachainStaking.delegateWithAutoCompound(
+//               alith.address,
+//               MIN_GLMR_DELEGATOR,
+//               100,
+//               maxTransactions,
+//               maxTransactions,
+//               0
+//             )
+//             .signAsync(account)
+//         )
+//       )
+//     );
+//   }
+
+//   expect((await context.polkadotJs().query.parachainStaking.delegatorState.keys()).length).to.equal(
+//     maxTransactions
+//   );
+
+//   for (const randomAccountsChunk of chunk(randomAccounts, 20)) {
+//     await expectOk(
+//       context.createBlock(
+//         randomAccountsChunk.map((account) =>
+//           context.polkadotJs().tx.parachainStaking.scheduleLeaveDelegators().signAsync(account)
+//         )
+//       )
+//     );
+//   }
+
+//   await jumpRounds(context, 3);
+
+//   await expectOk(
+//     context.createBlock(
+//       randomAccounts.map((account) =>
+//         context
+//           .polkadotJs()
+//           .tx.parachainStaking.executeLeaveDelegators(account.address, 1)
+//           .signAsync(account)
+//       )
+//     )
+//   );
+// });
+
+// // utils
+
+// function describeBenchmark(
+//   title: string,
+//   method: string,
+//   cb: (context: DevTestContext) => Promise<void>
+// ) {
+//   describeDevMoonbeam(`${title} - ${method}`, (context) => {
+//     it("should fit minimum 2", async function () {
+//       if (process.env["STAKING_BENCHMARK"] !== "1") {
+//         this.skip();
+//       }
+
+//       this.timeout(30000);
+//       const methodName = this.test.parent.title.split("-")[2].trim();
+//       await cb(context);
+//       const [numTransactions, weightUtil, proofUtil] = await countExtrinsics(context, methodName);
+//       console.log(
+//         `  ${chalk.yellow("○")} ${chalk.gray(methodName)} max ${chalk.green(
+//           numTransactions
+//         )} per block (w: ${(weightUtil * 100).toFixed(1)}%, p: ${(proofUtil * 100).toFixed(1)}%)`
+//       );
+//       expect(numTransactions).to.be.greaterThanOrEqual(2);
+//     });
+//   });
+// }
+
+async function createAccounts(
+  context: DevModeContext,
+  maxAccounts: number
+): Promise<KeyringPair[]> {
+  const randomAccounts = new Array(Number(maxAccounts))
+    .fill(0)
+    .map(() => generateKeyringPair("ethereum", generatePrivateKey()));
+
+  let alithNonce = await context
+    .viem()
+    .getTransactionCount({ address: alith.address as `0x${string}` });
+  await context.createBlock(
+    randomAccounts.map((randomCandidate) =>
+      context
+        .polkadotJs()
+        .tx.sudo.sudo(
+          context
+            .polkadotJs()
+            .tx.balances.setBalance(randomCandidate.address,12n* MIN_GLMR_STAKING + 50n * GLMR, 0n)
+        )
+        .signAsync(alith, { nonce: alithNonce++ })
+    ),
+    { allowFailures: false }
+  );
+
+  return randomAccounts;
+}
+
+async function countExtrinsics(
+  context: DevModeContext,
+  method: string,
+  logger: Debugger
+): Promise<[number, number, number]> {
+  const block = await context.polkadotJs().rpc.chain.getBlock();
+  const extrinsicCount = block.block.extrinsics.reduce(
+    (acc, ext) =>
+      acc + (ext.method.section === "parachainStaking" && ext.method.method === method ? 1 : 0),
+    0
+  );
+
+  const maxBlockWeights = context.polkadotJs().consts.system.blockWeights;
+  const blockWeights = await context.polkadotJs().query.system.blockWeight();
+
+  const weightUtil =
+    blockWeights.normal.refTime.toNumber() /
+    maxBlockWeights.perClass.normal.maxTotal.unwrap().refTime.toNumber();
+  const proofUtil =
+    blockWeights.normal.proofSize.toNumber() /
+    maxBlockWeights.perClass.normal.maxTotal.unwrap().proofSize.toNumber();
+
+  logger(
+    `  ${chalk.yellow("○")} ${chalk.gray(method)} max ${chalk.green(
+      extrinsicCount
+    )} per block (w: ${(weightUtil * 100).toFixed(1)}%, p: ${(proofUtil * 100).toFixed(1)}%)`
+  );
+
+  return [extrinsicCount, weightUtil, proofUtil];
+}
