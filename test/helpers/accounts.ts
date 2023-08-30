6d577a3647bdf5e5bdf838e3c0946d7f4d1a8325
Nisheeth Barthwal <nbaztec@gmail.com>
2023-08-30 11:56:53 +0200

    add conviction voting batch tests (#2423)
    * add conviction voting batch tests
    
    * fix duplicate functions

---
+++ test/helpers/accounts.ts
@@ -0,0 +1,29 @@
+import { DevModeContext } from "@moonwall/cli";
+import { KeyringPair } from "@moonwall/util";
+import { generateKeyringPair, alith } from "../../tests/util/accounts";
+import { MIN_GLMR_STAKING, GLMR } from "../../tests/util/constants";
+
+export async function createAccounts(
+  context: DevModeContext,
+  maxAccounts: number,
+  amount: bigint = MIN_GLMR_STAKING + 1n * GLMR
+): Promise<KeyringPair[]> {
+  const randomAccounts = new Array(Number(maxAccounts)).fill(0).map(() => generateKeyringPair());
+
+  let alithNonce = await context
+    .viem()
+    .getTransactionCount({ address: alith.address as `0x${string}` });
+  await context.createBlock(
+    randomAccounts.map((randomCandidate) =>
+      context
+        .polkadotJs()
+        .tx.sudo.sudo(
+          context.polkadotJs().tx.balances.forceSetBalance(randomCandidate.address, amount)
+        )
+        .signAsync(alith, { nonce: alithNonce++ })
+    ),
+    { allowFailures: false }
+  );
+
+  return randomAccounts;
+}
