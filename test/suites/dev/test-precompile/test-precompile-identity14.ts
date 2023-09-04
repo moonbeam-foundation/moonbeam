2ea0db7c18d907ddeda1a5f4d3f68262e10560e7
Tim B <79199034+timbrinded@users.noreply.github.com>
2023-09-04 21:03:55 +0100

    Deprecate `/tests` folder (#2467)
    * changed docs
    
    * fix readme
    
    * fix GH comments
    
    * fix ts test
    
    * add moonwall tests to coverage
    
    * fmt
    
    * fix
    
    * fix ts tests

--- test/suites/dev/test-precompile/test-precompile-identity14.ts
+++ test/suites/dev/test-precompile/test-precompile-identity14.ts
@@ -4,7 +4,7 @@ import { CHARLETH_PRIVATE_KEY, baltathar, charleth } from "@moonwall/util";
 import { expectEVMResult } from "../../../helpers/eth-transactions.js";
 import { expectSubstrateEvent } from "../../../helpers/expect.js";
 import { decodeEventLog } from "viem";
-import { PRECOMPILE_IDENTITY_ADDRESS } from "../../../../tests/util/constants.js";
+import { PRECOMPILE_IDENTITY_ADDRESS } from "../../../helpers/constants.ts";
 
 describeSuite({
   id: "D3413",
