392207f76cf7d833aa4f1a890d36ff39c08148b2
Andrea Giacobino <no.andrea@gmail.com>
2023-10-04 14:27:33 +0200

    prettier

--- test/suites/dev/test-conviction-voting/test-conviction-delegation-batch.ts
+++ test/suites/dev/test-conviction-voting/test-conviction-delegation-batch.ts
@@ -19,7 +19,9 @@ describeSuite({
     beforeAll(async () => {
       // make a proposal for each origin
       for (const [id, origin] of origins) {
-        const proposal = context.polkadotJs().tx.identity.setIdentity({ display: { raw: `Props ${id}` } });
+        const proposal = context
+          .polkadotJs()
+          .tx.identity.setIdentity({ display: { raw: `Props ${id}` } });
         proposals.push(await proposeReferendaAndDeposit(context, alith, proposal, origin));
       }
     });
@@ -28,31 +30,35 @@ describeSuite({
       id: "T01",
       title: "Alith should be able to delegate to Baltathar in batch",
       test: async function () {
-
         const blockResult = await context.createBlock(
           // in the same block delegate to all proposals
-          context.polkadotJs().tx.utility.batchAll(
-            origins.map(([id, _], i) =>
-              context
-                .polkadotJs()
-                .tx.convictionVoting.delegate(id, baltathar.address, "Locked1x", 100n * GLMR)
-            )
-          ), { allowFailures: false }
+          context
+            .polkadotJs()
+            .tx.utility.batchAll(
+              origins.map(([id, _], i) =>
+                context
+                  .polkadotJs()
+                  .tx.convictionVoting.delegate(id, baltathar.address, "Locked1x", 100n * GLMR)
+              )
+            ),
+          { allowFailures: false }
         );
 
         // the tests does not check for the events, but they should be there
         // expectSubstrateEvent(blockResult, "conviction  Voting", "Delegated");
 
         // check that all proposals have been delegated to baltathar
-        await Promise.all(origins.map(async ([id, _], i) => {
-          const votingFor = await context
-            .polkadotJs()
-            .query.convictionVoting.votingFor(ALITH_ADDRESS, id);
+        await Promise.all(
+          origins.map(async ([id, _], i) => {
+            const votingFor = await context
+              .polkadotJs()
+              .query.convictionVoting.votingFor(ALITH_ADDRESS, id);
 
-          expect(votingFor.isDelegating).toBe(true);
-          expect(votingFor.asDelegating.target.toString()).toBe(baltathar.address);
-          expect(votingFor.asDelegating.balance.toBigInt()).toBe(100n * GLMR);
-        }));
+            expect(votingFor.isDelegating).toBe(true);
+            expect(votingFor.asDelegating.target.toString()).toBe(baltathar.address);
+            expect(votingFor.asDelegating.balance.toBigInt()).toBe(100n * GLMR);
+          })
+        );
       },
     });
   },
