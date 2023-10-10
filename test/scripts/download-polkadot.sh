7ee2ad986b332e407f5ffadc5944755269af22ed
Cem Eliguzel <cem@moonsonglabs.com>
2023-10-10 08:50:01 +0000

    Fix the polkadot release extraction

--- test/scripts/download-polkadot.sh
+++ test/scripts/download-polkadot.sh
@@ -4,9 +4,8 @@
 set -e
 
 # Grab Polkadot version
-# branch=$(egrep -o '/polkadot.*#([^\"]*)' $(dirname $0)/../../Cargo.lock | head -1 | sed 's/.*release-//#')
-# polkadot_release=$(echo $branch | sed 's/#.*//' | sed 's/\/polkadot-sdk?branch=moonbeam-polkadot-v//')
-polkadot_release=1.1.0
+branch=$(egrep -o '/polkadot.*#([^\"]*)' $(dirname $0)/../../Cargo.lock | head -1 | sed 's/.*release-//#')
+polkadot_release=$(echo $branch | sed 's/#.*//' | sed 's/\/polkadot-sdk?branch=moonbeam-polkadot-v//')
 
 # Always run the commands from the "test" dir
 cd $(dirname $0)/..
