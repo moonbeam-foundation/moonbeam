69d8dcb3443045db21d23d2e263686e078ea8abd
Francisco Gamundi <52399794+fgamundi@users.noreply.github.com>
2023-10-10 10:38:58 +0200

    Disable creation of local XC-20s & remove tests (#2508)
    * Set LocalAsset ForceOrigin to never
    
    * Set for Moonriver and Moonbeam
    
    * Removed ts tests
    
    * clean & fmt

--- test/suites/dev/test-xcm-v2/test-mock-hrmp-asset-transfer-6.ts
+++ test/suites/dev/test-xcm-v2/test-mock-hrmp-asset-transfer-6.ts
@@ -2,17 +2,13 @@ import "@moonbeam-network/api-augment";
 import { beforeAll, describeSuite, expect } from "@moonwall/cli";
 
 import { BN } from "@polkadot/util";
-
 import { alith } from "@moonwall/util";
-
 import {
   registerForeignAsset,
   XcmFragment,
   RawXcmMessage,
   injectHrmpMessageAndSeal,
 } from "../../../helpers/xcm.js";
-
-const FOREIGN_TOKEN = 1_000_000_000_000n;
 
 const palletId = "0x6D6f646c617373746d6E67720000000000000000";
 const statemint_para_id = 1001;
@@ -36,44 +32,28 @@ const STATEMINT_LOCATION = {
     },
   },
 };
-const STATEMINT_ASSET_ONE_LOCATION = {
-  Xcm: {
-    parents: 1,
-    interior: {
-      X3: [
-        { Parachain: statemint_para_id },
-        { PalletInstance: statemint_assets_pallet_instance },
-        { GeneralIndex: 1 },
-      ],
-    },
-  },
-};
 
 describeSuite({
-  id: "D3413",
+  id: "D3415",
   title: "Mock XCM - receive horizontal transfer",
   foundationMethods: "dev",
   testCases: ({ context, it, log }) => {
-    let assetIdZero: string;
-    let assetIdOne: string;
+    let assetId: string;
 
     beforeAll(async () => {
-      // registerForeignAsset 0
-      const { registeredAssetId: registeredAssetIdZero, registeredAsset: registeredAssetZero } =
-        await registerForeignAsset(context, STATEMINT_LOCATION, assetMetadata);
-      assetIdZero = registeredAssetIdZero;
-      // registerForeignAsset 1
-      const { registeredAssetId: registeredAssetIdOne, registeredAsset: registeredAssetOne } =
-        await registerForeignAsset(context, STATEMINT_ASSET_ONE_LOCATION, assetMetadata, 0, 1);
-      assetIdOne = registeredAssetIdOne;
-
-      expect(registeredAssetZero.owner.toHex()).to.eq(palletId.toLowerCase());
-      expect(registeredAssetOne.owner.toHex()).to.eq(palletId.toLowerCase());
+      // registerForeignAsset
+      const { registeredAssetId, registeredAsset } = await registerForeignAsset(
+        context,
+        STATEMINT_LOCATION,
+        assetMetadata
+      );
+      assetId = registeredAssetId;
+      expect(registeredAsset.owner.toHex()).to.eq(palletId.toLowerCase());
     });
 
     it({
       id: "T01",
-      title: "Should receive 10 asset 0 tokens using statemint asset 1 as fee",
+      title: "Should not receive 10 asset 0 tokens because fee not supported",
       test: async function () {
         // We are going to test that, using one of them as fee payment (assetOne),
         // we can receive the other
@@ -82,26 +62,7 @@ describeSuite({
             {
               multilocation: {
                 parents: 1,
-                interior: {
-                  X3: [
-                    { Parachain: statemint_para_id },
-                    { PalletInstance: statemint_assets_pallet_instance },
-                    { GeneralIndex: 0n },
-                  ],
-                },
-              },
-              fungible: 10000000000000n,
-            },
-            {
-              multilocation: {
-                parents: 1,
-                interior: {
-                  X3: [
-                    { Parachain: statemint_para_id },
-                    { PalletInstance: statemint_assets_pallet_instance },
-                    { GeneralIndex: 1n },
-                  ],
-                },
+                interior: { X2: [{ Parachain: statemint_para_id }, { GeneralIndex: 0n }] },
               },
               fungible: 10000000000000n,
             },
@@ -111,7 +72,7 @@ describeSuite({
         })
           .reserve_asset_deposited()
           .clear_origin()
-          .buy_execution(1) // buy execution with asset at index 1
+          .buy_execution()
           .deposit_asset(2n)
           .as_v2();
 
@@ -122,13 +83,11 @@ describeSuite({
         } as RawXcmMessage);
 
         // Make sure the state has ALITH's foreign parachain tokens
-        const alithAssetZeroBalance = (
-          await context.polkadotJs().query.assets.account(assetIdZero, alith.address)
-        )
-          .unwrap()
-          .balance.toBigInt();
+        const alithAssetZeroBalance = await context
+          .polkadotJs()
+          .query.assets.account(assetId, alith.address);
 
-        expect(alithAssetZeroBalance).to.eq(10n * FOREIGN_TOKEN);
+        expect(alithAssetZeroBalance.isNone).to.eq(true);
       },
     });
   },
