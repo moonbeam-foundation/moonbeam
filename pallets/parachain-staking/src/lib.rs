e67508aca0b04253e77f1e796a6e9a70e58e95f4
Cem Eliguzel <cem@moonsonglabs.com>
2023-10-16 15:01:12 +0000

    Improve the check

--- pallets/parachain-staking/src/lib.rs
+++ pallets/parachain-staking/src/lib.rs
@@ -989,11 +989,11 @@ pub mod pallet {
 			candidate_count: u32,
 		) -> DispatchResultWithPostInfo {
 			let acc = ensure_signed(origin.clone())?;
-			let is_governance_origin = T::MonetaryGovernanceOrigin::ensure_origin(origin).is_ok();
 			ensure!(!Self::is_candidate(&acc), Error::<T>::CandidateExists);
 			ensure!(!Self::is_delegator(&acc), Error::<T>::DelegatorExists);
 			ensure!(
-				is_governance_origin || bond >= T::MinCandidateStk::get(),
+				bond >= T::MinCandidateStk::get()
+					|| T::MonetaryGovernanceOrigin::ensure_origin(origin).is_ok(),
 				Error::<T>::CandidateBondBelowMin
 			);
 			let mut candidates = <CandidatePool<T>>::get();
