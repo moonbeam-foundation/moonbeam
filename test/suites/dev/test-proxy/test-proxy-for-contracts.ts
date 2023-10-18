b8796da59aecfb9c01e7444788b3bd253fc46fcf
Ahmad Kaouk <ahmadkaouk.93@gmail.com>
2023-10-18 17:37:21 +0200

    add tests

--- test/suites/dev/test-proxy/test-proxy-for-contracts.ts
+++ test/suites/dev/test-proxy/test-proxy-for-contracts.ts
@@ -1,30 +1,42 @@
 import "@moonbeam-network/api-augment";
 import { deployCreateCompiledContract, describeSuite, expect, beforeAll } from "@moonwall/cli";
-import { ALITH_ADDRESS } from "@moonwall/util";
-import { hexToU8a } from "@polkadot/util";
-import { encodeDeployData, keccak256, numberToHex, toRlp } from "viem";
-import { verifyLatestBlockFees } from "../../../helpers/block.js";
+import { GLMR } from "@moonwall/util";
+import { ALITH_ADDRESS, alith } from "@moonwall/util";
 
 // TODO: expand these tests to do multiple txn types when added to viem
 describeSuite({
   id: "D4007",
-  title: "Contract creation",
+  title: "Proxy Call for Contract",
   foundationMethods: "dev",
   testCases: ({ context, it, log }) => {
+    let contractAddress: `0x${string}`;
+
     beforeAll(async () => {
-      const { contractAddress, abi } = await deployCreateCompiledContract(
-        context,
-        "ProxyForContract"
+      const { contractAddress } = await deployCreateCompiledContract(context, "ProxyForContract");
+      // Fund the contract account with some GLMR
+      await context.createBlock(
+        context.polkadotJs().tx.balances.transfer(contractAddress, 1000n * GLMR),
+        {
+          allowFailures: false,
+        }
       );
     });
 
     it({
       id: `T01`,
-      title: `should not be able to execute`,
+      title: `Proxy Call to Smart Contract account should not be able to execute`,
       test: async function () {
-        const { hash } = await deployCreateCompiledContract(context, "MultiplyBy7");
-        await context.createBlock();
-        expect(hash).toBeTruthy();
+        const { result } = await context.createBlock(
+          context
+            .polkadotJs()
+            .tx.proxy.proxy(
+              contractAddress,
+              "Any",
+              context.polkadotJs().tx.balances.transfer(contractAddress, 10n * GLMR)
+            )
+            .signAsync(alith.address)
+        );
+        console.log(ALITH_ADDRESS);
       },
     });
   },
