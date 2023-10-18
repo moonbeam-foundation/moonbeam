e732a2402005a05f3014434f8dc65d1b6863477d
Cem Eliguzel <cem@moonsonglabs.com>
2023-10-18 07:30:13 +0000

    Fix file doc

--- pallets/xcm-transactor/src/encode.rs
+++ pallets/xcm-transactor/src/encode.rs
@@ -14,56 +14,11 @@
 // You should have received a copy of the GNU General Public License
 // along with Moonbeam.  If not, see <http://www.gnu.org/licenses/>.
 
-//! # Xcm Transactor Module
+//! # Encode Module
 //!
 //! ## Overview
 //!
-//! Module to provide transact capabilities on other chains
-//!
-//! In this pallet we will make distinctions between sovereign, derivative accounts and
-//! multilocation-based derived accounts. The first is the account the parachain controls
-//! in the destination chain, the second is an account derived from the
-//! sovereign account itself, e.g., by hashing it with an index, while the third is an account
-//! derived from the multilocation of a use in this chain (tipically, hashing the ML).
-//! Such distinction is important since we want to keep the integrity of the sovereign account
-//!
-//! This pallet provides three ways of sending Transact operations to anothe chain
-//!
-//! - transact_through_derivative: Transact through an address derived from this chains sovereign
-//! 	account in the destination chain. For the transaction to successfully be dispatched in the
-//! 	destination chain, pallet-utility needs to be installed and at least paid xcm message
-//! 	execution should be allowed (and WithdrawAsset,BuyExecution and Transact messages allowed)
-//! 	in the destination chain
-//!
-//!
-//!
-//! 	The transactions are dispatched from a derivative account
-//! 	of the sovereign account
-//! 	This pallet only stores the index of the derivative account used, but
-//! 	not the derivative account itself. The only assumption this pallet makes
-//! 	is the existence of the pallet_utility pallet in the destination chain
-//! 	through the XcmTransact trait.
-//!
-//! 	All calls will be wrapped around utility::as_derivative. This makes sure
-//! 	the inner call is executed from the derivative account and not the sovereign
-//! 	account itself.
-//!
-//! 	Index registration happens through DerivativeAddressRegistrationOrigin.
-//! 	This derivative account can be funded by external users to
-//! 	ensure it has enough funds to make the calls
-//!
-//! - transact_through_sovereign: Transact through the sovereign account representing this chain.
-//! 	For the transaction to successfully be dispatched in the destination chain, at least paid
-//! 	xcm message execution should be allowed (and WithdrawAsset,BuyExecution and Transact
-//! 	messages allowed) in the destination chain. Only callable by Root
-//!
-//! - transact_through_signed: Transact through an account derived from the multilocation
-//! 	representing the signed user making the call. We ensure this by prepending DescendOrigin as
-//! 	the first instruction of the XCM message. For the transaction to successfully be dispatched
-//! 	in the destination chain, at least descended paid xcm message execution should be allowed
-//! 	(and DescendOrigin + WithdrawAsset + BuyExecution + Transact messages allowed) in the
-//! 	destination chain. Additionally, a ML-based derivation mechanism needs to be implemented
-//! 	in the destination chain.
+//! Module to provide `StakeEncodeCall`, `HrmpEncodeCall` and `UtilityEncodeCall` implementations for the Xcm Transactor pallet
 
 #![cfg_attr(not(feature = "std"), no_std)]
 
@@ -87,7 +42,7 @@ impl<T: Config> UtilityEncodeCall for Pallet<T> {
 				// pallet index
 				encoded_call.push(RelayIndices::<T>::get().pallets.utility);
 				// call index
-				encoded_call.push(RelayIndices::<T>::get().calls.utility.as_derivative);
+				encoded_call.push(RelayIndices::<T>::get().calls.as_derivative);
 				// encoded argument
 				encoded_call.append(&mut a.encode());
 				encoded_call.append(&mut b.clone());
@@ -105,7 +60,7 @@ impl<T: Config> HrmpEncodeCall for Pallet<T> {
 				// pallet index
 				encoded_call.push(RelayIndices::<T>::get().pallets.hrmp);
 				// call index
-				encoded_call.push(RelayIndices::<T>::get().calls.hrmp.init_open_channel);
+				encoded_call.push(RelayIndices::<T>::get().calls.init_open_channel);
 				// encoded arguments
 				encoded_call.append(&mut a.encode());
 				encoded_call.append(&mut b.encode());
@@ -117,7 +72,7 @@ impl<T: Config> HrmpEncodeCall for Pallet<T> {
 				// pallet index
 				encoded_call.push(RelayIndices::<T>::get().pallets.hrmp);
 				// call index
-				encoded_call.push(RelayIndices::<T>::get().calls.hrmp.accept_open_channel);
+				encoded_call.push(RelayIndices::<T>::get().calls.accept_open_channel);
 				// encoded argument
 				encoded_call.append(&mut a.encode());
 				Ok(encoded_call)
@@ -127,7 +82,7 @@ impl<T: Config> HrmpEncodeCall for Pallet<T> {
 				// pallet index
 				encoded_call.push(RelayIndices::<T>::get().pallets.hrmp);
 				// call index
-				encoded_call.push(RelayIndices::<T>::get().calls.hrmp.close_channel);
+				encoded_call.push(RelayIndices::<T>::get().calls.close_channel);
 				// encoded argument
 				encoded_call.append(&mut a.encode());
 				Ok(encoded_call)
@@ -137,7 +92,7 @@ impl<T: Config> HrmpEncodeCall for Pallet<T> {
 				// pallet index
 				encoded_call.push(RelayIndices::<T>::get().pallets.hrmp);
 				// call index
-				encoded_call.push(RelayIndices::<T>::get().calls.hrmp.cancel_open_request);
+				encoded_call.push(RelayIndices::<T>::get().calls.cancel_open_request);
 				// encoded argument
 				encoded_call.append(&mut a.encode());
 				encoded_call.append(&mut b.encode());
@@ -164,7 +119,7 @@ impl<T: Config> StakeEncodeCall for Pallet<T> {
 				// pallet index
 				encoded_call.push(RelayIndices::<T>::get().pallets.staking);
 				// call index
-				encoded_call.push(RelayIndices::<T>::get().calls.staking.bond);
+				encoded_call.push(RelayIndices::<T>::get().calls.bond);
 				// encoded arguments
 				encoded_call.append(&mut encode_compact_arg(b));
 				encoded_call.append(&mut c.encode());
@@ -176,7 +131,7 @@ impl<T: Config> StakeEncodeCall for Pallet<T> {
 				// pallet index
 				encoded_call.push(RelayIndices::<T>::get().pallets.staking);
 				// call index
-				encoded_call.push(RelayIndices::<T>::get().calls.staking.bond_extra);
+				encoded_call.push(RelayIndices::<T>::get().calls.bond_extra);
 				// encoded argument
 				encoded_call.append(&mut encode_compact_arg(a));
 				encoded_call
@@ -187,7 +142,7 @@ impl<T: Config> StakeEncodeCall for Pallet<T> {
 				// pallet index
 				encoded_call.push(RelayIndices::<T>::get().pallets.staking);
 				// call index
-				encoded_call.push(RelayIndices::<T>::get().calls.staking.unbond);
+				encoded_call.push(RelayIndices::<T>::get().calls.unbond);
 				// encoded argument
 				encoded_call.append(&mut encode_compact_arg(a));
 				encoded_call
@@ -198,7 +153,7 @@ impl<T: Config> StakeEncodeCall for Pallet<T> {
 				// pallet index
 				encoded_call.push(RelayIndices::<T>::get().pallets.staking);
 				// call index
-				encoded_call.push(RelayIndices::<T>::get().calls.staking.withdraw_unbonded);
+				encoded_call.push(RelayIndices::<T>::get().calls.withdraw_unbonded);
 				// encoded argument
 				encoded_call.append(&mut a.encode());
 				encoded_call
@@ -209,7 +164,7 @@ impl<T: Config> StakeEncodeCall for Pallet<T> {
 				// pallet index
 				encoded_call.push(RelayIndices::<T>::get().pallets.staking);
 				// call index
-				encoded_call.push(RelayIndices::<T>::get().calls.staking.validate);
+				encoded_call.push(RelayIndices::<T>::get().calls.validate);
 				// encoded argument
 				encoded_call.append(&mut a.encode());
 				encoded_call
@@ -220,7 +175,7 @@ impl<T: Config> StakeEncodeCall for Pallet<T> {
 				// pallet index
 				encoded_call.push(RelayIndices::<T>::get().pallets.staking);
 				// call index
-				encoded_call.push(RelayIndices::<T>::get().calls.staking.chill);
+				encoded_call.push(RelayIndices::<T>::get().calls.chill);
 				encoded_call
 			}
 
@@ -229,7 +184,7 @@ impl<T: Config> StakeEncodeCall for Pallet<T> {
 				// pallet index
 				encoded_call.push(RelayIndices::<T>::get().pallets.staking);
 				// call index
-				encoded_call.push(RelayIndices::<T>::get().calls.staking.set_payee);
+				encoded_call.push(RelayIndices::<T>::get().calls.set_payee);
 				// encoded argument
 				encoded_call.append(&mut a.encode());
 				encoded_call
@@ -240,7 +195,7 @@ impl<T: Config> StakeEncodeCall for Pallet<T> {
 				// pallet index
 				encoded_call.push(RelayIndices::<T>::get().pallets.staking);
 				// call index
-				encoded_call.push(RelayIndices::<T>::get().calls.staking.set_controller);
+				encoded_call.push(RelayIndices::<T>::get().calls.set_controller);
 				encoded_call
 			}
 
@@ -249,7 +204,7 @@ impl<T: Config> StakeEncodeCall for Pallet<T> {
 				// pallet index
 				encoded_call.push(RelayIndices::<T>::get().pallets.staking);
 				// call index
-				encoded_call.push(RelayIndices::<T>::get().calls.staking.rebond);
+				encoded_call.push(RelayIndices::<T>::get().calls.rebond);
 				// encoded argument
 				encoded_call.append(&mut encode_compact_arg(a));
 				encoded_call
@@ -260,7 +215,7 @@ impl<T: Config> StakeEncodeCall for Pallet<T> {
 				// pallet index
 				encoded_call.push(RelayIndices::<T>::get().pallets.staking);
 				// call index
-				encoded_call.push(RelayIndices::<T>::get().calls.staking.nominate);
+				encoded_call.push(RelayIndices::<T>::get().calls.nominate);
 				let nominated: Vec<
 					<AccountIdLookup<sp_runtime::AccountId32, ()> as StaticLookup>::Source,
 				> = a.iter().map(|add| (*add).clone().into()).collect();
