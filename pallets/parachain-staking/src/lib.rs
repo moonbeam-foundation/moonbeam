05744c067c5dfa6283a95fece7b08928cfc60d38
Cem Eliguzel <cem@moonsonglabs.com>
2023-10-18 06:58:31 +0000

    Fix formatting

--- pallets/parachain-staking/src/lib.rs
+++ pallets/parachain-staking/src/lib.rs
@@ -1470,9 +1470,8 @@ pub mod pallet {
 	impl<T: Config> Pallet<T> {
 		pub fn set_candidate_bond_to_zero(acc: &T::AccountId) -> DispatchResultWithPostInfo {
 			ensure!(Self::is_candidate(&acc), Error::<T>::CandidateDNE);
-			let actual_weight = T::WeightInfo::set_candidate_bond_to_zero(
-				<CandidatePool<T>>::get().0.len() as u32,
-			);
+			let actual_weight =
+				T::WeightInfo::set_candidate_bond_to_zero(<CandidatePool<T>>::get().0.len() as u32);
 			let mut state = <CandidateInfo<T>>::get(&acc).ok_or(Error::<T>::CandidateDNE)?;
 			state.bond_less::<T>(acc.clone(), state.bond);
 			<CandidateInfo<T>>::insert(&acc, state);
