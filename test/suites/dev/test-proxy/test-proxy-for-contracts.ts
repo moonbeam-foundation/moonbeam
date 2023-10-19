5ae25d033d1d62ecd063defabfac537d53055fc2
Ahmad Kaouk <ahmadkaouk.93@gmail.com>
2023-10-19 19:13:06 +0200

    fix test

--- test/suites/dev/test-proxy/test-proxy-for-contracts.ts
+++ test/suites/dev/test-proxy/test-proxy-for-contracts.ts
@@ -1,6 +1,6 @@
 import "@moonbeam-network/api-augment";
 import { deployCreateCompiledContract, describeSuite, beforeAll, expect } from "@moonwall/cli";
-import { GLMR } from "@moonwall/util";
+import { ALITH_ADDRESS, GLMR } from "@moonwall/util";
 import { alith } from "@moonwall/util";
 
 // TODO: expand these tests to do multiple txn types when added to viem
@@ -12,33 +12,30 @@ describeSuite({
     let contractAddress: `0x${string}`;
 
     beforeAll(async () => {
-      const { contractAddress } = await deployCreateCompiledContract(context, "ProxyForContract");
-      // Fund the contract account with some GLMR
-      await context.createBlock(
-        context.polkadotJs().tx.balances.transfer(contractAddress, 1000n * GLMR),
-        {
-          allowFailures: false,
-        }
+      const { contractAddress: deployedAddr } = await deployCreateCompiledContract(
+        context,
+        "ProxyForContractsDemo",
+        { value: 1000n * GLMR }
       );
+      contractAddress = deployedAddr;
     });
 
     it({
       id: `T01`,
-      title: `Proxy Call to Smart Contract account should not be able to execute`,
+      title: `Proxy Call to Smart Contract account should fail`,
       test: async function () {
-        const { result } = await context.createBlock(
-          context
-            .polkadotJs()
-            .tx.proxy.proxy(
-              contractAddress,
-              "Any",
-              context.polkadotJs().tx.balances.transfer(contractAddress, 10n * GLMR)
-            )
-            .signAsync(alith.address)
-        );
-
-        expect(result!.successful).to.be.false;
-        expect(result!.error!.name).to.equal("BadOrigin");
+        await expect(
+          context.createBlock(
+            context
+              .polkadotJs()
+              .tx.proxy.proxy(
+                contractAddress,
+                "Any",
+                context.polkadotJs().tx.balances.transferAll(ALITH_ADDRESS, true)
+              )
+              .signAsync(alith)
+          )
+        ).rejects.toThrow("Invalid Transaction: Transaction call is not expected");
       },
     });
   },
