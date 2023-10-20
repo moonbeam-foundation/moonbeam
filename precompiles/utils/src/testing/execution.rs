66275d412ffaaba829295ad763f2d1398083add3
Cem Eliguzel <cem@moonsonglabs.com>
2023-10-20 16:30:52 +0000

    Replace unwrap_or_else with unwrap_or_default

--- precompiles/utils/src/testing/execution.rs
+++ precompiles/utils/src/testing/execution.rs
@@ -104,7 +104,7 @@ impl<'p, P: PrecompileSet> PrecompilesTester<'p, P> {
 
 	pub fn expect_log(mut self, log: Log) -> Self {
 		self.expected_logs = Some({
-			let mut logs = self.expected_logs.unwrap_or_else(Vec::new);
+			let mut logs = self.expected_logs.unwrap_or_default();
 			logs.push(PrettyLog(log));
 			logs
 		});
