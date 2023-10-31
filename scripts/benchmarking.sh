456027eeced03ec78a2915659074a5e66d7684a9
Ahmad Kaouk <56095276+ahmadkaouk@users.noreply.github.com>
2023-10-31 14:52:06 +0100

    Fix Delegation through Proxy Pallet (#2533)
    * fix for proxy on contract WIP
    
    * fix compilation
    
    * add tests
    
    * update test
    
    * fix test
    
    * Account for an additional DB read
    
    * Add warning in benchmarking.sh
    
    * Fix message
    
    ---------
    
    Co-authored-by: Andrea Giacobino <no.andrea@gmail.com>

--- scripts/benchmarking.sh
+++ scripts/benchmarking.sh
@@ -65,6 +65,8 @@ function bench {
         --output "${OUTPUT}"
 }
 
+echo -e "\e[33m**Warning**: The benchmarking script will overwrite the pallet_proxy.rs file in runtime/common/src/weights. This file was manually modified to include an additional DB Read for proxy.proxy. Make sure to add it again after running the benchmarking script.\e[0m"
+
 if [[ "${@}" =~ "--help" ]]; then
     help
 else
