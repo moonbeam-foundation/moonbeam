5416c7174bf96c6120b576061d5bcadce2f971a0
Cem Eliguzel <cem@moonsonglabs.com>
2023-11-21 10:11:43 +0000

    Disable proposal submit for Democracy by rejecting the tx

--- runtime/moonbase/src/lib.rs
+++ runtime/moonbase/src/lib.rs
@@ -1148,6 +1148,7 @@ impl Contains<RuntimeCall> for NormalFilter {
 			// Note: It is also assumed that EVM calls are only allowed through `Origin::Root` so
 			// this can be seen as an additional security
 			RuntimeCall::EVM(_) => false,
+			RuntimeCall::Democracy(pallet_democracy::Call::propose { .. }) => false,
 			_ => true,
 		}
 	}
