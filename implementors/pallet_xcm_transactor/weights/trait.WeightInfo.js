c764965bea7ef1518e277cc586086180456e784f
timbrinded <timbrinded@users.noreply.github.com>
2023-12-22 14:02:34 +0000

    deploy: de2b75dfdb13fd1c20452002968738c46fa119e8

--- implementors/pallet_xcm_transactor/weights/trait.WeightInfo.js
+++ implementors/pallet_xcm_transactor/weights/trait.WeightInfo.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
-"moonbeam_runtime_common":[["impl&lt;T: Config&gt; WeightInfo for <a class=\"struct\" href=\"moonbeam_runtime_common/weights/pallet_xcm_transactor/struct.WeightInfo.html\" title=\"struct moonbeam_runtime_common::weights::pallet_xcm_transactor::WeightInfo\">WeightInfo</a>&lt;T&gt;"]],
+"moonbeam_runtime_common":[["impl&lt;T: Config&gt; <a class=\"trait\" href=\"pallet_xcm_transactor/weights/trait.WeightInfo.html\" title=\"trait pallet_xcm_transactor::weights::WeightInfo\">WeightInfo</a> for <a class=\"struct\" href=\"moonbeam_runtime_common/weights/pallet_xcm_transactor/struct.WeightInfo.html\" title=\"struct moonbeam_runtime_common::weights::pallet_xcm_transactor::WeightInfo\">WeightInfo</a>&lt;T&gt;"]],
 "pallet_xcm_transactor":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
