425bfbc32fdd49115b3a90da074ca6956551bdda
Ahmad Kaouk <ahmadkaouk.93@gmail.com>
2024-01-30 11:45:16 +0100

    increase recursion limit for moonbase runtime

--- runtime/moonbase/src/lib.rs
+++ runtime/moonbase/src/lib.rs
@@ -22,7 +22,7 @@
 
 #![cfg_attr(not(feature = "std"), no_std)]
 // `construct_runtime!` does a lot of recursion and requires us to increase the limit to 256.
-#![recursion_limit = "256"]
+#![recursion_limit = "512"]
 
 // Make the WASM binary available.
 #[cfg(feature = "std")]
