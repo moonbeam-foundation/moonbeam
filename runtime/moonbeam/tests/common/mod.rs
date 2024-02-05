466208c2c71da6753fc74e53fe26f0d78f38e775
Éloïs <c@elo.tf>
2024-02-05 11:21:53 +0100

    not call AuthorInherent::on_finalize

--- runtime/moonbeam/tests/common/mod.rs
+++ runtime/moonbeam/tests/common/mod.rs
@@ -64,7 +64,6 @@ pub fn rpc_run_to_block(n: u32) {
 pub fn run_to_block(n: u32, author: Option<NimbusId>) {
 	// Finalize the first block
 	Ethereum::on_finalize(System::block_number());
-	AuthorInherent::on_finalize(System::block_number());
 	while System::block_number() < n {
 		// Set the new block number and author
 		match author {
@@ -91,7 +90,6 @@ pub fn run_to_block(n: u32, author: Option<NimbusId>) {
 
 		// Finalize the block
 		Ethereum::on_finalize(System::block_number());
-		AuthorInherent::on_finalize(System::block_number());
 		ParachainStaking::on_finalize(System::block_number());
 	}
 }
