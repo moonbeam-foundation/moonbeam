284cb603ea85db7975240b294d0185a2c8ebfc2d
Rodrigo Quelhas <rodrigo_quelhas@outlook.pt>
2024-02-06 09:40:21 +0000

    test: support moonbeam in dev tests

---
+++ test/suites/dev/moonbase/test-balance/test-balance-transferable.ts
@@ -0,0 +1,101 @@
+import "@moonbeam-network/api-augment";
+import { beforeAll, describeSuite, expect } from "@moonwall/cli";
+import { ALITH_ADDRESS, GLMR, baltathar, checkBalance, generateKeyringPair } from "@moonwall/util";
+import { createProposal } from "../../../../helpers/voting.ts";
+
+describeSuite({
+  id: "D4005",
+  title: "Balance - Transferable",
+  foundationMethods: "dev",
+  testCases: ({ context, it }) => {
+    const randomAccount = generateKeyringPair();
+    const randomAddress = randomAccount.address as `0x${string}`;
+
+    beforeAll(async function () {
+      await context.createBlock(
+        context.polkadotJs().tx.balances.transferAllowDeath(randomAccount.address, 38n * GLMR),
+        { allowFailures: false }
+      );
+    });
+
+    it({
+      id: "T01",
+      title: "Consistent Transferable Balance Computation for Fee Calculation and Actual Balance",
+      test: async function () {
+        {
+          // In this test, the following actions are performed:
+          // 1. A new account is created with an initial balance of 38 GLMR.
+          // 2. An identity proposal is submitted by randomAccount (~15 GLMR)
+          // 3. 20 GLMR are delegated to Alith from randomAccount.
+          // 4. 5 GLMR are transferred to Balthazar from randomAccount.
+          // 5. A second transfer of 2 GLMR to Balthazar is performed from randomAccount.
+
+          // Delegate to Alith
+          const { result: res } = await context.createBlock(
+            context
+              .polkadotJs()
+              .tx.parachainStaking.delegate(ALITH_ADDRESS, 20n * GLMR, 10, 10)
+              .signAsync(randomAccount)
+          );
+          expect(res!.successful).to.be.true;
+
+          // Create a proposal
+          const propNum = await createProposal({ context, from: randomAccount });
+          expect(propNum).toBe(0);
+
+          // Balance after proposal
+          const balanceAfter = (
+            await context.polkadotJs().query.system.account(randomAccount.address)
+          ).data.free.toBigInt();
+
+          // Check the balance of randomAccount before tranfer
+          const balanceBeforeTransfer = await checkBalance(context, randomAddress);
+          expect(balanceBeforeTransfer).toBeGreaterThan(9n * GLMR);
+
+          // Get fee for transfer
+          const fee = await context
+            .polkadotJs()
+            .tx.balances.transferAllowDeath(randomAccount.address, 5n * GLMR)
+            .paymentInfo(randomAccount.address);
+
+          // Transfer 5 GLMR to Balthazar
+          const { result: res3 } = await context.createBlock(
+            context
+              .polkadotJs()
+              .tx.balances.transferAllowDeath(baltathar.address, 5n * GLMR)
+              .signAsync(randomAccount)
+          );
+
+          expect(res3!.successful).to.be.true;
+          expect(await checkBalance(context, randomAddress)).toBe(
+            balanceBeforeTransfer - 5n * GLMR - fee.partialFee.toBigInt()
+          );
+          expect(await checkBalance(context, randomAddress)).toBeGreaterThan(4n * GLMR);
+
+          // Do a second transfer of 2 GLMR to Balthazar
+          // const { result: res2 } = await context.createBlock(
+          //   context
+          //     .polkadotJs()
+          //     .tx.balances.transferAllowDeath(baltathar.address, 2n * GLMR)
+          //     .signAsync(randomAccount)
+          // );
+          // expect(res2!.successful).to.be.true;
+
+          // TODO Change this check once the transferable balance is fixed
+          // Check Ticket MOON-2598: https://opslayer.atlassian.net/browse/MOON-2598
+          expect(
+            async () =>
+              await context.createBlock(
+                context
+                  .polkadotJs()
+                  .tx.balances.transferAllowDeath(baltathar.address, 2n * GLMR)
+                  .signAsync(randomAccount)
+              )
+          ).rejects.toThrowError(
+            "1010: Invalid Transaction: Inability to pay some fees , e.g. account balance too low"
+          );
+        }
+      },
+    });
+  },
+});
---
+++ test/suites/dev/moonbase/test-staking/test-delegation-scheduled-requests6-bondless-collator.ts
@@ -0,0 +1,52 @@
+import "@moonbeam-network/api-augment";
+import { describeSuite, beforeAll, expect } from "@moonwall/cli";
+import { MIN_GLMR_DELEGATOR, alith, baltathar, ethan } from "@moonwall/util";
+import { jumpToRound } from "../../../../helpers/block.js";
+
+describeSuite({
+  id: "D2995",
+  title:
+    "Staking - Delegation Scheduled Requests with bondless collator \
+        - execute revoke on last delegation",
+  foundationMethods: "dev",
+  testCases: ({ context, it, log }) => {
+    let psTx: any;
+    let psQuery: any;
+    let sudo: any;
+    let createBlock: any;
+
+    beforeAll(async () => {
+      psTx = context.polkadotJs().tx.parachainStaking;
+      psQuery = context.polkadotJs().query.parachainStaking;
+      sudo = context.polkadotJs().tx.sudo.sudo;
+      createBlock = context.createBlock;
+
+      await createBlock(sudo(psTx.forceJoinCandidates(baltathar.address, 0, 1)).signAsync(alith));
+      await createBlock([
+        sudo(psTx.setBlocksPerRound(10)).signAsync(alith),
+        psTx.delegate(baltathar.address, MIN_GLMR_DELEGATOR, 0, 0).signAsync(ethan),
+      ]);
+
+      await createBlock(psTx.scheduleRevokeDelegation(baltathar.address).signAsync(ethan));
+
+      const delegationRequests = await psQuery.delegationScheduledRequests(baltathar.address);
+      await jumpToRound(context, delegationRequests[0].whenExecutable.toNumber());
+    });
+
+    it({
+      id: "T01",
+      title: "should succeed and leave as delegator",
+      test: async () => {
+        await createBlock(
+          psTx.executeDelegationRequest(ethan.address, baltathar.address).signAsync(ethan)
+        );
+        const delegatorState = await psQuery.delegatorState(ethan.address);
+        const delegationRequestsAfter = await psQuery.delegationScheduledRequests(
+          baltathar.address
+        );
+        expect(delegatorState.isNone).to.be.true; // last delegation revoked, so delegator left
+        expect(delegationRequestsAfter.isEmpty).toBe(true);
+      },
+    });
+  },
+});
