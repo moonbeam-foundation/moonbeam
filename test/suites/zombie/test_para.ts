0852a1827f9a7840831ed7f45bb126f906819f27
Tim B <79199034+timbrinded@users.noreply.github.com>
2024-02-07 10:21:23 +0000

    Update Zombienet runtime upgrade CI (#2638)
    * updated moonbase zombie tests
    
    * Refactored moonwall envs to single test file
    
    * remove test filter
    
    * PR comments + pkg updates

--- test/suites/zombie/test_para.ts
+++ test/suites/zombie/test_para.ts
@@ -1,14 +1,6 @@
 import "@moonbeam-network/api-augment";
 import { MoonwallContext, beforeAll, describeSuite, expect } from "@moonwall/cli";
-import {
-  ALITH_PRIVATE_KEY,
-  BALTATHAR_ADDRESS,
-  MIN_GAS_PRICE,
-  charleth,
-  createRawTransfer,
-  generateKeyringPair,
-  sendRawTransaction,
-} from "@moonwall/util";
+import { BALTATHAR_ADDRESS, alith, charleth } from "@moonwall/util";
 import { ApiPromise } from "@polkadot/api";
 import { ethers } from "ethers";
 import fs from "node:fs";
@@ -17,19 +9,13 @@ describeSuite({
   id: "ZAN",
   title: "Zombie AlphaNet Upgrade Test",
   foundationMethods: "zombie",
-  testCases: function ({ it, context, log }) {
+  testCases: ({ it, context, log }) => {
     let paraApi: ApiPromise;
     let relayApi: ApiPromise;
 
     beforeAll(async () => {
       paraApi = context.polkadotJs("parachain");
       relayApi = context.polkadotJs("relaychain");
-
-      const relayNetwork = relayApi.consts.system.version.specName.toString();
-      expect(relayNetwork, "Relay API incorrect").to.contain("rococo");
-
-      const paraNetwork = paraApi.consts.system.version.specName.toString();
-      expect(paraNetwork, "Para API incorrect").to.contain("moonbase");
 
       const currentBlock = (await paraApi.rpc.chain.getBlock()).block.header.number.toNumber();
       expect(currentBlock, "Parachain not producing blocks").to.be.greaterThan(0);
@@ -38,7 +24,7 @@ describeSuite({
     it({
       id: "T01",
       title: "Blocks are being produced on parachain",
-      test: async function () {
+      test: async () => {
         const blockNum = (await paraApi.rpc.chain.getBlock()).block.header.number.toNumber();
         expect(blockNum).to.be.greaterThan(0);
       },
@@ -48,26 +34,33 @@ describeSuite({
       id: "T02",
       title: "Chain can be upgraded",
       timeout: 600000,
-      test: async function () {
-        const blockNumberBefore = (
-          await paraApi.rpc.chain.getBlock()
-        ).block.header.number.toNumber();
+      test: async () => {
         const currentCode = (await paraApi.rpc.state.getStorage(":code")) as any;
         const codeString = currentCode.toString();
+        const upgradePath = (await MoonwallContext.getContext()).rtUpgradePath;
 
-        const wasm = fs.readFileSync((await MoonwallContext.getContext()).rtUpgradePath!);
+        if (!upgradePath) {
+          throw new Error("Runtime upgrade path not set");
+        }
+
+        const wasm = fs.readFileSync(upgradePath);
         const rtHex = `0x${wasm.toString("hex")}`;
 
         if (rtHex === codeString) {
           log("Runtime already upgraded, skipping test");
           return;
-        } else {
-          log("Runtime not upgraded, proceeding with test");
-          log("Current runtime hash: " + rtHex.slice(0, 10) + "..." + rtHex.slice(-10));
-          log("New runtime hash: " + codeString.slice(0, 10) + "..." + codeString.slice(-10));
         }
 
-        await context.upgradeRuntime({ logger: log });
+        log("Runtime not upgraded, proceeding with test");
+        log(`Current runtime hash: ${rtHex.slice(0, 10)}...${rtHex.slice(-10)}`);
+        log(`New runtime hash: ${codeString.slice(0, 10)}...${codeString.slice(-10)}`);
+
+        const blockNumberBefore = (
+          await paraApi.rpc.chain.getBlock()
+        ).block.header.number.toNumber();
+
+        await paraApi.tx.parachainSystem.enactAuthorizedUpgrade(rtHex).signAndSend(alith);
+
         await context.waitBlock(2);
         const blockNumberAfter = (
           await paraApi.rpc.chain.getBlock()
@@ -83,7 +76,7 @@ describeSuite({
       id: "T03",
       title: "Can connect to parachain and execute a transaction",
       timeout: 60000,
-      test: async function () {
+      test: async () => {
         const balBefore = (await paraApi.query.system.account(BALTATHAR_ADDRESS)).data.free;
 
         log("Please wait, this will take at least 30s for transaction to complete");
@@ -112,17 +105,17 @@ describeSuite({
     it({
       id: "T04",
       title: "Tags are present on emulated Ethereum blocks",
-      test: async function () {
+      test: async () => {
         expect(
-          (await context.ethers().provider!.getBlock("safe"))!.number,
+          (await context.ethers().provider?.getBlock("safe"))?.number,
           "Safe tag is not present"
         ).to.be.greaterThan(0);
         expect(
-          (await context.ethers().provider!.getBlock("finalized"))!.number,
+          (await context.ethers().provider?.getBlock("finalized"))?.number,
           "Finalized tag is not present"
         ).to.be.greaterThan(0);
         expect(
-          (await context.ethers().provider!.getBlock("latest"))!.number,
+          (await context.ethers().provider?.getBlock("latest"))?.number,
           "Latest tag is not present"
         ).to.be.greaterThan(0);
         // log(await ethersSigner.provider.getTransactionCount(ALITH_ADDRESS, "latest"));
@@ -130,28 +123,6 @@ describeSuite({
         //   .ethers()
         //   .sendTransaction({ to: BALTATHAR_ADDRESS, value: ethers.parseEther("1") });
         // log(await ethersSigner.provider.getTransactionCount(ALITH_ADDRESS, "pending"));
-      },
-    });
-
-    it({
-      id: "T05",
-      title: "RPC Provider can produce a pending ethereum block",
-      test: async function () {
-        const randomAccount = generateKeyringPair();
-        const randomAddress = randomAccount.address as `0x${string}`;
-
-        const rawTx = (await createRawTransfer(context as any, randomAddress, 512n, {
-          privateKey: ALITH_PRIVATE_KEY,
-          gasPrice: MIN_GAS_PRICE,
-          gas: 21000n,
-          txnType: "legacy",
-        })) as `0x${string}`;
-        log(rawTx);
-        await sendRawTransaction(context, rawTx);
-
-        expect(
-          await context.viem().getBalance({ address: randomAddress, blockTag: "pending" })
-        ).toBe(512n);
       },
     });
   },
