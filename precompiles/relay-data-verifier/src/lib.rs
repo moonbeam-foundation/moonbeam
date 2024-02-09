ebbc1734e45f9ee22e7ff9c854938499db9acd88
Ahmad Kaouk <ahmadkaouk.93@gmail.com>
2024-02-09 14:13:52 +0100

    update types

--- precompiles/relay-data-verifier/src/lib.rs
+++ precompiles/relay-data-verifier/src/lib.rs
@@ -14,38 +14,41 @@
 // You should have received a copy of the GNU General Public License
 // along with Moonbeam.  If not, see <http://www.gnu.org/licenses/>.
 
-//! Precompile for verifying relay storage proofs.
+//! Precompile for verifying relay data against a relay block number.
 
 #![cfg_attr(not(feature = "std"), no_std)]
 
 use core::marker::PhantomData;
+use cumulus_primitives_core::relay_chain::BlockNumber as RelayBlockNumber;
 use fp_evm::PrecompileHandle;
 use frame_support::traits::ConstU32;
 use precompile_utils::prelude::*;
 use sp_core::H256;
-use sp_std::{vec, vec::Vec};
+use sp_std::vec;
 
 pub const CALL_DATA_LIMIT: u32 = 2u32.pow(16);
 pub const ARRAY_LIMIT: u32 = 2u32.pow(9);
 
 type GetCallDataLimit = ConstU32<CALL_DATA_LIMIT>;
 type GetArrayLimit = ConstU32<ARRAY_LIMIT>;
+type RawStorageProof = BoundedVec<BoundedBytes<GetCallDataLimit>, GetArrayLimit>;
+type RawKey = BoundedBytes<GetCallDataLimit>;
 
-/// Relay proof verifier precompile.
-pub struct RelayProofVerifierPrecompile<Runtime>(PhantomData<Runtime>);
+/// Relay Data Verifier precompile.
+pub struct RelayDataVerifierPrecompile<Runtime>(PhantomData<Runtime>);
 
 #[precompile_utils::precompile]
-impl<Runtime> RelayProofVerifierPrecompile<Runtime> {
+impl<Runtime> RelayDataVerifierPrecompile<Runtime> {
 	#[precompile::public("verify(uint32,bytes[],bytes)")]
 	#[precompile::view]
 	fn verify(
 		_handle: &mut impl PrecompileHandle,
-		_relay_block_number: u32,
-		_proof: BoundedVec<BoundedBytes<GetCallDataLimit>, GetArrayLimit>,
-		_key: BoundedBytes<GetCallDataLimit>,
-	) -> EvmResult<Vec<u8>> {
+		_relay_block_number: RelayBlockNumber,
+		_proof: RawStorageProof,
+		_key: RawKey,
+	) -> EvmResult<UnboundedBytes> {
 		// TODO: to be implemented
-		Ok(vec![])
+		Ok(vec![].into())
 	}
 
 	#[precompile::public("verifyBatch(uint32,bytes[],bytes[])")]
@@ -53,18 +56,20 @@ impl<Runtime> RelayProofVerifierPrecompile<Runtime> {
 	#[precompile::view]
 	fn verify_batch(
 		_handle: &mut impl PrecompileHandle,
-		_relay_block_number: u32,
-		_proof: BoundedVec<BoundedBytes<GetCallDataLimit>, GetArrayLimit>,
-		_keys: BoundedVec<BoundedBytes<GetCallDataLimit>, GetArrayLimit>,
-	) -> EvmResult<Vec<Vec<u8>>> {
+		_relay_block_number: RelayBlockNumber,
+		_proof: RawStorageProof,
+		_keys: BoundedVec<RawKey, GetArrayLimit>,
+	) -> EvmResult<BoundedVec<UnboundedBytes, GetArrayLimit>> {
 		// TODO: to be implemented
-		Ok(vec![vec![]])
+		Ok(vec![vec![].into()].into())
 	}
 
 	#[precompile::public("lastRelayBlock()")]
 	#[precompile::public("last_relay_block()")]
 	#[precompile::view]
-	fn last_relay_block(_handle: &mut impl PrecompileHandle) -> EvmResult<(u32, H256)> {
+	fn last_relay_block(
+		_handle: &mut impl PrecompileHandle,
+	) -> EvmResult<(RelayBlockNumber, H256)> {
 		// TODO: to be implemented
 		Ok((0, H256::zero()))
 	}
