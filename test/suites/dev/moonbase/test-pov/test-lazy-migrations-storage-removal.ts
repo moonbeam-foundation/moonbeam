fc35aad8c5c4ace7f16f1d68d216b481ad8f29e1
Rodrigo Quelhas <rodrigo_quelhas@outlook.pt>
2024-02-14 11:17:37 +0000

    fix format

--- test/suites/dev/moonbase/test-pov/test-lazy-migrations-storage-removal.ts
+++ test/suites/dev/moonbase/test-pov/test-lazy-migrations-storage-removal.ts
@@ -4,7 +4,6 @@ import { alith, baltathar } from "@moonwall/util";
 import { ApiPromise } from "@polkadot/api";
 import { u128 } from "@polkadot/types";
 
-
 describeSuite({
   id: "D2406",
   title: "Remove LocalAssets storage - PoV Size validation",
@@ -28,20 +27,18 @@ describeSuite({
         const dummy_storage: [string, string][] = [];
         for (let i = 0; i < total_entries; i++) {
           const dummy_data = i.toString(16).padStart(200, "0");
-          dummy_storage.push([pallet_name_hash + dummy_data, dummy_data])
+          dummy_storage.push([pallet_name_hash + dummy_data, dummy_data]);
         }
 
         // Insert 500 entries per block to not reach limits
-        for (let i = 0; i < total_entries; i+=500) {
+        for (let i = 0; i < total_entries; i += 500) {
           await context.createBlock(
             api.tx.sudo
-              .sudo(
-                api.tx.system.setStorage(dummy_storage.slice(i, i+500))
-              )
+              .sudo(api.tx.system.setStorage(dummy_storage.slice(i, i + 500)))
               .signAsync(alith)
           );
         }
-        
+
         // Check the pallet storage size
         const full_size = (await api.rpc.state.getStorageSize(pallet_name_hash)).toNumber();
         expect(full_size).to.be.equal(1_800_000);
@@ -51,9 +48,11 @@ describeSuite({
         const reasonable_max_pov_size = MAX_POV_SIZE / 5; // 1MB
 
         let current_size = full_size;
-        while(current_size > 0) {
+        while (current_size > 0) {
           // The migration is not complet yet
-          expect((await api.query["moonbeamLazyMigrations"].localAssetsMigrationCompleted()).toHuman()).to.be.false;
+          expect(
+            (await api.query["moonbeamLazyMigrations"].localAssetsMigrationCompleted()).toHuman()
+          ).to.be.false;
 
           // Remove 2000 entries each time
           const entries_to_remove = 2000;
@@ -65,12 +64,14 @@ describeSuite({
 
           // Validate that we are within the reasonable proof size
           expect(result.block.proofSize).to.be.lessThan(reasonable_max_pov_size);
-          log(`Removed ${entries_to_remove} entries from LocalAssets storage (proof_size: ${result.block.proofSize}).`);
+          log(
+            `Removed ${entries_to_remove} entries from LocalAssets storage (proof_size: ${result.block.proofSize}).`
+          );
 
           // Validate that some storage got removed
           const new_size = (await api.rpc.state.getStorageSize(pallet_name_hash)).toNumber();
           expect(new_size).to.be.lessThan(current_size);
-          
+
           // Update current storage size
           current_size = new_size;
         }
@@ -80,7 +81,9 @@ describeSuite({
         expect(current_size).to.be.equal(0);
 
         // The migration should be complete
-        expect((await api.query["moonbeamLazyMigrations"].localAssetsMigrationCompleted()).toHuman()).to.be.true;
+        expect(
+          (await api.query["moonbeamLazyMigrations"].localAssetsMigrationCompleted()).toHuman()
+        ).to.be.true;
       },
     });
   },
