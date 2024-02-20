2a815c2286ce990d8714890d3d3e0ab6319d4fce
Ahmad Kaouk <ahmadkaouk.93@gmail.com>
2024-02-20 08:53:59 +0100

    refactor

--- pallets/precompile-benchmarks/src/lib.rs
+++ pallets/precompile-benchmarks/src/lib.rs
@@ -22,10 +22,7 @@
 use cumulus_primitives_core::relay_chain::BlockNumber as RelayBlockNumber;
 use frame_support::pallet;
 pub use pallet::*;
-use pallet_evm_precompile_relay_verifier::{
-	proof::StorageProofChecker, GetKeyLengthLimit, ReadProof,
-};
-use sp_core::H256;
+use pallet_evm_precompile_relay_verifier::{RawKey, ReadProof, RelayDataVerifierPrecompile};
 
 #[cfg(feature = "runtime-benchmarks")]
 mod benchmarks;
@@ -33,11 +30,13 @@ mod benchmarks;
 #[pallet]
 pub mod pallet {
 	use super::*;
-	use frame_support::pallet_prelude::*;
 	use sp_std::vec::Vec;
 
 	#[pallet::config]
-	pub trait Config: frame_system::Config + pallet_relay_storage_roots::Config {}
+	pub trait Config:
+		frame_system::Config + pallet_relay_storage_roots::Config + pallet_evm::Config
+	{
+	}
 
 	#[pallet::pallet]
 	#[pallet::without_storage_info]
@@ -49,42 +48,28 @@ pub mod pallet {
 	}
 
 	impl<T: Config> Pallet<T> {
-		pub fn verify_relay_entry(
+		pub fn verify_entry(
 			relay_block_number: RelayBlockNumber,
-			key: BoundedVec<u8, GetKeyLengthLimit>,
 			proof: ReadProof,
-		) -> Result<Vec<u8>, Error<T>> {
-			let storage_root = Self::get_storage_root(relay_block_number)?;
-
-			let proof_checker = StorageProofChecker::new(storage_root, proof.to_raw_proof())
+			key: RawKey,
+		) -> Result<(), Error<T>> {
+			RelayDataVerifierPrecompile::<T>::do_verify_entry(relay_block_number, proof, key)
 				.map_err(|_| Error::<T>::BenchmarkError)?;
-
-			let value: Vec<u8> = proof_checker
-				.read_entry(key.as_slice())
-				.map_err(|_| Error::<T>::BenchmarkError)?;
-
-			Ok(value)
+			Ok(())
 		}
 
-		pub fn verify_relay_entries(
+		pub fn verify_entries(
 			relay_block_number: RelayBlockNumber,
-			keys: Vec<Vec<u8>>,
 			proof: ReadProof,
-		) -> Result<Vec<Vec<u8>>, Error<T>> {
-			let storage_root = Self::get_storage_root(relay_block_number)?;
-
-			let proof_checker = StorageProofChecker::new(storage_root, proof.to_raw_proof())
-				.map_err(|_| Error::<T>::BenchmarkError)?;
-
-			let mut values = Vec::new();
-			for key in Vec::from(keys) {
-				let value: Vec<u8> = proof_checker
-					.read_entry(key.as_slice())
-					.map_err(|_| Error::<T>::BenchmarkError)?;
-				values.push(value.into());
-			}
-
-			Ok(values.into())
+			keys: Vec<RawKey>,
+		) -> Result<(), Error<T>> {
+			RelayDataVerifierPrecompile::<T>::do_verify_entries(
+				relay_block_number,
+				proof,
+				keys.into(),
+			)
+			.map_err(|_| Error::<T>::BenchmarkError)?;
+			Ok(())
 		}
 
 		#[allow(dead_code)]
@@ -93,15 +78,6 @@ pub mod pallet {
 				.last()
 				.cloned()
 				.ok_or(Error::<T>::BenchmarkError)
-		}
-
-		/// Returns the storage root at the given relay block number stored on-chain. Use the pallet
-		/// `pallet_relay_storage_roots` to store the storage roots on-chain.
-		fn get_storage_root(relay_block_number: RelayBlockNumber) -> Result<H256, Error<T>> {
-			let storage_root =
-				pallet_relay_storage_roots::RelayStorageRoot::<T>::get(relay_block_number)
-					.ok_or(Error::<T>::BenchmarkError)?;
-			Ok(storage_root)
 		}
 	}
 }
