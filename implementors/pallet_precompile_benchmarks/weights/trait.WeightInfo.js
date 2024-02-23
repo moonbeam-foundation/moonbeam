99416ba801f8a714e7919b87cdbd05ed0ae1d44c
noandrea <noandrea@users.noreply.github.com>
2024-02-23 08:41:35 +0000

    deploy: 3ae581f44544e04602ae283fbdb3af473d9d03a6

--- implementors/pallet_precompile_benchmarks/weights/trait.WeightInfo.js
+++ implementors/pallet_precompile_benchmarks/weights/trait.WeightInfo.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
-"moonbeam_runtime_common":[["impl&lt;T: Config&gt; <a class=\"trait\" href=\"pallet_precompile_benchmarks/weights/trait.WeightInfo.html\" title=\"trait pallet_precompile_benchmarks::weights::WeightInfo\">WeightInfo</a> for <a class=\"struct\" href=\"moonbeam_runtime_common/weights/pallet_precompile_benchmarks/struct.WeightInfo.html\" title=\"struct moonbeam_runtime_common::weights::pallet_precompile_benchmarks::WeightInfo\">WeightInfo</a>&lt;T&gt;"]],
+"moonbeam_runtime_common":[["impl&lt;T: Config&gt; WeightInfo for <a class=\"struct\" href=\"moonbeam_runtime_common/weights/pallet_precompile_benchmarks/struct.WeightInfo.html\" title=\"struct moonbeam_runtime_common::weights::pallet_precompile_benchmarks::WeightInfo\">WeightInfo</a>&lt;T&gt;"]],
 "pallet_precompile_benchmarks":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
