6569530f4f68e7a51e277f9a9f066c607616a9c7
Éloïs <c@elo.tf>
2024-02-27 13:32:47 +0100

    implement migration

--- pallets/parachain-staking/src/migrations.rs
+++ pallets/parachain-staking/src/migrations.rs
@@ -16,33 +16,108 @@
 
 //! # Migrations
 
-use crate::Config;
+use crate::{types::RoundInfo, Config, RoundIndex};
+use frame_support::pallet_prelude::*;
 use frame_support::storage::generator::StorageValue;
 use frame_support::storage::unhashed;
 use frame_support::traits::OnRuntimeUpgrade;
+use frame_system::pallet_prelude::*;
+use sp_runtime::Saturating;
 
 /// Migrates RoundInfo and add the field first_slot
 pub struct MigrateRoundWithFirstSlot<T: Config>(core::marker::PhantomData<T>);
 
-impl<T: Config> OnRuntimeUpgrade for MigrateRoundWithFirstSlot<T> {
+#[derive(Decode)]
+struct RoundInfoRt2800 {
+	/// Current round index
+	pub current: RoundIndex,
+	/// The first block of the current round
+	pub first: u64,
+	/// The length of the current round in number of blocks
+	pub length: u32,
+}
+impl<BlockNumber: From<u64>> From<RoundInfoRt2800> for RoundInfo<BlockNumber> {
+	fn from(round: RoundInfoRt2800) -> Self {
+		Self {
+			current: round.current,
+			first: round.first.into(),
+			length: round.length,
+			first_slot: 0,
+		}
+	}
+}
+
+#[derive(Decode)]
+struct RoundInfoRt2700 {
+	/// Current round index
+	pub current: RoundIndex,
+	/// The first block of the current round
+	pub first: u32,
+	/// The length of the current round in number of blocks
+	pub length: u32,
+}
+impl<BlockNumber: From<u32>> From<RoundInfoRt2700> for RoundInfo<BlockNumber> {
+	fn from(round: RoundInfoRt2700) -> Self {
+		Self {
+			current: round.current,
+			first: round.first.into(),
+			length: round.length,
+			first_slot: 0,
+		}
+	}
+}
+
+impl<T> OnRuntimeUpgrade for MigrateRoundWithFirstSlot<T>
+where
+	T: Config,
+	BlockNumberFor<T>: From<u32> + From<u64> + Into<u64>,
+{
 	fn on_runtime_upgrade() -> frame_support::pallet_prelude::Weight {
 		let raw_key = crate::Round::<T>::storage_prefix();
 
-		if let Some(bytes) = unhashed::get_raw(&raw_key) {
+		let mut round: RoundInfo<BlockNumberFor<T>> = if let Some(bytes) =
+			unhashed::get_raw(&raw_key)
+		{
 			let len = bytes.len();
 			match len {
+				// Migration already done
 				20 => {
-					// migration already done
+					log::info!("MigrateRoundWithFirstSlot already applied.");
+					return Default::default();
 				}
-				16 => {
-					// migrate from 2800
-				}
-				12 => {
-					// migrater from 2700
-				}
-				_ => panic!("corrupted storage"),
+				// Migrate from rt2800
+				16 => match RoundInfoRt2800::decode(&mut &bytes[..]) {
+					Ok(round) => round.into(),
+					Err(e) => panic!("corrupted storage: fail to decode RoundInfoRt2800: {}", e),
+				},
+				// Migrate from rt2700
+				12 => match RoundInfoRt2700::decode(&mut &bytes[..]) {
+					Ok(round) => round.into(),
+					Err(e) => panic!("corrupted storage: fail to decode RoundInfoRt2700: {}", e),
+				},
+				// Storage corrupted
+				x => panic!(
+					"corrupted storage: parachainStaking.Round invalid length: {} bytes",
+					x
+				),
 			}
-		}
+		} else {
+			panic!("corrupted storage: parachainStaking.Round don't exist");
+		};
+
+		// Compute new field `first_slot``
+		let current_block: BlockNumberFor<T> = <frame_system::Pallet<T>>::block_number();
+		let blocks_since_first: u64 = (current_block.saturating_sub(round.first)).into();
+		let current_slot = u64::from(T::SlotProvider::get());
+		let slots_since_first = match T::BlockTime::get() {
+			12_000 => blocks_since_first * 2,
+			6_000 => blocks_since_first,
+			_ => panic!("Unsupported BlockTime"),
+		};
+		round.first_slot = current_slot.saturating_sub(slots_since_first);
+
+		// Apply the migration (write new Round value)
+		crate::Round::<T>::put(round);
 
 		Default::default()
 	}
