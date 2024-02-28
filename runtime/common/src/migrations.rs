56f75d718bae494a08c5478ec51713e0d1550f5d
noandrea <no.andrea@gmail.com>
2024-02-28 12:33:55 -0800

    fix import

--- runtime/common/src/migrations.rs
+++ runtime/common/src/migrations.rs
@@ -36,7 +36,8 @@ use pallet_migrations::{GetMigrations, Migration};
 use pallet_parachain_staking::{Round, RoundIndex, RoundInfo};
 use parity_scale_codec::{Decode, Encode};
 use sp_consensus_slots::Slot;
-use sp_core::{twox_128, Get};
+use sp_core::Get;
+use sp_io::hashing::twox_128;
 use sp_std::{marker::PhantomData, prelude::*, vec};
 
 #[derive(Copy, Clone, PartialEq, Eq, Encode, Decode)]
