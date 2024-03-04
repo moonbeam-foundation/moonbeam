09aa6d8b72fea7c571070002231630969038921d
Andrea Giacobino <no.andrea@gmail.com>
2024-03-04 22:50:08 +0100

    Increase target block fullness to 50%  (#2692)
    * increase target block fullness to 50%
    * fix tests
    * change the target block fullness for moonbeam and moonriver

--- runtime/moonbase/src/lib.rs
+++ runtime/moonbase/src/lib.rs
@@ -411,7 +411,7 @@ parameter_types! {
 		= U256::from(NORMAL_DISPATCH_RATIO * MAXIMUM_BLOCK_WEIGHT.ref_time() / WEIGHT_PER_GAS);
 	/// The portion of the `NORMAL_DISPATCH_RATIO` that we adjust the fees with. Blocks filled less
 	/// than this will decrease the weight and more will increase.
-	pub const TargetBlockFullness: Perquintill = Perquintill::from_percent(25);
+	pub const TargetBlockFullness: Perquintill = Perquintill::from_percent(50);
 	/// The adjustment variable of the runtime. Higher values will cause `TargetBlockFullness` to
 	/// change the fees more rapidly. This fast multiplier responds by doubling/halving in
 	/// approximately one hour at extreme block congestion levels.
