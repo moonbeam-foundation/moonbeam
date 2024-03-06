fbda23c300a1d0fcdd96e8a52bb1468e3817f679
elfedy <federico@moonsonglabs.com>
2024-03-06 14:55:14 -0300

    Cleanup test

--- test/suites/tracing-tests/test-trace-ethereum-xcm-3.ts
+++ test/suites/tracing-tests/test-trace-ethereum-xcm-3.ts
@@ -1,7 +1,6 @@
 import { beforeAll, customDevRpcRequest, describeSuite, expect } from "@moonwall/cli";
 import {
   XcmFragment,
-  injectHrmpMessageAndSeal,
   injectHrmpMessage,
   descendOriginFromAddress20,
   RawXcmMessage,
@@ -22,11 +21,12 @@ describeSuite({
       incremetorAddress = contractAddress;
       incremetorABI = abi;
 
-      const { originAddress: originAddress1, descendOriginAddress: descendOriginAddress1 } = descendOriginFromAddress20(context);
+      const { originAddress: originAddress1, descendOriginAddress: descendOriginAddress1 } =
+        descendOriginFromAddress20(context);
       const sendingAddress1 = originAddress1;
-      const { originAddress: originAddress2, descendOriginAddress: descendOriginAddress2 } = descendOriginFromAddress20(
-        context, "0x0101010101010101010101010101010101010101", 2);
-      const sendingAddress2 = originAddress1;
+      const { originAddress: originAddress2, descendOriginAddress: descendOriginAddress2 } =
+        descendOriginFromAddress20(context, "0x0101010101010101010101010101010101010101", 2);
+      const sendingAddress2 = originAddress2;
       const transferredBalance = 10_000_000_000_000_000_000n;
 
       // We first fund parachain 2000 sovreign account
@@ -49,28 +49,29 @@ describeSuite({
         .find(({ name }) => name.toString() == "Balances")!
         .index.toNumber();
 
-      const xcmTransactions = [
-        {
-          V2: {
-            gas_limit: 100000,
-            action: {
-              Call: incremetorAddress,
-            },
-            value: 0n,
-            input: encodeFunctionData({
-              abi: incremetorABI,
-              functionName: "incr",
-              args: [],
-            }),
-            access_list: null,
+      const xcmTransaction = {
+        V2: {
+          gas_limit: 100000,
+          action: {
+            Call: incremetorAddress,
           },
+          value: 0n,
+          input: encodeFunctionData({
+            abi: incremetorABI,
+            functionName: "incr",
+            args: [],
+          }),
+          access_list: null,
         },
-      ];
+      };
 
-      for (const xcmTransaction of xcmTransactions) {
-        const transferCall = context.polkadotJs().tx.ethereumXcm.transact(xcmTransaction);
-        const transferCallEncoded = transferCall?.method.toHex();
-        const xcmMessage1 = new XcmFragment({
+      const transferCall = context.polkadotJs().tx.ethereumXcm.transact(xcmTransaction);
+      const transferCallEncoded = transferCall?.method.toHex();
+      for (const [paraId, sendingAddress] of [
+        [1, sendingAddress1],
+        [2, sendingAddress2],
+      ]) {
+        const xcmMessage = new XcmFragment({
           assets: [
             {
               multilocation: {
@@ -79,7 +80,7 @@ describeSuite({
                   X1: { PalletInstance: balancesPalletIndex },
                 },
               },
-              fungible: transferredBalance / 2n,
+              fungible: transferredBalance,
             },
           ],
           weight_limit: {
@@ -88,45 +89,7 @@ describeSuite({
             //proofSize: 80000n,
             proofSize: 60000n,
           } as any,
-          descend_origin: sendingAddress1,
-        })
-          .descend_origin()
-          .withdraw_asset()
-          .buy_execution()
-          .push_any({
-            Transact: {
-              originKind: "SovereignAccount",
-              requireWeightAtMost: {
-                //refTime: 3000000000n,
-                refTime: 3000000000n,
-                //proofSize: 50000n,
-                proofSize: 30000n,
-              },
-              call: {
-                encoded: transferCallEncoded,
-              },
-            },
-          })
-          .as_v3();
-        const xcmMessage2 = new XcmFragment({
-          assets: [
-            {
-              multilocation: {
-                parents: 0,
-                interior: {
-                  X1: { PalletInstance: balancesPalletIndex },
-                },
-              },
-              fungible: transferredBalance / 2n,
-            },
-          ],
-          weight_limit: {
-            //refTime: 4000000000n,
-            refTime: 4000000000n,
-            //proofSize: 80000n,
-            proofSize: 60000n,
-          } as any,
-          descend_origin: sendingAddress2,
+          descend_origin: sendingAddress,
         })
           .descend_origin()
           .withdraw_asset()
@@ -148,31 +111,15 @@ describeSuite({
           .as_v3();
 
         // Send an XCM and create block to execute it
-        await injectHrmpMessage(context, 1, {
+        await injectHrmpMessage(context, paraId, {
           type: "XcmVersionedXcm",
-          payload: xcmMessage1,
+          payload: xcmMessage,
         } as RawXcmMessage);
-        await injectHrmpMessage(context, 2, {
-          type: "XcmVersionedXcm",
-          payload: xcmMessage2,
-        } as RawXcmMessage);
-        await context.createBlock();
-
-        const allRecords = await context.polkadotJs().query.system.events();
-        allRecords.forEach(({event}) => {
-          console.log(`${event.section}.${event.method}`);
-          if (context.polkadotJs().events.xcmpQueue.Fail.is(event)) {
-            
-            console.log("reason: ", event.data[2].toHuman());
-
-          }
-        })
-
-        // Retrieve the stored ethereum transaction hash
-
-        const txHashes = (await context.viem().getBlock({ blockTag: "latest" })).transactions;
-        transactionHashes.push(...txHashes);
       }
+      await context.createBlock();
+
+      const txHashes = (await context.viem().getBlock({ blockTag: "latest" })).transactions;
+      transactionHashes.push(...txHashes);
     });
 
     it({
