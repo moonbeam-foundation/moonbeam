92a81b1034ecb6c5c5ef16e60ecec712e5c123a1
ahmadkaouk <ahmadkaouk@users.noreply.github.com>
2024-04-08 12:35:29 +0000

    deploy: 7ef7faf974f22a4824de1d3e83e39ab9de338204

--- implementors/pallet_asset_manager/weights/trait.WeightInfo.js
+++ implementors/pallet_asset_manager/weights/trait.WeightInfo.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
-"moonbeam_runtime_common":[["impl&lt;T: Config&gt; <a class=\"trait\" href=\"pallet_asset_manager/weights/trait.WeightInfo.html\" title=\"trait pallet_asset_manager::weights::WeightInfo\">WeightInfo</a> for <a class=\"struct\" href=\"moonbeam_runtime_common/weights/pallet_asset_manager/struct.WeightInfo.html\" title=\"struct moonbeam_runtime_common::weights::pallet_asset_manager::WeightInfo\">WeightInfo</a>&lt;T&gt;"]],
+"moonbeam_runtime_common":[["impl&lt;T: Config&gt; WeightInfo for <a class=\"struct\" href=\"moonbeam_runtime_common/weights/pallet_asset_manager/struct.WeightInfo.html\" title=\"struct moonbeam_runtime_common::weights::pallet_asset_manager::WeightInfo\">WeightInfo</a>&lt;T&gt;"]],
 "pallet_asset_manager":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
