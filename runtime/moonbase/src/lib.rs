df1f4f74e6a2781fb5d2d021b2a02b136c086b3a
Agustín Rodriguez <agus@moonsonglabs.com>
2024-04-07 22:10:26 -0300

    Fix `ExpectedBlockTime` const (#2737)
    * fix ExpectedBlockTime const
    
    * retrigger checks

--- runtime/moonbase/src/lib.rs
+++ runtime/moonbase/src/lib.rs
@@ -878,7 +878,7 @@ impl pallet_author_slot_filter::Config for Runtime {
 impl pallet_async_backing::Config for Runtime {
 	type AllowMultipleBlocksPerSlot = ConstBool<true>;
 	type GetAndVerifySlot = pallet_async_backing::RelaySlot;
-	type ExpectedBlockTime = ConstU64<6>;
+	type ExpectedBlockTime = ConstU64<6000>;
 }
 
 parameter_types! {
