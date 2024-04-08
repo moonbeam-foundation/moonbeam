3fcc2f644bda64bb03bc48081369cea9b68bb00f
Andrea Giacobino <no.andrea@gmail.com>
2024-04-08 13:29:24 +0300

    Improve release related content (#2743)
    * add Moonkit to the diff in the release page body
    
    * fix docker image tag in client release issu body

--- tools/github/generate-runtimes-body.ts
+++ tools/github/generate-runtimes-body.ts
@@ -99,7 +99,7 @@ async function main() {
     getRuntimeInfo(argv["srtool-report-folder"], runtimeName)
   );
 
-  const moduleLinks = ["polkadot-sdk", "frontier"].map((repoName) => ({
+  const moduleLinks = ["polkadot-sdk", "frontier", "moonkit"].map((repoName) => ({
     name: repoName,
     link: getCompareLink(repoName, previousTag, newTag),
   }));
