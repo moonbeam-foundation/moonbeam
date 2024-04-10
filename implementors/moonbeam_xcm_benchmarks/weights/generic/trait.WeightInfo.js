3b52f820449d44c647499925b20063e650e0123c
RomarQ <RomarQ@users.noreply.github.com>
2024-04-10 09:17:29 +0000

    deploy: 56cccd0acea97be154454d3bcea16a86db161fb6

--- implementors/moonbeam_xcm_benchmarks/weights/generic/trait.WeightInfo.js
+++ implementors/moonbeam_xcm_benchmarks/weights/generic/trait.WeightInfo.js
@@ -1,4 +1,3 @@
 (function() {var implementors = {
-"moonbeam_runtime_common":[["impl&lt;T: Config&gt; WeightInfo for <a class=\"struct\" href=\"moonbeam_runtime_common/weights/moonbeam_xcm_benchmarks_weights_generic/struct.WeightInfo.html\" title=\"struct moonbeam_runtime_common::weights::moonbeam_xcm_benchmarks_weights_generic::WeightInfo\">WeightInfo</a>&lt;T&gt;"]],
 "moonbeam_xcm_benchmarks":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
