f74f43e40f48cfd1e51bd82c86b1afece8617cb6
timbrinded <timbrinded@users.noreply.github.com>
2024-04-19 07:38:20 +0000

    deploy: 6ef54af341a3315559546b1caca187aa551f78ce

--- implementors/pallet_parachain_staking/weights/trait.WeightInfo.js
+++ implementors/pallet_parachain_staking/weights/trait.WeightInfo.js
@@ -1,7 +1,7 @@
 (function() {var implementors = {
 "moonbase_runtime":[],
 "moonbeam_runtime":[],
-"moonbeam_runtime_common":[["impl&lt;T: Config&gt; <a class=\"trait\" href=\"pallet_parachain_staking/weights/trait.WeightInfo.html\" title=\"trait pallet_parachain_staking::weights::WeightInfo\">WeightInfo</a> for <a class=\"struct\" href=\"moonbeam_runtime_common/weights/pallet_parachain_staking/struct.WeightInfo.html\" title=\"struct moonbeam_runtime_common::weights::pallet_parachain_staking::WeightInfo\">WeightInfo</a>&lt;T&gt;"]],
+"moonbeam_runtime_common":[["impl&lt;T: Config&gt; WeightInfo for <a class=\"struct\" href=\"moonbeam_runtime_common/weights/pallet_parachain_staking/struct.WeightInfo.html\" title=\"struct moonbeam_runtime_common::weights::pallet_parachain_staking::WeightInfo\">WeightInfo</a>&lt;T&gt;"]],
 "moonriver_runtime":[],
 "pallet_parachain_staking":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
