9b26d3cc93141cbcf2eae3c7314542edea013dee
timbrinded <79199034+timbrinded@users.noreply.github.com>
2024-04-29 14:32:06 +0100

    ci: :green_heart: update max tx in block

--- test/suites/dev/moonbase/test-txpool/test-txpool-limits.ts
+++ test/suites/dev/moonbase/test-txpool/test-txpool-limits.ts
@@ -9,7 +9,7 @@ describeSuite({
   testCases: ({ context, it, log }) => {
     it({
       id: "T01",
-      title: "should be able to fill a block with 2650 tx",
+      title: "should be able to fill a block with 2704 tx",
       test: async function () {
         for (let i = 0; i < 3000; i++) {
           const rawTxn = await createRawTransfer(context, BALTATHAR_ADDRESS, 1n, {
@@ -19,7 +19,7 @@ describeSuite({
         }
 
         await context.createBlock();
-        expect((await context.viem().getBlock()).transactions.length).toBe(2650);
+        expect((await context.viem().getBlock()).transactions.length).toBe(2704);
       },
     });
   },
