51c35e432ddeeef79ff1827f2b52b3790c4747fc
Agusrodri <Agusrodri@users.noreply.github.com>
2024-05-20 02:51:28 +0000

    deploy: f2cd74e0713109c9d25366884da0b5dd651b27c9

--- pallet_evm_precompileset_assets_erc20/sidebar-items.js
+++ pallet_evm_precompileset_assets_erc20/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":["SELECTOR_LOG_APPROVAL","SELECTOR_LOG_TRANSFER"],"enum":["Erc20AssetsPrecompileSetCall"],"struct":["Erc20AssetsPrecompileSet"],"trait":["AccountIdAssetIdConversion"],"type":["AssetIdOf","BalanceOf"]};
+window.SIDEBAR_ITEMS = {"constant":["SELECTOR_LOG_APPROVAL","SELECTOR_LOG_TRANSFER"],"enum":["Erc20AssetsPrecompileSetCall"],"struct":["Erc20AssetsPrecompileSet"],"type":["AssetIdOf","BalanceOf"]};
