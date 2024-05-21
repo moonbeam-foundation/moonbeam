62ead6e201b6a6f1f860a744ad25cd02f045ba79
noandrea <noandrea@users.noreply.github.com>
2024-05-21 21:53:45 +0000

    deploy: 4c7670c1b20992d56b34aaf259023e1c28424c22

--- implementors/pallet_parachain_staking/traits/trait.OnNewRound.js
+++ implementors/pallet_parachain_staking/traits/trait.OnNewRound.js
@@ -1,6 +1,6 @@
 (function() {var implementors = {
 "moonbase_runtime":[["impl <a class=\"trait\" href=\"pallet_parachain_staking/traits/trait.OnNewRound.html\" title=\"trait pallet_parachain_staking::traits::OnNewRound\">OnNewRound</a> for <a class=\"struct\" href=\"moonbase_runtime/struct.OnNewRound.html\" title=\"struct moonbase_runtime::OnNewRound\">OnNewRound</a>"]],
-"moonbeam_runtime":[["impl <a class=\"trait\" href=\"pallet_parachain_staking/traits/trait.OnNewRound.html\" title=\"trait pallet_parachain_staking::traits::OnNewRound\">OnNewRound</a> for <a class=\"struct\" href=\"moonbeam_runtime/struct.OnNewRound.html\" title=\"struct moonbeam_runtime::OnNewRound\">OnNewRound</a>"]],
-"moonriver_runtime":[["impl <a class=\"trait\" href=\"pallet_parachain_staking/traits/trait.OnNewRound.html\" title=\"trait pallet_parachain_staking::traits::OnNewRound\">OnNewRound</a> for <a class=\"struct\" href=\"moonriver_runtime/struct.OnNewRound.html\" title=\"struct moonriver_runtime::OnNewRound\">OnNewRound</a>"]],
+"moonbeam_runtime":[["impl OnNewRound for <a class=\"struct\" href=\"moonbeam_runtime/struct.OnNewRound.html\" title=\"struct moonbeam_runtime::OnNewRound\">OnNewRound</a>"]],
+"moonriver_runtime":[["impl OnNewRound for <a class=\"struct\" href=\"moonriver_runtime/struct.OnNewRound.html\" title=\"struct moonriver_runtime::OnNewRound\">OnNewRound</a>"]],
 "pallet_parachain_staking":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
