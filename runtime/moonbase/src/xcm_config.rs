575c40a2b2c4651a1722956316f8d32cc64237a6
Ahmad Kaouk <ahmadkaouk.93@gmail.com>
2024-06-06 11:56:59 +0200

    Replace V4V3LocationConverter with WithLatestLocationConverter

--- runtime/moonbase/src/xcm_config.rs
+++ runtime/moonbase/src/xcm_config.rs
@@ -480,7 +480,9 @@ impl Into<Option<xcm::v3::Location>> for AssetType {
 impl Into<Option<Location>> for AssetType {
 	fn into(self) -> Option<Location> {
 		match self {
-			Self::Xcm(location) => xcm_builder::V4V3LocationConverter::convert_back(&location),
+			Self::Xcm(location) => {
+				xcm_builder::WithLatestLocationConverter::convert_back(&location)
+			}
 		}
 	}
 }
@@ -704,7 +706,7 @@ impl pallet_erc20_xcm_bridge::Config for Runtime {
 #[cfg(feature = "runtime-benchmarks")]
 mod testing {
 	use super::*;
-	use xcm_builder::V4V3LocationConverter;
+	use xcm_builder::WithLatestLocationConverter;
 
 	/// This From exists for benchmarking purposes. It has the potential side-effect of calling
 	/// AssetManager::set_asset_type_asset_id() and should NOT be used in any production code.
@@ -719,7 +721,7 @@ mod testing {
 				asset_id
 			} else {
 				let asset_type = AssetType::Xcm(
-					V4V3LocationConverter::convert(&location).expect("convert to v3"),
+					WithLatestLocationConverter::convert(&location).expect("convert to v3"),
 				);
 				let asset_id: AssetId = asset_type.clone().into();
 				AssetManager::set_asset_type_asset_id(asset_type, asset_id);
