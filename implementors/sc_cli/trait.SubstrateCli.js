103798ebf6618df974b943aec2f7f9b7fb599391
RomarQ <RomarQ@users.noreply.github.com>
2024-06-07 09:50:26 +0000

    deploy: 69458304ddf40713fa3225c7de673c26ebace7af

--- implementors/sc_cli/trait.SubstrateCli.js
+++ implementors/sc_cli/trait.SubstrateCli.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"moonbeam_cli":[["impl SubstrateCli for <a class=\"struct\" href=\"moonbeam_cli/struct.Cli.html\" title=\"struct moonbeam_cli::Cli\">Cli</a>"],["impl SubstrateCli for <a class=\"struct\" href=\"moonbeam_cli/struct.RelayChainCli.html\" title=\"struct moonbeam_cli::RelayChainCli\">RelayChainCli</a>"]]
+"moonbeam_cli":[["impl SubstrateCli for <a class=\"struct\" href=\"moonbeam_cli/struct.RelayChainCli.html\" title=\"struct moonbeam_cli::RelayChainCli\">RelayChainCli</a>"],["impl SubstrateCli for <a class=\"struct\" href=\"moonbeam_cli/struct.Cli.html\" title=\"struct moonbeam_cli::Cli\">Cli</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
