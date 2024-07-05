bbb88c48f7fe3731ab76a67b8966b1321dc7e580
Rodrigo Quelhas <rodrigo_quelhas@outlook.pt>
2024-07-05 08:29:05 +0100

    add tests

--- precompiles/secp256r1/src/lib.rs
+++ precompiles/secp256r1/src/lib.rs
@@ -37,16 +37,19 @@ impl P256Verify {
 
 	/// (Signed payload) Hash of the original message
 	/// 32 bytes of the signed data hash
+	#[inline]
 	fn message_hash(input: &[u8]) -> &[u8] {
 		&input[..32]
 	}
 
 	/// r and s signature components
+	#[inline]
 	fn signature(input: &[u8]) -> &[u8] {
 		&input[32..96]
 	}
 
 	/// x and y coordinates of the public key
+	#[inline]
 	fn public_key(input: &[u8]) -> &[u8] {
 		&input[96..160]
 	}
@@ -132,29 +135,6 @@ mod tests {
 	}
 
 	#[test]
-	fn test_empty_input() -> Result<(), PrecompileFailure> {
-		let input = Vec::<u8>::new();
-		let cost = P256Verify::BASE_GAS;
-		let mut handle = prepare_handle(input, cost);
-
-		match P256Verify::execute(&mut handle) {
-			Ok(_) => panic!("Test not expected to pass"),
-			Err(e) => {
-				assert_eq!(
-					e,
-					PrecompileFailure::Error {
-						exit_status: ExitError::Other(
-							"input length for P256VERIFY precompile should be exactly 160 bytes"
-								.into()
-						)
-					}
-				);
-				Ok(())
-			}
-		}
-	}
-
-	#[test]
 	fn test_out_of_gas() -> Result<(), PrecompileFailure> {
 		let input = Vec::<u8>::new();
 		let cost = P256Verify::BASE_GAS.checked_sub(1).unwrap();
@@ -175,17 +155,39 @@ mod tests {
 	}
 
 	#[test]
-	fn test_valid_signature() -> Result<(), PrecompileFailure> {
-		let input = hex!("b5a77e7a90aa14e0bf5f337f06f597148676424fae26e175c6e5621c34351955289f319789da424845c9eac935245fcddd805950e2f02506d09be7e411199556d262144475b1fa46ad85250728c600c53dfd10f8b3f4adf140e27241aec3c2da3a81046703fccf468b48b145f939efdbb96c3786db712b3113bb2488ef286cdcef8afe82d200a5bb36b5462166e8ce77f2d831a52ef2135b2af188110beaefb1").to_vec();
-		let cost = P256Verify::BASE_GAS;
-		let mut handle = prepare_handle(input, cost);
+	fn test_valid_signature() {
+		let inputs = vec![
+			(true, hex!("b5a77e7a90aa14e0bf5f337f06f597148676424fae26e175c6e5621c34351955289f319789da424845c9eac935245fcddd805950e2f02506d09be7e411199556d262144475b1fa46ad85250728c600c53dfd10f8b3f4adf140e27241aec3c2da3a81046703fccf468b48b145f939efdbb96c3786db712b3113bb2488ef286cdcef8afe82d200a5bb36b5462166e8ce77f2d831a52ef2135b2af188110beaefb1").to_vec(), None),
+			(true, hex!("4cee90eb86eaa050036147a12d49004b6b9c72bd725d39d4785011fe190f0b4da73bd4903f0ce3b639bbbf6e8e80d16931ff4bcf5993d58468e8fb19086e8cac36dbcd03009df8c59286b162af3bd7fcc0450c9aa81be5d10d312af6c66b1d604aebd3099c618202fcfe16ae7770b0c49ab5eadf74b754204a3bb6060e44eff37618b065f9832de4ca6ca971a7a1adc826d0f7c00181a5fb2ddf79ae00b4e10e").to_vec(), None),
+			(false, hex!("afec5769b5cf4e310a7d150508e82fb8e3eda1c2c94c61492d3bd8aea99e06c9e22466e928fdccef0de49e3503d2657d00494a00e764fd437bdafa05f5922b1fbbb77c6817ccf50748419477e843d5bac67e6a70e97dde5a57e0c983b777e1ad31a80482dadf89de6302b1988c82c29544c9c07bb910596158f6062517eb089a2f54c9a0f348752950094d3228d3b940258c75fe2a413cb70baa21dc2e352fc5").to_vec(), None),
+			(false, hex!("3cee90eb86eaa050036147a12d49004b6b9c72bd725d39d4785011fe190f0b4da73bd4903f0ce3b639bbbf6e8e80d16931ff4bcf5993d58468e8fb19086e8cac36dbcd03009df8c59286b162af3bd7fcc0450c9aa81be5d10d312af6c66b1d604aebd3099c618202fcfe16ae7770b0c49ab5eadf74b754204a3bb6060e44eff37618b065f9832de4ca6ca971a7a1adc826d0f7c00181a5fb2ddf79ae00b4e10e").to_vec(), None),
+			(false, hex!("4cee90eb86eaa050036147a12d49004b6a").to_vec(), Some(PrecompileFailure::Error {
+				exit_status: ExitError::Other(
+					"input length for P256VERIFY precompile should be exactly 160 bytes"
+						.into()
+				)
+			})),
+		];
+		for input in inputs {
+			let cost = P256Verify::BASE_GAS;
+			let mut handle = prepare_handle(input.1.clone(), cost);
 
-		let mut expected_result = [0u8; 32];
-		expected_result[31] = 1;
+			let mut success_result = [0u8; 32];
+			success_result[31] = 1;
 
-		match P256Verify::execute(&mut handle) {
-			Ok(result) => Ok(assert_eq!(result.output, expected_result.to_vec())),
-			Err(_) => panic!("Test not expected to fail"),
+			let unsuccessful_result = Vec::<u8>::new();
+
+			match (input.0, P256Verify::execute(&mut handle)) {
+				(true, Ok(result)) => assert_eq!(result.output, success_result.to_vec()),
+				(false, Ok(result)) => assert_eq!(result.output, unsuccessful_result),
+				(_, Err(e)) => {
+					if let Some(err) = input.2 {
+						assert_eq!(e, err)
+					} else {
+						panic!("Test not expected to fail for input: {:?}", input)
+					}
+				}
+			}
 		}
 	}
 }
---
+++ test/suites/dev/moonbase/test-precompile/test-precompile-secp256r1.ts
@@ -0,0 +1,38 @@
+import "@moonbeam-network/api-augment";
+
+import { beforeAll, describeSuite, expect } from "@moonwall/cli";
+import { expectEVMResult } from "../../../../helpers";
+
+
+const CONTRACT_NAME = "P256Verify";
+
+describeSuite({
+  id: "D012803",
+  title: "Precompiles - p256verify",
+  foundationMethods: "dev",
+  testCases: ({ context, log, it }) => {
+    let contractAddress: `0x${string}`;
+
+    beforeAll(async function () {
+      const { contractAddress: _contractAddress } = await context.deployContract!(CONTRACT_NAME);
+      contractAddress = _contractAddress;
+    });
+
+    it({
+      id: "T01",
+      title: "should be accessible from a smart contract",
+      test: async function () {
+        const rawTx = await context.writeContract!({
+          contractName: CONTRACT_NAME,
+          contractAddress: contractAddress,
+          functionName: "test",
+          rawTxOnly: true,
+        });
+        const { result } = await context.createBlock(rawTx);
+
+        expect(result.successful, "Succeed");
+        expectEVMResult(result!.events, "Succeed");
+      },
+    });
+  },
+});
