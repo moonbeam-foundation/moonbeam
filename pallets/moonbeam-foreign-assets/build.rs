4726e18768e76e336b4fa5f29ff463136f4fd9d1
Éloïs <c@elo.tf>
2024-07-17 19:15:44 +0200

    improve build script: support direct copy of hex calldata

--- pallets/moonbeam-foreign-assets/build.rs
+++ pallets/moonbeam-foreign-assets/build.rs
@@ -1,6 +1,9 @@
 use std::fs::File;
 use std::io::prelude::*;
 
+// Length of encoded constructor parameters
+const PARAMS_LEN: usize = 256;
+
 fn main() {
 	let hex_str = include_str!("resources/foreign_erc20_initcode.hex");
 	let prefix_0x = hex_str.chars().nth(1) == Some('x');
@@ -11,8 +14,16 @@ fn main() {
 	}
 	.expect("fail to decode hexadecimal string in file foreign_erc20_initcode.hex");
 
+	// The encoded parameters at the end of the initializer bytecode should be removed,
+	// (the runtime will append the constructor parameters dynamically).
+	let bytecode_end = if bytecode.len() > PARAMS_LEN {
+		bytecode.len() - PARAMS_LEN
+	} else {
+		0
+	};
+
 	let mut file = File::create("resources/foreign_erc20_initcode.bin")
 		.expect("Fail to create file resources/foreign_erc20_initcode.bin");
-	file.write_all(&bytecode)
+	file.write_all(&bytecode[..bytecode_end])
 		.expect("fail to write bytecode in /foreign_erc20_initcode.bin");
 }
