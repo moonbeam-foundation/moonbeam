34da4cd506c4f526341ac5ca0a0f564b2ac41e2f
Éloïs <c@elo.tf>
2024-07-17 19:16:44 +0200

    improve solidity impl: disable transfer & approve when paused

--- pallets/moonbeam-foreign-assets/resources/foreign_erc20.sol
+++ pallets/moonbeam-foreign-assets/resources/foreign_erc20.sol
@@ -9,14 +9,15 @@ import "@openzeppelin/contracts@5.0.2/access/Ownable.sol";
 import "@openzeppelin/contracts@5.0.2/token/ERC20/extensions/ERC20Permit.sol";
 
 contract MyToken is ERC20, ERC20Pausable, Ownable, ERC20Permit {
-    constructor(address initialOwner, uint8 tokenDecimals, string memory ticker, string memory tokenName)
-        ERC20(tokenName, ticker)
-        Ownable(initialOwner)
-        ERC20Permit(tokenName)
-    {
-      _decimals = tokenDecimals;
+    constructor(
+        address initialOwner,
+        uint8 tokenDecimals,
+        string memory ticker,
+        string memory tokenName
+    ) ERC20(tokenName, ticker) Ownable(initialOwner) ERC20Permit(tokenName) {
+        _decimals = tokenDecimals;
     }
-    
+
     uint8 private _decimals;
 
     function pause() public onlyOwner {
@@ -38,17 +39,53 @@ contract MyToken is ERC20, ERC20Pausable, Ownable, ERC20Permit {
     function burnAllFrom(address account) public onlyOwner {
         _burn(account, balanceOf(account));
     }
-    
+
     function decimals() public view override returns (uint8) {
         return _decimals;
     }
 
+    // override ERC20 methods that should be disabled when the asset is "paused"
+
+    function approve(address spender, uint256 value)
+        public
+        override(ERC20)
+        whenNotPaused
+        returns (bool)
+    {
+        address owner = _msgSender();
+        _approve(owner, spender, value);
+        return true;
+    }
+
+    function transfer(address to, uint256 value)
+        public
+        override(ERC20)
+        whenNotPaused
+        returns (bool)
+    {
+        address owner = _msgSender();
+        _transfer(owner, to, value);
+        return true;
+    }
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 value
+    ) public override(ERC20) whenNotPaused returns (bool) {
+        address spender = _msgSender();
+        _spendAllowance(from, spender, value);
+        _transfer(from, to, value);
+        return true;
+    }
+
     // The following functions are overrides required by Solidity.
 
-    function _update(address from, address to, uint256 value)
-        internal
-        override(ERC20, ERC20Pausable)
-    {
+    function _update(
+        address from,
+        address to,
+        uint256 value
+    ) internal override(ERC20, ERC20Pausable) {
         super._update(from, to, value);
     }
 }
