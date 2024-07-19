5ad800368c22a2f03b77b9d36cf0e78fc158c0d1
Éloïs <c@elo.tf>
2024-07-19 12:51:00 +0200

    rust tests: fix pov ratio and storage ratio

--- pallets/moonbeam-foreign-assets/src/mock.rs
+++ pallets/moonbeam-foreign-assets/src/mock.rs
@@ -109,21 +109,11 @@ impl pallet_timestamp::Config for Test {
 	type WeightInfo = ();
 }
 
-const MAX_POV_SIZE: u64 = 5 * 1024 * 1024;
-/// Block storage limit in bytes. Set to 40 KB.
-const BLOCK_STORAGE_LIMIT: u64 = 40 * 1024;
-
 parameter_types! {
 	pub BlockGasLimit: U256 = U256::from(u64::MAX);
 	pub const WeightPerGas: Weight = Weight::from_parts(1, 0);
-	pub GasLimitPovSizeRatio: u64 = {
-		let block_gas_limit = BlockGasLimit::get().min(u64::MAX.into()).low_u64();
-		block_gas_limit.saturating_div(MAX_POV_SIZE)
-	};
-	pub GasLimitStorageGrowthRatio: u64 = {
-		let block_gas_limit = BlockGasLimit::get().min(u64::MAX.into()).low_u64();
-		block_gas_limit.saturating_div(BLOCK_STORAGE_LIMIT)
-	};
+	pub GasLimitPovSizeRatio: u64 = 16;
+	pub GasLimitStorageGrowthRatio: u64 = 366;
 }
 
 impl pallet_evm::Config for Test {
