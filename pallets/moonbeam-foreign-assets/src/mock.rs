51e42663dfbc941440e82c9838c07036cd0ffd21
Éloïs <c@elo.tf>
2024-07-22 11:08:56 +0200

    fix copyright: new code should be own by MBF

--- pallets/moonbeam-foreign-assets/src/mock.rs
+++ pallets/moonbeam-foreign-assets/src/mock.rs
@@ -1,18 +1,18 @@
-// Copyright Moonsong Labs
-// This file is part of Moonkit.
+// Copyright 2024 Moonbeam Foundation.
+// This file is part of Moonbeam.
 
-// Moonkit is free software: you can redistribute it and/or modify
+// Moonbeam is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 
-// Moonkit is distributed in the hope that it will be useful,
+// Moonbeam is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 
 // You should have received a copy of the GNU General Public License
-// along with Moonkit.  If not, see <http://www.gnu.org/licenses/>.
+// along with Moonbeam.  If not, see <http://www.gnu.org/licenses/>.
 
 use super::*;
 use crate as pallet_moonbeam_foreign_assets;
