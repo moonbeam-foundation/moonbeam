2a9d3fb174e604e04d61796f63d19f80d8aeabe0
Éloïs <c@elo.tf>
2024-08-05 19:43:15 +0200

    fix more shard 3 tests

--- test/suites/dev/moonbase/test-xcm-v4/test-mock-hrmp-transact-ethereum-8.ts
+++ test/suites/dev/moonbase/test-xcm-v4/test-mock-hrmp-transact-ethereum-8.ts
@@ -25,7 +25,7 @@ describeSuite({
       sendingAddress = originAddress;
       descendAddress = descendOriginAddress;
       random = generateKeyringPair();
-      transferredBalance = 10_000_000_000_000_000_000n;
+      transferredBalance = 100_000_000_000_000_000_000n;
 
       // We first fund parachain 2000 sovreign account
       await context.createBlock(
@@ -93,7 +93,7 @@ describeSuite({
 
         let expectedTransferredAmountPlusFees = 0n;
 
-        const targetXcmWeight = 1_325_000_000n + 25_000_000n;
+        const targetXcmWeight = 5_000_000_000n + 25_000_000n;
         const targetXcmFee = targetXcmWeight * 50_000n;
 
         for (const xcmTransaction of xcmTransactions) {
@@ -118,7 +118,7 @@ describeSuite({
             ],
             weight_limit: {
               refTime: targetXcmWeight,
-              proofSize: 110000n,
+              proofSize: 120_000n,
             },
             descend_origin: sendingAddress,
           })
