1404ccbcdbd0c1d5362a4cc26ecb446c5c7198ff
Aleksandar Brayanov <aleksandar@moonsonglab.com>
2024-08-05 16:14:41 +0100

    tracing: fix unused with_log and added additional initial tracing test for with_log parameter (currently failing)

--- test/suites/tracing-tests/test-trace-call.ts
+++ test/suites/tracing-tests/test-trace-call.ts
@@ -35,5 +35,30 @@ describeSuite({
         expect(logs[1].depth).to.be.equal(1);
       },
     });
+
+    it({
+      id: "T02",
+      title: "should trace contract calls with logs",
+      test: async function () {
+        const contracts = await createContracts(context);
+        const callParams = {
+          to: contracts.callerAddr,
+          data: encodeFunctionData({
+            abi: contracts.abiCaller,
+            functionName: "someAction",
+            args: [contracts.calleeAddr, 6],
+          }),
+          with_logs: true,
+        };
+        const traceTx = await customDevRpcRequest("debug_traceCall", [callParams, "latest"]);
+        const logs: any[] = [];
+        for (const log of traceTx.structLogs) {
+            logs.push(log);
+        }
+        expect(logs).to.be.lengthOf(2);
+        expect(logs[0].depth).to.be.equal(2);
+        expect(logs[1].depth).to.be.equal(1);
+      },
+    });
   },
 });
