806312aab59eb726d0954b4249c15da93f7fb48b
Tarek Mohamed Abdalla <tarekkma@gmail.com>
2024-08-07 09:41:26 +0100

    format

--- runtime/moonbase/src/runtime_params.rs
+++ runtime/moonbase/src/runtime_params.rs
@@ -1,13 +1,13 @@
-use frame_support::dynamic_params::{dynamic_pallet_params, dynamic_params};
-use crate::{Runtime, Balance};
-use crate::currency::{UNIT, SUPPLY_FACTOR};
+use crate::currency::{SUPPLY_FACTOR, UNIT};
+use crate::{Balance, Runtime};
 use account::AccountId20;
+use frame_support::dynamic_params::{dynamic_pallet_params, dynamic_params};
 
 #[dynamic_params(RuntimeParameters, pallet_parameters::Parameters::<Runtime>)]
 pub mod dynamic_params {
 	use super::*;
 
-    #[dynamic_pallet_params]
+	#[dynamic_pallet_params]
 	#[codec(index = 0)]
 	pub mod pallet_referenda {
 
@@ -23,17 +23,16 @@ pub mod dynamic_params {
 		/// Xcm fees will go to the treasury account
 		pub static XcmFeesAccount: AccountId20 = crate::Treasury::account_id();
 	}
-
 }
 
 #[cfg(feature = "runtime-benchmarks")]
 impl Default for RuntimeParameters {
-    fn default() -> Self {
-        RuntimeParameters::PalletReferenda(
+	fn default() -> Self {
+		RuntimeParameters::PalletReferenda(
 			dynamic_params::pallet_referenda::Parameters::SubmissionDeposit(
 				dynamic_params::pallet_referenda::SubmissionDeposit,
 				Some(10 * UNIT * SUPPLY_FACTOR),
-			)
+			),
 		)
-    }
+	}
 }
