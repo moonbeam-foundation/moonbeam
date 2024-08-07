081701b7356789c01c93805597f469eed6d36042
Tarek Mohamed Abdalla <tarekkma@gmail.com>
2024-08-07 12:22:17 +0100

    fix copyright

--- runtime/moonbase/src/runtime_params.rs
+++ runtime/moonbase/src/runtime_params.rs
@@ -1,4 +1,4 @@
-// Copyright 2019-2022 PureStake Inc.
+// Copyright 2024 Moonbeam Foundation.
 // This file is part of Moonbeam.
 
 // Moonbeam is free software: you can redistribute it and/or modify
