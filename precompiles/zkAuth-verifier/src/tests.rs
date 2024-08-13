2b762aadcc7c033159b3b4000f67ef5add5e1efd
Agusrodri <agusrodriguez2456@gmail.com>
2024-08-13 07:00:53 -0700

    add selector tests to precompile

--- precompiles/zkAuth-verifier/src/tests.rs
+++ precompiles/zkAuth-verifier/src/tests.rs
@@ -23,6 +23,35 @@ fn precompiles() -> Precompiles<Runtime> {
 }
 
 #[test]
+fn test_solidity_interface_has_all_function_selectors_documented_and_implemented() {
+	check_precompile_implements_solidity_interfaces(&["ZkAuth.sol"], PCall::supports_selector)
+}
+
+#[test]
+fn selectors() {
+	assert!(PCall::verify_proof_selectors().contains(&0x55c265fe));
+}
+
+#[test]
+fn selector_less_than_four_bytes() {
+	ExtBuilder::default().build().execute_with(|| {
+		// This selector is only three bytes long when four are required.
+		precompiles()
+			.prepare_test(Alice, Precompile1, vec![1u8, 2u8, 3u8])
+			.execute_reverts(|output| output == b"Tried to read selector out of bounds");
+	});
+}
+
+#[test]
+fn no_selector_exists_but_length_is_right() {
+	ExtBuilder::default().build().execute_with(|| {
+		precompiles()
+			.prepare_test(Alice, Precompile1, vec![1u8, 2u8, 3u8, 4u8])
+			.execute_reverts(|output| output == b"Unknown selector");
+	});
+}
+
+#[test]
 fn test_mocked_verification() {
 	ExtBuilder::default()
 		.with_balances(vec![(Alice.into(), 1000)])
