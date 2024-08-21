252af7f8af3c77f9baabe8e9dbf606a91a435515
Tarek Mohamed Abdalla <tarekkma@gmail.com>
2024-08-21 12:34:58 +0100

    change state_version

--- runtime/moonbase/src/lib.rs
+++ runtime/moonbase/src/lib.rs
@@ -199,7 +199,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 2,
-	state_version: 0,
+	state_version: 1,
 };
 
 /// The version information used to identify this runtime when compiled natively.
