6fde130c6bd17fc1e97da12b33dc431aee8e4fb1
RomarQ <RomarQ@users.noreply.github.com>
2024-08-22 09:46:48 +0000

    deploy: f0cbe6f2b33c76f71faaa10ee057a5ac259163fb

---
+++ trait.impl/cumulus_pallet_parachain_system/weights/trait.WeightInfo.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"moonbeam_runtime_common":[["impl&lt;T: Config&gt; WeightInfo for <a class=\"struct\" href=\"moonbeam_runtime_common/weights/cumulus_pallet_parachain_system/struct.WeightInfo.html\" title=\"struct moonbeam_runtime_common::weights::cumulus_pallet_parachain_system::WeightInfo\">WeightInfo</a>&lt;T&gt;"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
