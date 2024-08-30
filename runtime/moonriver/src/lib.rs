bd366a84b5906fed44a51b45f75db1c549e5f9c0
Andrea Giacobino <no.andrea@gmail.com>
2024-08-30 10:33:17 +0300

    Update runtime spec version to 3101

--- runtime/moonriver/src/lib.rs
+++ runtime/moonriver/src/lib.rs
@@ -194,7 +194,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("moonriver"),
 	impl_name: create_runtime_str!("moonriver"),
 	authoring_version: 3,
-	spec_version: 3100,
+	spec_version: 3101,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 2,
