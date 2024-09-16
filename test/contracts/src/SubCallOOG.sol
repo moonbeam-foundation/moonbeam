ed78073ef84ed5dbf80d5e71a0bdabe816b3d347
Éloïs <c@elo.tf>
2024-09-16 15:49:21 +0200

    add tests

---
+++ test/contracts/src/SubCallOOG.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: GPL-3.0-only
+pragma solidity >=0.8.3;
+
+interface IBloatedContract {
+    function doSomething() external;
+}
+
+interface ILooper {
+    function incrementalLoop(uint256 n) external;
+}
+
+contract SubCallOOG {
+    event SubCallSucceed();
+    event SubCallFail();
+
+    function subCallPov(address[] memory addresses) public {
+        for (uint256 i = 0; i < addresses.length; i++) {
+            try IBloatedContract(addresses[i]).doSomething() {
+                emit SubCallSucceed();
+            } catch (bytes memory) {
+                emit SubCallFail();
+            }
+        }
+    }
+
+    function subCallLooper(address target, uint256 n) public {
+        try ILooper(target).incrementalLoop(n) {
+            emit SubCallSucceed();
+        } catch (bytes memory) {
+            emit SubCallFail();
+        }
+    }
+}
