c3f63cfc75feeaef0241f1a1e9f80dfa7eae4dc8
Andrea Giacobino <no.andrea@gmail.com>
2024-09-16 20:59:27 +0300

    add benchmarks for runtime-3200 (#2948)
    * add benchmarks for runtime-3200
    
    * Fix pallet_asset_manager.rs benchmarks
    
    Remove unexpected parameter
    
    * Fix pallet_asset_manager.rs
    
    remove parameter in method body
    
    * Fix pallet_asset_manager.rs
    
    add missing parenthesis
    
    * fix rust tests
    
    * Adjust TARGET_FILL_AMOUNT
    
    * refactor tests & remove flaky test
    
    ---------
    
    Co-authored-by: Agusrodri <agusrodriguez2456@gmail.com>
    Co-authored-by: Pablo Labarta <pablitolabarta@gmail.com>

--- runtime/moonbase/tests/integration_test.rs
+++ runtime/moonbase/tests/integration_test.rs
@@ -2282,7 +2282,7 @@ fn transact_through_signed_precompile_works_v1() {
 						call: bytes.into(),
 					},
 				)
-				.expect_cost(23278)
+				.expect_cost(23239)
 				.expect_no_logs()
 				.execute_returns(());
 		});
@@ -2322,7 +2322,7 @@ fn transact_through_signed_precompile_works_v2() {
 						overall_weight: total_weight,
 					},
 				)
-				.expect_cost(23278)
+				.expect_cost(23239)
 				.expect_no_logs()
 				.execute_returns(());
 		});
@@ -2404,7 +2404,7 @@ fn author_mapping_precompile_associate_update_and_clear() {
 						nimbus_id: [1u8; 32].into(),
 					},
 				)
-				.expect_cost(14212)
+				.expect_cost(14255)
 				.expect_no_logs()
 				.execute_returns(());
 
@@ -2426,7 +2426,7 @@ fn author_mapping_precompile_associate_update_and_clear() {
 						new_nimbus_id: [2u8; 32].into(),
 					},
 				)
-				.expect_cost(13817)
+				.expect_cost(13829)
 				.expect_no_logs()
 				.execute_returns(());
 
@@ -2447,7 +2447,7 @@ fn author_mapping_precompile_associate_update_and_clear() {
 						nimbus_id: [2u8; 32].into(),
 					},
 				)
-				.expect_cost(14251)
+				.expect_cost(14294)
 				.expect_no_logs()
 				.execute_returns(());
 
@@ -2490,7 +2490,7 @@ fn author_mapping_register_and_set_keys() {
 						.into(),
 					},
 				)
-				.expect_cost(15996)
+				.expect_cost(16049)
 				.expect_no_logs()
 				.execute_returns(());
 
@@ -2515,7 +2515,7 @@ fn author_mapping_register_and_set_keys() {
 						.into(),
 					},
 				)
-				.expect_cost(15996)
+				.expect_cost(16049)
 				.expect_no_logs()
 				.execute_returns(());
 
