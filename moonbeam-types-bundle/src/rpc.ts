cc3880b4b499fd554f1564bf1e407e1ce5d83c5a
timbrinded <79199034+timbrinded@users.noreply.github.com>
2024-09-17 13:39:45 +0100

    fmt

--- moonbeam-types-bundle/src/rpc.ts
+++ moonbeam-types-bundle/src/rpc.ts
@@ -1,81 +1,78 @@
-import type {
-    DefinitionRpc,
-    DefinitionRpcSub
-} from "@polkadot/types/types";
+import type { DefinitionRpc, DefinitionRpcSub } from "@polkadot/types/types";
 
-  // Moonbeam specific rpc methods
-  export const rpcDefinitions: Record<string, Record<string, DefinitionRpc | DefinitionRpcSub>> = {
-    txpool: {
-      content: {
-        aliasSection: "txpool",
-        description:
-          "The detailed information regarding Ethereum transactions that are currently in the " +
-          "Substrate transaction pool.",
-        params: [],
-        type: "TxPoolResultContent",
-      },
-      inspect: {
-        aliasSection: "txpool",
-        description:
-          "Summarized information of the Ethereum transactions that are currently in the Substrate" +
-          " transaction pool.",
-        params: [],
-        type: "TxPoolResultInspect",
-      },
-      status: {
-        aliasSection: "txpool",
-        description:
-          "The number of Ethereum transaction that are currently in the Substrate transaction pool.",
-        params: [],
-        type: "TxPoolResultStatus",
-      },
+// Moonbeam specific rpc methods
+export const rpcDefinitions: Record<string, Record<string, DefinitionRpc | DefinitionRpcSub>> = {
+  txpool: {
+    content: {
+      aliasSection: "txpool",
+      description:
+        "The detailed information regarding Ethereum transactions that are currently in the " +
+        "Substrate transaction pool.",
+      params: [],
+      type: "TxPoolResultContent",
     },
-    trace: {
-      filter: {
-        aliasSection: "trace",
-        description: "Trace Filter",
-        params: [{ name: "filter", type: "FilterRequest" }],
-        type: "Result<Vec<TransactionTrace>>",
-      },
+    inspect: {
+      aliasSection: "txpool",
+      description:
+        "Summarized information of the Ethereum transactions that are currently in the Substrate" +
+        " transaction pool.",
+      params: [],
+      type: "TxPoolResultInspect",
     },
-    debug: {
-      traceTransaction: {
-        aliasSection: "debug",
-        description: "Debug trace tx",
-        params: [{ name: "transaction_hash", type: "H256" }],
-        type: "Result<Vec<TransactionTrace>>",
-      },
+    status: {
+      aliasSection: "txpool",
+      description:
+        "The number of Ethereum transaction that are currently in the Substrate transaction pool.",
+      params: [],
+      type: "TxPoolResultStatus",
     },
-    xcm: {
-      injectDownwardMessage: {
-        description: "Inject a downward message from the relay chain.",
-        params: [{ name: "message", type: "Vec<u8>" }],
-        type: "Result<()>",
-      },
-      injectHrmpMessage: {
-        description: "Inject an HRMP message from a dedicated channel from a sibling parachain",
-        params: [
-          { name: "sender", type: "ParaId" },
-          { name: "message", type: "Vec<u8>" },
-        ],
-        type: "Result<()>",
-      },
+  },
+  trace: {
+    filter: {
+      aliasSection: "trace",
+      description: "Trace Filter",
+      params: [{ name: "filter", type: "FilterRequest" }],
+      type: "Result<Vec<TransactionTrace>>",
     },
-    moon: {
-      isBlockFinalized: {
-        description: "Returns whether an Ethereum block is finalized",
-        params: [{ name: "blockHash", type: "Hash" }],
-        type: "bool",
-      },
-      isTxFinalized: {
-        description: "Returns whether an Ethereum transaction is finalized",
-        params: [{ name: "txHash", type: "Hash" }],
-        type: "bool",
-      },
-      getLatestSyncedBlock: {
-        description: "Returns the latest synced block from frontier's backend",
-        params: [],
-        type: "u32",
-      },
+  },
+  debug: {
+    traceTransaction: {
+      aliasSection: "debug",
+      description: "Debug trace tx",
+      params: [{ name: "transaction_hash", type: "H256" }],
+      type: "Result<Vec<TransactionTrace>>",
     },
-  };
+  },
+  xcm: {
+    injectDownwardMessage: {
+      description: "Inject a downward message from the relay chain.",
+      params: [{ name: "message", type: "Vec<u8>" }],
+      type: "Result<()>",
+    },
+    injectHrmpMessage: {
+      description: "Inject an HRMP message from a dedicated channel from a sibling parachain",
+      params: [
+        { name: "sender", type: "ParaId" },
+        { name: "message", type: "Vec<u8>" },
+      ],
+      type: "Result<()>",
+    },
+  },
+  moon: {
+    isBlockFinalized: {
+      description: "Returns whether an Ethereum block is finalized",
+      params: [{ name: "blockHash", type: "Hash" }],
+      type: "bool",
+    },
+    isTxFinalized: {
+      description: "Returns whether an Ethereum transaction is finalized",
+      params: [{ name: "txHash", type: "Hash" }],
+      type: "bool",
+    },
+    getLatestSyncedBlock: {
+      description: "Returns the latest synced block from frontier's backend",
+      params: [],
+      type: "u32",
+    },
+  },
+};
