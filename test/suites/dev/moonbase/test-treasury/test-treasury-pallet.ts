705eca72ece7f7a66883eab68793138d2f633715
Gonza Montiel <gon.montiel@gmail.com>
2024-09-19 17:09:09 +0200

    fix remaining dev tests

--- test/suites/dev/moonbase/test-treasury/test-treasury-pallet.ts
+++ test/suites/dev/moonbase/test-treasury/test-treasury-pallet.ts
@@ -1,59 +1,51 @@
 import "@moonbeam-network/api-augment";
-import { describeSuite, expect } from "@moonwall/cli";
-import { baltathar, ethan, alith } from "@moonwall/util";
+import { beforeAll, describeSuite, expect } from "@moonwall/cli";
+import { ApiPromise } from "@polkadot/api";
+import { alith, baltathar, ethan } from "@moonwall/util";
+import { Signer } from "@polkadot/types/types";
+import { jumpBlocks } from "../../../../helpers";
 
 describeSuite({
   id: "D013801",
   title: "Treasury pallet tests",
   foundationMethods: "dev",
-  testCases: ({ context, it, log }) => {
+  testCases: ({ context, log, it }) => {
+    let api: ApiPromise;
+
+    beforeAll(async function () {
+      api = context.polkadotJs();
+    });
+
     it({
       id: "T01",
-      title: "Non root can not spend from treasury",
+      title: "Non root cannot spend (local)",
       test: async function () {
+        expect((await api.query.treasury.spendCount()).toNumber()).to.equal(0);
+
+        // Creates a proposal
         const proposal_value = 1000000000n;
-        const assetKind = null;
-        expect(
-          await context
-            .createBlock(
-              context
-                .polkadotJs()
-                .tx.treasury.spend(assetKind, proposal_value, baltathar.address, null)
-                .signAsync(ethan)
-            )
-            .catch((e) => e.toString())
-        ).to.equal("RpcError: 1010: Invalid Transaction: Transaction call is not expected");
+        const tx = api.tx.treasury.spendLocal(proposal_value, ethan.address);
+        const signedTx = await tx.signAsync(baltathar);
+        await context.createBlock([signedTx]);
+
+        expect((await api.query.treasury.spendCount()).toNumber()).to.equal(0);
       },
     });
 
     it({
       id: "T02",
-      title: "Root can spend from treasury",
+      title: "Root should be able to spend (local) and approve a proposal",
       test: async function () {
-        const api = context.polkadotJs();
-
         expect((await api.query.treasury.spendCount()).toNumber()).to.equal(0);
-        const balanceBefore = (
-          await api.query.system.account(baltathar.address)
-        ).data.free.toBigInt();
-
-        // Value needs to be higher than the transaction fee paid by dave, but lower than the total treasury pot
+        // Creates a proposal
+        // Value needs to be higher than the transaction fee paid by ethan, but lower than the total treasury pot
         const proposal_value = 1000000000n;
-        const assetKind = null;
-        const signedTx = api.tx.sudo.sudo(
-          api.tx.treasury.spend(assetKind, proposal_value, ethan.address, null)
-        );
-        const { result } = await context.createBlock([signedTx]);
+        const tx = api.tx.treasury.spendLocal(proposal_value, ethan.address);
+        const signedTx = await api.tx.sudo.sudo(tx).signAsync(alith);
+        await context.createBlock([signedTx]);
 
-        console.log(result);
-        expect((await api.query.treasury.spendCount()).toNumber()).to.equal(1);
-
-        const tx2 = api.tx.treasury.payout(0);
-        const signedTx2 = await tx2.signAsync(ethan);
-        await context.createBlock([signedTx2]);
-
-        const balanceAfter = (await api.query.system.account(ethan.address)).data.free.toBigInt();
-        expect(balanceAfter).toBeGreaterThan(balanceBefore);
+        // Local spends dont upadte the spend count
+        expect((await api.query.treasury.spendCount()).toNumber()).to.equal(0);
       },
     });
   },
