ed46225f6c4b7fcfaa2068ec107a1bdc63a9a3d8
Gonza Montiel <gon.montiel@gmail.com>
2024-09-23 14:11:55 +0200

    fix download-polkadot script

--- test/scripts/download-polkadot.sh
+++ test/scripts/download-polkadot.sh
@@ -6,6 +6,8 @@ set -e
 # Grab Polkadot version
 branch=$(egrep -o '/polkadot.*#([^\"]*)' $(dirname $0)/../../Cargo.lock | head -1 | sed 's/.*release-//#')
 polkadot_release=$(echo $branch | sed 's/#.*//' | sed 's/\/polkadot-sdk?branch=moonbeam-polkadot-v?//')
+# Workaround for supporting 'stable2407' versioning
+polkadot_release=$(echo $polkadot_release | sed 's/#.*//' | sed 's/\/polkadot-sdk?branch=moonbeam-polkadot-//')
 
 # Always run the commands from the "test" dir
 cd $(dirname $0)/..
