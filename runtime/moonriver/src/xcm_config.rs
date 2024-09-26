f7b044c91d9c98cb9923da9a3aff1e1bacda6604
Gonza Montiel <gon.montiel@gmail.com>
2024-09-26 12:42:35 +0200

    Revert "apply review suggestions"
    This reverts commit 6daef572fb5feb408dd9205e78ca0ef971ca141b.

--- runtime/moonriver/src/xcm_config.rs
+++ runtime/moonriver/src/xcm_config.rs
@@ -373,7 +373,7 @@ impl cumulus_pallet_xcmp_queue::Config for Runtime {
 	type PriceForSiblingDelivery = polkadot_runtime_common::xcm_sender::NoPriceForMessageDelivery<
 		cumulus_primitives_core::ParaId,
 	>;
-	type MaxActiveOutboundChannels = MessageQueueHeapSize;
+	type MaxActiveOutboundChannels = ConstU32<128>;
 	// Most on-chain HRMP channels are configured to use 102400 bytes of max message size, so we
 	// need to set the page size larger than that until we reduce the channel size on-chain.
 	type MaxPageSize = ConstU32<{ 103 * 1024 }>;
