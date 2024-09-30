6419f892cf1203ec3509795afb37d23bd1ebb6c3
Rodrigo Quelhas <rodrigo_quelhas@outlook.pt>
2024-09-30 16:18:41 +0100

    update chopsticks and upgradeRestrictionSignal check

--- test/suites/chopsticks/test-upgrade-chain.ts
+++ test/suites/chopsticks/test-upgrade-chain.ts
@@ -1,8 +1,57 @@
 import "@moonbeam-network/api-augment";
-import { MoonwallContext, beforeAll, describeSuite, expect } from "@moonwall/cli";
+import {
+  MoonwallContext,
+  beforeAll,
+  describeSuite,
+  expect,
+  ChopsticksContext,
+} from "@moonwall/cli";
 import { alith } from "@moonwall/util";
 import { ApiPromise } from "@polkadot/api";
+import { HexString } from "@polkadot/util/types";
+import { u32 } from "@polkadot/types";
+import { hexToU8a, u8aConcat, u8aToHex } from "@polkadot/util";
+import { blake2AsHex, xxhashAsU8a } from "@polkadot/util-crypto";
 import { parseEther } from "ethers";
+import { existsSync, readFileSync } from "node:fs";
+
+const hash = (prefix: HexString, suffix: Uint8Array) => {
+  return u8aToHex(u8aConcat(hexToU8a(prefix), xxhashAsU8a(suffix, 64), suffix));
+};
+
+const upgradeRestrictionSignal = (paraId: u32) => {
+  const prefix = "0xcd710b30bd2eab0352ddcc26417aa194f27bbb460270642b5bcaf032ea04d56a";
+
+  return hash(prefix, paraId.toU8a());
+};
+
+const upgradeRuntime = async (context: ChopsticksContext) => {
+  const path = (await MoonwallContext.getContext()).rtUpgradePath;
+  if (!existsSync(path)) {
+    throw new Error(`Runtime wasm not found at path: ${path}`);
+  }
+  const rtWasm = readFileSync(path);
+  const rtHex = `0x${rtWasm.toString("hex")}`;
+  const rtHash = blake2AsHex(rtHex);
+  const api = context.polkadotJs();
+  const signer = context.keyring.alice;
+
+  await context.setStorage({
+    module: "system",
+    method: "authorizedUpgrade",
+    methodParams: `${rtHash}01`, // 01 is for the RT ver check = true
+  });
+  await context.createBlock();
+
+  await api.tx.system.applyAuthorizedUpgrade(rtHex).signAndSend(signer);
+
+  let paraId: u32 = await api.query.parachainInfo.parachainId();
+
+  await api.rpc("dev_newBlock", {
+    count: 1,
+    relayChainStateOverrides: [[upgradeRestrictionSignal(paraId), "0x00"]],
+  });
+};
 
 describeSuite({
   id: "C01",
@@ -19,7 +68,7 @@ describeSuite({
       log("About to upgrade to runtime at:");
       log((await MoonwallContext.getContext()).rtUpgradePath);
 
-      await context.upgradeRuntime();
+      await upgradeRuntime(context);
 
       const rtafter = api.consts.system.version.specVersion.toNumber();
       log(`RT upgrade has increased specVersion from ${rtBefore} to ${rtafter}`);
