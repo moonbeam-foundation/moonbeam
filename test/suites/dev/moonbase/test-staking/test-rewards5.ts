b4105f82a3aee3dcbd37716c898325810cd5395f
Tarek Mohamed Abdalla <tarekkma@gmail.com>
2024-10-03 16:30:00 +0000

    fix

--- test/suites/dev/moonbase/test-staking/test-rewards5.ts
+++ test/suites/dev/moonbase/test-staking/test-rewards5.ts
@@ -2,6 +2,7 @@ import "@moonbeam-network/api-augment";
 import { beforeAll, describeSuite, expect } from "@moonwall/cli";
 import { MIN_GLMR_STAKING, alith, baltathar, ethan, dorothy, charleth } from "@moonwall/util";
 import { jumpRounds } from "../../../../helpers";
+import { FrameSystemEventRecord } from "@polkadot/types/lookup";
 
 describeSuite({
   id: "D0134655",
@@ -29,40 +30,58 @@ describeSuite({
           .signAsync(alith),
       ]);
 
-      await context.createBlock(
-        [
-          context
-            .polkadotJs()
-            .tx.sudo.sudo(context.polkadotJs().tx.parachainStaking.setBlocksPerRound(10))
-            .signAsync(alith),
-          context
-            .polkadotJs()
-            .tx.parachainStaking.delegate(alith.address, BOND_AMOUNT, 0, 0)
-            .signAsync(ethan),
-          context
-            .polkadotJs()
-            .tx.parachainStaking.delegate(alith.address, BOND_AMOUNT, 1, 0)
-            .signAsync(baltathar),
-        ],
-        { allowFailures: false }
-      );
-
     });
 
     it({
       id: "T01",
       title: "should reward charleth and dorothy correct amounts",
       test: async () => {
+        const startBlockHash = (await context.createBlock(
+          [
+            context
+              .polkadotJs()
+              .tx.sudo.sudo(context.polkadotJs().tx.parachainStaking.setBlocksPerRound(10))
+              .signAsync(alith),
+            context
+              .polkadotJs()
+              .tx.parachainStaking.delegate(alith.address, BOND_AMOUNT, 0, 0)
+              .signAsync(ethan),
+            context
+              .polkadotJs()
+              .tx.parachainStaking.delegate(alith.address, BOND_AMOUNT, 1, 0)
+              .signAsync(baltathar),
+          ],
+          { allowFailures: false }
+        )).block.hash.toString();
+
         const rewardDelay = context.polkadotJs().consts.parachainStaking.rewardPaymentDelay;
-        let blockHash = (await context.createBlock()).block.hash.toString();
-        const allEvents = await (await context.polkadotJs().at(blockHash)).query.system.events();
+        const round = (await context.polkadotJs().query.parachainStaking.round()).length;
+        console.log(`Round length: ${round}`);
+        console.log(`Reward delay: ${rewardDelay.toString()}`);
         await jumpRounds(context, rewardDelay.addn(1).toNumber());
-        blockHash = (await context.createBlock()).block.hash.toString();
-        allEvents.push(...await (await context.polkadotJs().at(blockHash)).query.system.events());
+        const endBlockHash = (await context.createBlock()).block.hash.toString();
+
+        const allEvents: FrameSystemEventRecord[] = [];
+        const parent = (await context.polkadotJs().rpc.chain.getHeader(startBlockHash)).parentHash.toString();
+        let c = 0;
+        for (let hash = endBlockHash; hash != parent; hash = (await context.polkadotJs().rpc.chain.getHeader(hash)).parentHash.toString()) {
+          const events = await (await context.polkadotJs().at(hash)).query.system.events();
+          const bnumber = (await context.polkadotJs().rpc.chain.getHeader(hash)).number.toNumber();
+          console.log(`[${bnumber}] Events at block ${hash}`);
+          events.forEach((event) => {
+            if (context.polkadotJs().events.parachainStaking.Rewarded.is(event.event)) {
+              console.log(event.event.section, event.event.method);
+              console.log("\t>" + event.event.data.account.toString(), event.event.data.rewards.toBigInt().toString());
+            } else if (context.polkadotJs().events.parachainStaking.InflationDistributed.is(event.event)) {
+              console.log(event.event.section, event.event.method);
+              console.log("\t>" + event.event.data.account.toString(), event.event.data.value.toBigInt().toString());
+            }
+          });
+          allEvents.push(...events);
+        }
 
         const rewardedEvents = allEvents.reduce(
           (acc: { account: string; amount: bigint }[], event) => {
-            console.log(event.event.section, event.event.method);
             if (context.polkadotJs().events.parachainStaking.Rewarded.is(event.event)) {
               acc.push({
                 account: event.event.data.account.toString(),
@@ -79,6 +98,7 @@ describeSuite({
           []
         );
 
+        const rewardedAlith = rewardedEvents.find(({ account }) => account == alith.address);
         const rewardedEthan = rewardedEvents.find(({ account }) => account == ethan.address);
         const rewardedBalathar = rewardedEvents.find(({ account }) => account == baltathar.address);
 
@@ -87,6 +107,7 @@ describeSuite({
 
 
 
+        expect(rewardedAlith).is.not.undefined;
         expect(rewardedEthan).is.not.undefined;
         expect(rewardedBalathar).is.not.undefined;
         expect(rewardedPbr).is.not.undefined;
@@ -99,6 +120,17 @@ describeSuite({
 
         const reservedRewardPercentage = ((reservedReward * 100n) / totalReward);
         const actualOtherPercentage = ((otherReward * 100n) / totalReward);
+
+        //log all the above values
+        console.log(`Total reward: ${totalReward}`);
+        console.log(`Reserved reward: ${reservedReward}`);
+        console.log(`Other reward: ${otherReward}`);
+        console.log(`Reserved reward percentage: ${reservedRewardPercentage}`);
+        console.log(`Other reward percentage: ${actualOtherPercentage}`);
+        console.log(`PBR reward: ${rewardedPbr!.amount}`);
+        console.log(`Treasury reward: ${rewardedTreasury!.amount}`);
+
+
 
         expect(reservedRewardPercentage.toString(), "Reserved reward percentage is not correct")
           .toEqual((PBR_PERCENTAGE + TREASURY_PERCENTAGE).toString());
