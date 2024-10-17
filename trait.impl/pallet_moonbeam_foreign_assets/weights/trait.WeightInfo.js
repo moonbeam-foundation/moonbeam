da9a93e4cdf0286b00c2f507c6847cbaced4f28b
RomarQ <RomarQ@users.noreply.github.com>
2024-10-17 09:21:08 +0000

    deploy: 30fbc0a63be612c9c984f47e180ec82023935b01

--- trait.impl/pallet_moonbeam_foreign_assets/weights/trait.WeightInfo.js
+++ trait.impl/pallet_moonbeam_foreign_assets/weights/trait.WeightInfo.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
-"moonbeam_runtime_common":[["impl&lt;T: Config&gt; WeightInfo for <a class=\"struct\" href=\"moonbeam_runtime_common/weights/pallet_moonbeam_foreign_assets/struct.WeightInfo.html\" title=\"struct moonbeam_runtime_common::weights::pallet_moonbeam_foreign_assets::WeightInfo\">WeightInfo</a>&lt;T&gt;"]],
+"moonbeam_runtime_common":[["impl&lt;T: Config&gt; <a class=\"trait\" href=\"pallet_moonbeam_foreign_assets/weights/trait.WeightInfo.html\" title=\"trait pallet_moonbeam_foreign_assets::weights::WeightInfo\">WeightInfo</a> for <a class=\"struct\" href=\"moonbeam_runtime_common/weights/pallet_moonbeam_foreign_assets/struct.WeightInfo.html\" title=\"struct moonbeam_runtime_common::weights::pallet_moonbeam_foreign_assets::WeightInfo\">WeightInfo</a>&lt;T&gt;"]],
 "pallet_moonbeam_foreign_assets":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
