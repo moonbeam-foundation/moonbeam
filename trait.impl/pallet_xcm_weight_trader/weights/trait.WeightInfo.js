bc143078e6123716d0d9b339418e3974778f1f3f
RomarQ <RomarQ@users.noreply.github.com>
2024-11-20 10:49:41 +0000

    deploy: 9e2ddbc9ae8bf65f11701e7ccde50075e5fe2790

--- trait.impl/pallet_xcm_weight_trader/weights/trait.WeightInfo.js
+++ trait.impl/pallet_xcm_weight_trader/weights/trait.WeightInfo.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
-"moonbeam_runtime_common":[["impl&lt;T: Config&gt; <a class=\"trait\" href=\"pallet_xcm_weight_trader/weights/trait.WeightInfo.html\" title=\"trait pallet_xcm_weight_trader::weights::WeightInfo\">WeightInfo</a> for <a class=\"struct\" href=\"moonbeam_runtime_common/weights/pallet_xcm_weight_trader/struct.WeightInfo.html\" title=\"struct moonbeam_runtime_common::weights::pallet_xcm_weight_trader::WeightInfo\">WeightInfo</a>&lt;T&gt;"]],
+"moonbeam_runtime_common":[["impl&lt;T: Config&gt; WeightInfo for <a class=\"struct\" href=\"moonbeam_runtime_common/weights/pallet_xcm_weight_trader/struct.WeightInfo.html\" title=\"struct moonbeam_runtime_common::weights::pallet_xcm_weight_trader::WeightInfo\">WeightInfo</a>&lt;T&gt;"]],
 "pallet_xcm_weight_trader":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- trait.impl/sc_executor/trait.RuntimeVersionOf.js
+++ trait.impl/sc_executor/trait.RuntimeVersionOf.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"moonbeam_service":[["impl&lt;B, E, Block&gt; RuntimeVersionOf for <a class=\"struct\" href=\"moonbeam_service/lazy_loading/call_executor/struct.LazyLoadingCallExecutor.html\" title=\"struct moonbeam_service::lazy_loading::call_executor::LazyLoadingCallExecutor\">LazyLoadingCallExecutor</a>&lt;Block, B, E&gt;<div class=\"where\">where\n    E: RuntimeVersionOf,\n    Block: BlockT,</div>"]]
+"moonbeam_service":[["impl&lt;Block, Executor&gt; RuntimeVersionOf for <a class=\"struct\" href=\"moonbeam_service/lazy_loading/call_executor/struct.LazyLoadingCallExecutor.html\" title=\"struct moonbeam_service::lazy_loading::call_executor::LazyLoadingCallExecutor\">LazyLoadingCallExecutor</a>&lt;Block, Executor&gt;<div class=\"where\">where\n    Executor: RuntimeVersionOf,\n    Block: BlockT,</div>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
