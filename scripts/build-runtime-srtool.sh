869eb32ab1f1ad9f20580f2f961eb6896a5415de
crystalin <alan@moonsonglabs.com>
2024-11-27 18:53:30 +0100

    Removes extra docker uid mapping from opslayer

--- scripts/build-runtime-srtool.sh
+++ scripts/build-runtime-srtool.sh
@@ -9,7 +9,6 @@
 CMD="docker run \
   -i \
   --rm \
-  $(~/srtool/uid-gid-mapping.sh 1001 | xargs) \
   -e CARGO_NET_GIT_FETCH_WITH_CLI=true \
   -e PACKAGE=${GH_WORKFLOW_MATRIX_CHAIN}-runtime \
   -e RUNTIME_DIR=runtime/${GH_WORKFLOW_MATRIX_CHAIN} \
