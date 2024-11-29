41c5484ee62ecce88a7119cd3b3b653fb65e6f96
timbrinded <timbrinded@users.noreply.github.com>
2024-11-29 14:16:30 +0000

    deploy: b1f85085a3c161b79f09ac613b69dea0e3ef96f5

--- moonbeam_client_evm_tracing/types/block/sidebar-items.js
+++ moonbeam_client_evm_tracing/types/block/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["TransactionTraceAction","TransactionTraceOutput","TransactionTraceResult"],"struct":["TransactionTrace"]};
+window.SIDEBAR_ITEMS = {"enum":["TransactionTraceAction","TransactionTraceOutput","TransactionTraceResult"],"struct":["BlockTransactionTrace","TransactionTrace"]};
