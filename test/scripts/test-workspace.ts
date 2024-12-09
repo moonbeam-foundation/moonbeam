baee7a22bc0c09ad3cff5bec48649aee62d1f024
timbrinded <79199034+timbrinded@users.noreply.github.com>
2024-12-09 09:54:09 +0000

    refactor: :sparkles: Dual publish typesbundle properly

---
+++ test/scripts/test-workspace.ts
@@ -0,0 +1,16 @@
+// import "@moonbeam-network/api-augment"
+import { ApiPromise, WsProvider } from "@polkadot/api";
+import { typesBundlePre900 } from "moonbeam-types-bundle";
+
+const api = await ApiPromise.create({
+  provider: new WsProvider("wss://wss.api.moonbase.moonbeam.network"),
+  typesBundle: typesBundlePre900,
+});
+
+console.log(api.consts.system.version.specName.toString());
+
+console.log(api.consts.system.version.specVersion.toNumber());
+
+const headBlock = (await api.rpc.chain.getHeader()).hash.toHex();
+
+console.log((await api.rpc.moon.isBlockFinalized(headBlock)).toHuman());
