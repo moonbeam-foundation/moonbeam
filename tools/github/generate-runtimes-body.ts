6b4488d8e692202f26ee1f1a020980ac78b514a0
Steve Degosserie <723552+stiiifff@users.noreply.github.com>
2024-12-18 19:23:02 +0100

    Fix comment

--- tools/github/generate-runtimes-body.ts
+++ tools/github/generate-runtimes-body.ts
@@ -8,11 +8,11 @@ import { blake2AsHex } from "@polkadot/util-crypto";
 
 const BREAKING_CHANGES_LABEL = "breaking";
 const RUNTIME_CHANGES_LABEL = "B7-runtimenoteworthy";
-// `ParachainSystem` is pallet index 0. `authorize_upgrade` is extrinsic index 9.
+// `System` is pallet index 0. `authorize_upgrade` is extrinsic index 9.
 const MOONBASE_PREFIX_SYSTEM_AUTHORIZE_UPGRADE = "0x0009";
-// `ParachainSystem` is pallet index 0. `authorize_upgrade` is extrinsic index 9.
+// `System` is pallet index 0. `authorize_upgrade` is extrinsic index 9.
 const MOONRIVER_PREFIX_SYSTEM_AUTHORIZE_UPGRADE = "0x0009";
-// `ParachainSystem` is pallet index 0. `authorize_upgrade` is extrinsic index 9.
+// `System` is pallet index 0. `authorize_upgrade` is extrinsic index 9.
 const MOONBEAM_PREFIX_SYSTEM_AUTHORIZE_UPGRADE = "0x0009";
 
 function capitalize(s) {
