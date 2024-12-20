2b2fc8b117cacd7877942051215f6350f6835fd8
Rodrigo Quelhas <rodrigo_quelhas@outlook.pt>
2024-12-20 14:18:26 +0000

    Revert "chore(ci): fix tracing tests"
    This reverts commit ad0b03da0eb0a3f5c87b56a46bfcd0c080af959f.

--- scripts/build-last-tracing-runtime.sh
+++ scripts/build-last-tracing-runtime.sh
@@ -2,7 +2,6 @@
 
 LOCAL_GIT_BRANCH="$(git symbolic-ref HEAD 2>/dev/null)"
 LOCAL_GIT_BRANCH=${LOCAL_GIT_BRANCH##refs/heads/}
-GIT_BRANCH_REMOTE=$(git ls-remote --get-url origin)
 
 echo ${1:-"$LOCAL_GIT_BRANCH"}
 
@@ -11,6 +10,6 @@ mkdir -p build/wasm
 git clone --depth 1 -b master-without-wasm https://github.com/moonbeam-foundation/moonbeam-runtime-overrides build/moonbeam-runtime-overrides
 
 cd build/moonbeam-runtime-overrides
-./scripts/import-tracing-runtime.sh local ${1:-"$LOCAL_GIT_BRANCH"} "$GIT_BRANCH_REMOTE"
+./scripts/import-tracing-runtime.sh local ${1:-"$LOCAL_GIT_BRANCH"}
 ./scripts/build-tracing-runtime.sh local moonbase
 mv wasm/moonbase-runtime-local-substitute-tracing.wasm ../wasm/moonbase-runtime-local-substitute-tracing.wasm
