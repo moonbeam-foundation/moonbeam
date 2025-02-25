8b2f9abd9f80904002be659c05051212d846d258
Pablo Labarta <pablitolabarta@gmail.com>
2025-02-25 09:18:56 -0300

    update gas costs 2

--- test/suites/dev/moonbase/test-randomness/test-randomness-babe-lottery3.ts
+++ test/suites/dev/moonbase/test-randomness/test-randomness-babe-lottery3.ts
@@ -110,7 +110,7 @@ describeSuite({
           account: BALTATHAR_ADDRESS,
         });
         log("Estimated Gas for startLottery", estimatedGas);
-        expect(estimatedGas).toMatchInlineSnapshot(`298280n`);
+        expect(estimatedGas).toMatchInlineSnapshot(`300210n`);
 
         const rawTxn = await context.writePrecompile!({
           precompileName: "Randomness",
