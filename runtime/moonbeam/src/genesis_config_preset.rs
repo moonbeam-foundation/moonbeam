f3e90fea2b4ec2fb7b036b146507457089e9660f
Rodrigo Quelhas <rodrigo_quelhas@outlook.pt>
2025-03-10 16:06:43 +0000

    update genesis state

--- runtime/moonbeam/src/genesis_config_preset.rs
+++ runtime/moonbeam/src/genesis_config_preset.rs
@@ -255,6 +255,8 @@ pub fn development() -> serde_json::Value {
 			AccountId::from(sp_core::hex2array!(
 				"773539d4Ac0e786233D90A233654ccEE26a613D9"
 			)),
+			// Bridge Location::new(1, [Parachain(<bp_moonbeam::Moonbeam as bp_runtime::Parachain>::PARACHAIN_ID)])
+			AccountId::from(hex!("7369626cd4070000000000000000000000000000")),
 		],
 		1_500_000 * GLMR * SUPPLY_FACTOR,
 		Default::default(), // para_id
