58638bca8161c0b2b9205ca387d1269e7a37da21
snowmead <michael.assaf.edge@gmail.com>
2025-03-25 09:27:09 -0400

    wip: use foreign asset helper functions

--- test/suites/dev/moonbase/test-treasury/test-treasury-other-assets.ts
+++ test/suites/dev/moonbase/test-treasury/test-treasury-other-assets.ts
@@ -3,16 +3,14 @@ import { beforeAll, describeSuite, expect, fetchCompiledContract } from "@moonwa
 import type { ApiPromise } from "@polkadot/api";
 import { alith, ALITH_ADDRESS, baltathar, ethan, TREASURY_ACCOUNT } from "@moonwall/util";
 import { FrameSupportPalletId } from "@polkadot/types/lookup";
-import { sendCallAsPara } from "../../../../helpers/xcm";
-import { expectSystemEvent } from "../../../../helpers/expect";
+import { descendOriginFromAddress20 } from "../../../../helpers/xcm";
 import {
-  addAssetToWeightTrader,
   assetContractAddress,
   foreignAssetBalance,
-  mockAssetBalance,
   PARA_1000_SOURCE_LOCATION,
+  registerAndFundAsset,
+  TestAsset,
 } from "../../../../helpers";
-import { getContract } from "viem";
 
 describeSuite({
   id: "D013803",
@@ -23,43 +21,36 @@ describeSuite({
     let treasuryAddress: string;
     let api: ApiPromise;
     let assetKind;
-    const assetId = 111n;
+    const initialBalance: bigint = 500_000_000_000_000_000n;
+    const xcIntrAsset: TestAsset = {
+      id: 1000100010001000n,
+      location: PARA_1000_SOURCE_LOCATION,
+      metadata: {
+        name: "TEST",
+        symbol: "TEST",
+        decimals: 19n,
+        isFrozen: false,
+      },
+      relativePrice: 1_000_000_000_000_000_000n,
+    };
 
     beforeAll(async function () {
       api = context.polkadotJs();
-      assetKind = api.createType("FrameSupportTokensFungibleUnionOfNativeOrWithId", assetId);
+      assetKind = api.createType("FrameSupportTokensFungibleUnionOfNativeOrWithId", xcIntrAsset.id);
     });
 
     it({
       id: "T01",
       title: "Treasury can spend in non-native assets",
       test: async function () {
-        const assetLocation = PARA_1000_SOURCE_LOCATION;
+        // Register foreign asset used to pay fees (i.e. xcINTR)
+        await registerAndFundAsset(context, xcIntrAsset, initialBalance, TREASURY_ACCOUNT);
 
-        const createForeignAssetCall = context
-          .polkadotJs()
-          .tx.evmForeignAssets.createForeignAsset(assetId, assetLocation, 18, "TEST", "TEST");
+        const newBalance = await foreignAssetBalance(context, BigInt(xcIntrAsset.id), TREASURY_ACCOUNT);
+        expect(newBalance).toBe(initialBalance);
 
-        const sudoCall = context.polkadotJs().tx.sudo.sudo(createForeignAssetCall);
-        const block = await context.createBlock(sudoCall, { allowFailures: false });
-        await expectSystemEvent(
-          block.block.hash,
-          "evmForeignAssets",
-          "ForeignAssetCreated",
-          context
-        );
-
-        // Add asset to weight trader
-        await addAssetToWeightTrader(assetLocation, 1_000_000_000_000_000_000n, context);
-
-        // Fund the treasury account
-        const treasuryBalance = 100_000_000_000_000_000_000n;
-        await mockAssetBalance(context, treasuryBalance, assetId, alith, TREASURY_ACCOUNT);
-        const newBalance = await foreignAssetBalance(context, assetId, TREASURY_ACCOUNT);
-        expect(newBalance).toBe(treasuryBalance);
-
-        // Spend from treasury account
-        const proposal_value = 1_000_000_000_000_000_000n;
+        // Treasury proposal spend value is half of the balance to ensure reducible balance covers the amount
+        const proposal_value = initialBalance / 2n;
         const tx = api.tx.treasury.spend(assetKind, proposal_value, ethan.address, null);
         const signedTx = await api.tx.sudo.sudo(tx).signAsync(alith);
         await context.createBlock(signedTx, {
@@ -67,27 +58,16 @@ describeSuite({
           expectEvents: [api.events.treasury.AssetSpendApproved],
         });
 
-        // Spending was successfully submitted
         expect((await api.query.treasury.spendCount()).toNumber()).to.equal(1);
-
-        const contractAddress = assetContractAddress(assetId);
-        const { abi, bytecode } = fetchCompiledContract("ERC20Instance");
-
-        const totalSupply = await context.viem().readContract({
-          address: contractAddress,
-          abi,
-          functionName: "totalSupply",
-        });
-
-        console.log("Total supply: ", totalSupply);
 
         // Trigger payout
         await context.createBlock(await api.tx.treasury.payout(0).signAsync(ethan), {
           allowFailures: false,
           expectEvents: [api.events.treasury.Paid],
         });
-        // const newBalanceAfter = await foreignAssetBalance(context, assetId, TREASURY_ACCOUNT);
-        // expect(newBalanceAfter).toBe(treasuryBalance - proposal_value);
+
+        const newBalanceAfter = await foreignAssetBalance(context, BigInt(xcIntrAsset.id), TREASURY_ACCOUNT);
+        expect(newBalanceAfter).toBe(initialBalance - proposal_value);
       },
     });
   },
