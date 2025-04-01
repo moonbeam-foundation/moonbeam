e05f50178dcbad537ca1ffd097d4983b9fc038e6
snowmead <michael.assaf.edge@gmail.com>
2025-04-01 10:34:45 -0400

    amend; fix copyright header, have erc20 balance and total supply gas limits identical

--- pallets/moonbeam-foreign-assets/src/evm.rs
+++ pallets/moonbeam-foreign-assets/src/evm.rs
@@ -41,7 +41,7 @@ pub(crate) const ERC20_TRANSFER_GAS_LIMIT: u64 = 160_000; // highest failure: 15
 pub(crate) const ERC20_APPROVE_GAS_LIMIT: u64 = 160_000; // highest failure: 153_000
 const ERC20_UNPAUSE_GAS_LIMIT: u64 = 160_000; // highest failure: 149_500
 pub(crate) const ERC20_TOTAL_SUPPLY_GAS_LIMIT: u64 = 160_000; // highest failure: 150_500
-pub(crate) const ERC20_BALANCE_OF_GAS_LIMIT: u64 = 155_000; // Calculated effective gas: max(used: 24276, pov: 150736, storage: 0) = 150736
+pub(crate) const ERC20_BALANCE_OF_GAS_LIMIT: u64 = 160_000; // Calculated effective gas: max(used: 24276, pov: 150736, storage: 0) = 150736
 
 #[derive(Debug)]
 pub enum EvmError {
