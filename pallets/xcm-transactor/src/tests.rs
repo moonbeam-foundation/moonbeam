342561377f560155fa3373724e092c327f7952ba
Manuel Mauro <manuel.mauro@protonmail.com>
2025-04-01 12:17:17 +0200

    fix: :bug: fix XCM version conversions tests

--- pallets/xcm-transactor/src/tests.rs
+++ pallets/xcm-transactor/src/tests.rs
@@ -1207,70 +1207,6 @@ fn test_hrmp_manipulator_init() {
 }
 
 #[test]
-fn test_hrmp_manipulator_init_v2_convert_works() {
-	ExtBuilder::default()
-		.with_balances(vec![])
-		.build()
-		.execute_with(|| {
-			// We are gonna use a total weight of 10_100, a tx weight of 100,
-			// and a total fee of 100
-			let total_weight: Weight = 10_100u64.into();
-			let tx_weight: Weight = 100_u64.into();
-			let total_fee = 100u128;
-
-			// Change xcm version
-			CustomVersionWrapper::set_version(2);
-
-			assert_ok!(XcmTransactor::hrmp_manage(
-				RuntimeOrigin::root(),
-				HrmpOperation::InitOpen(HrmpInitParams {
-					para_id: 1u32.into(),
-					proposed_max_capacity: 1,
-					proposed_max_message_size: 1
-				}),
-				CurrencyPayment {
-					currency: Currency::AsMultiLocation(Box::new(xcm::VersionedLocation::from(
-						Location::parent()
-					))),
-					fee_amount: Some(total_fee)
-				},
-				TransactWeights {
-					transact_required_weight_at_most: tx_weight,
-					overall_weight: Some(Limited(total_weight))
-				}
-			));
-
-			let sent_messages = mock::sent_xcm();
-			let (_, sent_message) = sent_messages.first().unwrap();
-			// Lets make sure the message is as expected
-			assert!(sent_message
-				.0
-				.contains(&WithdrawAsset((Location::here(), total_fee).into())));
-			assert!(sent_message.0.contains(&BuyExecution {
-				fees: (Location::here(), total_fee).into(),
-				weight_limit: Limited(total_weight),
-			}));
-			assert!(sent_message.0.contains(&Transact {
-				origin_kind: OriginKind::Native,
-				fallback_max_weight: Some(tx_weight),
-				call: vec![0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0].into(),
-			}));
-
-			// Check message contains the new appendix
-			assert!(sent_message.0.contains(&SetAppendix(Xcm(vec![
-				RefundSurplus,
-				DepositAsset {
-					assets: Wild(AllCounted(1)),
-					beneficiary: Location {
-						parents: 0,
-						interior: [Junction::Parachain(100)].into()
-					}
-				}
-			]))));
-		})
-}
-
-#[test]
 fn test_hrmp_manipulator_init_v3_convert_works() {
 	ExtBuilder::default()
 		.with_balances(vec![])
@@ -1335,7 +1271,7 @@ fn test_hrmp_manipulator_init_v3_convert_works() {
 }
 
 #[test]
-fn test_hrmp_manipulator_init_v5_convert_fails() {
+fn test_hrmp_manipulator_init_v5_convert_works() {
 	ExtBuilder::default()
 		.with_balances(vec![])
 		.build()
@@ -1348,6 +1284,70 @@ fn test_hrmp_manipulator_init_v5_convert_fails() {
 
 			// Change xcm version
 			CustomVersionWrapper::set_version(5);
+
+			assert_ok!(XcmTransactor::hrmp_manage(
+				RuntimeOrigin::root(),
+				HrmpOperation::InitOpen(HrmpInitParams {
+					para_id: 1u32.into(),
+					proposed_max_capacity: 1,
+					proposed_max_message_size: 1
+				}),
+				CurrencyPayment {
+					currency: Currency::AsMultiLocation(Box::new(xcm::VersionedLocation::from(
+						Location::parent()
+					))),
+					fee_amount: Some(total_fee)
+				},
+				TransactWeights {
+					transact_required_weight_at_most: tx_weight,
+					overall_weight: Some(Limited(total_weight))
+				}
+			));
+
+			let sent_messages = mock::sent_xcm();
+			let (_, sent_message) = sent_messages.first().unwrap();
+			// Lets make sure the message is as expected
+			assert!(sent_message
+				.0
+				.contains(&WithdrawAsset((Location::here(), total_fee).into())));
+			assert!(sent_message.0.contains(&BuyExecution {
+				fees: (Location::here(), total_fee).into(),
+				weight_limit: Limited(total_weight),
+			}));
+			assert!(sent_message.0.contains(&Transact {
+				origin_kind: OriginKind::Native,
+				fallback_max_weight: Some(tx_weight),
+				call: vec![0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0].into(),
+			}));
+
+			// Check message contains the new appendix
+			assert!(sent_message.0.contains(&SetAppendix(Xcm(vec![
+				RefundSurplus,
+				DepositAsset {
+					assets: Wild(AllCounted(1)),
+					beneficiary: Location {
+						parents: 0,
+						interior: [Junction::Parachain(100)].into()
+					}
+				}
+			]))));
+		})
+}
+
+#[test]
+fn test_hrmp_manipulator_init_v6_convert_fails() {
+	ExtBuilder::default()
+		.with_balances(vec![])
+		.build()
+		.execute_with(|| {
+			// We are gonna use a total weight of 10_100, a tx weight of 100,
+			// and a total fee of 100
+			let total_weight: Weight = 10_100u64.into();
+			let tx_weight: Weight = 100_u64.into();
+			let total_fee = 100u128;
+
+			// Change xcm version
+			CustomVersionWrapper::set_version(6);
 
 			assert_noop!(
 				XcmTransactor::hrmp_manage(
