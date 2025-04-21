80c3ceeb9763c7acf7ac0f2089c6aec7ac60328d
Rodrigo Quelhas <rodrigo_quelhas@outlook.pt>
2025-04-21 17:01:38 +0100

    fix tests

--- runtime/moonbase/src/lib.rs
+++ runtime/moonbase/src/lib.rs
@@ -1000,7 +1000,7 @@ impl pallet_evm_precompile_proxy::EvmProxyCallFilter for ProxyType {
 						// Note: Checking the presence of the code is not enough because some
 						// precompiles have no code.
 						!recipient_has_code
-							&& precompile_utils::precompile_set::is_precompile_or_fail::<Runtime>(
+							&& !precompile_utils::precompile_set::is_precompile_or_fail::<Runtime>(
 								call.to.0, gas,
 							)?
 					}
