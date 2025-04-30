52ae50c00488ec6830e4cc3449d7aa7951f71e13
Manuel Mauro <manuel.mauro@protonmail.com>
2025-04-30 12:46:09 +0300

    test: :white_check_mark: remove use of toMatchInlineSnapshot out of test context

--- test/suites/dev/moonbase/test-randomness/test-randomness-babe-lottery2.ts
+++ test/suites/dev/moonbase/test-randomness/test-randomness-babe-lottery2.ts
@@ -47,7 +47,7 @@ describeSuite({
         args: [0],
         account: BALTATHAR_ADDRESS,
       });
-      expect(estimatedGas).toMatchInlineSnapshot(`153999n`);
+      expect(estimatedGas).toBe(155784n);
 
       const rawTxn = await context.writePrecompile!({
         precompileName: "Randomness",
@@ -64,7 +64,7 @@ describeSuite({
       fulFillReceipt = await context
         .viem()
         .getTransactionReceipt({ hash: result![1].hash as `0x${string}` });
-      expect(fulFillReceipt.gasUsed).toMatchInlineSnapshot(`82956n`);
+      expect(fulFillReceipt.gasUsed).toBe(82956n);
     });
 
     it({
