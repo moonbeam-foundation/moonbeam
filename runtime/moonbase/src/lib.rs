d0bfc8b82ce564b8e0abb10c41d1078abc05db4d
Manuel Mauro <manuel.mauro@protonmail.com>
2025-05-06 18:44:04 +0300

    Revert "fix: :bug: add weight info for pallet_transaction_payment"
    This reverts commit 02c52378971b77cf28e77750c2709ce024e819c2.

--- runtime/moonbase/src/lib.rs
+++ runtime/moonbase/src/lib.rs
@@ -387,7 +387,7 @@ impl pallet_transaction_payment::Config for Runtime {
 	type WeightToFee = ConstantMultiplier<Balance, ConstU128<{ currency::WEIGHT_FEE }>>;
 	type LengthToFee = LengthToFee;
 	type FeeMultiplierUpdate = FastAdjustingFeeUpdate<Runtime>;
-	type WeightInfo = pallet_transaction_payment::weights::SubstrateWeight<Runtime>;
+	type WeightInfo = ();
 }
 
 impl pallet_sudo::Config for Runtime {
