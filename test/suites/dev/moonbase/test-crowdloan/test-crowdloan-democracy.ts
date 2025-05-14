e7ef43c0ae87103ff07a56947e277f084779c4b1
Rodrigo Quelhas <rodrigo_quelhas@outlook.pt>
2025-05-14 16:30:11 +0100

    fix crowdloan test

--- test/suites/dev/moonbase/test-crowdloan/test-crowdloan-democracy.ts
+++ test/suites/dev/moonbase/test-crowdloan/test-crowdloan-democracy.ts
@@ -1,12 +1,19 @@
 import "@moonbeam-network/api-augment";
 import {
-  describeSuite,
+  describeSuite, DevModeContext, execOpenTechCommitteeProposal,
   expect,
-  fastFowardToNextEvent,
+  fastFowardToNextEvent, filterAndApply,
   maximizeConvictionVotingOf,
-  whiteListTrackNoSend,
+  notePreimage,
 } from "@moonwall/cli";
-import { DEFAULT_GENESIS_BALANCE, ethan, GLMR, GOLIATH_ADDRESS } from "@moonwall/util";
+import {
+  baltathar,
+  DEFAULT_GENESIS_BALANCE,
+  ethan,
+  faith,
+  GLMR,
+  GOLIATH_ADDRESS
+} from "@moonwall/util";
 import type { SubmittableExtrinsic } from "@polkadot/api/promise/types";
 import {
   getAccountPayable,
@@ -14,6 +21,67 @@ import {
   RELAYCHAIN_ARBITRARY_ADDRESS_2,
   VESTING_PERIOD,
 } from "../../../../helpers";
+import {ApiTypes} from "@polkadot/api/types";
+
+export const whiteListTrackNoSend = async <
+  Call extends SubmittableExtrinsic
+>(
+  context: DevModeContext,
+  proposal: string | Call
+) => {
+  const proposalHash =
+    typeof proposal === "string" ? proposal : await notePreimage(context, proposal);
+
+  const proposalLen = (await context.pjsApi.query.preimage.requestStatusFor(proposalHash)).unwrap()
+    .asUnrequested.len;
+  const dispatchWLCall = context.pjsApi.tx.whitelist.dispatchWhitelistedCall(
+    proposalHash,
+    proposalLen,
+    {
+      refTime: 2_000_000_000,
+      proofSize: 4_000_000,
+    }
+  );
+
+  const wLPreimage = await notePreimage(context, dispatchWLCall);
+  const wLPreimageLen = dispatchWLCall.encodedLength - 2;
+  console.log(
+    `📝 DispatchWhitelistedCall preimage noted: ${wLPreimage.slice(0, 6)}...${wLPreimage.slice(
+      -4
+    )}, len: ${wLPreimageLen}`
+  );
+
+  const openGovProposal = await context.pjsApi.tx.referenda
+    .submit(
+      {
+        Origins: { whitelistedcaller: "WhitelistedCaller" },
+      },
+      { Lookup: { hash: wLPreimage, len: wLPreimageLen } },
+      { After: { After: 0 } }
+    )
+    .signAsync(faith);
+  const { result } = await context.createBlock(openGovProposal);
+
+  if (!result?.events) {
+    throw new Error("No events in block");
+  }
+
+  let proposalId: number | undefined;
+  filterAndApply(result.events, "referenda", ["Submitted"], (found) => {
+    proposalId = (found.event as any).data.index.toNumber();
+  });
+
+  if (typeof proposalId === "undefined") {
+    throw new Error("No proposal id found");
+  }
+
+  console.log(`🏛️ Referendum submitted with proposal id: ${proposalId}`);
+  await context.createBlock(context.pjsApi.tx.referenda.placeDecisionDeposit(proposalId));
+
+  const whitelistCall = context.pjsApi.tx.whitelist.whitelistCall(proposalHash);
+  await execOpenTechCommitteeProposal(context, whitelistCall);
+  return { proposalHash, whitelistedHash: wLPreimage };
+};
 
 describeSuite({
   id: "D010805",
@@ -24,35 +92,30 @@ describeSuite({
       id: "T01",
       title: "should be able to initialize through democracy",
       test: async () => {
-        const batchedCalls: SubmittableExtrinsic[] = [];
-        batchedCalls.push(
-          context.polkadotJs().tx.crowdloanRewards.initializeRewardVec([
+        const initializeRewardVec = context.polkadotJs().tx.crowdloanRewards.initializeRewardVec([
             [RELAYCHAIN_ARBITRARY_ADDRESS_1, GOLIATH_ADDRESS, 1_500_000n * GLMR],
             [RELAYCHAIN_ARBITRARY_ADDRESS_2, null, 1_500_000n * GLMR],
-          ])
-        );
+        ]);
 
         const initBlock = await context.polkadotJs().query.crowdloanRewards.initRelayBlock();
-        batchedCalls.push(
-          context
+        const completeInitialization = context
             .polkadotJs()
-            .tx.crowdloanRewards.completeInitialization(initBlock.toBigInt() + VESTING_PERIOD)
-        );
+            .tx.crowdloanRewards.completeInitialization(initBlock.toBigInt() + VESTING_PERIOD);
 
-        // Here we build the utility call
-        const proposal = context.polkadotJs().tx.utility.batchAll(batchedCalls);
-
-        await whiteListTrackNoSend(context, proposal);
+        await whiteListTrackNoSend(context, initializeRewardVec);
+        await whiteListTrackNoSend(context, completeInitialization);
 
         await maximizeConvictionVotingOf(context, [ethan], 0);
+        await maximizeConvictionVotingOf(context, [baltathar], 1);
         await context.createBlock();
 
         await fastFowardToNextEvent(context); // ⏩️ until preparation done
         await fastFowardToNextEvent(context); // ⏩️ until proposal confirmed
         await fastFowardToNextEvent(context); // ⏩️ until proposal enacted
 
-        const isInitialized = await context.polkadotJs().query.crowdloanRewards.initialized();
+        await fastFowardToNextEvent(context); // ⏩️ until proposal 2 is enacted
 
+        const isInitialized = await context.polkadotJs().query.crowdloanRewards.initialized();
         expect(isInitialized.toHuman()).to.be.true;
 
         const reward_info_associated = await getAccountPayable(context, GOLIATH_ADDRESS);
