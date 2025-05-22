19637b41a862efb974e8444f208011195cd07181
Pablo Labarta <pablitolabarta@gmail.com>
2025-05-22 15:31:44 -0300

    update test-precompile-over-pov2 snapshot

--- test/suites/dev/moonbase/test-pov/test-precompile-over-pov2.ts
+++ test/suites/dev/moonbase/test-pov/test-precompile-over-pov2.ts
@@ -72,7 +72,7 @@ describeSuite({
         });
 
         const { result, block } = await context.createBlock(rawSigned);
-        expect(block.proofSize).toMatchInlineSnapshot(`109578`);
+        expect(block.proofSize).toMatchInlineSnapshot(`109332`);
         expect(result?.successful).to.equal(true);
       },
     });
