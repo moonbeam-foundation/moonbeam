3d6d19a0d15d3dbd74990afb0f119acacb94e4ec
RomarQ <RomarQ@users.noreply.github.com>
2025-05-23 11:52:12 +0000

    deploy: 98c88d936c49d77d4e3f378e17920a884cd43248

--- trait.impl/frame_support/migrations/trait.FailedMigrationHandler.js
+++
@@ -1,5 +0,0 @@
-(function() {var implementors = {
-"moonbase_runtime":[["impl FailedMigrationHandler for <a class=\"struct\" href=\"moonbase_runtime/struct.EnterMaintenanceModeOnFailedMigration.html\" title=\"struct moonbase_runtime::EnterMaintenanceModeOnFailedMigration\">EnterMaintenanceModeOnFailedMigration</a>"]],
-"moonbeam_runtime":[["impl FailedMigrationHandler for <a class=\"struct\" href=\"moonbeam_runtime/struct.EnterMaintenanceModeOnFailedMigration.html\" title=\"struct moonbeam_runtime::EnterMaintenanceModeOnFailedMigration\">EnterMaintenanceModeOnFailedMigration</a>"]],
-"moonriver_runtime":[["impl FailedMigrationHandler for <a class=\"struct\" href=\"moonriver_runtime/struct.EnterMaintenanceModeOnFailedMigration.html\" title=\"struct moonriver_runtime::EnterMaintenanceModeOnFailedMigration\">EnterMaintenanceModeOnFailedMigration</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
