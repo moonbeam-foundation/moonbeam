06e6c153ef21f931667dd60fcebf40314553d081
Tarek Mohamed Abdalla <tarekkma@gmail.com>
2025-05-23 14:08:19 +0300

    add try catch

--- test/suites/dev/common/test-pov-limit/test-pov-limit.ts
+++ test/suites/dev/common/test-pov-limit/test-pov-limit.ts
@@ -25,44 +25,46 @@ describeSuite({
 
       const accounts: [string, string][] = [];
 
-      let nonce = (await context.polkadotJs().query.system.account(BALTATHAR_ADDRESS)).nonce.toNumber();
+      let nonce = (
+        await context.polkadotJs().query.system.account(BALTATHAR_ADDRESS)
+      ).nonce.toNumber();
 
-      for (let i = 0; i < 15; i++) {
+      for (let i = 0; i < 25; i++) {
         const randomPrivateKey = generatePrivateKey();
         const randomAddress = privateKeyToAccount(randomPrivateKey as `0x${string}`).address;
         accounts.push([randomPrivateKey as `0x${string}`, randomAddress]);
 
-        context.polkadotJs().tx.balances.transferKeepAlive(randomAddress, 1_000_000_000_000_000_000n * 1000n).signAndSend(
-          baltathar,
-          {
+        context
+          .polkadotJs()
+          .tx.balances.transferKeepAlive(randomAddress, 1_000_000_000_000_000_000n * 1000n)
+          .signAndSend(baltathar, {
             nonce: nonce++,
-          }
-        );
+          });
       }
 
       await context.createBlock();
 
-
       for (const [privateKey, address] of accounts) {
+        try {
+          const gasEstimate = await context.viem().estimateGas({
+            account: address,
+            to: contractAddress,
+            value: 0n,
+            data: callData,
+          });
 
-        const gasEstimate = await context.viem().estimateGas({
-          account: address,
-          to: contractAddress,
-          value: 0n,
-          data: callData,
-        });
+          const signer = new Wallet(privateKey, context.ethers().provider);
 
-        const signer = new Wallet(privateKey, context.ethers().provider);
-
-        const rawSigned = await signer.sendTransaction({
-          to: contractAddress,
-          data: callData,
-          gasLimit: gasEstimate,
-        });
-
-        await new Promise(resolve => setTimeout(resolve, 1));
+          await signer.sendTransaction({
+            to: contractAddress,
+            data: callData,
+            gasLimit: gasEstimate,
+          });
+        } catch (error) {
+          console.log(`Error: ${error}`);
+        }
       }
-
+      await new Promise((resolve) => setTimeout(resolve, 50));
     }
 
     it({
@@ -79,21 +81,23 @@ describeSuite({
 
         const blockWeight = await context.polkadotJs().query.system.blockWeight();
 
-        const proofSize = blockWeight.normal.proofSize.toBigInt() +
+        const proofSize =
+          blockWeight.normal.proofSize.toBigInt() +
           blockWeight.operational.proofSize.toBigInt() +
           blockWeight.mandatory.proofSize.toBigInt();
 
         const fullPov = 10 * 1024 * 1024;
-        const floatPov = parseFloat(proofSize.toString());
-        console.log(`Proof size: ${proofSize} bytes (${(floatPov / fullPov) * 100}% of FullPov 10MB)`);
+        const floatPov = Number.parseFloat(proofSize.toString());
+        console.log(
+          `Proof size: ${proofSize} bytes (${(floatPov / fullPov) * 100}% of FullPov 10MB)`
+        );
         console.log(`Proof size: ${floatPov / 1024} KB`);
         console.log(`Proof size: ${floatPov / 1024 / 1024} MB`);
 
         // 75% of 10MB
         const limit = 10 * 1024 * 1024 * 0.75;
         expect(floatPov).toBeGreaterThanOrEqual(limit);
-      }
+      },
     });
-
   },
 });
