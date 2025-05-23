4c633989c1bf1c83e8f37baeaf11dcb0f2b397de
Rodrigo Quelhas <rodrigo_quelhas@outlook.pt>
2025-05-23 12:44:11 +0100

    bump spec_version to 3601

--- runtime/moonbase/src/lib.rs
+++ runtime/moonbase/src/lib.rs
@@ -201,7 +201,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("moonbase"),
 	impl_name: create_runtime_str!("moonbase"),
 	authoring_version: 4,
-	spec_version: 3600,
+	spec_version: 3601,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 3,
