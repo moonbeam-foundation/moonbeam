a75e109b4d917f0f6edc6aa55e5cafda22f3c7a4
Rodrigo Quelhas <rodrigo_quelhas@outlook.pt>
2025-05-23 09:27:44 +0100

    bump spec_version to 3701

--- runtime/moonriver/src/lib.rs
+++ runtime/moonriver/src/lib.rs
@@ -215,7 +215,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: Cow::Borrowed("moonriver"),
 	impl_name: Cow::Borrowed("moonriver"),
 	authoring_version: 3,
-	spec_version: 3700,
+	spec_version: 3701,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 3,
