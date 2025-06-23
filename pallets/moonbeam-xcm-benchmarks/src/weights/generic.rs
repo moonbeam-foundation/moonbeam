b88732bb4d62a292804b27472e64b726a4291d5f
Rodrigo Quelhas <rodrigo_quelhas@outlook.pt>
2025-06-23 09:51:56 +0100

    clean-up

--- pallets/moonbeam-xcm-benchmarks/src/weights/generic.rs
+++ pallets/moonbeam-xcm-benchmarks/src/weights/generic.rs
@@ -391,26 +391,26 @@ impl<T: frame_system::Config> WeightInfo for SubstrateWeight<T> {
 		// Minimum execution time: 500_000_000_000 picoseconds.
 		Weight::from_parts(500_000_000_000, 0)
 	}
-    /// Storage: Benchmark Override (r:0 w:0)
-    /// Proof Skipped: Benchmark Override (max_values: None, max_size: None, mode: Measured)
-    fn universal_origin() -> Weight {
-        // Proof Size summary in bytes:
-        //  Measured:  `0`
-        //  Estimated: `0`
-        // Minimum execution time: 500_000_000_000 picoseconds.
-        Weight::from_parts(2_705_000, 1489)
-            .saturating_add(T::DbWeight::get().reads(1))
-    }
-    /// Storage: Benchmark Override (r:0 w:0)
-    /// Proof Skipped: Benchmark Override (max_values: None, max_size: None, mode: Measured)
-    /// The range of component `x` is `[1, 1000]`.
-    fn export_message() -> Weight {
-        // Proof Size summary in bytes:
-        //  Measured:  `0`
-        //  Estimated: `0`
-        // Minimum execution time: 18_446_744_073_709_551_000 picoseconds.
-        Weight::from_parts(500_000_000_000, 0)
-    }
+	/// Storage: Benchmark Override (r:0 w:0)
+	/// Proof Skipped: Benchmark Override (max_values: None, max_size: None, mode: Measured)
+	fn universal_origin() -> Weight {
+		// Proof Size summary in bytes:
+		//  Measured:  `0`
+		//  Estimated: `0`
+		// Minimum execution time: 500_000_000_000 picoseconds.
+		Weight::from_parts(2_705_000, 1489)
+			.saturating_add(T::DbWeight::get().reads(1))
+	}
+	/// Storage: Benchmark Override (r:0 w:0)
+	/// Proof Skipped: Benchmark Override (max_values: None, max_size: None, mode: Measured)
+	/// The range of component `x` is `[1, 1000]`.
+	fn export_message() -> Weight {
+		// Proof Size summary in bytes:
+		//  Measured:  `0`
+		//  Estimated: `0`
+		// Minimum execution time: 18_446_744_073_709_551_000 picoseconds.
+		Weight::from_parts(500_000_000_000, 0)
+	}
 	fn set_fees_mode() -> Weight {
 		// Proof Size summary in bytes:
 		//  Measured:  `0`
