8eddc85515779e993d00ce7377e4cfea5448b24c
RomarQ <RomarQ@users.noreply.github.com>
2025-06-23 12:44:17 +0000

    deploy: b943e2ee099b7074c35f793ff12374bf7a24eb6d

---
+++ moonbase_runtime/moonbase_xcm_weights/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"mod":["fungible","generic"],"struct":["XcmWeight"],"trait":["WeightInfo"]};
--- trait.impl/moonbeam_xcm_benchmarks/weights/generic/trait.WeightInfo.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {
-"moonbeam_xcm_benchmarks":[]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
