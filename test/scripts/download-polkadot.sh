a0c88270d1e8cb867c0adccc1b089d5e97974f37
Rodrigo Quelhas <rodrigo_quelhas@outlook.pt>
2025-06-25 11:01:02 +0100

    revert chnages to download-polkadot.sh

--- test/scripts/download-polkadot.sh
+++ test/scripts/download-polkadot.sh
@@ -6,9 +6,6 @@ set -e
 # Grab Polkadot version
 branch=$(egrep -o '/polkadot.*#([^\"]*)' $(dirname $0)/../../Cargo.lock | head -1 | sed 's/.*release-//#')
 polkadot_release=$(echo $branch | sed 's/#.*//' | sed 's/\/polkadot-sdk?branch=moonbeam-polkadot-//')
-# TODO: Remove once we have updated to polkadot-stable2503
-# Context: https://github.com/paritytech/polkadot-sdk/pull/7760
-polkadot_release=stable2503-5
 
 # Always run the commands from the "test" dir
 cd $(dirname $0)/..
