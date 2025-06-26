28569cb34cac8351e3a12f7ea90bf453c70d369c
Éloïs <c@elo.tf>
2025-06-26 17:17:15 +0200

    bump spec_version to 3702

--- runtime/moonbase/src/lib.rs
+++ runtime/moonbase/src/lib.rs
@@ -210,7 +210,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: Cow::Borrowed("moonbase"),
 	impl_name: Cow::Borrowed("moonbase"),
 	authoring_version: 4,
-	spec_version: 3701,
+	spec_version: 3702,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 3,
