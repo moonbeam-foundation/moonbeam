e78ab294f8f94cc39c2af2e0923cec3ceb935367
Manuel Mauro <manuel.mauro@protonmail.com>
2025-07-03 12:17:25 +0300

    Update runtime spec version to 3900

--- runtime/moonbase/src/lib.rs
+++ runtime/moonbase/src/lib.rs
@@ -215,7 +215,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: Cow::Borrowed("moonbase"),
 	impl_name: Cow::Borrowed("moonbase"),
 	authoring_version: 4,
-	spec_version: 3800,
+	spec_version: 3900,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 3,
