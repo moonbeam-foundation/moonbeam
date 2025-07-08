675111658676c2cdf5b032175306fad4bdc77062
Éloïs <c@elo.tf>
2025-07-08 21:13:17 +0200

    increase timeout

--- test/suites/dev/moonbase/test-pov/test-pov-limit.ts
+++ test/suites/dev/moonbase/test-pov/test-pov-limit.ts
@@ -64,7 +64,7 @@ describeSuite({
           console.log(`Error: ${error}`);
         }
       }
-      await new Promise((resolve) => setTimeout(resolve, 5_000));
+      await new Promise((resolve) => setTimeout(resolve, 50_000));
     });
 
     it({
