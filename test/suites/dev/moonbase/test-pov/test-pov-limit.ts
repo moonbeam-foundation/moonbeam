a5a062f9a8715f4d5e8334ee0e28cea12740b24f
Éloïs <c@elo.tf>
2025-07-09 17:16:11 +0200

    fully rework pov test to not rely on timeouts

--- test/suites/dev/moonbase/test-pov/test-pov-limit.ts
+++ test/suites/dev/moonbase/test-pov/test-pov-limit.ts
@@ -1,81 +1,73 @@
 import { beforeAll, deployCreateCompiledContract, describeSuite, expect } from "@moonwall/cli";
-import { encodeFunctionData } from "viem";
-import { deployHeavyContracts, getBlockDetails } from "../../../../helpers";
-import { baltathar, BALTATHAR_ADDRESS } from "@moonwall/util";
-import { Wallet } from "ethers";
-import { generatePrivateKey, privateKeyToAccount } from "viem/accounts";
+import { type Abi, encodeFunctionData } from "viem";
+import { deployHeavyContracts, type HeavyContract } from "../../../../helpers";
+import { ALITH_ADDRESS, createEthersTransaction } from "@moonwall/util";
 
 describeSuite({
   id: "D0121111",
   title: "PoV Limit",
   foundationMethods: "dev",
   testCases: ({ context, it, log }) => {
+
+    let contractCallForwarderAddress: `0x${string}`;
+    let contracts: HeavyContract[];
+    let callData: `0x${string}`;
+    let proxyAbi: Abi;
+
+    // The goal of this test is to fill the max PoV limit with normal transactions (75% of 10MB)
+    const MAX_POV_LIMIT = 10 * 1024 * 1024 * 0.75;
+    const MAX_CONTRACTS = Math.floor(MAX_POV_LIMIT / 24_000);
+
     beforeAll(async function () {
-      const { contractAddress, abi } = await deployCreateCompiledContract(context, "CallForwarder");
+      const { contractAddress: _contractAddress, abi } = await deployCreateCompiledContract(context, "CallForwarder");
+      contractCallForwarderAddress = _contractAddress;
+      proxyAbi = abi;
 
-      const MAX_CONTRACTS = 40;
-
-      const contracts = await deployHeavyContracts(context, 6000, 6000 + MAX_CONTRACTS);
-
-      const callData = encodeFunctionData({
-        abi: abi,
-        functionName: "callRange",
-        args: [contracts[0].account, contracts[MAX_CONTRACTS].account],
-      });
-
-      const accounts: [string, string][] = [];
-
-      let nonce = (
-        await context.polkadotJs().query.system.account(BALTATHAR_ADDRESS)
-      ).nonce.toNumber();
-
-      for (let i = 0; i < 25; i++) {
-        const randomPrivateKey = generatePrivateKey();
-        const randomAddress = privateKeyToAccount(randomPrivateKey as `0x${string}`).address;
-        accounts.push([randomPrivateKey as `0x${string}`, randomAddress]);
-
-        context
-          .polkadotJs()
-          .tx.balances.transferKeepAlive(randomAddress, 1_000_000_000_000_000_000n * 1000n)
-          .signAndSend(baltathar, {
-            nonce: nonce++,
-          });
-      }
+      // Deploy heavy contracts (test won't use more than what is needed for reaching max pov)
+      contracts = await deployHeavyContracts(context, 6000, 6000 + MAX_CONTRACTS);
 
       await context.createBlock();
-
-      for (const [privateKey, address] of accounts) {
-        try {
-          const gasEstimate = await context.viem().estimateGas({
-            account: address,
-            to: contractAddress,
-            value: 0n,
-            data: callData,
-          });
-
-          const signer = new Wallet(privateKey, context.ethers().provider);
-
-          await signer.sendTransaction({
-            to: contractAddress,
-            data: callData,
-            gasLimit: gasEstimate,
-          });
-        } catch (error) {
-          console.log(`Error: ${error}`);
-        }
-      }
-      await new Promise((resolve) => setTimeout(resolve, 50_000));
     });
 
     it({
       id: "T01",
-      title: "Test PoV Limit",
+      title: "Should be able to fill 75% of block proof size limit with normal transactions",
       test: async function () {
-        const res = await context.createBlock();
+        const HEAVY_CONTRACTS_PER_TX = 40;
+        const transactions: `0x${string}`[] = [];
 
-        const blockDetails = await getBlockDetails(context.polkadotJs(), res.block.hash);
-        const txCount = blockDetails.txWithEvents.length;
-        console.log(`Number of added extrinsics: ${txCount}`);
+        // Get initial nonce for Alith
+        let nonce = await context.viem().getTransactionCount({address: ALITH_ADDRESS});
+
+        // Split into batches of HEAVY_CONTRACTS_PER_TX contracts
+        for (let i = 0; i < MAX_CONTRACTS; i += HEAVY_CONTRACTS_PER_TX) {
+          const endIndex = Math.min(i + HEAVY_CONTRACTS_PER_TX, MAX_CONTRACTS);
+          
+          const callData = encodeFunctionData({
+            abi: proxyAbi,
+            functionName: "callRange", 
+            args: [contracts[i].account, contracts[endIndex].account],
+          });
+
+          const gasEstimate = await context.viem().estimateGas({
+            account: ALITH_ADDRESS,
+            to: contractCallForwarderAddress,
+            value: 0n,
+            data: callData,
+          });
+
+          const rawSigned = await createEthersTransaction(context, {
+            to: contractCallForwarderAddress,
+            data: callData,
+            txnType: "eip1559",
+            gasLimit: gasEstimate,
+            nonce: nonce++
+          });
+
+          transactions.push(rawSigned);
+        }
+
+        const { result, block } = await context.createBlock(transactions);
 
         const blockWeight = await context.polkadotJs().query.system.blockWeight();
 
@@ -84,20 +76,13 @@ describeSuite({
           blockWeight.operational.proofSize.toBigInt() +
           blockWeight.mandatory.proofSize.toBigInt();
 
-        const fullPov = 10 * 1024 * 1024;
-        const floatPov = Number.parseFloat(proofSize.toString());
-        console.log(
-          `Proof size: ${proofSize} bytes (${(floatPov / fullPov) * 100}% of FullPov 10MB)`
-        );
-        console.log(`Proof size: ${floatPov / 1024} KB`);
-        console.log(`Proof size: ${floatPov / 1024 / 1024} MB`);
-
-        // 75% of 85% of 10MB
-        const limit = 10 * 1024 * 1024 * 0.75 * 0.85;
+        log(`Consumed block proofSize: ${proofSize} / ${MAX_POV_LIMIT}`);
+        // In practice the total block proof size consumed should be greather than 75%
+        // because some proof size is consumed outisde of normal transactions (e.g. on_initialize stuff)
         expect(
-          floatPov,
-          `Included ${txCount} extrinsics did not fit in the PoV limit`
-        ).toBeGreaterThanOrEqual(limit);
+          proofSize,
+          `Proof size is not greater than ${MAX_POV_LIMIT}`
+        ).toBeGreaterThanOrEqual(MAX_POV_LIMIT);
       },
     });
   },
