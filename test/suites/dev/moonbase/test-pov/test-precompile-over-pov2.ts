03c17cdc0efc6861b85393f79dbfbf1e6fd5e2cf
Rodrigo Quelhas <rodrigo_quelhas@outlook.pt>
2025-07-18 22:17:40 +0100

    fix test

--- test/suites/dev/moonbase/test-pov/test-precompile-over-pov2.ts
+++ test/suites/dev/moonbase/test-pov/test-precompile-over-pov2.ts
@@ -72,7 +72,7 @@ describeSuite({
         });
 
         const { result, block } = await context.createBlock(rawSigned);
-        expect(block.proofSize).toMatchInlineSnapshot(`109340`);
+        expect(block.proofSize).toMatchInlineSnapshot(`109307`);
         expect(result?.successful).to.equal(true);
       },
     });
