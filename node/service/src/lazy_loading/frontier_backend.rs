08489d937b47143607f33cec9ec54c503ac1bab9
Rodrigo Quelhas <rodrigo_quelhas@outlook.pt>
2025-07-30 22:23:51 +0100

    improvement

--- node/service/src/lazy_loading/frontier_backend.rs
+++ node/service/src/lazy_loading/frontier_backend.rs
@@ -14,16 +14,41 @@
 // You should have received a copy of the GNU General Public License
 // along with Moonbeam.  If not, see <http://www.gnu.org/licenses/>.
 
+//! Lazy loading implementation for Frontier backend.
+//!
+//! This module provides a wrapper around the standard Frontier backend that enables
+//! lazy loading functionality. It intercepts Ethereum-specific queries and routes them
+//! through the RPC client to fetch data on-demand from a remote node.
+
 use fc_api::{LogIndexerBackend, TransactionMetadata};
 use serde::de::DeserializeOwned;
 use sp_core::H256;
 use sp_runtime::traits::{Block as BlockT, Header};
 use std::sync::Arc;
 
+/// A wrapper around the Frontier backend that supports lazy loading.
+///
+/// This backend intercepts Ethereum-specific queries (block hash lookups, transaction metadata)
+/// and fetches the data from a remote RPC node when needed, while delegating other operations
+/// to the underlying Frontier backend.
 #[derive(Clone)]
 pub struct LazyLoadingFrontierBackend<Block: BlockT> {
 	pub(crate) rpc_client: Arc<super::rpc_client::RPC>,
 	pub(crate) frontier_backend: Arc<dyn fc_api::Backend<Block> + Send + Sync>,
+}
+
+impl<Block: BlockT + DeserializeOwned> LazyLoadingFrontierBackend<Block>
+where
+	<Block::Header as Header>::Number: From<u32>,
+{
+	fn get_substrate_block_hash(
+		&self,
+		block_number: <Block::Header as Header>::Number,
+	) -> Result<Option<Block::Hash>, String> {
+		self.rpc_client
+			.block_hash::<Block>(Some(block_number))
+			.map_err(|e| format!("failed to get substrate block hash: {:?}", e))
+	}
 }
 
 #[async_trait::async_trait]
@@ -32,52 +57,43 @@ where
 	<Block::Header as Header>::Number: From<u32>,
 {
 	async fn block_hash(&self, eth_block_hash: &H256) -> Result<Option<Vec<Block::Hash>>, String> {
-		let result = self
+		let block = self
 			.rpc_client
 			.block_by_hash(eth_block_hash, false)
 			.map_err(|e| format!("failed to get block by hash: {:?}", e))?;
 
-		if let Some(block) = result {
-			let block_number: <Block::Header as Header>::Number =
-				block.header.number.as_u32().into();
-
-			let substrate_block_hash = self
-				.rpc_client
-				.block_hash::<Block>(Some(block_number))
-				.map_err(|e| format!("failed to get block: {:?}", e))?;
-
-			return Ok(substrate_block_hash.map(|h| vec![h]));
+		match block {
+			Some(block) => {
+				let block_number = block.header.number.as_u32().into();
+				let substrate_block_hash = self.get_substrate_block_hash(block_number)?;
+				Ok(substrate_block_hash.map(|h| vec![h]))
+			}
+			None => Ok(None),
 		}
-
-		Ok(None)
 	}
 
 	async fn transaction_metadata(
 		&self,
 		eth_transaction_hash: &H256,
 	) -> Result<Vec<TransactionMetadata<Block>>, String> {
-		let result = self
+		let transaction = self
 			.rpc_client
 			.transaction_by_hash(eth_transaction_hash)
 			.map_err(|e| format!("failed to get transaction by hash: {:?}", e))?;
 
-		if let Some(tx) = result {
-			let block_number: <Block::Header as Header>::Number =
-				tx.block_number.unwrap_or_default().as_u32().into();
+		match transaction {
+			Some(tx) => {
+				let block_number = tx.block_number.unwrap_or_default().as_u32().into();
+				let substrate_block_hash = self.get_substrate_block_hash(block_number)?;
 
-			let substrate_block_hash = self
-				.rpc_client
-				.block_hash::<Block>(Some(block_number))
-				.map_err(|e| format!("failed to get block: {:?}", e))?;
-
-			return Ok(vec![TransactionMetadata::<Block> {
-				ethereum_index: tx.transaction_index.unwrap_or_default().as_u32(),
-				ethereum_block_hash: tx.block_hash.unwrap_or_default(),
-				substrate_block_hash: substrate_block_hash.unwrap_or_default(),
-			}]);
+				Ok(vec![TransactionMetadata::<Block> {
+					ethereum_index: tx.transaction_index.unwrap_or_default().as_u32(),
+					ethereum_block_hash: tx.block_hash.unwrap_or_default(),
+					substrate_block_hash: substrate_block_hash.unwrap_or_default(),
+				}])
+			}
+			None => Ok(vec![]),
 		}
-
-		Ok(vec![])
 	}
 
 	fn log_indexer(&self) -> &dyn LogIndexerBackend<Block> {
