9748e28a0536f40f7712c336d9a84aea0e490744
Manuel Mauro <manuel.mauro@protonmail.com>
2025-08-13 19:25:51 +0300

    test: :white_check_mark: update snapshot

--- test/suites/dev/moonbase/test-randomness/test-randomness-vrf-lottery4.ts
+++ test/suites/dev/moonbase/test-randomness/test-randomness-vrf-lottery4.ts
@@ -20,7 +20,7 @@ describeSuite({
         value: 1n * GLMR,
       });
       log("Estimated Gas for startLottery", estimatedGas);
-      expect(estimatedGas).to.equal(111657n);
+      expect(estimatedGas).to.equal(112323n);
 
       await context.writeContract!({
         contractAddress: lotteryContract,
