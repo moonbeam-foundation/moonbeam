95c6b3149df7da21c20839af4707302000cab4f6
Rodrigo Quelhas <rodrigo_quelhas@outlook.pt>
2025-08-13 20:48:02 +0100

    fix ts tests

---
+++ test/suites/dev/moonbase/test-staking/test-inflation-distribution-config.ts
@@ -0,0 +1,77 @@
+import "@moonbeam-network/api-augment";
+import { describeSuite, expect } from "@moonwall/cli";
+import { ZERO_ADDRESS, dorothy, charleth } from "@moonwall/util";
+
+const TWENTY_PERCENT = 20;
+
+describeSuite({
+  id: "D023450",
+  title: "Staking - setInflationDistributionConfig",
+  foundationMethods: "dev",
+  testCases: ({ context, it }) => {
+    it({
+      id: "T01",
+      title: "should be initialized at address zero",
+      test: async function () {
+        const inflationDistributionConfig = await context
+          .polkadotJs()
+          .query.parachainStaking.inflationDistributionInfo();
+        expect(inflationDistributionConfig[0].account.toString()).to.equal(ZERO_ADDRESS);
+        expect(inflationDistributionConfig[0].percent.toNumber()).to.equal(30);
+
+        // Treasury account
+        expect(inflationDistributionConfig[1].account.toString()).to.equal(ZERO_ADDRESS);
+        expect(inflationDistributionConfig[1].percent.toNumber()).to.equal(0);
+      },
+    });
+
+    it({
+      id: "T02",
+      title: "should be able set the parachain bond reserve percent with sudo",
+      test: async function () {
+        // should be able to register the genesis account
+        const { result } = await context.createBlock(
+          context.polkadotJs().tx.sudo.sudo(
+            context.polkadotJs().tx.parachainStaking.setInflationDistributionConfig([
+              {
+                account: dorothy.address,
+                percent: TWENTY_PERCENT,
+              },
+              {
+                account: charleth.address,
+                percent: TWENTY_PERCENT,
+              },
+            ])
+          )
+        );
+        expect(result!.successful).to.be.true;
+
+        const inflationDistributionConfig = await context
+          .polkadotJs()
+          .query.parachainStaking.inflationDistributionInfo();
+        expect(inflationDistributionConfig[0].percent.toBigInt()).to.equal(20n);
+      },
+    });
+
+    it({
+      id: "T03",
+      title: "should NOT be able set the parachain bond reserve percent without sudo",
+      test: async function () {
+        const { result } = await context.createBlock(
+          context.polkadotJs().tx.parachainStaking.setInflationDistributionConfig([
+            {
+              account: dorothy.address,
+              percent: TWENTY_PERCENT,
+            },
+            {
+              account: charleth.address,
+              percent: TWENTY_PERCENT,
+            },
+          ])
+        );
+        expect(result!.successful).to.be.false;
+        expect(result!.error!.name).to.equal("BadOrigin");
+      },
+    });
+  },
+});
