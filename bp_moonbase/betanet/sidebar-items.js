e1fdd2a597011d48f14f2700f74c4dbb528a3a45
RomarQ <RomarQ@users.noreply.github.com>
2025-09-08 12:35:10 +0000

    deploy: 6d9e07a739f8f261ebc5304a2fcd3dd9caa87c48

--- bp_moonbase/betanet/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-window.SIDEBAR_ITEMS = {"constant":["PARACHAIN_ID","WITH_BRIDGE_MESSAGES_PALLET_NAME"],"struct":["Betanet"],"type":["AccountId","Balance","BlockNumber","Hash","Header","Signature"]};
--- trait.impl/bp_runtime/chain/trait.Chain.js
+++ trait.impl/bp_runtime/chain/trait.Chain.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["bp_moonbase",[["impl Chain for <a class=\"struct\" href=\"bp_moonbase/betanet/struct.Betanet.html\" title=\"struct bp_moonbase::betanet::Betanet\">Betanet</a>"],["impl Chain for <a class=\"struct\" href=\"bp_moonbase/stagenet/struct.Stagenet.html\" title=\"struct bp_moonbase::stagenet::Stagenet\">Stagenet</a>"]]],["bp_moonbeam",[["impl Chain for <a class=\"struct\" href=\"bp_moonbeam/bp_kusama/struct.Kusama.html\" title=\"struct bp_moonbeam::bp_kusama::Kusama\">Kusama</a>"],["impl Chain for <a class=\"struct\" href=\"bp_moonbeam/struct.Moonbeam.html\" title=\"struct bp_moonbeam::Moonbeam\">Moonbeam</a>"]]],["bp_moonriver",[["impl Chain for <a class=\"struct\" href=\"bp_moonriver/bp_polkadot/struct.Polkadot.html\" title=\"struct bp_moonriver::bp_polkadot::Polkadot\">Polkadot</a>"],["impl Chain for <a class=\"struct\" href=\"bp_moonriver/struct.Moonriver.html\" title=\"struct bp_moonriver::Moonriver\">Moonriver</a>"]]]]);
+    var implementors = Object.fromEntries([["bp_moonbeam",[["impl Chain for <a class=\"struct\" href=\"bp_moonbeam/bp_kusama/struct.Kusama.html\" title=\"struct bp_moonbeam::bp_kusama::Kusama\">Kusama</a>"],["impl Chain for <a class=\"struct\" href=\"bp_moonbeam/struct.Moonbeam.html\" title=\"struct bp_moonbeam::Moonbeam\">Moonbeam</a>"]]],["bp_moonriver",[["impl Chain for <a class=\"struct\" href=\"bp_moonriver/bp_polkadot/struct.Polkadot.html\" title=\"struct bp_moonriver::bp_polkadot::Polkadot\">Polkadot</a>"],["impl Chain for <a class=\"struct\" href=\"bp_moonriver/struct.Moonriver.html\" title=\"struct bp_moonriver::Moonriver\">Moonriver</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[316,299,317]}
+//{"start":57,"fragment_lengths":[298,317]}
