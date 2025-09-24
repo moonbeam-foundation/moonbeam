49a8f1e4bba3fad255efbf1f728a467c5cfb323b
Manuel Mauro <manuel.mauro@protonmail.com>
2025-09-24 16:52:03 +0300

    test: :white_check_mark: update one last snapshot

--- test/suites/dev/moonbase/test-randomness/test-randomness-babe-lottery2.ts
+++ test/suites/dev/moonbase/test-randomness/test-randomness-babe-lottery2.ts
@@ -64,7 +64,7 @@ describeSuite({
       fulFillReceipt = await context
         .viem()
         .getTransactionReceipt({ hash: result![1].hash as `0x${string}` });
-      expect(fulFillReceipt.gasUsed).to.equal(91420n);
+      expect(fulFillReceipt.gasUsed).to.equal(89420n);
     });
 
     it({
