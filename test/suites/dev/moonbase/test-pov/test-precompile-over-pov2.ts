cc7f0d0a6d51a815eda1276ff28d247e50df41cd
Manuel Mauro <manuel.mauro@protonmail.com>
2025-09-29 17:27:21 +0300

    test: :white_check_mark: update inline snapshots

--- test/suites/dev/moonbase/test-pov/test-precompile-over-pov2.ts
+++ test/suites/dev/moonbase/test-pov/test-precompile-over-pov2.ts
@@ -72,7 +72,7 @@ describeSuite({
         });
 
         const { result, block } = await context.createBlock(rawSigned);
-        expect(block.proofSize).toMatchInlineSnapshot(`107861`);
+        expect(block.proofSize).toMatchInlineSnapshot(`107828`);
         expect(result?.successful).to.equal(true);
       },
     });
