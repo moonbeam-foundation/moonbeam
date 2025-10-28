c3377b237d39ac4e9cfe9fa85d4ded242e99716d
Rodrigo Quelhas <rodrigo_quelhas@outlook.pt>
2025-10-28 11:48:47 +0000

    fix ts tests

--- test/suites/dev/moonbase/test-randomness/test-randomness-babe-lottery2.ts
+++ test/suites/dev/moonbase/test-randomness/test-randomness-babe-lottery2.ts
@@ -64,7 +64,7 @@ describeSuite({
       fulFillReceipt = await context
         .viem()
         .getTransactionReceipt({ hash: result![1].hash as `0x${string}` });
-      expect(fulFillReceipt.gasUsed).to.equal(89420n);
+      expect(fulFillReceipt.gasUsed).to.equal(89377n);
     });
 
     it({
