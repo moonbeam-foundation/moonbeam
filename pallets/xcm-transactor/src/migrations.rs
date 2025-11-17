36c2727ebb3eb1e5aa2096e1d5734259f371f75b
Rodrigo Quelhas <rodrigo_quelhas@outlook.pt>
2025-11-17 12:26:57 +0000

    add migration

--- pallets/xcm-transactor/src/migrations.rs
+++ pallets/xcm-transactor/src/migrations.rs
@@ -13,3 +13,31 @@
 
 // You should have received a copy of the GNU General Public License
 // along with Moonbeam.  If not, see <http://www.gnu.org/licenses/>.
+
+//! A module that is responsible for migration of storage.
+
+use crate::{relay_indices::RelayChainIndices, Config, RelayIndices};
+use cumulus_primitives_core::Weight;
+use frame_support::traits::{Get, OnRuntimeUpgrade};
+use sp_std::marker::PhantomData;
+
+/// Migrates the pallet storage to v1.
+pub struct UpdateRelayChainIndices<T, RelayChainIndices>(PhantomData<(T, RelayChainIndices)>);
+
+impl<T: Config, R: Get<RelayChainIndices>> OnRuntimeUpgrade for UpdateRelayChainIndices<T, R> {
+	fn on_runtime_upgrade() -> Weight {
+		RelayIndices::<T>::set(R::get());
+
+		T::DbWeight::get().reads_writes(1, 1)
+	}
+
+	#[cfg(feature = "try-runtime")]
+	fn pre_upgrade() -> Result<sp_std::vec::Vec<u8>, sp_runtime::DispatchError> {
+		Ok(vec![])
+	}
+
+	#[cfg(feature = "try-runtime")]
+	fn post_upgrade(state: sp_std::vec::Vec<u8>) -> Result<(), sp_runtime::DispatchError> {
+		Ok(())
+	}
+}
