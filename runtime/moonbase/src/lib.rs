db5ce90a44709cd58b1e4545ec4825afd810c355
Rodrigo Quelhas <22591718+RomarQ@users.noreply.github.com>
2025-11-27 18:55:45 +0000

    revert moonbase change

--- runtime/moonbase/src/lib.rs
+++ runtime/moonbase/src/lib.rs
@@ -852,7 +852,7 @@ impl pallet_parachain_staking::Config for Runtime {
 	/// Maximum delegations per delegator
 	type MaxDelegationsPerDelegator = ConstU32<100>;
 	/// Minimum stake required to be reserved to be a candidate
-	type MinCandidateStk = ConstU128<{ 100 * currency::UNIT * currency::SUPPLY_FACTOR }>;
+	type MinCandidateStk = ConstU128<{ 500 * currency::UNIT * currency::SUPPLY_FACTOR }>;
 	/// Minimum stake required to be reserved to be a delegator
 	type MinDelegation = ConstU128<{ 1 * currency::UNIT * currency::SUPPLY_FACTOR }>;
 	type BlockAuthor = AuthorInherent;
@@ -1750,7 +1750,7 @@ mod tests {
 		// staking minimums
 		assert_eq!(
 			get!(pallet_parachain_staking, MinCandidateStk, u128),
-			Balance::from(100 * UNIT)
+			Balance::from(500 * UNIT)
 		);
 		assert_eq!(
 			get!(pallet_parachain_staking, MinDelegation, u128),
