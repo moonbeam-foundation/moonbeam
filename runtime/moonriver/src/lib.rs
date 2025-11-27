21eb3c7f9e62c5e02068331ef037cf7f7c5c5245
Rodrigo Quelhas <22591718+RomarQ@users.noreply.github.com>
2025-11-27 18:01:13 +0000

    Update lib.rs

--- runtime/moonriver/src/lib.rs
+++ runtime/moonriver/src/lib.rs
@@ -1951,7 +1951,7 @@ mod tests {
 		// staking minimums
 		assert_eq!(
 			get!(pallet_parachain_staking, MinCandidateStk, u128),
-			Balance::from(500 * MOVR)
+			Balance::from(100 * MOVR)
 		);
 		assert_eq!(
 			get!(pallet_parachain_staking, MinDelegation, u128),
