b212ecadc6962b3368f864a776f9b38ed8a57155
Manuel Mauro <manuel.mauro@protonmail.com>
2025-12-11 15:08:09 +0200

    Update runtime spec version to 4200 (#3587)

--- runtime/moonriver/src/lib.rs
+++ runtime/moonriver/src/lib.rs
@@ -217,7 +217,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: Cow::Borrowed("moonriver"),
 	impl_name: Cow::Borrowed("moonriver"),
 	authoring_version: 3,
-	spec_version: 4100,
+	spec_version: 4200,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 3,
