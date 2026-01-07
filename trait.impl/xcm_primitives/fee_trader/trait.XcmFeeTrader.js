fac69ad14f4122b786300adf230ba8aa635e56ff
RomarQ <RomarQ@users.noreply.github.com>
2026-01-07 13:45:35 +0000

    deploy: 353410b5b9b6b994b4825a9727274e8e26fee32f

--- trait.impl/xcm_primitives/fee_trader/trait.XcmFeeTrader.js
+++ trait.impl/xcm_primitives/fee_trader/trait.XcmFeeTrader.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["moonbeam_tests_primitives",[["impl <a class=\"trait\" href=\"xcm_primitives/fee_trader/trait.XcmFeeTrader.html\" title=\"trait xcm_primitives::fee_trader::XcmFeeTrader\">XcmFeeTrader</a> for <a class=\"struct\" href=\"moonbeam_tests_primitives/struct.MemoryFeeTrader.html\" title=\"struct moonbeam_tests_primitives::MemoryFeeTrader\">MemoryFeeTrader</a>"]]],["moonbeam_tests_primitives",[["impl XcmFeeTrader for <a class=\"struct\" href=\"moonbeam_tests_primitives/struct.MemoryFeeTrader.html\" title=\"struct moonbeam_tests_primitives::MemoryFeeTrader\">MemoryFeeTrader</a>"]]],["pallet_xcm_weight_trader",[["impl&lt;T: <a class=\"trait\" href=\"pallet_xcm_weight_trader/pallet/trait.Config.html\" title=\"trait pallet_xcm_weight_trader::pallet::Config\">Config</a>&gt; <a class=\"trait\" href=\"xcm_primitives/fee_trader/trait.XcmFeeTrader.html\" title=\"trait xcm_primitives::fee_trader::XcmFeeTrader\">XcmFeeTrader</a> for <a class=\"struct\" href=\"pallet_xcm_weight_trader/pallet/struct.Pallet.html\" title=\"struct pallet_xcm_weight_trader::pallet::Pallet\">Pallet</a>&lt;T&gt;"]]]]);
+    var implementors = Object.fromEntries([["moonbeam_tests_primitives",[["impl XcmFeeTrader for <a class=\"struct\" href=\"moonbeam_tests_primitives/struct.MemoryFeeTrader.html\" title=\"struct moonbeam_tests_primitives::MemoryFeeTrader\">MemoryFeeTrader</a>"]]],["pallet_xcm_weight_trader",[["impl&lt;T: <a class=\"trait\" href=\"pallet_xcm_weight_trader/pallet/trait.Config.html\" title=\"trait pallet_xcm_weight_trader::pallet::Config\">Config</a>&gt; <a class=\"trait\" href=\"xcm_primitives/fee_trader/trait.XcmFeeTrader.html\" title=\"trait xcm_primitives::fee_trader::XcmFeeTrader\">XcmFeeTrader</a> for <a class=\"struct\" href=\"pallet_xcm_weight_trader/pallet/struct.Pallet.html\" title=\"struct pallet_xcm_weight_trader::pallet::Pallet\">Pallet</a>&lt;T&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[359,221,510]}
+//{"start":57,"fragment_lengths":[220,510]}
