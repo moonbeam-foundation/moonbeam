a63beba20971c56924e93eb2d9232fbb2fb6c50e
Rodrigo Quelhas <rodrigo_quelhas@outlook.pt>
2026-01-16 17:01:19 +0000

    remove old todo

--- node/service/src/lazy_loading/mod.rs
+++ node/service/src/lazy_loading/mod.rs
@@ -701,7 +701,6 @@ where
 	> = Default::default();
 	let pubsub_notification_sinks = Arc::new(pubsub_notification_sinks);
 
-	/* TODO: only enable this when frontier backend is compatible with lazy-loading
 	rpc::spawn_essential_tasks(
 		rpc::SpawnTasksParams {
 			task_manager: &task_manager,
@@ -716,7 +715,6 @@ where
 		sync_service.clone(),
 		pubsub_notification_sinks.clone(),
 	);
-	*/
 
 	let ethapi_cmd = rpc_config.ethapi.clone();
 	let tracing_requesters =
