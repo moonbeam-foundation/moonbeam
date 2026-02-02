e0c98acec208cfcc6f25073e01f720a781d1de6e
RomarQ <22591718+RomarQ@users.noreply.github.com>
2026-02-02 05:36:03 +0000

    Update Rust/TS bindings

--- typescript-api/src/moonbase/interfaces/lookup.ts
+++ typescript-api/src/moonbase/interfaces/lookup.ts
@@ -5320,13 +5320,14 @@ export default {
       "GasPriceTooLow",
       "InvalidNonce",
       "GasLimitTooLow",
-      "GasLimitTooHigh",
+      "GasLimitExceedsBlockLimit",
       "InvalidChainId",
       "InvalidSignature",
       "Reentrancy",
       "TransactionMustComeFromEOA",
       "Undefined",
-      "CreateOriginNotAllowed"
+      "CreateOriginNotAllowed",
+      "TransactionGasLimitExceedsCap"
     ]
   },
   /**
