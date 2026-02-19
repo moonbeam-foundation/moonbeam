9e782061ff244ca845b28169a0c0d178809b3127
librelois <librelois@users.noreply.github.com>
2026-02-19 13:14:40 +0000

    deploy: 903aacda4923adf23210aa7e669f97cf7946cdb1

--- trait.impl/xcm_primitives/transactor_traits/trait.UtilityEncodeCall.js
+++ trait.impl/xcm_primitives/transactor_traits/trait.UtilityEncodeCall.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["pallet_xcm_transactor",[["impl&lt;T: <a class=\"trait\" href=\"pallet_xcm_transactor/pallet/trait.Config.html\" title=\"trait pallet_xcm_transactor::pallet::Config\">Config</a>&gt; <a class=\"trait\" href=\"xcm_primitives/transactor_traits/trait.UtilityEncodeCall.html\" title=\"trait xcm_primitives::transactor_traits::UtilityEncodeCall\">UtilityEncodeCall</a> for <a class=\"struct\" href=\"pallet_xcm_transactor/pallet/struct.Pallet.html\" title=\"struct pallet_xcm_transactor::pallet::Pallet\">Pallet</a>&lt;T&gt;"]]]]);
+    var implementors = Object.fromEntries([["pallet_xcm_transactor",[["impl&lt;T: <a class=\"trait\" href=\"pallet_xcm_transactor/pallet/trait.Config.html\" title=\"trait pallet_xcm_transactor::pallet::Config\">Config</a>&gt; <a class=\"trait\" href=\"xcm_primitives/transactor_traits/trait.UtilityEncodeCall.html\" title=\"trait xcm_primitives::transactor_traits::UtilityEncodeCall\">UtilityEncodeCall</a> for <a class=\"struct\" href=\"pallet_xcm_transactor/pallet/struct.Pallet.html\" title=\"struct pallet_xcm_transactor::pallet::Pallet\">Pallet</a>&lt;T&gt;"]]],["pallet_xcm_transactor",[["impl&lt;T: <a class=\"trait\" href=\"pallet_xcm_transactor/pallet/trait.Config.html\" title=\"trait pallet_xcm_transactor::pallet::Config\">Config</a>&gt; UtilityEncodeCall for <a class=\"struct\" href=\"pallet_xcm_transactor/pallet/struct.Pallet.html\" title=\"struct pallet_xcm_transactor::pallet::Pallet\">Pallet</a>&lt;T&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[523]}
+//{"start":57,"fragment_lengths":[523,361]}
