e227547609015e38bbba907790fb590b24fe2b5a
timbrinded <timbrinded@users.noreply.github.com>
2026-02-20 10:23:21 +0000

    deploy: 003467e8e76d32e5bcc75589eb75ddcbd9f5afa0

--- trait.impl/xcm_primitives/transactor_traits/trait.HrmpEncodeCall.js
+++ trait.impl/xcm_primitives/transactor_traits/trait.HrmpEncodeCall.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["pallet_xcm_transactor",[["impl&lt;T: <a class=\"trait\" href=\"pallet_xcm_transactor/pallet/trait.Config.html\" title=\"trait pallet_xcm_transactor::pallet::Config\">Config</a>&gt; <a class=\"trait\" href=\"xcm_primitives/transactor_traits/trait.HrmpEncodeCall.html\" title=\"trait xcm_primitives::transactor_traits::HrmpEncodeCall\">HrmpEncodeCall</a> for <a class=\"struct\" href=\"pallet_xcm_transactor/pallet/struct.Pallet.html\" title=\"struct pallet_xcm_transactor::pallet::Pallet\">Pallet</a>&lt;T&gt;"]]],["pallet_xcm_transactor",[["impl&lt;T: <a class=\"trait\" href=\"pallet_xcm_transactor/pallet/trait.Config.html\" title=\"trait pallet_xcm_transactor::pallet::Config\">Config</a>&gt; HrmpEncodeCall for <a class=\"struct\" href=\"pallet_xcm_transactor/pallet/struct.Pallet.html\" title=\"struct pallet_xcm_transactor::pallet::Pallet\">Pallet</a>&lt;T&gt;"]]],["xcm_primitives",[]]]);
+    var implementors = Object.fromEntries([["pallet_xcm_transactor",[["impl&lt;T: <a class=\"trait\" href=\"pallet_xcm_transactor/pallet/trait.Config.html\" title=\"trait pallet_xcm_transactor::pallet::Config\">Config</a>&gt; <a class=\"trait\" href=\"xcm_primitives/transactor_traits/trait.HrmpEncodeCall.html\" title=\"trait xcm_primitives::transactor_traits::HrmpEncodeCall\">HrmpEncodeCall</a> for <a class=\"struct\" href=\"pallet_xcm_transactor/pallet/struct.Pallet.html\" title=\"struct pallet_xcm_transactor::pallet::Pallet\">Pallet</a>&lt;T&gt;"]]],["xcm_primitives",[]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[514,358,22]}
+//{"start":57,"fragment_lengths":[514,22]}
