539bafe7683b0bb7869c44c38fb649c57cd31876
Artur Gontijo <arturgontijo@gmail.com>
2026-03-02 13:06:28 -0300

    Fix some moonwall tests

--- test/suites/dev/moonbase/test-randomness/test-randomness-vrf-lottery4.ts
+++ test/suites/dev/moonbase/test-randomness/test-randomness-vrf-lottery4.ts
@@ -19,7 +19,7 @@ describeSuite({
         value: 1n * GLMR,
       });
       log(`Estimated Gas for startLottery ${estimatedGas}`);
-      expect(estimatedGas).to.equal(111850n);
+      expect(estimatedGas).to.equal(112363n);
 
       await context.writeContract!({
         contractAddress: lotteryContract,
@@ -46,7 +46,7 @@ describeSuite({
           args: [0],
         });
         log(`Estimated Gas for startLottery ${estimatedGas}`);
-        expect(estimatedGas).to.equal(162997n);
+        expect(estimatedGas).to.equal(165855n);
 
         const rawTxn = await context.writePrecompile!({
           precompileName: "Randomness",
